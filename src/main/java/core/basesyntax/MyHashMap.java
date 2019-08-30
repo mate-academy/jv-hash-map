2a91e1cfc9bf7b03cd5307f1a7e13e0e63a6121e
Тарас <tarashryts@gmail.com>
2019-08-30 19:28:33 +0300

    add hw-91-hashmap

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -6,19 +6,66 @@ package core.basesyntax;
  * За бажанням можна реалізувати інші методи інтрефейсу Map.</p>
  */
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final double LOAD_FACTOR = 0.75d;
+    private static final int BASE_SIZE = 16;
+    private static final int INDEX_OFF = -1;
+    private int size;
+    private int actualSize;
+    private Entry[] entries;
+
+    public MyHashMap() {
+        this.entries = new Entry[BASE_SIZE];
+        actualSize = 0;
+        size = BASE_SIZE;
+    }
+
+    private int indexOf(K key) {
+        for (int i = 0; i < actualSize; i++) {
+            if (entries[i].getKey() == null && key == null) {
+                return i;
+            }
+            if (key.equals(entries[i].getKey())) {
+                return i;
+            }
+        }
+        return INDEX_OFF;
+    }
+
+    private void resize() {
+        if (actualSize > size * LOAD_FACTOR) {
+            size = (int) (size * 2 * LOAD_FACTOR);
+            Entry[] newEntries = new Entry[size];
+            System.arraycopy(entries, 0, newEntries, 0, actualSize);
+            entries = newEntries;
+        }
+    }
 
     @Override
     public void put(K key, V value) {
-
+        resize();
+        if (indexOf(key) == INDEX_OFF) {
+            Entry temp = new Entry(key, value);
+            entries[actualSize] = temp;
+            actualSize++;
+        } else {
+            Entry temp = new Entry(key, value);
+            entries[indexOf(key)] = temp;
+        }
+        Entry temp = new Entry(key, value);
+        entries[actualSize] = temp;
     }
 
     @Override
     public V getValue(K key) {
-        return null;
+
+        if (indexOf(key) == INDEX_OFF) {
+            return null;
+        }
+        return (V) entries[indexOf(key)].getValue();
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return actualSize;
     }
 }
