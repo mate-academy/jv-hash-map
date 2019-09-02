7ebad46b1c08b55f69581e38f30ca42e3ff57eb6
Taranarnik <pavelkurilyuk97@gmail.com>
2019-09-02 16:34:14 +0300

    added solution for my hash map homework

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.HashMap;
+
 /**
  * <p>Реалізувати свою HashMap, а саме методи `put(K key, V value)`, `getValue()` та `getSize()`.
  * Дотриматися основних вимог щодо реалізації мапи (initial capacity, load factor, resize...)
@@ -9,7 +11,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private static final int DEFAULT_CAPACITY = 16;
     private static final double LOAD_FACTOR = 0.75;
-    private static double threshold;
     private int size;
     private int capacity;
     Node<K, V>[] table;
@@ -18,22 +19,17 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         size = 0;
         capacity = DEFAULT_CAPACITY;
         table = new Node[capacity];
-        threshold = capacity * LOAD_FACTOR;
     }
 
     @Override
     public void put(K key, V value) {
-        if (value == null) {
-            return;
-        }
         if (key == null) {
             putWithNullKey(value);
         } else {
-            int hash = hash(key.hashCode());
-            int index = indexFor(hash);
+            int index = indexFor(getHash(key.hashCode()));
             if (table[index] == null) {
                 addEntry(key, value, index);
-                ++size;
+                size++;
             } else {
                 Node<K, V> nodeToCheck = table[index];
                 while (nodeToCheck != null) {
@@ -41,16 +37,13 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                         nodeToCheck.value = value;
                         return;
                     }
-                    if (value.equals(nodeToCheck.value)) {
-                        return;
-                    }
                     nodeToCheck = nodeToCheck.next;
                 }
                 addEntry(key, value, index);
-                ++size;
+                size++;
             }
         }
-        if (size > threshold) {
+        if (size > capacity * LOAD_FACTOR) {
             resize();
         }
     }
@@ -60,13 +53,9 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if (key == null) {
             return table[0] == null ? null : table[0].value;
         }
-        int hash = hash(key.hashCode());
-        int index = indexFor(hash);
+        int index = indexFor(getHash(key.hashCode()));
         if (index < 0 || index >= table.length) {
             return null;
-        }
-        if (table[index] == null) {
-            return  null;
         }
         Node<K, V> nodeToCheck = table[index];
         while (nodeToCheck != null) {
@@ -90,19 +79,18 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         for (int i = 0; i < oldTable.length; i++) {
             Node<K, V> node = oldTable[i];
             while (node != null) {
-                int hash = hash(node.key.hashCode());
+                int hash = getHash(node.key.hashCode());
                 int index = indexFor(hash);
                 table[index] = new Node<K, V>(node.key, node.value, null);
                 node = node.next;
             }
         }
-        threshold = capacity * LOAD_FACTOR;
     }
 
     private void putWithNullKey(V value) {
         if (table[0] == null) {
             table[0] = new Node<K, V>(null, value, null);
-            ++size;
+            size++;
         } else {
             table[0].value = value;
         }
@@ -117,7 +105,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return hash & (table.length - 1);
     }
 
-    private int hash(int hashCode) {
+    private int getHash(int hashCode) {
         hashCode ^= (hashCode >>> 20) ^ (hashCode >>> 12);
         return hashCode ^ (hashCode >>> 7) ^ (hashCode >>> 4);
     }
