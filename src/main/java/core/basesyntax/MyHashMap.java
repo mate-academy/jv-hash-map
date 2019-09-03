31c3ca75dc9c8a57910eca66acc8a5359658275c
Max <skaterweather41@gmail.com>
2019-09-03 20:54:37 +0200

    added solution to task 1MyHashMap

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,24 +1,145 @@
 package core.basesyntax;
 
 /**
- * <p>Реалізувати свою HashMap, а саме методи `put(K key, V value)`, `getValue()` та `getSize()`.
- * Дотриматися основних вимог щодо реалізації мапи (initial capacity, load factor, resize...)
+ * <p>Реалізувати свою HashMap, а саме методи`put(K key,V value)`,`getValue()` та`getsizeOfStock()`.
+ * Дотриматися основних вимог щодо реалізації мапи (initial capacity, load factor, resizeOfStock...)
  * За бажанням можна реалізувати інші методи інтрефейсу Map.</p>
  */
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final double LOAD_FACTOR = 0.75;
+    private Node<K, V>[] bucketStock;
+    private int size;
+
+    public <K, V> MyHashMap() {
+        bucketStock = new Node[DEFAULT_CAPACITY];
+        size = 0;
+    }
 
     @Override
     public void put(K key, V value) {
+        resize();
+        if (key == null) {
+            putForNullKey(value);
+            return;
+        }
 
+        int index = getIndexFromHash(getHash(key.hashCode()));
+        if (bucketStock[index] == null) {
+            bucketStock[index] = new Node<>(key.hashCode(), key, value, null);
+        } else {
+            Node<K, V> lastBucketNode = bucketStock[index];
+            if (lastBucketNode.key.equals(key)) {
+                lastBucketNode.value = value;
+                return;
+            }
+            while (lastBucketNode.nextNode != null) {
+                if (lastBucketNode.key.equals(key)) {
+                    lastBucketNode.value = value;
+                    return;
+                }
+                lastBucketNode = lastBucketNode.nextNode;
+            }
+            lastBucketNode.nextNode = new Node<>(key.hashCode(), key, value, null);
+        }
+        size++;
+    }
+
+    private void putForNullKey(V value) {
+        if (bucketStock[0] == null) {
+            bucketStock[0] = new Node<>(0, null, value, null);
+        } else {
+            Node<K, V> lastBucketNode = bucketStock[0];
+            if (lastBucketNode.key == null) {
+                lastBucketNode.value = value;
+                return;
+            }
+            while (lastBucketNode.nextNode != null) {
+                if (lastBucketNode.key == null) {
+                    lastBucketNode.value = value;
+                    return;
+                }
+                lastBucketNode = lastBucketNode.nextNode;
+            }
+            lastBucketNode.nextNode = new Node<K, V>(0, null, value, null);
+        }
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        if (key == null) {
+            return getForNullKey();
+        }
+
+        int index = getIndexFromHash(getHash(key.hashCode()));
+        if (index >= bucketStock.length || bucketStock[index] == null) {
+            return null;
+        }
+        Node<K, V> returnNode = bucketStock[index];
+        while (returnNode != null) {
+            if (returnNode.key.equals(key)) {
+                return returnNode.value;
+            }
+            returnNode = returnNode.nextNode;
+        }
         return null;
+    }
+
+    private V getForNullKey() {
+        Node<K, V> returnNode = bucketStock[0];
+        while (returnNode != null) {
+            if (returnNode.key == null) {
+                return returnNode.value;
+            }
+            returnNode = returnNode.nextNode;
+        }
+        return null;
+    }
+
+    private int getHash(int hashCode) {
+        hashCode ^= (hashCode >>> 20) ^ (hashCode >>> 12);
+        return hashCode ^ (hashCode >>> 7) ^ (hashCode >>> 4);
+
+    }
+
+    private int getIndexFromHash(int hash) {
+        return Math.abs(hash % bucketStock.length);
+    }
+
+    private void resize() {
+        if (size >= bucketStock.length * LOAD_FACTOR) {
+            Node<K, V>[] oldBucketStock = bucketStock;
+            bucketStock = new Node[bucketStock.length * 2];
+            //transfer
+            for (int i = 0; i < oldBucketStock.length; i++) {
+                if (bucketStock[i] == null) {
+                    continue;
+                }
+                while (bucketStock[i] != null) {
+                    put(bucketStock[i].key, bucketStock[i].value);
+                    bucketStock[i] = bucketStock[i].nextNode;
+                }
+            }
+        }
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    public static class Node<K, V> {
+        private int hash;
+        private K key;
+        private V value;
+        private Node<K, V> nextNode;
+
+        public Node(int hash, K key, V value, Node<K, V> nextNode) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.nextNode = nextNode;
+        }
     }
 }
