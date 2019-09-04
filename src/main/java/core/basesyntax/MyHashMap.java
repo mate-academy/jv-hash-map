6c544909bb3f1a62905de290926e8aec7dc295e8
Taras Khalak <tarasulo@gmail.com>
2019-09-04 10:57:16 +0300

    'Solution'

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -6,19 +6,105 @@ package core.basesyntax;
  * За бажанням можна реалізувати інші методи інтрефейсу Map.</p>
  */
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int INITIAL_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+    private Entry<K, V>[] table;
+    private int size;
+    private int capacity = (int) (INITIAL_CAPACITY * LOAD_FACTOR);
+
+    private static class Entry<K, V> {
+        private K key;
+        private V value;
+        Entry<K, V> next;
+
+        private Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public MyHashMap() {
+        this.table = new Entry[INITIAL_CAPACITY];
+    }
+
+    private int hashCode(K key) {
+        if (key == null) {
+            return 0;
+        } else {
+            return Math.abs(key.hashCode()) % table.length;
+        }
+    }
 
     @Override
     public void put(K key, V value) {
+        int index = hashCode(key);
+        if (table[index] == null) {
+            table[index] = new Entry<>(key, value);
+            size++;
+            return;
+        }
+        if (index == 0) {
+            table[index].value = value;
+            return;
+        }
 
+        Entry<K, V> entry = table[index];
+        while (entry != null) {
+            if (entry.key.equals(key)) {
+                entry.value = value;
+                return;
+            } else {
+                entry = entry.next;
+            }
+        }
+        if (size++ > capacity) {
+            rehash();
+            index = hashCode(key);
+        }
+        addEntry(key, value, index);
+    }
+
+    private void addEntry(K key, V value, int index) {
+        Entry<K, V> entry = new Entry<>(key, value);
+        entry.next = table[index];
+        table[index] = entry;
+    }
+
+    private void rehash() {
+        Entry<K, V>[] oldTable = table;
+        int newCapacity = table.length * 2 + 1;
+        capacity = (int) (newCapacity * LOAD_FACTOR);
+        table = new Entry[newCapacity];
+        for (int i = table.length - 1; i >= 0; i--) {
+            Entry<K, V> entry = oldTable[i];
+            while (entry != null) {
+                int index = hashCode(entry.key);
+                Entry<K, V> next = entry.next;
+                entry.next = table[index];
+                table[index] = entry;
+                entry = next;
+            }
+        }
     }
 
     @Override
     public V getValue(K key) {
+        if (key == null) {
+            return table[0].value;
+        }
+        int index = hashCode(key);
+        Entry<K, V> entry = table[index];
+        while (entry != null) {
+            if (entry.key.equals(key)) {
+                return entry.value;
+            }
+            entry = entry.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
     }
 }
