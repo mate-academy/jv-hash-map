3a5041703a9026329e84cea82e199091e0f31b4a
TatianaPirch <pirch.tatiana@gmail.com>
2019-09-04 11:57:37 +0300

    removed threshold variable added a variable to write the result of the checkKey method

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -10,7 +10,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
     private int capacity;
     private int size;
-    private int threshold;
     private Entry<K, V>[] table;
 
     private static class Entry<K,V> {
@@ -29,7 +28,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         size = 0;
         capacity = DEFAULT_INITIAL_CAPACITY;
         table = new Entry[capacity];
-        threshold = countThreshold(capacity);
     }
 
     private int hash(K key) {
@@ -70,12 +68,13 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void putForNullKey(V value) {
-        if (checkKey(null) == null) {
+        Entry<K, V> checkKeyNull = checkKey(null);
+        if (checkKeyNull == null) {
             Entry<K, V> bucket = table[0];
             table[0] = new Entry(null, value, bucket);
             size++;
         } else {
-            checkKey(null).value = value;
+            checkKeyNull.value = value;
         }
     }
 
@@ -93,30 +92,31 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         capacity = table.length * 2;
         Entry<K, V>[] oldTable = table;
         table = new Entry[capacity];
-        threshold = countThreshold(capacity);
         rehash(oldTable);
     }
 
 
     @Override
     public void put(K key, V value) {
-        if (size > threshold) {
+        if (size > countThreshold(capacity)) {
             resizeHashMap();
         }
         if (key == null) {
             putForNullKey(value);
         } else {
-            if (checkKey(key) == null) {
+            Entry<K, V> checkKey = checkKey(key);
+            if (checkKey == null) {
                 addNewEntry(key, value);
             } else {
-                checkKey(key).value = value;
+                checkKey.value = value;
             }
         }
     }
 
     @Override
     public V getValue(K key) {
-        return checkKey(key) != null ? checkKey(key).value : null;
+        Entry<K, V> checkKey = checkKey(key);
+        return checkKey != null ? checkKey.value : null;
     }
 
     @Override
