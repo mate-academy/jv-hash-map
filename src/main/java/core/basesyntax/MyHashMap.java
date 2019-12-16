bcec9370ba8df133eb8fa4c0df68ab8bae992f15
Yaroslava Bondarchuk <yaroslava7bondarchuk@gmail.com>
2019-12-16 21:01:03 +0200

    initial commit

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -7,18 +7,118 @@ package core.basesyntax;
  */
 public class MyHashMap<K, V> implements MyMap<K, V> {
 
+    private static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private static final int MAX_CAPACITY = 1 << 30;
+    private Node<K, V>[] table;
+    private int size;
+    private int capacity;
+    private int threshold;
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_INITIAL_CAPACITY];
+        capacity = DEFAULT_INITIAL_CAPACITY;
+        threshold = (int) (capacity / DEFAULT_LOAD_FACTOR);
+    }
+
+    public MyHashMap(int capacity) {
+        if (capacity < 0 || capacity > MAX_CAPACITY) {
+            throw new IllegalArgumentException();
+        }
+        this.capacity = capacity;
+        table = new Node[capacity];
+        threshold = (int) (capacity / DEFAULT_LOAD_FACTOR);
+    }
+
     @Override
     public void put(K key, V value) {
+        Node<K, V> current = table[indexOfHash(key)];
+        if (current == null) {
+            table[indexOfHash(key)] = new Node<>(key, value, null);
+            size++;
+            return;
+        }
 
+        while (current != null) {
+            if (key == current.key || (key != null && key.equals(current.key))) {
+                current.value = value;
+                return;
+            }
+            if (current.next == null) {
+                current.next = new Node<>(key, value, null);
+                break;
+            }
+            current = current.next;
+        }
+        size++;
+        if (threshold == size) {
+            resizeHashMap();
+        }
     }
 
     @Override
     public V getValue(K key) {
+        Node<K, V> current = table[indexOfHash(key)];
+        if (current == null) {
+            return null;
+        }
+
+        while (current != null) {
+            if (key == current.key || (key != null && key.equals(current.key))) {
+                return current.value;
+            }
+            current = current.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private int indexOfHash(K key) {
+        return Math.abs(key == null ? 0 : key.hashCode() % capacity);
+    }
+
+    private void resizeHashMap() {
+        if (capacity == MAX_CAPACITY) {
+            return;
+        }
+        if (capacity << 1 >= MAX_CAPACITY) {
+            capacity = MAX_CAPACITY;
+        } else {
+            capacity = capacity << 1;
+        }
+
+        Node<K, V>[] oldHashMap = table;
+        table = new Node[capacity];
+        threshold = (int) (capacity / DEFAULT_LOAD_FACTOR);
+
+        for (int i = 0; i < oldHashMap.length; i++) {
+            Node<K, V> current = oldHashMap[i];
+            if (current == null) {
+                continue;
+            }
+            while (current != null) {
+                put(current.key, current.value);
+                size--;
+                current = current.next;
+            }
+        }
+    }
+
+    private static class Node<K, V> {
+        private int hash;
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        private Node(K key, V value, Node<K, V> next) {
+            this.hash = key == null ? 0 : key.hashCode();
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
