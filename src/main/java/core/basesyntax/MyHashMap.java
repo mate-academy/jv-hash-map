520d34cbe2aad91b446f78339180fc3aecb8a1b3
ElvenNurse <58395217+ElvenNurse@users.noreply.github.com>
2019-12-17 13:57:09 +0200

    Slightly simplified methods, renamed method for checking duplicates

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -10,27 +10,20 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
 
     private Node<K, V>[] table;
-    private float loadfactor;
-    private int treshold;
     private int capacity;
     private int size;
 
     public MyHashMap() {
-        table = new Node[DEFAULT_CAPACITY];
+        table = (Node<K, V>[]) new Node[DEFAULT_CAPACITY];
         capacity = DEFAULT_CAPACITY;
-        loadfactor = DEFAULT_LOAD_FACTOR;
-        treshold = (int) (loadfactor * capacity);
     }
 
     @Override
     public void put(K key, V value) {
         sizeCheck();
         int hash = hashKey(key);
-        int bucket = 0;
-        if (key != null) {
-            bucket = hash % capacity;
-        }
-        if (checkBucket(bucket, hash, key, value)) {
+        int bucket = hash % capacity;
+        if (!overwriteDuplicates(bucket, hash, key, value)) {
             Node<K, V> newNode = new Node<>(hash, key, value, null);
             if (table[bucket] == null) {
                 table[bucket] = newNode;
@@ -44,17 +37,14 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public V getValue(K key) {
         int hash = hashKey(key);
-        int bucket = 0;
-        if (key != null) {
-            bucket = hash % capacity;
-        }
+        int bucket = hash % capacity;
         if (table[bucket] == null) {
             return null;
         }
         Node<K, V> node = table[bucket];
         do {
-            if (node.hash == hash && (node.key == key || key != null
-                    && node.key != null && node.key.equals(key))) {
+            if (node.hash == hash && (node.key == key
+                    || node.key != null && node.key.equals(key))) {
                 return node.value;
             }
             node = node.next;
@@ -68,7 +58,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void sizeCheck() {
-        if (size > treshold) {
+        if (size > capacity * DEFAULT_LOAD_FACTOR) {
             resize();
         }
     }
@@ -76,9 +66,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private void resize() {
         final Node<K, V>[] oldTable = table;
         capacity = capacity << 1;
-        treshold = (int) (capacity * loadfactor);
-        Node<K, V>[] newTable = new Node[capacity];
-        table = newTable;
+        table = (Node<K, V>[]) new Node[capacity];
         size = 0;
         for (Node<K, V> node: oldTable) {
             while (node != null) {
@@ -88,20 +76,19 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    private boolean checkBucket(int bucket, int hash, K key, V value) {
+    private boolean overwriteDuplicates(int bucket, int hash, K key, V value) {
         if (table[bucket] != null) {
             Node<K, V> start = table[bucket];
             do {
                 if (start.hash == hash && (start.key == key
-                        || key != null && start.key != null
-                        && start.key.equals(key))) {
+                        || start.key != null && start.key.equals(key))) {
                     start.value = value;
-                    return false;
+                    return true;
                 }
                 start = start.next;
             } while (start != null);
         }
-        return true;
+        return false;
     }
 
     private void addInBucket(Node<K, V> node, int bucket) {
@@ -113,12 +100,12 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         size++;
     }
 
-    private final int hashKey(K key) {
+    private int hashKey(K key) {
         int h;
         return (key == null) ? 0 : Math.abs((h = key.hashCode()) ^ (h >>> 16));
     }
 
-    private class Node<K, V> {
+    private static class Node<K, V> {
         private int hash;
         private K key;
         private V value;
