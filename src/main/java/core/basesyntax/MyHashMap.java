c83dc920526c84df34b9481868c6589f7ac47bff
Mykhailo Kramar <3805033@gmail.com>
2019-12-17 21:01:03 +0200

    hw9.1

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -6,19 +6,115 @@ package core.basesyntax;
  * За бажанням можна реалізувати інші методи інтрефейсу Map.</p>
  */
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private Node<K, V>[] hashTable = new Node[DEFAULT_CAPACITY];
+    private static final float LOAD_FACTOR = 0.75f;
+    private int size = 0;
+
+    MyHashMap() {
+    }
+
+    private int hash(K key) {
+        int hash = 31;
+        hash = hash * 17 + key.hashCode();
+        return key == null ? 0 : Math.abs(hash) % hashTable.length;
+    }
 
     @Override
     public void put(K key, V value) {
+        if (key == null) {
+            putValueWithKeyNull(key, value);
+            return;
+        }
+        if (size > hashTable.length * LOAD_FACTOR) {
+            resize();
+        }
+        int index = hash(key);
+        if (hashTable[index] == null) {
+            hashTable[index] = new Node(key, value, null);
+            size++;
+        } else {
+            Node<K, V> tempNode = hashTable[index];
+            while (tempNode != null) {
+                if (key == tempNode.key || key.equals(tempNode.key)) {
+                    tempNode.value = value;
+                    return;
+                }
+                tempNode = tempNode.linkToNextNode;
+            }
+            Node<K, V> newNode = new Node<>(key, value, hashTable[index]);
+            hashTable[index] = newNode;
+            size++;
+        }
+    }
 
+    private void putValueWithKeyNull(K key, V value) {
+        if (hashTable[0] == null) {
+            hashTable[0] = new Node<>(key, value, null);
+            size++;
+        } else if (hashTable[0].key == key) {
+            hashTable[0].value = value;
+        } else {
+            Node<K, V> tempNode = hashTable[0];
+            while (tempNode != null) {
+                if (tempNode.key == key || tempNode.key.equals(key)) {
+                    tempNode.value = value;
+                    return;
+                }
+                tempNode = tempNode.linkToNextNode;
+            }
+            hashTable[0] = new Node<>(key, value, hashTable[0]);
+            size++;
+        }
     }
 
     @Override
     public V getValue(K key) {
+        if (key == null) {
+            return (V) hashTable[0].value;
+        }
+        int index = hash(key);
+        Node<K, V> newNode = hashTable[index];
+        while (newNode != null) {
+            if (key.equals(newNode.key)) {
+                return newNode.value;
+            }
+            newNode = newNode.linkToNextNode;
+        }
         return null;
+    }
+
+    public void resize() {
+        Node<K, V>[] oldHashTable = hashTable;
+        hashTable = new Node[hashTable.length * 2];
+        size = 0;
+        for (int i = 0; i < oldHashTable.length; i++) {
+            if (oldHashTable[i] == null) {
+                continue;
+            }
+            Node<K, V> node = oldHashTable[i];
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.linkToNextNode;
+            }
+        }
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    class Node<K, V> {
+        private Node<K, V> linkToNextNode;
+        private K key;
+        private V value;
+        private int hash;
+
+        public Node(K key, V value, Node<K, V> linkToNextNode) {
+            this.key = key;
+            this.value = value;
+            this.linkToNextNode = linkToNextNode;
+        }
     }
 }
