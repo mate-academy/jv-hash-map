9ae87c8d73e73c529b536d5b9078b3f89051dfd3
Taraban <Taraban.dmitry@gmail.com>
2019-12-18 16:12:19 +0200

    added hashMap solution

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 /**
  * <p>Реалізувати свою HashMap, а саме методи `put(K key, V value)`, `getValue()` та `getSize()`.
  * Дотриматися основних вимог щодо реалізації мапи (initial capacity, load factor, resize...)
@@ -7,18 +9,106 @@ package core.basesyntax;
  */
 public class MyHashMap<K, V> implements MyMap<K, V> {
 
+    private static final float LOAD_FACTOR = 0.75f;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private static int capacity = DEFAULT_INITIAL_CAPACITY;
+    private int size = 0;
+
+    private Node[] nodes;
+
+    public MyHashMap() {
+        this.nodes = new Node[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    public boolean timeToResize() {
+        return size >= nodes.length * LOAD_FACTOR;
+    }
+
+    private void resize() {
+        Node[] oldNodes = nodes;
+        nodes = new Node[nodes.length * 2];
+        size = 0;
+        for (Node<K, V> node : oldNodes) {
+            Node<K, V> item = node;
+            while (item != null) {
+                put(item.key, item.value);
+                item = item.next;
+            }
+        }
+    }
+
     @Override
     public void put(K key, V value) {
-
+        if (timeToResize()) {
+            resize();
+        }
+        int index = hash(key);
+        Node<K, V> node = nodes[index];
+        while (node != null) {
+            if (Objects.equals(key, node.key)) {
+                node.value = value;
+                return;
+            }
+            node = node.next;
+        }
+        Node<K, V> newNode = new Node<>(key, value, nodes[index]);
+        nodes[index] = newNode;
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        Node<K, V> currentNode = nodes[hash(key)];
+        if (currentNode == null) {
+            return null;
+        }
+        while (currentNode != null) {
+            if (Objects.equals(key, currentNode.key)) {
+                return currentNode.value;
+            }
+            currentNode = currentNode.next;
+        }
         return null;
+    }
+
+    private int hash(K key) {
+        return key == null ? 0 : key.hashCode() & (nodes.length - 1);
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private class Node<K, V> {
+
+        final K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        public final String toString() {
+            return key + "=" + value;
+        }
+
+        public final V setValue(V newValue) {
+            V oldValue = value;
+            value = newValue;
+            return oldValue;
+        }
     }
 }
