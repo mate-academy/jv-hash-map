373d3375bea224f62067a2cc03f4b63b0037a3fc
Dmytro Hryhoruk <dimamyhgrygoruk@gmail.com>
2019-12-18 16:35:25 +0200

    added openAdressing solution

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,15 +1,16 @@
 package core.basesyntax;
 
 import java.util.Objects;
-
 /**
  * <p>Реалізувати свою HashMap, а саме методи `put(K key, V value)`, `getValue()` та `getSize()`.
  * Дотриматися основних вимог щодо реалізації мапи (initial capacity, load factor, resize...)
  * За бажанням можна реалізувати інші методи інтрефейсу Map.</p>
  */
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPASITY = 16;
-    private static final float LOAD_FACTOR = 0.75f;
+    private static final float LOAD_FACTOR = 0.35f;
+    private int greatestCommonDivisor;
     private Bucket[] buckets;
     private int size;
 
@@ -22,7 +23,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private class Bucket<K, V> {
-        private Bucket next;
         private K key;
         private V value;
 
@@ -32,9 +32,23 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
+    private int hash(K key) {
+        return key == null ? 0 : key.hashCode();
+    }
+
     private int findIndexByHash(K key) {
-        return key == null ? 0
-                : Math.abs(key.hashCode() % buckets.length);
+        int x = 0;
+        while (true) {
+            int index = Math.abs((hash(key) + 2 * x++) % buckets.length);
+            if (buckets[index] == null) {
+                return index;
+            } else {
+                if (Objects.equals(buckets[index].key, key)) {
+                    return index;
+                }
+            }
+
+        }
     }
 
     @Override
@@ -44,22 +58,10 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
         int index = findIndexByHash(key);
         if (buckets[index] == null) {
-            buckets[index] = new Bucket<>(key, value);
+            buckets[index] = new Bucket(key, value);
             size++;
         } else {
-            Bucket<K, V> bucket = buckets[index];
-            while (true) {
-                if (Objects.equals(key, bucket.key)) {
-                    bucket.value = value;
-                    return;
-                }
-                if (bucket.next == null) {
-                    bucket.next = new Bucket<>(key, value);
-                    size++;
-                    return;
-                }
-                bucket = bucket.next;
-            }
+            buckets[index].value = value;
         }
     }
 
@@ -67,28 +69,18 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     public V getValue(K key) {
         int index = findIndexByHash(key);
         if (buckets[index] != null) {
-            Bucket<K, V> bucket = buckets[index];
-            while (bucket != null) {
-                if (Objects.equals(key, bucket.key)) {
-                    return bucket.value;
-                }
-                bucket = bucket.next;
-            }
+            return (V) buckets[index].value;
         }
         return null;
     }
 
     private void resizeHashMap() {
         size = 0;
-        Bucket[] copyOfBuckets = buckets;
+        Bucket<K, V>[] copyOfBuckets = buckets;
         buckets = new Bucket[buckets.length * 2];
         for (int i = 0; i < copyOfBuckets.length; i++) {
             if (copyOfBuckets[i] != null) {
-                Bucket<K, V> bucket = copyOfBuckets[i];
-                while (bucket != null) {
-                    put(bucket.key, bucket.value);
-                    bucket = bucket.next;
-                }
+                put(copyOfBuckets[i].key, copyOfBuckets[i].value);
             }
         }
     }
@@ -96,5 +88,14 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public int getSize() {
         return size;
+    }
+
+    @Override
+    public String toString() {
+        String sout = "";
+        for (Bucket b : buckets) {
+            sout += b.value + "\n";
+        }
+        return sout;
     }
 }
