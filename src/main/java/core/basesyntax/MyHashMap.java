a657b3aa978975ef63022c41b3e17cbaca709e92
Yaroslav-Serhii <u.vusochanskiy@gmail.com>
2020-04-09 17:36:12 +0300

    Added solution.

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -6,19 +6,180 @@ package core.basesyntax;
  * За бажанням можна реалізувати інші методи інтрефейсу Map.</p>
  */
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_ARRAY_CAPACITY = 16;
+    private static final int LOAD_FACTOR = 75;
+    private Node<K, V>[] buckets;
+    private int size;
+
+    public MyHashMap() {
+        size = 0;
+        buckets = new Node[DEFAULT_ARRAY_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
-
+        if (ensureCapacity(size)) {
+            resize();
+        }
+        if (key == null) {
+            putForNullKey(value);
+            return;
+        }
+        int index = index(key);
+        if (buckets[index] == null) {
+            buckets[index] = new Node(key, value);
+            size++;
+            return;
+        }
+        if (buckets[index].value == null) {
+            buckets[index] = new Node(key, value);
+            size++;
+        } else {
+            setBucket(index, key, value);
+        }
     }
 
     @Override
     public V getValue(K key) {
-        return null;
+        if (key == null) {
+            return getForNullKey();
+        }
+        int index = index(key);
+        return getBucket(index, key);
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    //////////////////////My_Methods///////////////////////////////////
+
+    private int index(K key) {
+        return Math.abs(key.hashCode() % (buckets.length));
+    }
+
+    private boolean ensureCapacity(int size) {
+        return size > (buckets.length * LOAD_FACTOR) / 100;
+    }
+
+    private void putForNullKey(V value) {
+        if (buckets[0] == null) {
+            buckets[0] = new Node(null, value);
+            size++;
+            return;
+        }
+        if (buckets[0].key == null) {
+            buckets[0].value = value;
+            return;
+        }
+        Node<K, V> currentNode = getNoge(buckets[0]);
+        while (currentNode.next != null) {
+            if (currentNode.next.key == null) {
+                currentNode.next.value = value;
+                return;
+            }
+            currentNode = currentNode.next;
+        }
+        Node<K, V> entry = new Node(value);
+        currentNode.next = entry;
+        size++;
+    }
+
+    private V getForNullKey() {
+        if (buckets[0] == null) {
+            return null;
+        }
+        if (buckets[0].key == null) {
+            return buckets[0].value;
+        }
+        Node<K, V> currentNode = buckets[0];
+        while (currentNode.next != null) {
+            if (currentNode.next.key == null) {
+                return currentNode.next.value;
+            }
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
+
+    private void resize() {
+        Node<K, V>[] oldData = buckets;
+        buckets = new Node[buckets.length * 2];
+        size = 0;
+        for (int i = 0; i < oldData.length; i++) {
+            if (oldData[i] != null) {
+                putLL(oldData[i]);
+            }
+        }
+    }
+
+    private void setValue(int index, K key, V value) {
+        buckets[index].key = key;
+        buckets[index].value = value;
+    }
+
+    private void setBucket(int index, K key, V value) {
+        if (key.equals(buckets[index].key)) {
+            setValue(index, key, value);
+            return;
+        }
+        Node<K, V> currentNode = getNoge(buckets[index]);
+        while (currentNode.next != null) {
+            if (key.equals(currentNode.next.key)) {
+                currentNode.next.value = value;
+                return;
+            }
+            currentNode = getNoge(currentNode.next);
+        }
+        Node<K, V> entry = new Node(key, value);
+        currentNode.next = entry;
+        size++;
+    }
+
+    private V getBucket(int index, K key) {
+        if (buckets[index] == null) {
+            return null;
+        }
+        if (key.equals(buckets[index].key)) {
+            return buckets[index].value;
+        }
+        Node<K, V> currentNode = getNoge(buckets[index]);
+        while (currentNode.next != null) {
+            if (key.equals(currentNode.next.key)) {
+                return currentNode.next.value;
+            }
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
+
+    private Node getNoge(Node node) {
+        return node;
+    }
+
+    private void putLL(Node<K, V> oldNode) {
+        put(oldNode.key, oldNode.value);
+        while (oldNode.next != null) {
+            oldNode = oldNode.next;
+            put(oldNode.key, oldNode.value);
+        }
+    }
+
+    ////////////////////////Class_Node///////////////////////////////
+
+    private class Node<K, V> {
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(V value) {
+            this.value = value;
+        }
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
