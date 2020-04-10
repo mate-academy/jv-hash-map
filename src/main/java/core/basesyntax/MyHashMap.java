f1afb7b0928c36c23149332cf4536c1177ae9f50
Максим Матуляк <maksymmatuliak@gmail.com>
2020-04-11 01:31:10 +0300

    solution with recomendation

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -6,81 +6,12 @@ package core.basesyntax;
  * За бажанням можна реалізувати інші методи інтрефейсу Map.</p>
  */
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    private static final int CAPACITY = 16;
-    private static final double LOAD_FACTOR = 0.75;
     private Node<K, V>[] table;
     private int size = 0;
     private int arrayCapacity = 16;
 
     public MyHashMap() {
-        this.table = new Node[CAPACITY];
-    }
-
-    private int index(K key) {
-        return Math.abs(key.hashCode()) % (arrayCapacity - 1);
-    }
-
-    private Node<K, V> theLastNode(Node<K, V> node) {
-        Node<K, V> currentNode = node;
-        while (currentNode.next != null) {
-            currentNode = currentNode.next;
-        }
-        return currentNode;
-    }
-
-    private void resize() {
-        if (LOAD_FACTOR * table.length > size) {
-            return;
-        }
-        Node<K, V>[] newTable = new Node[table.length * 2];
-        arrayCapacity = newTable.length;
-        for (Node<K, V> node: table) {
-            Node<K, V> currentNode = node;
-            while (currentNode != null) {
-                if (newTable[index(currentNode.key)] == null) {
-                    newTable[index(currentNode.key)] = currentNode;
-                } else {
-                    theLastNode(newTable[index(currentNode.key)]).next = currentNode;
-                }
-                Node<K, V> copy = currentNode;
-                currentNode = currentNode.next;
-                copy.next = null;
-            }
-        }
-        table = newTable;
-    }
-
-    private void putForNull(Node newNode) {
-        Node currentNode = table[0];
-        if (currentNode == null) {
-            table[0] = newNode;
-            size++;
-            return;
-        }
-        while (currentNode != null) {
-            if (currentNode.key == null) {
-                currentNode.value = newNode.value;
-                return;
-            }
-            if (currentNode.next == null) {
-                currentNode.next = newNode;
-                size++;
-                return;
-            }
-            currentNode = currentNode.next;
-        }
-    }
-
-    private class Node<K, V> {
-        private K key;
-        private V value;
-        private Node<K, V> next;
-
-        private Node(K key, V value) {
-            this.key = key;
-            this.value = value;
-            this.next = null;
-        }
+        this.table = new Node[16];
     }
 
     @Override
@@ -113,22 +44,10 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public V getValue(K key) {
-        if (key == null) {
-            Node<K, V> currentNode = table[0];
-            while (currentNode != null) {
-                if (currentNode.key == null) {
-                    return currentNode.value;
-                }
-                currentNode = currentNode.next;
-            }
-            return null;
-        }
-        Node<K, V> currentNode = table[index(key)];
-        if (currentNode == null) {
-            return null;
-        }
+        Node<K, V> currentNode = key == null ? table[0] : table[index(key)];
         while (currentNode != null) {
-            if (key.equals(currentNode.key)) {
+            if ((currentNode.key == null && key == null)
+                    || (currentNode.key != null && currentNode.key.equals(key))) {
                 return currentNode.value;
             }
             if (currentNode.next == null) {
@@ -142,5 +61,70 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public int getSize() {
         return size;
+    }
+
+    private int index(K key) {
+        return Math.abs(key.hashCode()) % (arrayCapacity - 1);
+    }
+
+    private Node<K, V> theLastNode(Node<K, V> node) {
+        Node<K, V> currentNode = node;
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+        }
+        return currentNode;
+    }
+
+    private void resize() {
+        if (0.75 * table.length > size) {
+            return;
+        }
+        Node<K, V>[] newTable = new Node[table.length * 2];
+        arrayCapacity = newTable.length;
+        for (Node<K, V> node: table) {
+            Node<K, V> currentNode = node;
+            while (currentNode != null) {
+                int index = index(currentNode.key);
+                if (newTable[index] == null) {
+                    newTable[index] = currentNode;
+                } else {
+                    theLastNode(newTable[index]).next = currentNode;
+                }
+                Node<K, V> copy = currentNode;
+                currentNode = currentNode.next;
+                copy.next = null;
+            }
+        }
+        table = newTable;
+    }
+
+    private void putForNull(Node newNode) {
+        Node currentNode = table[0];
+        while (currentNode != null) {
+            if (currentNode.key == null) {
+                currentNode.value = newNode.value;
+                return;
+            }
+            if (currentNode.next == null) {
+                currentNode.next = newNode;
+                size++;
+                return;
+            }
+            currentNode = currentNode.next;
+        }
+        table[0] = newNode;
+        size++;
+    }
+
+    private class Node<K, V> {
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        private Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
     }
 }
