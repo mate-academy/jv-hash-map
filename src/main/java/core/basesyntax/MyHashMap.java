e7b00666dc1a4946b94a8487fcf965eb6015cbf0
serg-ksv <serg.ksv1@gmail.com>
2020-04-11 22:53:33 +0300

    Added solution

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -6,19 +6,109 @@ package core.basesyntax;
  * За бажанням можна реалізувати інші методи інтрефейсу Map.</p>
  */
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int INITIAL_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+    private int threshold;
+    private Node<K, V>[] table;
+    private int size;
+
+    public MyHashMap() {
+        table = new Node[INITIAL_CAPACITY];
+        threshold = (int) (INITIAL_CAPACITY * LOAD_FACTOR);
+        size = 0;
+    }
 
     @Override
     public void put(K key, V value) {
+        if (size >= threshold) {
+            resize();
+        }
+        Node<K, V> newNode = new Node<>(key, value, null);
+        int keyHash = getHash(key);
+        int index = getIndex(keyHash, table.length);
 
+        if (table[index] == null) {
+            table[index] = newNode;
+            size++;
+        } else if (containsKey(index, key)) {
+            Node<K, V> current = getNode(index, key);
+            current.value = value;
+        } else {
+            newNode.next = table[index];
+            table[index] = newNode;
+            size++;
+        }
     }
 
     @Override
     public V getValue(K key) {
-        return null;
+        Node<K, V> node = getNode(getIndex(getHash(key), table.length), key);
+        return node == null ? null : node.value;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private static class Node<K, V> {
+        Node<K, V> next;
+        private final K key;
+        private V value;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private Node<K, V> getNode(int index, K key) {
+        Node<K, V> currentNode = table[index];
+        if (currentNode == null) {
+            return null;
+        }
+        while (currentNode != null) {
+            if (isEqualKey(currentNode.key, key)) {
+                return currentNode;
+            }
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
+
+    private int getIndex(int hash, int capacity) {
+        return hash % capacity;
+    }
+
+    private int getHash(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode());
+    }
+
+    private boolean containsKey(int index, K key) {
+        return getNode(index, key) != null;
+    }
+
+    private boolean isEqualKey(K key1, K key2) {
+        return getHash(key1) == getHash(key2)
+                && (key1 == null ? key2 == null : key1.equals(key2));
+    }
+
+    private void resize() {
+        final Node<K, V>[] oldTable = table;
+        int newCapacity = table.length * 2;
+        table = new Node[newCapacity];
+        threshold = (int) (newCapacity * LOAD_FACTOR);
+        size = 0;
+        transfer(oldTable);
+    }
+
+    private void transfer(Node<K, V>[] oldTable) {
+        for (Node<K, V> node : oldTable) {
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.next;
+            }
+        }
     }
 }
