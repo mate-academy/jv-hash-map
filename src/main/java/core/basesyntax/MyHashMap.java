3d8921d51145cb176ab5e276c76889d76c271121
interami <interami@ukr.net>
2020-04-11 14:17:47 +0300

    added solution

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -6,19 +6,90 @@ package core.basesyntax;
  * За бажанням можна реалізувати інші методи інтрефейсу Map.</p>
  */
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final double DEFAULT_LOAD_FACTOR = 0.75;
+    private static final int DEFAULT_CAPACITY = 16;
+    int size;
+    private double threshold;
+    private Node<K, V>[] buckets;
+
+    public MyHashMap() {
+        size = 0;
+        threshold = DEFAULT_CAPACITY * DEFAULT_LOAD_FACTOR;
+        buckets = new Node[DEFAULT_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
-
+        if (size >= threshold) {
+            resize();
+        }
+        Node<K, V> bucket = buckets[getIndex(key, buckets.length)];
+        Node<K, V> prevNode;
+        if (bucket == null) {
+            buckets[getIndex(key, buckets.length)] = new Node<>(key, value, null);
+            size++;
+        } else {
+            do {
+                if (compareKey(bucket.key, key)) {
+                    bucket.value = value;
+                    return;
+                }
+                prevNode = bucket;
+                bucket = bucket.next;
+            } while (bucket != null);
+            prevNode.next = new Node(key, value, null);
+            size++;
+        }
     }
 
     @Override
     public V getValue(K key) {
+        Node<K, V> bucket = buckets[getIndex(key, buckets.length)];
+        while (bucket != null) {
+            if (compareKey(bucket.key, key)) {
+                return bucket.value;
+            }
+            bucket = bucket.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private class Node<K, V> {
+        K key;
+        V value;
+        Node next;
+
+        public Node(K key, V value, Node next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private void resize() {
+        Node<K, V>[] resizedBuckets = new Node[buckets.length * 2];
+        threshold = resizedBuckets.length * DEFAULT_LOAD_FACTOR;
+        size = 0;
+        Node<K, V>[] old = buckets;
+        buckets = resizedBuckets;
+        for (Node<K, V> bucket : old) {
+            while (bucket != null) {
+                put(bucket.key, bucket.value);
+                bucket = bucket.next;
+            }
+        }
+    }
+
+    private int getIndex(K key, int sizeOfBuckets) {
+        return key == null ? 0 : Math.abs((key.hashCode() % sizeOfBuckets));
+    }
+
+    private boolean compareKey(K key1, K key2) {
+        return key1 == key2 || key1 != null && key1.equals(key2);
     }
 }
