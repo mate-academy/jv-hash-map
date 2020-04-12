4e87714fa34339297828d7d32f8ea3dfef6682b2
NastiaFasova <fasovaaa@fmail.com>
2020-04-12 13:15:33 +0300

    Added solution to HW-11

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,24 +1,87 @@
 package core.basesyntax;
 
-/**
- * <p>Реалізувати свою HashMap, а саме методи `put(K key, V value)`, `getValue()` та `getSize()`.
- * Дотриматися основних вимог щодо реалізації мапи (initial capacity, load factor, resize...)
- * За бажанням можна реалізувати інші методи інтрефейсу Map.</p>
- */
 public class MyHashMap<K, V> implements MyMap<K, V> {
+
+    static final int CAPACITY = 16;
+    static final float DEFAULT_LOAD_FACTOR = 0.75F;
+    private int size;
+    private float threshold;
+    private Node<K,V>[] buckets;
+
+    public MyHashMap() {
+        buckets = new Node[CAPACITY];
+        threshold = CAPACITY * DEFAULT_LOAD_FACTOR;
+        size = 0;
+    }
 
     @Override
     public void put(K key, V value) {
-
+        if (size == threshold) {
+            resize();
+        }
+        int index = getIndex(key);
+        Node<K, V> node = buckets[index];
+        while (node != null) {
+            if (isEqualKey(node, key)) {
+                node.value = value;
+                return;
+            }
+            node = node.next;
+        }
+        node = new Node<K, V>(key, value, buckets[index]);
+        buckets[index] = node;
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        int index = getIndex(key);
+        Node<K, V> node = buckets[index];
+        while (node != null) {
+            if (isEqualKey(node, key)) {
+                return node.value;
+            }
+            node = node.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private void resize() {
+        Node<K, V>[] bigNodes = new Node[buckets.length * 2];
+        threshold = buckets.length * DEFAULT_LOAD_FACTOR;
+        size = 0;
+        Node<K, V>[] littleNodes = buckets;
+        buckets = bigNodes;
+        for (Node<K, V> node : littleNodes) {
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.next;
+            }
+        }
+    }
+
+    private boolean isEqualKey(Node<K, V> node, K key) {
+        return key == node.key || key != null && key.equals(node.key);
+    }
+
+    private int getIndex(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode()) % buckets.length;
+    }
+
+    private static class Node<K, V> {
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
