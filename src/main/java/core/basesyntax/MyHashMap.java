5a0d928d2d88bc30292e5cf22886186d9f37392a
VovkAA <vovkandrew@gmail.com>
2020-04-12 22:09:51 +0300

    Done

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -7,18 +7,88 @@ package core.basesyntax;
  */
 public class MyHashMap<K, V> implements MyMap<K, V> {
 
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final double DEFAULT_LOAD_FACTOR = 0.75;
+
+    private Node<K, V>[] hashMapArray;
+    private int size;
+
+    private static class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public MyHashMap() {
+        hashMapArray = new Node[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
     @Override
     public void put(K key, V value) {
-
+        if (size == hashMapArray.length * DEFAULT_LOAD_FACTOR) {
+            resize();
+        }
+        int index = findIndex(key, hashMapArray.length);
+        Node<K, V> newElement = findLastNode(key, index);
+        if (newElement != null) {
+            newElement.value = value;
+        } else {
+            hashMapArray[index] = new Node<K, V>(key, value, hashMapArray[index]);
+            size++;
+        }
     }
 
     @Override
     public V getValue(K key) {
+        int index = findIndex(key, hashMapArray.length);
+        Node<K, V> newElement = findLastNode(key, index);
+        if (newElement != null) {
+            return newElement.value;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private int findIndex(K key, int length) {
+        if (key == null) {
+            return 0;
+        }
+        return Math.abs(key.hashCode()) % length;
+    }
+
+    private Node findLastNode(K key, int index) {
+        Node<K, V> newNode = hashMapArray[index];
+        while (newNode != null) {
+            if (key == newNode.key || key != null && key.equals(newNode.key)) {
+                return newNode;
+            }
+            newNode = newNode.next;
+        }
+        return null;
+    }
+
+    private void resize() {
+        Node<K, V>[] newHashMapArray = new Node[hashMapArray.length * 2];
+        size = 0;
+        Node<K, V>[] oldHashMapArray = hashMapArray;
+        hashMapArray = newHashMapArray;
+        for (int i = 0; i < oldHashMapArray.length; i++) {
+            while (oldHashMapArray[i] != null) {
+                put(oldHashMapArray[i].key, oldHashMapArray[i].value);
+                oldHashMapArray[i] = oldHashMapArray[i].next;
+            }
+        }
     }
 }
+
