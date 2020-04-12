9a6b053cb3de8d4eea32a62348b579cc5c6099e5
Polina Kulyk <polina.kulyk@protonmail.com>
2020-04-12 15:17:42 +0300

    Added solution for HW_9.1

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 /**
  * <p>Реалізувати свою HashMap, а саме методи `put(K key, V value)`, `getValue()` та `getSize()`.
  * Дотриматися основних вимог щодо реалізації мапи (initial capacity, load factor, resize...)
@@ -7,18 +9,75 @@ package core.basesyntax;
  */
 public class MyHashMap<K, V> implements MyMap<K, V> {
 
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+
+    private Bucket<K, V>[] buckets = new Bucket[DEFAULT_CAPACITY];
+    private int size;
+
     @Override
     public void put(K key, V value) {
+        if (size > (int)(buckets.length * LOAD_FACTOR)) {
+            increaseSize();
+        }
+        int bucketIdx = key != null ? (key.hashCode() >>> 1) % buckets.length : 0;
+        Bucket<K, V> bucket = buckets[bucketIdx];
+        if (bucket == null) {
+            buckets[bucketIdx] = new Bucket<>(key, value);
+            ++size;
+            return;
+        }
+        do {
+            if (Objects.equals(bucket.key, key)) {
+                bucket.value = value;
+                return;
+            }
+            if (bucket.next == null) {
+                break;
+            }
+            bucket = bucket.next;
+        } while (true);
+        bucket.next = new Bucket<>(key, value);
+        ++size;
+    }
 
+    private void increaseSize() {
+        Bucket<K, V>[] oldBuckets = buckets;
+        buckets = new Bucket[buckets.length << 1];
+        size = 0;
+        for (Bucket<K, V> bucket : oldBuckets) {
+            while (bucket != null) {
+                put(bucket.key, bucket.value);
+                bucket = bucket.next;
+            }
+        }
     }
 
     @Override
     public V getValue(K key) {
+        int bucketIdx = key != null ? (key.hashCode() >>> 1) % buckets.length : 0;
+        Bucket<K, V> bucket = buckets[bucketIdx];
+        for (; bucket != null; bucket = bucket.next) {
+            if (Objects.equals(bucket.key, key)) {
+                return bucket.value;
+            }
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private static class Bucket<K, V> {
+        private final K key;
+        private V value;
+        private Bucket<K, V> next;
+
+        Bucket(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
