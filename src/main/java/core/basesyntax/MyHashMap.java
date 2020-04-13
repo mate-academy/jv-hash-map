b23f1aafd0fe5d835ff00c0e3f7b95f147a38a12
maks45 <maksim.durov45@gmail.com>
2020-04-13 13:59:38 +0300

    removed cast to Node<K,V>[] , removed addToTable parameter and replace all what was in it to put method, now check to equals with Objects.equals(), in resize method now I crete old table with old values and then = to table new array with length * 2 , and then I put all nodes from old table to new, chenged compare size with load factor.

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 /**
  * <p>Реалізувати свою HashMap, а саме методи `put(K key, V value)`, `getValue()` та `getSize()`.
  * Дотриматися основних вимог щодо реалізації мапи (initial capacity, load factor, resize...)
@@ -13,22 +15,35 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     public MyHashMap() {
         size = 0;
-        table = (Node<K, V>[]) new Node[DEFAULT_CAPACITY];
+        table = new Node[DEFAULT_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
         resize();
-        if (addToTable(key, value, table)) {
-            size++;
+        int hash = hash(key, table.length);
+        Node<K, V> lastNode = table[hash];
+        while (lastNode != null) {
+            if (Objects.equals(key, lastNode.key)) {
+                lastNode.value = value;
+                return;
+            }
+            if (lastNode.nextNode == null) {
+                lastNode.nextNode = new Node<>(key, value, null);
+                size++;
+                return;
+            }
+            lastNode = lastNode.nextNode;
         }
+        table[hash] = new Node<>(key, value, null);
+        size++;
     }
 
     @Override
     public V getValue(K key) {
         Node<K, V> nodeByKey = table[hash(key, table.length)];
         while (nodeByKey != null) {
-            if ((key != null && key.equals(nodeByKey.key)) || key == nodeByKey.key) {
+            if (Objects.equals(key, nodeByKey.key)) {
                 return nodeByKey.value;
             }
             nodeByKey = nodeByKey.nextNode;
@@ -41,38 +56,17 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private boolean addToTable(K key, V value, Node<K, V>[] table) {
-        int hash = hash(key, table.length);
-        if (table[hash] == null) {
-            table[hash] = new Node<>(key, value, null);
-            return true;
-        } else {
-            Node<K, V> lastNode = table[hash];
-            while (lastNode != null) {
-                if ((lastNode.key != null && lastNode.key.equals(key))
-                        || lastNode.key == key) {
-                    lastNode.value = value;
-                    break;
-                } else if (lastNode.nextNode == null) {
-                    lastNode.nextNode = new Node<>(key, value, null);
-                    return true;
-                }
-                lastNode = lastNode.nextNode;
-            }
-        }
-        return false;
-    }
-
     private void resize() {
-        if (((float) size) / table.length > LOAD_FACTOR) {
-            Node<K,V>[] newTable = (Node<K, V>[]) new Node[table.length * 2];
-            for (Node<K, V> node : table) {
+        if (size > table.length * LOAD_FACTOR) {
+            Node<K, V>[] oldTable = table;
+            table = new Node[table.length * 2];
+            size = 0;
+            for (Node<K, V> node : oldTable) {
                 while (node != null) {
-                    addToTable(node.key, node.value, newTable);
+                    put(node.key, node.value);
                     node = node.nextNode;
                 }
             }
-            table = newTable;
         }
     }
 
