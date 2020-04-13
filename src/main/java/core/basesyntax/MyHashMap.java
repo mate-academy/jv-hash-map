4a3aeaff5241f7d3ccafee23d57d1cd21e6acb5d
Vinglish <khan.ildar94@gmail.com>
2020-04-13 16:37:09 +0300

    change methods

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 /**
  * <p>Реалізувати свою HashMap, а саме методи `put(K key, V value)`, `getValue()` та `getSize()`.
  * Дотриматися основних вимог щодо реалізації мапи (initial capacity, load factor, resize...)
@@ -8,23 +10,21 @@ package core.basesyntax;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final double DEFAULT_LOAD_FACTOR = 0.75;
     private static final int DEFAULT_CAPACITY = 16;
-    private int newCapacity;
     private int size;
     private Node<K, V>[] table;
 
     public MyHashMap() {
         size = 0;
         table = new Node[DEFAULT_CAPACITY];
-        newCapacity = DEFAULT_CAPACITY;
     }
 
     @Override
     public void put(K key, V value) {
 
-        if ((double) size / table.length >= DEFAULT_LOAD_FACTOR) {
+        if (size > table.length * DEFAULT_LOAD_FACTOR) {
             resize();
         }
-        putNode(key, value, table, true);
+        putNode(key, value);
     }
 
     @Override
@@ -32,7 +32,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         Node<K, V> node = table[getKeyIndex(key)];
         if (node != null) {
             while (node != null) {
-                if (compareKeys(key, node.key)) {
+                if (Objects.equals(key, node.key)) {
                     return node.value;
                 }
                 node = node.next;
@@ -46,34 +46,30 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private boolean compareKeys(K key1, K key2) {
-        return getHash(key1) == getHash(key2) && (key1 == null ? key2 == null : key1.equals(key2));
-    }
-
     private int getHash(K key) {
         return key == null ? 0 : Math.abs(key.hashCode());
     }
 
     private int getKeyIndex(K key) {
-        return getHash(key) % newCapacity;
+        return getHash(key) % table.length;
     }
 
-    private void putNode(K key, V value, Node<K, V>[] table, boolean increment) {
+    private void putNode(K key, V value) {
         int index = getKeyIndex(key);
         if (table[index] == null) {
             table[index] = new Node<>(key, value, null);
-            size = increment ? size + 1 : size;
+            size++;
             return;
         }
         Node<K, V> node = table[index];
         while (node != null) {
-            if (compareKeys(node.key, key)) {
+            if (Objects.equals(node.key, key)) {
                 node.value = value;
                 return;
             }
             if (node.next == null) {
                 node.next = new Node<>(key, value, null);
-                size = increment ? size + 1 : size;
+                size++;
                 return;
             } else {
                 node = node.next;
@@ -83,15 +79,16 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private void resize() {
         int newCapacity = table.length * 2;
-        Node<K, V>[] newTable = new Node[newCapacity];
-        for (Node<K, V> cell : table) {
+        Node<K, V>[] oldTable = table;
+        size = 0;
+        table = (Node<K, V>[]) new Node[newCapacity];
+        for (Node<K, V> cell : oldTable) {
             Node<K, V> node = cell;
             while (node != null) {
-                putNode(node.key, node.value, newTable, false);
+                putNode(node.key, node.value);
                 node = node.next;
             }
         }
-        table = newTable;
     }
 
     private static class Node<K, V> {
