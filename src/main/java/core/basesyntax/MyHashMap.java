3822fcbc044a44565ef59004d0ac9c4f9ae8dd0b
Leonid <rommel@ukr.net>
2020-08-11 16:41:57 +0300

    Node class simplified

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -49,37 +49,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             key = node.key;
             value = node.value;
         }
-
-        public K getKey() {
-            return key;
-        }
-
-        public V getValue() {
-            return value;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            Node node = (Node) o;
-            return Objects.equals(key, node.key)
-                   && Objects.equals(value, node.value);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(key, value);
-        }
-
-        @Override
-        public String toString() {
-            return key + "=" + value;
-        }
     }
 
     @Override
