41c79f7fcc1a41d5c6c827d2433bddf42e3abfde
Mikhail Shevchenko <michaelyshevchenko@gmail.com>
2020-08-11 16:40:05 +0300

    added solution to hw-12

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -6,19 +6,95 @@ package core.basesyntax;
  * За бажанням можна реалізувати інші методи інтрефейсу Map.</p>
  */
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+
+    private Entry[] buckets;
+    private int threshold;
+    private int size;
+
+    MyHashMap() {
+        buckets = new Entry[DEFAULT_CAPACITY];
+        threshold = (int) (LOAD_FACTOR * buckets.length);
+        size = 0;
+    }
 
     @Override
     public void put(K key, V value) {
-
+        resize();
+        if (buckets[indexToAssign(key)] == null) {
+            buckets[indexToAssign(key)] = new Entry(key, value, null);
+            size++;
+            return;
+        }
+        for (Entry holder = buckets[indexToAssign(key)]; holder != null; holder = holder.next) {
+            if (holder.key == key || (holder.key != null && holder.key.equals(key))) {
+                holder.value = value;
+                break;
+            } else if (holder.next == null) {
+                Entry toExtend = buckets[indexToAssign(key)];
+                buckets[indexToAssign(key)] = new Entry(key, value, toExtend);
+                size++;
+            }
+        }
     }
 
     @Override
     public V getValue(K key) {
+        for (Entry holder = buckets[indexToAssign(key)]; holder != null; holder = holder.next) {
+            if (holder.key == key || (holder.key != null && holder.key.equals(key))) {
+                return (V) holder.value;
+            }
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private static class Entry<K, V> {
+        private K key;
+        private V value;
+        private Entry<K, V> next;
+
+        Entry(K key, V value, Entry<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private int indexToAssign(K key) {
+        if (key == null) {
+            return 0;
+        }
+        return (key.hashCode()) & (buckets.length - 1);
+    }
+
+    private void resize() {
+        if (size > threshold) {
+            Entry[] currentBuckets = buckets;
+            buckets = new Entry[currentBuckets.length * 2];
+            transfer(currentBuckets);
+            threshold = (int) (currentBuckets.length * LOAD_FACTOR);
+        }
+    }
+
+    private void transfer(Entry[] oldBuckets) {
+        for (Entry<K, V> element : oldBuckets) {
+            while (element != null) {
+                if (buckets[indexToAssign(element.key)] == null) {
+                    buckets[indexToAssign(element.key)]
+                            = new Entry(element.key, element.value, null);
+                } else {
+                    Entry present = buckets[indexToAssign(element.key)];
+                    buckets[indexToAssign(element.key)]
+                            = new Entry(element.key, element.value, present);
+                }
+                element = element.next;
+            }
+        }
     }
 }
