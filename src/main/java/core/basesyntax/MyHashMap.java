243068cc5b6438d7809341508d55ebc83ecfcaa7
Victoria-Moiseienko <teasevic@gmail.com>
2020-08-12 14:41:33 +0300

    update without LinkedList

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,8 +1,5 @@
 package core.basesyntax;
 
-import java.util.LinkedList;
-import java.util.List;
-
 /**
  * <p>Реалізувати свою HashMap, а саме методи `put(K key, V value)`, `getValue()` та `getSize()`.
  * Дотриматися основних вимог щодо реалізації мапи (initial capacity, load factor, resize...)
@@ -12,27 +9,26 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final double LOAD_FACTOR = 0.75;
 
-    private LinkedList<Entry<K, V>>[] elementData;
+    private Entry<K, V>[] elementData;
     private int size;
-    private int currentCapacity = DEFAULT_CAPACITY;
 
     public MyHashMap() {
-        elementData = new LinkedList[DEFAULT_CAPACITY];
-        size = 0;
+        elementData = new Entry[DEFAULT_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
-        if (size > currentCapacity * LOAD_FACTOR) {
+        if (size > elementData.length * LOAD_FACTOR) {
             resizeAndTransfer();
         }
+
         if (key == null) {
             putForNullKey(value);
             return;
         }
 
         int index = getIndexFor(key.hashCode());
-        if (null == elementData[index]) {
+        if (elementData[index] == null) {
             addEntry(key, value, index);
         } else {
             replaceOrAddEntry(key, value, index);
@@ -40,15 +36,15 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void resizeAndTransfer() {
-        currentCapacity = currentCapacity * 2;
-        LinkedList<Entry<K, V>>[] oldElementData = elementData;
-        elementData = new LinkedList[currentCapacity];
+
+        Entry<K, V>[] oldElementData = elementData;
+        elementData = new Entry[elementData.length * 2];
         size = 0;
-        for (List<Entry<K, V>> list : oldElementData) {
-            if (list != null) {
-                for (Entry<K, V> entry : list) {
-                    put(entry.key, entry.value);
-                }
+        for (int i = 0; i < oldElementData.length; i++) {
+            Entry<K, V> entry = oldElementData[i];
+            while (entry != null) {
+                put(entry.key, entry.value);
+                entry = entry.next;
             }
         }
     }
@@ -56,16 +52,21 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public V getValue(K key) {
         int index = key == null ? 0 : getIndexFor(key.hashCode());
-        List<Entry<K, V>> list = elementData[index];
-        if (list == null) {
+        Entry<K, V> entry = elementData[index];
+        if (entry == null) {
             return null;
         }
-        for (Entry<K, V> currentEntry : list) {
-            if (key == currentEntry.key || (key != null && key.equals(currentEntry.key))) {
-                return currentEntry.value;
+        while (entry != null) {
+            if (theSameKey(key, entry.key)) {
+                return entry.value;
             }
+            entry = entry.next;
         }
         return null;
+    }
+
+    private boolean theSameKey(K key, K key1) {
+        return key == key1 || (key != null && key.equals(key1));
     }
 
     @Override
@@ -83,34 +84,41 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private void replaceOrAddEntry(K key, V value, int index) {
         boolean replaced = false;
-        List<Entry<K, V>> list = elementData[index];
-        for (Entry<K, V> currentEntry : list) {
-            if (key == currentEntry.key || (key != null && key.equals(currentEntry.key))) {
-                currentEntry.value = value;
+        Entry<K, V> entry = elementData[index];
+
+        while (!replaced) {
+            if (theSameKey(key, entry.key)) {
+                entry.value = value;
                 replaced = true;
+            }
+
+            if (entry.next != null) {
+                entry = entry.next;
+            } else {
                 break;
             }
         }
+
         if (!replaced) {
-            list.add(new Entry<K, V>(key, value));
+            entry.next = new Entry<>(key, value);
             size++;
         }
     }
 
     private void addEntry(K key, V value, int index) {
-        LinkedList<Entry<K, V>> list = new LinkedList();
-        list.add(new Entry<>(key, value));
-        elementData[index] = list;
+        Entry<K, V> entry = new Entry<>(key, value);
+        elementData[index] = entry;
         size++;
     }
 
     private int getIndexFor(int hashCode) {
-        return Math.abs(hashCode) % currentCapacity;
+        return Math.abs(hashCode) % elementData.length;
     }
 
     private static class Entry<K, V> {
         K key;
         V value;
+        Entry<K, V> next;
 
         Entry(K key, V value) {
             this.key = key;
