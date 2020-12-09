b07f9dc9fb6f2210502649f4d4b3120be6b7644f
Rostyslav Mandybur <mandyburostyk@gmail.com>
2020-12-09 16:31:45 +0200

    local backup

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,111 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final int TABLE_MULTIPLIER = 2;
+    private static final float LOAD_FACTOR = 0.75f;
+    private int size = 0;
+    private int capacity;
+    private int threshold;
+    private Node<K, V>[] table;
+
+    private void resize() {
+        Node<K, V>[] localTable = new Node[capacity * TABLE_MULTIPLIER];
+        capacity *= TABLE_MULTIPLIER;
+        threshold = (int) (capacity * LOAD_FACTOR);
+        for (Node node: table) {
+            Node<K, V> localNode = node;
+            while (localNode != null) {
+                setNode(localTable, localNode);
+                localNode = localNode.next;
+            }
+        }
+        table = localTable;
+    }
+
+    private void setNode(Node<K, V>[] localTable, Node node) {
+        Node<K, V> localNode = localTable[node.hash % capacity];
+        if (localNode == null) {
+            node.next = null;
+            localTable[node.hash % capacity] = node;
+            return;
+        }
+        while (localNode.next != null) {
+            localNode = localNode.next;
+        }
+        localNode.next = node;
+    }
+
+    private boolean checkKeys(K key, V value) {
+        int hash = (key == null) ? 0 : Math.abs(key.hashCode());
+        Node localNode = table[hash % capacity];
+        while (localNode != null) {
+            if (Objects.equals(localNode.key, key)) {
+                localNode.value = value;
+                return true;
+            }
+            localNode = localNode.next;
+        }
+        return false;
+    }
 
     @Override
     public void put(K key, V value) {
-
+        int hash = 0;
+        if (key != null) {
+            hash = Math.abs(key.hashCode());
+        }
+        if (size == 0) {
+            table = new Node[DEFAULT_CAPACITY];
+            capacity = DEFAULT_CAPACITY;
+            threshold = (int) (capacity * LOAD_FACTOR);
+            size++;
+            table[hash % capacity] = new Node<>(hash, key, value, null);
+            return;
+        }
+        if (checkKeys(key, value)) {
+            return;
+        }
+        if (++size > threshold) {
+            resize();
+        }
+        setNode(table, new Node<>(hash, key, value, null));
     }
 
     @Override
     public V getValue(K key) {
+        if (size == 0) {
+            return null;
+        }
+        int hash = key == null ? 0 : Math.abs(key.hashCode());
+        Node<K, V> localNode = table[hash % capacity];
+        while (localNode != null) {
+            if (Objects.equals(localNode.key, key)) {
+                return localNode.value;
+            }
+            localNode = localNode.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    class Node<K, V> {
+        private final int hash;
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
