5180063a2443e311e303dd7401d92c50e8462fa9
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-12 13:36:14 +0200

    Added node class to MyHashMap and private variables

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,6 +1,11 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_SIZE = 16;
+    private static final float loadFactor = 0.75f;
+    private int threshold;
+    private int size;
+    private int actualCapacity;
 
     @Override
     public void put(K key, V value) {
@@ -15,5 +20,19 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public int getSize() {
         return 0;
+    }
+
+    private static class Node<K, V> {
+        private Node<K, V> next;
+        private K key;
+        private V value;
+        private int hash;
+
+        public Node(int hash, K key, V value, Node<K, V> next) {
+            this.next = next;
+            this.key = key;
+            this.value = value;
+            this.hash = hash;
+        }
     }
 }
