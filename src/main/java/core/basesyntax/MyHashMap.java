523cea86303f46e76ebb5b9f9e9f72f205c47793
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-13 14:40:58 +0200

    method putToTable changed implementation

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final short GROWTH_FACTOR = 2;
@@ -18,7 +20,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if (size == threshold) {
             resize();
         }
-        putToTable(new Node<>(getHash(key), key,  value, null), table, false);
+        putToTable(key, value, table, false);
     }
 
     @Override
@@ -44,47 +46,45 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private void resize() {
         capacity = capacity * GROWTH_FACTOR;
         threshold = threshold * GROWTH_FACTOR;
-        size = 0;
         Node<K, V>[] newTable = (Node<K, V>[]) new Node[capacity];
         for (Node<K, V> node : table) { //Transfer
             while (node != null) {
-                putToTable(node, newTable, true);
+                putToTable(node.key, node.value, newTable, true);
                 node = node.next;
             }
         }
         table = newTable;
     }
 
-    private void putToTable(Node<K, V> node, Node<K, V>[] table, boolean resize) {
-        int hash = getHash(node);
-        if (table[hash] != null) { // check if bucket is empty
-            Node<K, V> tableNode = table[hash];
-            while (tableNode.next != null) {
-                if (node.key == tableNode.key
-                        || tableNode.key != null
-                        && tableNode.key.equals(node.key)) {
-                    tableNode.value = node.value;
-                    return;
-                }
-                tableNode = tableNode.next;
-            } // loop ends here
-            tableNode.next = node;
-        } else {
-            table[hash] = node; // bucket was empty, we assign to it a node
+    private void putToTable(K key, V value, Node<K, V>[] table, boolean resize) {
+        Node<K, V> node = new Node<>(getHash(key), key, value, null);
+        if (table[node.hash] == null) {
+            table[node.hash] = node;
+            size++;
+            return;
         }
+        Node<K, V> tableNode = table[node.hash];
+        while (tableNode.next != null) {
+            if (Objects.equals(tableNode.key, node.key)) {
+                tableNode.value = node.value;
+                return;
+            }
+            tableNode = tableNode.next;
+        }
+        tableNode.next = node;
         size = resize ? size : size + 1;
-    }
-
-    /*
-    returns hash value (position in the table) for rearrangement of the
-    table after resize;
-     */
-    private int getHash(Node<K, V> node) {
-        return node.key == null ? 0 : Math.abs(node.key.hashCode() % capacity);
     }
 
     private int getHash(K key) {
         return key == null ? 0 : Math.abs(key.hashCode() % capacity);
+    }
+
+    private void updateHash(){
+        for (Node<K, V> node : table) {
+            while (node != null) {
+                node.hash = getHash(node.key);
+            }
+        }
     }
 
     private static class Node<K, V> {
