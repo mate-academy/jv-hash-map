c8f1d1ffc1e4b22e5ecd7b7f93482c362bf112df
nikelok01@gmail.com <163amefeZ>
2020-12-14 11:36:28 +0200

    - Renamed constant "DEFAULT_LOAD_FACTORY" to "LOAD_FACTORY"; - Changed method getValue() deleted useless if statement; - Changed method resize(), from now it's using method put(); - Changed method isUniqueKey(), from now, it isn't taking table[] as variable;

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    private static final float DEFAULT_LOAD_FACTORY = 0.75f;
+    private static final float LOAD_FACTORY = 0.75f;
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
     private static final int CAPACITY_AND_THRESHOLD_MULTIPLIER = 2;
     private final int capacity;
@@ -14,8 +14,8 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     public MyHashMap() {
         size = 0;
         capacity = DEFAULT_INITIAL_CAPACITY;
-        table = (Node<K,V>[])new Node[capacity];
-        threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTORY);
+        table = (Node<K, V>[]) new Node[capacity];
+        threshold = (int) (DEFAULT_INITIAL_CAPACITY * LOAD_FACTORY);
     }
 
     @Override
@@ -26,7 +26,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             table[indexForValue] = newNode;
             size++;
         } else {
-            isUniqueKey(newNode, table);
+            isUniqueKey(newNode);
         }
         if (size > threshold) {
             resize();
@@ -39,16 +39,12 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if (table[indexWantedValue] == null) {
             return null;
         }
-        if (Objects.equals(table[indexWantedValue].key, key)) {
-            return table[indexWantedValue].value;
-        } else {
-            Node<K, V> wantedNode = table[indexWantedValue];
-            while (wantedNode != null) {
-                if (isEquals(wantedNode.key, key)) {
-                    return wantedNode.value;
-                }
-                wantedNode = wantedNode.next;
+        Node<K, V> wantedNode = table[indexWantedValue];
+        while (wantedNode != null) {
+            if (isEquals(wantedNode.key, key)) {
+                return wantedNode.value;
             }
+            wantedNode = wantedNode.next;
         }
         return null;
     }
@@ -58,40 +54,26 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private static class Node<K, V> {
-        int hash;
-        K key;
-        V value;
-        Node<K,V> next;
-
-        public Node(int hash, K key, V value, Node<K, V> next) {
-            this.hash = hash;
-            this.key = key;
-            this.value = value;
-            this.next = next;
-        }
-    }
-
     private void resize() {
         int newThreshold = threshold * CAPACITY_AND_THRESHOLD_MULTIPLIER;
         int newCapacity = capacity * CAPACITY_AND_THRESHOLD_MULTIPLIER;
         Node<K, V>[] oldTable = table;
         table = (Node<K, V>[]) new Node[newCapacity];
         threshold = newThreshold;
-        int indexInNewTable;
         for (Node<K, V> currentNode : oldTable) {
             if (currentNode != null) {
-                indexInNewTable = indexInTable(currentNode.key);
-                if (table[indexInNewTable] == null) {
-                    table[indexInNewTable] = currentNode;
-                } else {
-                    isUniqueKey(currentNode, table);
+                put(currentNode.key, currentNode.value);
+                size--;
+                while (currentNode.next != null) {
+                    put(currentNode.next.key, currentNode.next.value);
+                    currentNode = currentNode.next;
+                    size--;
                 }
             }
         }
     }
 
-    private void isUniqueKey(Node<K, V> current, Node<K, V>[] table) {
+    private void isUniqueKey(Node<K, V> current) {
         int indexForValue = indexInTable(current.key);
         Node<K, V> checkedNode = table[indexForValue];
         if (isEquals(current.key, checkedNode.key)) {
@@ -123,5 +105,19 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private int indexInTable(K key) {
         return Math.abs(key == null ? 0 : key.hashCode() % table.length);
+    }
+
+    private static class Node<K, V> {
+        int hash;
+        K key;
+        V value;
+        Node<K, V> next;
+
+        public Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
