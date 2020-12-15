e7b3058ebed781ec923f318dbe61560b21b22c12
Tonitrus <grom4er@gmail.com>
2020-12-15 16:32:37 +0200

    Fix: Delete commentary. Style check. Test passed

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -43,22 +43,20 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return (keyOne == keyTwo) || (keyOne != null && keyOne.equals(keyTwo));
     }
 
-
     private Node<K, V>[] resize() {
         if (size == 0) {
             array = new Node[INITIAL_CAPACITY];
             threshold = (int) (INITIAL_CAPACITY * LOAD_FACTOR);
             return array;
         }
-       // capacity = array.length;
-        Node<K, V>[] tempArray = array;
+        Node<K, V> [] tempArray = array;
         array = (Node<K, V>[]) new Node[array.length * MULTIPLIER];
         threshold = (int) (array.length * LOAD_FACTOR);
-        size = 0;
         for (Node<K, V> tempNode : tempArray) {
             while (tempNode != null) {
                 put(tempNode.key, tempNode.value);
                 tempNode = tempNode.next;
+                size--;
             }
         }
         return null;
@@ -92,7 +90,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return array == null || array.length == 0;
     }
 
-
     @Override
     public int getSize() {
         return size;
@@ -110,35 +107,5 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             this.value = value;
             this.next = next;
         }
-
-
     }
 }
-/*
-Хешмапа
-1. Имеет поле Node[] - размером в 16 👌
-a) Node имеет поле int hash  👌
-2. Внутри метода put, есть метод который считает хеш ключа(правка в Б), который передается
-подпункт по put, вызывает метод putVal(hash(key), key, value ... ) остальное вряд ли нужно 👌
-а)   в случае null = 0 || key.hashcode() ^ >>> 16 (скорее всего просто делиться по остатку на 16) 👌
-б) (!) Вычисляет не хеш, а место в массиве куда этот key можно закинуть. 👌
-с) Пункт хеш понадобиться для get(key) (мы вычисляем по этому значению). 👌
-3. Есть внутренний класс Node(Нагло скопировал) 👌
-a) Создается нода, в которую мы помещаем ключ/value/hash по hash позиции. 👌
-б) В случае put, если key с одинаковым хешем equals key с уже готовым ключем, второй теряется в небытие ;(
-4. В методе put, вызываем метод putValue(hash, key, value) 👌
-В ресайзе
-1. Мы проверяем что основная таблица (array) в нулл или length == 0
-В случае тру, мы инициализируем через resize() изничальную хешмапу. и где-то запоминаем длину(а надо?)
-а) Метод ресайз возвращает массив нового.
-Подпункты по ресайзу:
-1) Проверяем старое значение capacity
-В случае если capactity == 0 && old thr == 0 TODO
-capacity = 16
-newThr = load factor * 16
-thr нужен, чтоб определиться когда нам нужно делать resize()
-if(++size
-
-
-
- */
