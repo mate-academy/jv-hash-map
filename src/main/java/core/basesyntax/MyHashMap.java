4e2dee1c1d8ac549fb4ff6c5a9d6aab8059c9dcb
YuraBondarenko <yuriybondarenko2001@gmail.com>
2020-12-15 19:31:39 +0200

    removed variable "size", "hash" and method "getHashCode", also improved checks in methods

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,13 +5,10 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
     private static final double LOAD_FACTOR = 0.75;
-    private int size;
     private int currentSize;
     private Node<K, V>[] node;
 
     public MyHashMap() {
-        size = DEFAULT_INITIAL_CAPACITY;
-        currentSize = 0;
         node = new Node[DEFAULT_INITIAL_CAPACITY];
     }
 
@@ -22,34 +19,29 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if (currentNode == null) {
             node[getIndex(key)] = new Node<>(key, value);
             currentSize++;
-        } else if (Objects.equals(currentNode.key, key)) {
-            node[getIndex(key)].value = value;
-        } else {
-            while (currentNode.next != null && !Objects.equals(currentNode.key, key)) {
-                currentNode = currentNode.next;
-            }
+            return;
+        }
+        while (currentNode.next != null || Objects.equals(currentNode.key, key)) {
             if (Objects.equals(currentNode.key, key)) {
                 currentNode.value = value;
-            } else {
-                currentNode.next = new Node<>(key, value);
-                currentSize++;
+                return;
             }
+            currentNode = currentNode.next;
         }
+        currentNode.next = new Node<>(key, value);
+        currentSize++;
     }
 
     @Override
     public V getValue(K key) {
         Node<K, V> currentNode = node[getIndex(key)];
-        if (currentNode == null || getIndex(key) > size) {
+        if (currentNode == null) {
             return null;
         }
-        while (currentNode.next != null && !Objects.equals(currentNode.key, key)) {
+        while (!Objects.equals(currentNode.key, key)) {
             currentNode = currentNode.next;
         }
-        if (Objects.equals(currentNode.key, key)) {
-            return currentNode.value;
-        }
-        return null;
+        return currentNode.value;
     }
 
     @Override
@@ -58,9 +50,9 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void resize() {
-        if (currentSize > size * LOAD_FACTOR) {
-            size += size;
-            Node<K, V>[] oldNode = new Node[size];
+        if (currentSize > node.length * LOAD_FACTOR) {
+            currentSize = node.length * 2;
+            Node<K, V>[] oldNode = new Node[currentSize];
             Node<K, V>[] newNode = node;
             node = oldNode;
             currentSize = 0;
@@ -74,21 +66,15 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private int getIndex(K key) {
-        return key == null ? 0 : Math.abs(key.hashCode()) % size;
-    }
-
-    private static <K> int getHashCode(K key) {
-        return key == null ? 0 : key.hashCode();
+        return key == null ? 0 : Math.abs(key.hashCode()) % node.length;
     }
 
     private static class Node<K, V> {
-        private final int hash;
         private final K key;
         private V value;
         private Node<K,V> next;
 
         public Node(K key, V value) {
-            this.hash = getHashCode(key);
             this.key = key;
             this.value = value;
             this.next = null;
