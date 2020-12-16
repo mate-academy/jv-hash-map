76e06cdb58ea57cf16d10abfbfc34e4e2fda470c
egorka <entertainment.veks@gmail.com>
2020-12-16 23:00:38 +0200

    changed isKeysEquals on 87

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
@@ -82,7 +84,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private boolean isKeysEquals(K key1, K key2) {
-        return key1 == null ? key1 == key2 : key1.equals(key2);
+        return Objects.equals(key1, key2);
     }
 
     private class Node<K, V> {
