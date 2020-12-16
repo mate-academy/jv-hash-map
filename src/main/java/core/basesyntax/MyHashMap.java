5ea8e3d7057248a124feb982410f107917283b01
Volodymyr Lykhvar <vlykhvar@gmail.com>
2020-12-16 11:27:51 +0200

    initial commit

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,11 +1,15 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int INITIAL_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75F;
+    private int size = 0;
+    private Node<K, V>[] nodesArray;
 
     @Override
     public void put(K key, V value) {
-
     }
+
 
     @Override
     public V getValue(K key) {
@@ -14,6 +18,27 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
     }
+
+    private int setHash(K key) {
+        return (key == null) ? 0 : (key.hashCode()%size);
+    }
+
+    private class Node<K, V> {
+        int hash;
+        K key;
+        V value;
+        Node<K, V> next;
+
+        public Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+
+    }
+
 }
