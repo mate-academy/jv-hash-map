060e81c92e37e87266fc7d024815cf19f48fcdbf
Alex <koleksandr4@gmail.com>
2020-12-16 18:58:06 +0200

    done some refactoring

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -3,15 +3,14 @@ package core.basesyntax;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
     private static final int DEFAULT_CAPACITY = 16;
-    private static final int PRIME_NUMBER = 17;
     private static final int MULTIPLIER = 2;
     private Node<K, V>[] table;
-    private int size = 0;
+    private int size;
     private int threshold;
 
     public MyHashMap() {
         table = (Node<K, V>[]) new Node[DEFAULT_CAPACITY];
-        threshold = (int) (DEFAULT_CAPACITY * DEFAULT_LOAD_FACTOR);
+        threshold = (int) (table.length * DEFAULT_LOAD_FACTOR);
     }
 
     @Override
@@ -19,17 +18,17 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if (size >= threshold) {
             resize();
         }
-        putVal(hash(key), key, value);
+        putVal(getHash(key), key, value);
     }
 
     @Override
     public V getValue(K key) {
-        int index = hash(key) % table.length;
-        Node<K, V> currentNode = table[index];
+        Node<K, V> currentNode = table[getHash(key) % table.length];
         while (true) {
             if (currentNode == null) {
                 return null;
-            } else if (isKeyEquals(currentNode.key, key)) {
+            }
+            if (isKeyEquals(currentNode.key, key)) {
                 return currentNode.value;
             }
             currentNode = currentNode.next;
@@ -64,12 +63,8 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    private int hash(K key) {
-        if (key == null) {
-            return 0;
-        }
-        int hash = key.hashCode() * PRIME_NUMBER;
-        return (hash >= 0) ? hash : -1 * hash;
+    private int getHash(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode());
     }
 
     private void resize() {
@@ -79,10 +74,10 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         size = 0;
         for (Node<K, V> currentNode: oldTable) {
             if (currentNode != null) {
-                putVal(hash(currentNode.key), currentNode.key, currentNode.value);
+                putVal(getHash(currentNode.key), currentNode.key, currentNode.value);
                 while (currentNode.next != null) {
                     currentNode = currentNode.next;
-                    putVal(hash(currentNode.key), currentNode.key, currentNode.value);
+                    putVal(getHash(currentNode.key), currentNode.key, currentNode.value);
                 }
             }
         }
@@ -92,5 +87,19 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return (key1 == null && key1 == key2)
                 || (key1 != null
                 && key1.equals(key2));
+    }
+
+    private class Node<K, V> {
+        private final int hash;
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
