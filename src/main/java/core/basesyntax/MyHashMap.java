ad195e553faf4a3b761e2c23860a80634a901e30
Okulov Vadim <Vedroidua@gmail.com>
2020-12-16 15:54:08 +0200

    Fixed some requested changes

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    private static final int DEFAULT_INITIAL_CAPACITY = 16;
-    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private static final int INITIAL_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
     
     private Node<K, V>[] table;
     private int threshold;
@@ -10,22 +10,28 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     
     @SuppressWarnings("unchecked")
     public MyHashMap() {
-        table = (Node<K, V>[]) new Node[DEFAULT_INITIAL_CAPACITY];
-        threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
+        table = (Node<K, V>[]) new Node[INITIAL_CAPACITY];
+        threshold = (int) (INITIAL_CAPACITY * LOAD_FACTOR);
     }
     
-    private static class Node<K, V> {
-        private final int hash;
-        private final K key;
-        private V value;
-        private Node<K, V> next;
-        
-        Node(int hash, K key, V value, Node<K, V> next) {
-            this.hash = hash;
-            this.key = key;
-            this.value = value;
-            this.next = next;
+    @Override
+    public int getSize() {
+        return size;
+    }
+    
+    @Override
+    public V getValue(K key) {
+        if (size == 0) {
+            return null;
         }
+        Node<K, V> currentNode = table[hash(key)];
+        while (currentNode != null) {
+            if (key == currentNode.key || (key != null && key.equals(currentNode.key))) {
+                return currentNode.value;
+            }
+            currentNode = currentNode.next;
+        }
+        return null;
     }
     
     @Override
@@ -53,31 +59,11 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
     
-    @Override
-    public V getValue(K key) {
-        if (size == 0) {
-            return null;
-        }
-        Node<K, V> currentNode = table[hash(key)];
-        while (currentNode != null) {
-            if (key == currentNode.key || (key != null && key.equals(currentNode.key))) {
-                return currentNode.value;
-            }
-            currentNode = currentNode.next;
-        }
-        return null;
-    }
-    
-    @Override
-    public int getSize() {
-        return size;
-    }
-    
     @SuppressWarnings("unchecked")
     private void resize() {
         size = 0;
         int newCap = table.length * 2;
-        threshold = (int) (newCap * DEFAULT_LOAD_FACTOR);
+        threshold = (int) (newCap * LOAD_FACTOR);
         Node<K, V>[] oldTable = table;
         table = (Node<K, V>[]) new Node[newCap];
         for (Node<K, V> node : oldTable) {
@@ -92,7 +78,20 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
     
     private int hash(K key) {
-        int hash = (key == null) ? 0 : key.hashCode() % threshold;
-        return hash < 0 ? -hash : hash;
+        return key == null ? 0 : Math.abs(key.hashCode() % threshold);
+    }
+    
+    private static class Node<K, V> {
+        private final int hash;
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+        
+        Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
