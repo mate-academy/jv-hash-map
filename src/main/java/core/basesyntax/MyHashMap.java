7bc46da226df3fd15f9a75cc0f47757c41dc2cf0
Andrii Bardin <andrii.bardin@gmail.com>
2020-12-17 10:06:14 +0200

    fixed naming

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -4,20 +4,20 @@ import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final double LOAD_FACTOR = 0.75;
-    private static final int CAPACITY = 16;
+    private static final int INITIAL_CAPACITY = 16;
     private Node<K, V>[] nodes;
     private int size;
 
     public MyHashMap() {
-        nodes = new Node[CAPACITY];
+        nodes = new Node[INITIAL_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
         thresholdCheck();
-        Node<K, V> previous = nodes[getHash(key)];
+        Node<K, V> previous = nodes[getIndex(key)];
         if (previous == null) {
-            nodes[getHash(key)] = new Node<K, V>(key, value, null);
+            nodes[getIndex(key)] = new Node<K, V>(key, value, null);
             size++;
             return;
         }
@@ -44,7 +44,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private Node<K, V> getNode(K key) {
-        Node<K, V> node = nodes[getHash(key)];
+        Node<K, V> node = nodes[getIndex(key)];
         while (node != null) {
             if (Objects.equals(node.key,key)) {
                 break;
@@ -56,15 +56,15 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private void resize() {
         int newSize = nodes.length * 2;
-        Node<K, V>[] old = new Node[newSize];
-        Node<K, V>[] resized = nodes;
-        nodes = old;
+        Node<K, V>[] newNodes = new Node[newSize];
+        Node<K, V>[] current = nodes;
+        nodes = newNodes;
         size = 0;
-        transfer(resized);
+        transfer(current);
     }
 
-    private void transfer(Node<K, V>[] resized) {
-        for (Node<K, V> node : resized) {
+    private void transfer(Node<K, V>[] current) {
+        for (Node<K, V> node : current) {
             while (node != null) {
                 put(node.key, node.value);
                 node = node.next;
@@ -72,7 +72,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
     
-    private int getHash(K key) {
+    private int getIndex(K key) {
         return (key == null) ? 0 : Math.abs(key.hashCode() % nodes.length);
     }
 
