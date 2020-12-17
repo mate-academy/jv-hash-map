7d1a9e698fe9c4309f1a156bf6358952cb26a46d
engineer027 <dp270597dma@gmail.com>
2020-12-17 11:26:28 +0200

    fix

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,21 +1,22 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    static final int DEFAULT_INITIAL_CAPACITY = 16;
-    static final float DEFAULT_LOAD_FACTOR = 0.75f;
-    private Node<K, V>[] map; // наш масив
-    private int size = 0; // кількість елементів
-    private double threshold; // значення критичного заповненення масиву
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private Node<K, V>[] nodes;
+    private int size = 0;
+    private double threshold;
 
     public MyHashMap() {
-        map = (Node<K, V>[]) new Node[DEFAULT_INITIAL_CAPACITY];
+        nodes = (Node<K, V>[]) new Node[DEFAULT_INITIAL_CAPACITY];
         threshold = DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR;
     }
 
     private static class Node<K,V> {
         V value;
-        MyHashMap.Node<K,V> next;
-        private int hash;
+        Node<K,V> next;
         private K key;
 
         public Node(K key, V value, Node<K, V> next) {
@@ -31,15 +32,15 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             resize();
         }
         Node<K, V> newNode = new Node<>(key, value, null);
-        int index = getIndex(key); // знаходимо індекс
-        if (map[index] == null) {
-            map[index] = newNode;
+        int index = getIndex(key);
+        if (nodes[index] == null) {
+            nodes[index] = newNode;
             size++;
             return;
         }
-        Node<K, V> node = map[index];
+        Node<K, V> node = nodes[index];
         while (node != null) {
-            if (key == node.key || key != null && key.equals(node.key)) {
+            if (Objects.equals(key, node.key)) {
                 node.value = value;
                 return;
             }
@@ -54,11 +55,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public V getValue(K key) {
-        if (size == 0) {
-            return null;
-        }
-        int index = getIndex(key);
-        Node<K, V> node = map[index];
+        Node<K, V> node = nodes[getIndex(key)];
         while (node != null) {
             if (key == node.key || key != null && key.equals(node.key)) {
                 return node.value;
@@ -75,20 +72,18 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private void resize() {
         size = 0;
-        Node<K, V>[] mapOld = map;
-        map = (Node<K, V>[]) new Node[(int) (map.length * 2)];
-        threshold = map.length * DEFAULT_LOAD_FACTOR;
+        Node<K, V>[] mapOld = nodes;
+        nodes = (Node<K, V>[]) new Node[(int) (nodes.length * 2)];
+        threshold = nodes.length * DEFAULT_LOAD_FACTOR;
         for (Node<K, V> kvNode : mapOld) {
-            Node<K, V> nodeOld = kvNode;
-            while (nodeOld != null) {
-                put(nodeOld.key, nodeOld.value);
-                nodeOld = nodeOld.next;
+            while (kvNode != null) {
+                put(kvNode.key, kvNode.value);
+                kvNode = kvNode.next;
             }
         }
     }
 
     private int getIndex(K key) {
-        return key == null ? 0 : Math.abs(key.hashCode() % map.length);
+        return key == null ? 0 : Math.abs(key.hashCode() % nodes.length);
     }
 }
-
