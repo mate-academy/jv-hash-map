b79605564a9d7e776d265102ab0732f1c0787aa4
Igor Bochkor <igorbochkor@gmail.com>
2020-12-17 13:40:15 +0200

    fixed bugs#2: removed unused variable, create method getIndex(), refactored

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
@@ -15,13 +17,11 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private static class Node<K, V> {
-        private int hash;
         private K key;
         private V value;
         private Node<K, V> next;
 
-        public Node(int hash, K key, V value, Node<K, V> next) {
-            this.hash = hash;
+        public Node(K key, V value, Node<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
@@ -32,29 +32,31 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return Math.abs(key == null ? 0 : (key.hashCode()));
     }
 
+    private int getIndex(K key) {
+        return getHash(key) % table.length;
+    }
+
     @Override
     public void put(K key, V value) {
         if (size > threshold) {
             resize();
         }
         Node<K, V>[] tab = table;
-        int index = getHash(key) % tab.length;
-        int hash = getHash(key);
+        int index = getIndex(key);
         if (tab[index] == null) {
-            tab[index] = new Node<>(hash, key, value, null);
+            tab[index] = new Node<>(key, value, null);
             size++;
             return;
         }
         Node<K, V> temp = tab[index];
-        while (temp.next != null
-                || (key == temp.key || (key != null && key.equals(temp.key)))) {
-            if (key == temp.key || (key != null && key.equals(temp.key))) {
+        while (temp.next != null || (Objects.equals(key, temp.key))) {
+            if (Objects.equals(key, temp.key)) {
                 temp.value = value;
                 return;
             }
             temp = temp.next;
         }
-        Node<K, V> newNode = new Node<>(hash, key, value, null);
+        Node<K, V> newNode = new Node<>(key, value, null);
         temp.next = newNode;
         size++;
     }
@@ -75,12 +77,9 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public V getValue(K key) {
-        if (size == 0) {
-            return null;
-        }
-        Node<K, V> temp = table[getHash(key) % table.length];
+        Node<K, V> temp = table[getIndex(key)];
         while (temp != null) {
-            if (key == temp.key || key != null && key.equals(temp.key)) {
+            if (Objects.equals(key, temp.key)) {
                 return temp.value;
             }
             temp = temp.next;
