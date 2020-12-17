dcff9448e1f7a6d0f7077f8bc756747e2be66bcc
Sashk52 <a.tsven@gmail.com>
2020-12-17 14:11:16 +0200

    made correction

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -11,8 +11,11 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     public MyHashMap() {
         table = new Node[INITIAL_SIZE];
-        this.size = 0;
         this.threshold = (int)(table.length * LOAD_FACTOR);
+    }
+
+    private int hashKey(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode() % table.length);
     }
 
     public static class Node<K, V> {
@@ -29,17 +32,16 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public void put(K key, V value) {
-        if (size + 1 == threshold) {
+        if (size > threshold) {
             resize();
         }
 
-        int index = (key == null) ? 0 : Math.abs(key.hashCode() % table.length);
+        int index = hashKey(key);
         Node<K, V> addNode = table[index];
         if (addNode == null) {
             table[index] = new Node<K,V>(key,value,null);
             size++;
             return;
-
         }
 
         while (addNode != null) {
@@ -58,7 +60,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public V getValue(K key) {
-        int index = (key == null) ? 0 : Math.abs(key.hashCode() % table.length);
+        int index = hashKey(key);
         Node<K,V> gettedNode = table[index];
         while (gettedNode != null) {
             if (gettedNode.key == key || gettedNode.key != null && gettedNode.key.equals(key)) {
@@ -76,11 +78,11 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     public void resize() {
         Node<K,V> [] newTable = new Node[table.length * INCREASE_ARRAY_SIZE_INDEX];
-        threshold = (int) ((int) newTable.length * LOAD_FACTOR);
+        threshold = (int) (newTable.length * LOAD_FACTOR);
         Node<K,V> [] oldTable = table;
         table = newTable;
         size = 0;
-        for (int i = 0;i < oldTable.length;i++) {
+        for (int i = 0; i < oldTable.length;i++) {
             if (oldTable[i] != null) {
                 while (oldTable[i].nextNode != null) {
                     put(oldTable[i].key, oldTable[i].value);
