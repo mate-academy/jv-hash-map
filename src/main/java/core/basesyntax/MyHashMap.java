682b7e36cd0e950a0265e7aae15669965ac466e8
m.kislukhina <marharita.kislukhina@gmail.com>
2020-12-17 17:08:29 +0100

    added final MULTIPLIER,

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,21 +5,32 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final double LOAD_FACTOR = 0.75;
+    private static final int MULTIPLIER = 2;
 
     private int size;
     private int threshold;
+
     private Node<K, V>[] table;
+
+    public static class Node<K, V> {
+        K key;
+        V value;
+        Node<K, V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
 
     public MyHashMap() {
         threshold = (int) (DEFAULT_CAPACITY * LOAD_FACTOR);
-        table = (Node<K, V>[]) new Node[DEFAULT_CAPACITY];
+        table = new Node[DEFAULT_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
-        if (size > threshold) {
-            resize();
-        }
         int index = hash(key);
         Node<K, V> newNode = new Node<>(key, value, null);
         if (table[index] == null) {
@@ -28,6 +39,9 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             return;
         }
         checkKey(newNode);
+        if (size > threshold) {
+            resize();
+        }
     }
 
     @Override
@@ -50,8 +64,8 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     public void resize() {
         size = 0;
-        int newThreshold = threshold * 2;
-        int newCapacity = table.length * 2;
+        int newThreshold = threshold * MULTIPLIER;
+        int newCapacity = table.length * MULTIPLIER;
         threshold = newThreshold;
         Node<K, V>[] oldTable = table;
         table = (Node<K, V>[]) new Node[newCapacity];
@@ -63,20 +77,8 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    public static class Node<K, V> {
-        K key;
-        V value;
-        Node<K, V> next;
-
-        public Node(K key, V value, Node<K, V> next) {
-            this.key = key;
-            this.value = value;
-            this.next = next;
-        }
-    }
-
     private int hash(K key) {
-        return Math.abs(key == null ? 0 : key.hashCode() % table.length);
+        return key == null ? 0 : Math.abs(key.hashCode() % table.length);
     }
 
     private void checkKey(Node<K, V> current) {
