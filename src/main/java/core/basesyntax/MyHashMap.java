49139d13dbab90c562ff165292b5235881b4acf7
Tetiana Chupika <mtv1301@gmail.com>
2021-04-07 21:02:09 +0300

    added some refactoring

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -3,11 +3,9 @@ package core.basesyntax;
 import java.util.Arrays;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    private static int DEFAULT_CAPACITY = 16;
-    private static double LOAD_FACTOR = 0.75;
-    private Node<K, V> [] table = new Node[DEFAULT_CAPACITY];
-    private int hash;
-    private int positionElem;
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final double LOAD_FACTOR = 0.75;
+    private Node<K, V>[] table = new Node[DEFAULT_CAPACITY];
     private int size = 0;
     private int threshold = (int) (DEFAULT_CAPACITY * LOAD_FACTOR);
 
@@ -21,8 +19,8 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public void put(K key, V value) {
         Node<K, V> newNode = new Node<>(key, value, null);
-        hash = key == null ? 0 : key.hashCode();
-        positionElem = Math.abs(hash % table.length);
+        int hash = key == null ? 0 : key.hashCode();
+        int positionElem = Math.abs(hash % table.length);
         if (table[positionElem] == null) {
             table[positionElem] = newNode;
         } else {
@@ -32,7 +30,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                 current.value = value;
                 return;
             }
-            Node<K,V> last = current;
+            Node<K, V> last = current;
             while (current != null) {
                 if (key != null && key.equals(current.getKey()) || key == null
                         && current.getKey() == null) {
@@ -53,43 +51,42 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    private Node<K, V> [] resize() {
+    private Node<K, V>[] resize() {
         int newCapacity = table.length * 2;
         threshold = (int) (newCapacity * LOAD_FACTOR);
-        Node<K, V> [] newTable = new Node[newCapacity];
+        Node<K, V>[] newTable = new Node[newCapacity];
         transfer(newCapacity);
         return newTable;
     }
 
     private void transfer(int newCapacity) {
-        Node<K,V>[] newTable = new Node[newCapacity];
+        Node<K, V>[] newTable = new Node[newCapacity];
         for (int i = 0; i < table.length; i++) {
             Node<K, V> current = table[i];
-            if (current != null) {
-                while (current != null) {
-                    positionElem = Math.abs((current.getKey().hashCode()) % newCapacity);
-                    Node<K, V> newNode = new Node<>(current.getKey(), current.getValue(), null);
-                    if (newTable[positionElem] == null) {
-                        newTable[positionElem] =
-                                new Node<>(current.getKey(), current.getValue(), null);
-                    } else {
-                        Node<K, V> temp = newTable[positionElem];
-                        while (temp.next != null) {
-                            temp = temp.next;
-                        }
-                        temp.next = newNode;
+            while (current != null) {
+                int positionElem = Math.abs((current.getKey().hashCode()) % newCapacity);
+                Node<K, V> newNode = new Node<>(current.getKey(), current.getValue(), null);
+                if (newTable[positionElem] == null) {
+                    newTable[positionElem] =
+                            new Node<>(current.getKey(), current.getValue(), null);
+                } else {
+                    Node<K, V> temp = newTable[positionElem];
+                    while (temp.next != null) {
+                        temp = temp.next;
                     }
-                    current = current.next;
+                    temp.next = newNode;
                 }
+                current = current.next;
             }
+
         }
         table = newTable;
     }
 
     @Override
     public V getValue(K key) {
-        hash = key == null ? 0 : key.hashCode();
-        positionElem = Math.abs(hash % table.length);
+        int hash = key == null ? 0 : key.hashCode();
+        int positionElem = Math.abs(hash % table.length);
         Node<K, V> current = table[positionElem];
         if (current == null) {
             return null;
