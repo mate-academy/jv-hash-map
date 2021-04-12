ce88d8d18e2f83d6e4ab307eee814fdd7bdfa854
safordog <safordog@gmail.com>
2021-04-12 23:50:22 +0300

    added new solution

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -3,117 +3,12 @@ package core.basesyntax;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
-    private int currentCapacity;
+    private static final int ARRAY_LENGTH_INCREASE_FACTOR = 2;
     private int size;
     private Node<K, V>[] table;
     private int threshold;
 
-    public MyHashMap() {
-        currentCapacity = DEFAULT_INITIAL_CAPACITY;
-        table = new Node[DEFAULT_INITIAL_CAPACITY];
-        threshold = (int) (currentCapacity * DEFAULT_LOAD_FACTOR);
-    }
-
-    @Override
-    public void put(K key, V value) {
-        if (checkLoadOfMap()) {
-            table = resizeNodesArray();
-        }
-        if (addNodeToArray(key, value, table)) {
-            size++;
-        }
-    }
-
-    @Override
-    public V getValue(K key) {
-        int hash = (key == null ? 0 : generateHashCodeByKey(key));
-        Node<K, V> node = table[hash];
-        if (node == null) {
-            return null;
-        }
-        if (key == node.key) {
-            return table[hash].value;
-        }
-        while (!node.key.equals(key)) {
-            node = node.next;
-        }
-        return node.value;
-    }
-
-    @Override
-    public int getSize() {
-        return size;
-    }
-
-    private int generateHashCodeByKey(K key) {
-        if (key == null) {
-            return 0;
-        }
-        int result = 17;
-        result = 31 * result + Math.abs(key.hashCode());
-        result = result % currentCapacity;
-        return result;
-    }
-
-    private boolean putNodeWithExistedHash(Node<K, V> newNode, Node<K, V> nodeInTable) {
-        Node<K, V> previousNode = null;
-        while (nodeInTable != null) {
-            if (nodeInTable.equals(newNode)) {
-                return false;
-            }
-            previousNode = nodeInTable;
-            nodeInTable = nodeInTable.next;
-        }
-        previousNode.next = newNode;
-        return true;
-    }
-
-    private boolean checkLoadOfMap() {
-        return size == threshold;
-    }
-
-    private Node<K, V>[] resizeNodesArray() {
-        currentCapacity *= 2;
-        threshold = (int) (currentCapacity * DEFAULT_LOAD_FACTOR);
-        return replaceDataToResizedArray();
-    }
-
-    private Node[] replaceDataToResizedArray() {
-        Node[] enlargedArray = new Node[currentCapacity];
-        for (int i = 0; i < table.length; i++) {
-            if (table[i] == null) {
-                continue;
-            }
-            addNodeToArray(table[i].key, table[i].value, enlargedArray);
-            if (table[i].next != null) {
-                table[i] = table[i].next;
-                i--;
-            }
-        }
-        return enlargedArray;
-    }
-
-    private boolean addNodeToArray(K key, V value, Node<K, V>[] nodes) {
-        boolean increasingSize = false;
-        int hash = generateHashCodeByKey(key);
-        Node<K, V> newNode = new Node<>(hash, key, value, null);
-        if (nodes[hash] == null) {
-            nodes[hash] = newNode;
-            increasingSize = true;
-        } else if (equalsKeys(key, nodes[hash].key)) {
-            nodes[hash].value = value;
-        } else {
-            Node<K, V> nodeInTable = nodes[hash];
-            increasingSize = putNodeWithExistedHash(newNode, nodeInTable);
-        }
-        return increasingSize;
-    }
-
-    private boolean equalsKeys(K key1, K key2) {
-        return key1 == key2 || key1 != null && key1.equals(key2);
-    }
-
-    public static class Node<K, V> {
+    static class Node<K, V> {
         private final int hash;
         private final K key;
         private V value;
@@ -145,9 +40,94 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         @Override
         public int hashCode() {
             int result = 17;
-            result = 31 * result + this.key.hashCode();
-            result = 31 * result + this.value.hashCode();
+            result = 31 * result + key.hashCode();
+            result = 31 * result + value.hashCode();
             return result;
         }
+    }
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_INITIAL_CAPACITY];
+        threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
+    }
+
+    @Override
+    public void put(K key, V value) {
+        if (checkLoadOfMap()) {
+            resizeNodesArray();
+        }
+        int hash = generateHashCodeByKey(key);
+        Node<K, V> newNode = new Node<>(hash, key, value, null);
+        if (table[hash] == null) {
+            table[hash] = newNode;
+            size++;
+        } else {
+            insertValue(newNode);
+        }
+    }
+
+    @Override
+    public V getValue(K key) {
+        int hash = generateHashCodeByKey(key);
+        Node<K, V> currentNode = table[hash];
+        while (currentNode != null) {
+            if (key == currentNode.key
+                    || (currentNode != null && equalsKeys(key, currentNode.key))) {
+                return currentNode.value;
+            }
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    private int generateHashCodeByKey(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode()) % table.length;
+    }
+
+    private void insertValue(Node<K, V> newNode) {
+        Node<K, V> currentNode = table[newNode.hash];
+        Node<K, V> previousNode = null;
+        while (currentNode != null) {
+            if (equalsKeys(currentNode.key, newNode.key)) {
+                currentNode.value = newNode.value;
+                return;
+            }
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+        currentNode = newNode;
+        previousNode.next = currentNode;
+        size++;
+    }
+
+    private boolean checkLoadOfMap() {
+        return size == threshold;
+    }
+
+    private void resizeNodesArray() {
+        int tableSize = ARRAY_LENGTH_INCREASE_FACTOR * table.length;
+        threshold = (int) (tableSize * DEFAULT_LOAD_FACTOR);
+        Node<K, V>[] oldTable = table;
+        table = new Node[tableSize];
+        size = 0;
+        for (int i = 0; i < oldTable.length; i++) {
+            if (oldTable[i] == null) {
+                continue;
+            }
+            put(oldTable[i].key, oldTable[i].value);
+            if (oldTable[i].next != null) {
+                oldTable[i] = oldTable[i].next;
+                i--;
+            }
+        }
+    }
+
+    private boolean equalsKeys(K key1, K key2) {
+        return key1 == key2 || key1 != null && key1.equals(key2);
     }
 }
