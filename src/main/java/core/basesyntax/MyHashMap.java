1a6bbe7b90ca900f359d90703a9126cbe11d8b82
anufriiev <dimaanufriev1012@gmail.com>
2021-04-15 15:56:47 +0300

    fixes

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -3,14 +3,12 @@ package core.basesyntax;
 import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-
     private static final int DEFAULT_CAPACITY = 16;
     private static final float LOAD_FACTOR = 0.75F;
     private static final int RESIZE_COEFFICIENT = 2;
     private Node<K, V>[] table;
     private int size;
     private int threshold;
-    private int indexBucket;
 
     public MyHashMap() {
         table = new Node[DEFAULT_CAPACITY];
@@ -23,7 +21,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             size = 0;
             reSize();
         }
-        indexBucket = getIndex(key);
+        int indexBucket = getIndex(key);
         Node<K, V> newNode = new Node<>(key, value, null);
         Node<K, V> nodeInTable = table[indexBucket];
         if (nodeInTable != null) {
@@ -58,19 +56,16 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private Node<K, V>[] reSize() {
-        threshold = threshold * 2;
-        final Node<K, V>[] oldTable = table;
+    private void reSize() {
+        threshold = threshold * RESIZE_COEFFICIENT;
+        Node<K, V>[] oldTable = table;
         table = new Node[table.length * RESIZE_COEFFICIENT];
-
         for (Node<K, V> current : oldTable) {
-            Node<K, V> node = current;
-            while (node != null) {
-                put(node.key, node.value);
-                node = node.next;
+            while (current != null) {
+                put(current.key, current.value);
+                current = current.next;
             }
         }
-        return table;
     }
 
     private int getIndex(K key) {
@@ -78,7 +73,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     public static class Node<K, V> {
-
         private final K key;
         private V value;
         private Node<K, V> next;
