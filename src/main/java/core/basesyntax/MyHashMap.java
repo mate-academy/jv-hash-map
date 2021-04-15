9ffff84f4d7bf3a275bcc71b2d3e143bfd249105
Vlad-Andr <andreev.v99@gmail.com>
2021-04-15 16:19:58 +0300

    fixed everything

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -3,73 +3,47 @@ package core.basesyntax;
 import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-
-    private static final int DEFAULT_INITIAL_CAPACITY = 16;
-    private static final int MAXIMUM_CAPACITY = Integer.MAX_VALUE / 2;
-    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
     private static final int VALUE_FOR_INCREASE = 2;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75F;
     private Node<K, V>[] entryTable;
     private int size;
     private int threshold;
-    private final float loadFactor;
 
     public MyHashMap() {
-        this(DEFAULT_INITIAL_CAPACITY, DEFAULT_LOAD_FACTOR);
-    }
-
-    private MyHashMap(int initialCapacity, float loadFactor) {
-        if (initialCapacity < 0) {
-            throw new IllegalArgumentException("Wrong initial capacity: " + initialCapacity);
-        }
-
-        if (initialCapacity > MAXIMUM_CAPACITY) {
-            initialCapacity = MAXIMUM_CAPACITY;
-        }
-
-        if (loadFactor <= 0 || Float.isNaN(loadFactor)) {
-            throw new IllegalArgumentException("Wrong load factor: " + loadFactor);
-        }
-
-        int capacity = 1;
-        while (capacity < initialCapacity) {
-            capacity++;
-        }
-
-        entryTable = new Node[capacity];
-        this.loadFactor = loadFactor;
-        threshold = (int) (capacity * loadFactor);
+        entryTable = new Node[16];
+        threshold = (int) (entryTable.length * DEFAULT_LOAD_FACTOR);
     }
 
     @Override
     public void put(K key, V value) {
         int numberOfBucket = getBucket(key);
-        Node<K, V> element;
         if (size >= threshold) {
-            changeSize(entryTable.length * VALUE_FOR_INCREASE);
+            changeSize();
         }
-        for (element = entryTable[numberOfBucket]; element != null; element = element.next) {
-            if (numberOfBucket == element.hash && Objects.equals(key, element.nodeKey)) {
-                element.nodeValue = value;
+        for (Node<K, V> element = entryTable[numberOfBucket];
+                element != null; element = element.next) {
+            if (Objects.equals(key, element.key)) {
+                element.value = value;
                 return;
-            } else if (numberOfBucket == element.hash && element.next == null) {
-                element.next = new Node<>(numberOfBucket, key, value, null);
+            }
+            if (element.next == null) {
+                element.next = new Node(key, value, null);
                 size++;
                 return;
             }
         }
 
-        entryTable[numberOfBucket] = new Node<>(numberOfBucket, key, value, null);
+        entryTable[numberOfBucket] = new Node(key, value, null);
         size++;
     }
 
     @Override
     public V getValue(K key) {
         int numberOfBucket = getBucket(key);
-        Node<K, V>[] table = entryTable;
-        Node<K, V> element = table[numberOfBucket];
+        Node<K, V> element = entryTable[numberOfBucket];
         while (element != null) {
-            if (element.hash == numberOfBucket && Objects.equals(key, element.nodeKey)) {
-                return element.nodeValue;
+            if (Objects.equals(key, element.key)) {
+                return element.value;
             }
             element = element.next;
         }
@@ -81,11 +55,9 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private void changeSize(int newCapacity) {
-        if (size >= threshold) {
-            threshold = (int) (newCapacity * loadFactor);
-            makeTransfer();
-        }
+    private void changeSize() {
+        threshold = (int) (entryTable.length * VALUE_FOR_INCREASE * DEFAULT_LOAD_FACTOR);
+        makeTransfer();
     }
 
     private void makeTransfer() {
@@ -94,7 +66,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         size = 0;
         for (Node<K, V> element : oldTable) {
             while (element != null) {
-                put(element.nodeKey, element.nodeValue);
+                put(element.key, element.value);
                 element = element.next;
             }
         }
@@ -104,17 +76,15 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return key == null ? 0 : Math.abs(key.hashCode()) % entryTable.length;
     }
 
-    static class Node<K, V> {
-        private int hash;
-        private K nodeKey;
-        private V nodeValue;
+    private static class Node<K, V> {
+        private K key;
+        private V value;
         private Node<K, V> next;
 
-        public Node(int hash, K k, V v, Node<K, V> next) {
-            this.nodeKey = k;
-            this.nodeValue = v;
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
             this.next = next;
-            this.hash = hash;
         }
     }
 }
