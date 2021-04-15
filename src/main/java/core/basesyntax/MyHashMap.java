89cf832f175b4fb8e1263a0d79a237b6b1ea601f
Darina <to.darina.to>
2021-04-15 14:08:35 +0300

    student review1 done

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -57,7 +57,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         private V value;
         private Node<K, V> next;
 
-        public Node(K key, V value, Node<K, V> next) {
+        public Node(int hash, K key, V value, Node<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
