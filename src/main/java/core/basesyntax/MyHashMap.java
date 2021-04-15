730c9508ed259690bfee7eddc4c361e283857ab5
MakeDreamCode <65960125+MakeDreamCode@users.noreply.github.com>
2021-04-15 17:01:32 +0300

    Ready for review.

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -3,63 +3,63 @@ package core.basesyntax;
 import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    static final int DEFAULT_INITIAL_CAPACITY = 16;
-    static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
 
     private Node<K, V>[] table;
     private int threshold;
     private int size;
 
-    private class Node<K, V> {
-        private K key;
-        private V value;
-        private Node<K, V> next;
-
-        private Node(K key, V value, Node<K, V> next) {
-            this.key = key;
-            this.value = value;
-            this.next = next;
-        }
-    }
-
     public MyHashMap() {
-        this.table = new Node[DEFAULT_INITIAL_CAPACITY];
-        this.threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
+        table = new Node[DEFAULT_INITIAL_CAPACITY];
+        threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
     }
 
     @Override
     public void put(K key, V value) {
-        if (size + 1 > threshold) {
+        if (size >= threshold) {
             resize();
         }
         int index = hash(key);
-
         if (table[index] == null) {
-            simpleAdd(index, key, value);
-            return;
+            Node<K, V> newNode = new Node<>(key, value, null);
+            table[index] = newNode;
+            size++;
         } else {
-            collisionHandling(key, value, index);
+            Node<K, V> node = table[index];
+            while (node != null) {
+                if (Objects.equals(node.key, key)) {
+                    node.value = value;
+                    return;
+                }
+                node = node.next;
+            }
+            Node<K, V> lastNode = new Node<K, V>(key, value, table[index]);
+            table[index] = lastNode;
+            size++;
         }
     }
 
-    private void collisionHandling(K key, V value, int index) {
+    @Override
+    public V getValue(K key) {
+        int index = hash(key);
         Node<K, V> node = table[index];
         while (node != null) {
             if (Objects.equals(node.key, key)) {
-                node.value = value;
-                return;
+                return node.value;
             }
             node = node.next;
         }
-        Node<K, V> lastNode = new Node<K, V>(key, value, table[index]);
-        table[index] = lastNode;
-        size++;
+        return null;
     }
 
-    private void simpleAdd(int index, K key, V value) {
-        Node<K, V> newNode = new Node<>(key, value, null);
-        table[index] = newNode;
-        size++;
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    private int hash(K key) {
+        return key != null ? Math.abs(key.hashCode() % table.length) : 0;
     }
 
     private void resize() {
@@ -75,27 +75,15 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         threshold = (int) (table.length * DEFAULT_LOAD_FACTOR);
     }
 
-    @Override
-    public V getValue(K key) {
-        int index = hash(key);
-        if (table != null && index < table.length && table[index] != null) {
-            Node<K, V> node = table[index];
-            while (node != null) {
-                if (Objects.equals(node.key, key)) {
-                    return node.value;
-                }
-                node = node.next;
-            }
+    private class Node<K, V> {
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        private Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
         }
-        return null;
-    }
-
-    @Override
-    public int getSize() {
-        return size;
-    }
-
-    private int hash(K key) {
-        return key != null ? Math.abs(key.hashCode() % table.length) : 0;
     }
 }
