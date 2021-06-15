bd2c32fe11c720f4d375ac127af987bd98b9da38
olesiasmitiukh <d.olleska@gmail.com>
2021-06-15 22:54:51 +0300

    Added class fields, constructor, class Node and its constructor Node.

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,27 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    static final int DEFAULT_CAPACITY = 1 << 4;
+    static final float LOAD_FACTOR = 0.75f;
+    private Node<K,V>[] table;
+    private int size;
+    private float threshold;
 
-    @Override
-    public void put(K key, V value) {
-
+    public MyHashMap() {
+        table = new Node[DEFAULT_CAPACITY];
+        threshold = DEFAULT_CAPACITY * LOAD_FACTOR;
     }
 
-    @Override
-    public V getValue(K key) {
-        return null;
-    }
 
-    @Override
-    public int getSize() {
-        return 0;
+    private static class Node<K,V> {
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        private Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
