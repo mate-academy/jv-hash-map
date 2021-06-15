db373f7317412841e239d2541dea7960145ecc45
Борис Скворцов <dhakmar@gmail.com>
2021-06-15 07:47:52 +0300

    Created variable + inner static class Node

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,6 +1,25 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static int DEFAULT_CAPACITY = 16;
+    private static float DEFAULT_LOAD_FACTOR = 0.75f;
+    private static int MAGNIFICATION_FACTOR = 2;
+    private int size;
+    private Node<K, V> table;
+
+
+    static class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
 
     @Override
     public void put(K key, V value) {
