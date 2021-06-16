54fd695bd380228760b0dae0b17768e024ee0b9d
Yura Lug <LUV97@ukr.net>
2021-06-16 18:10:06 +0300

    Create Program, create new private methods and inner class, create methods

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,82 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+    private static final byte RESIZE = 2;
+    private int size;
+    private Node[] table;
+    private int threshold = (int) (DEFAULT_CAPACITY * LOAD_FACTOR);
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
-
+        if (size >= threshold) {
+            resize();
+        }
+        int index = hashCode(key);
+        Node node = table[hashCode(key)];
+        while (node != null) {
+            if (Objects.equals(key, node.key)) {
+                node.value = value;
+                return;
+            }
+            node = node.next;
+        }
+        node = new Node<K, V>(key, value, table[index]);
+        table[index] = node;
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        int index = hashCode(key);
+        Node<K, V> node = table[index];
+        while (node != null) {
+            if (Objects.equals(key, node.key)) {
+                return node.value;
+            }
+            node = node.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private void resize() {
+        threshold *= RESIZE;
+        Node<K, V>[] oldTable = table;
+        table = new Node[table.length * RESIZE];
+        for (Node<K, V> currentNode : oldTable) {
+            while (currentNode != null) {
+                this.put(currentNode.key, currentNode.value);
+                size--;
+                currentNode = currentNode.next;
+            }
+        }
+    }
+
+    private int hashCode(K key) {
+        return (key == null) ? 0 : Math.abs(key.hashCode()) % table.length;
+    }
+
+    private static class Node<K,V> {
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        private Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
