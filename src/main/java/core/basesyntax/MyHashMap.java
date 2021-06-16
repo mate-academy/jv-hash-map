45fadb99c9133c0d1c5ca7970b512cac3dafbe40
Daniel Kuruch <kuruch1004@gmail.com>
2021-06-16 16:45:21 +0300

    refactored code,remove method isNullNode(...)

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -19,30 +19,27 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if (size == threshold) {
             resize();
         }
-        if (putVal(key, value, table, getIndex(key, table.length))) {
+        boolean wasNewNodeCreated = putValue(key, value, table, getIndex(key, table.length));
+        if (wasNewNodeCreated) {
             size++;
         }
     }
 
-    private boolean putVal(K key, V value, Node<K, V>[] nodes, int hash) {
-        if (isNullNode(nodes[hash])) {
-            nodes[hash] = new Node<>(hash, key, value, null);
+    private boolean putValue(K key, V value, Node<K, V>[] nodes, int hash) {
+        if (nodes[hash] == null) {
+            nodes[hash] = new Node<>(key, value, null);
             return true;
         }
         Node<K, V> currentNode = nodes[hash];
         while (!Objects.equals(currentNode.key, key)) {
-            if (isNullNode(currentNode.next)) {
-                currentNode.next = new Node<>(hash, key, value, null);
+            if (currentNode.next == null) {
+                currentNode.next = new Node<>(key, value, null);
                 return true;
             }
             currentNode = currentNode.next;
         }
         currentNode.value = value;
         return false;
-    }
-
-    private boolean isNullNode(Node<K, V> node) {
-        return node == null;
     }
 
     @Override
@@ -67,26 +64,24 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         threshold = (int) (newNodes.length * LOAD_FACTOR);
         for (Node<K, V> node : table) {
             while (node != null) {
-                int hash = getIndex(node.key, newNodes.length);
-                putVal(node.key, node.value, newNodes, hash);
+                int index = getIndex(node.key, newNodes.length);
+                putValue(node.key, node.value, newNodes, index);
                 node = node.next;
             }
         }
         table = newNodes;
     }
 
-    private int getIndex(K key,int capacity) {
+    private int getIndex(K key, int capacity) {
         return key == null ? 0 : Math.abs(key.hashCode() % capacity);
     }
 
     private static class Node<K, V> {
-        private final int hash;
         private final K key;
         private V value;
         private Node<K, V> next;
 
-        public Node(int hash, K key, V value, Node<K, V> next) {
-            this.hash = hash;
+        public Node(K key, V value, Node<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
