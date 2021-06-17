481c8393d7b6dc6474f77ebacc3b9bbe7ea6bb38
docer <vlasovengineer@gmail.com>
2021-06-17 12:29:30 +0300

    correct checks if in method put

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -17,7 +17,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public void put(K key, V value) {
-        if (threshold < size) {
+        if (threshold == size) {
             resize();
         }
         int indexBucket = indexForBucket(key);
