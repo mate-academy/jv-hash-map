1b98342d9115e9d1b7e745ab29d4ee62a8c095b4
Yura Lug <LUV97@ukr.net>
2021-06-17 10:15:17 +0300

    Fix diamond operator and some checkStyle

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,9 +5,9 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final float LOAD_FACTOR = 0.75f;
-    private static final byte RESIZE = 2;
+    private static final byte COEFF_RESIZE = 2;
     private int size;
-    private Node[] table;
+    private Node<K, V>[] table;
     private int threshold;
 
     public MyHashMap() {
@@ -20,8 +20,8 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if (size >= threshold) {
             resize();
         }
-        int index = hashCode(key);
-        Node node = table[index];
+        int index = calculateIndex(key);
+        Node<K, V> node = table[index];
         while (node != null) {
             if (Objects.equals(key, node.key)) {
                 node.value = value;
@@ -29,14 +29,14 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             }
             node = node.next;
         }
-        node = new Node<K, V>(key, value, table[index]);
+        node = new Node<>(key, value, table[index]);
         table[index] = node;
         size++;
     }
 
     @Override
     public V getValue(K key) {
-        int index = hashCode(key);
+        int index = calculateIndex(key);
         Node<K, V> node = table[index];
         while (node != null) {
             if (Objects.equals(key, node.key)) {
@@ -53,19 +53,19 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void resize() {
-        threshold *= RESIZE;
+        size = 0;
+        threshold *= COEFF_RESIZE;
         Node<K, V>[] oldTable = table;
-        table = new Node[table.length * RESIZE];
+        table = new Node[table.length * COEFF_RESIZE];
         for (Node<K, V> currentNode : oldTable) {
             while (currentNode != null) {
                 put(currentNode.key, currentNode.value);
-                size--;
                 currentNode = currentNode.next;
             }
         }
     }
 
-    private int hashCode(K key) {
+    private int calculateIndex(K key) {
         return key == null ? 0 : Math.abs(key.hashCode()) % table.length;
     }
 
