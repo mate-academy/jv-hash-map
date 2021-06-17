94f8e244b259ad962b8d4fc4fa4b8677ade8d328
sSlonIk <kryachok@gmail.com>
2021-06-17 09:27:28 +0300

    fixed modifiers and created index to local

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -7,12 +7,12 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DOUBLER = 2;
     private static final float LOAD_FACTOR = 0.75f;
     private Node<K, V>[] table;
-    private int unacceptableCapacity;
+    private int threshold;
     private int size;
 
     public MyHashMap() {
         table = new Node[DEFAULT_CAPACITY];
-        unacceptableCapacity = (int) (table.length * LOAD_FACTOR);
+        threshold = (int) (table.length * LOAD_FACTOR);
     }
 
     public static class Node<K, V> {
@@ -29,13 +29,14 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public void put(K key, V value) {
-        if (size == unacceptableCapacity) {
+        if (size == threshold) {
             resize();
         }
-        if (table[getIndex(key)] == null) {
-            table[getIndex(key)] = new Node<>(key, value, null);
+        int index = getIndex(key);
+        if (table[index] == null) {
+            table[index] = new Node<>(key, value, null);
         } else {
-            Node<K, V> current = table[getIndex(key)];
+            Node<K, V> current = table[index];
             while (current != null) {
                 if (Objects.equals(current.key, key)) {
                     current.value = value;
@@ -68,11 +69,11 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    public void resize() {
+    private void resize() {
         Node<K, V>[] oldTable = table;
         table = new Node[oldTable.length * DOUBLER];
         size = 0;
-        unacceptableCapacity = unacceptableCapacity * DOUBLER;
+        threshold = threshold * DOUBLER;
         for (Node<K, V> noda : oldTable) {
             while (noda != null) {
                 put(noda.key, noda.value);
@@ -81,7 +82,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    public int getIndex(K key) {
+    private int getIndex(K key) {
         return key == null ? 0 : Math.abs(key.hashCode() % table.length);
     }
 }
