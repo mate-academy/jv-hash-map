9394d5681d72ca28314106dd3cdbc5df006ffa06
kryzhen <krutozavr@yahoo.com>
2021-08-16 23:08:41 +0300

    hashMap solution after review 1

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -4,9 +4,108 @@ import java.util.Arrays;
 import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    private final float loadingFactor = 0.75f;
-    private int mapCapacity = 16;
-    private int mapSize = 0;
+    private static final float LOADING_FACTOR = 0.75f;
+    private static final int DEFAULT_CAPACITY = 16;
+    private int size = 0;
+    private int capacity = 16;
+
+    private Node<K, V>[] table;
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_CAPACITY];
+    }
+
+    public int getIndex(K key) {
+        return Math.abs(Objects.hash(key) % capacity);
+    }
+
+    public void grow() {
+        Node<K, V> [] tableCopy = Arrays.copyOf(table, capacity);
+        table = new Node[capacity * 2];
+        int oldCapacity = capacity;
+        capacity *= 2;
+        size = 0;
+        for (int i = 0; i < oldCapacity; i++) {
+            Node<K, V> currentNode = tableCopy[i];
+            if (currentNode != null) {
+                put(currentNode.key, currentNode.value);
+                if (currentNode.next != null) {
+                    while (currentNode.next != null) {
+                        put(currentNode.next.key, currentNode.next.value);
+                        currentNode = currentNode.next;
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void put(K key, V value) {
+        if (size == capacity * LOADING_FACTOR) {
+            grow();
+            put(key, value);
+            return;
+        }
+
+        if (key == null) {
+            Node<K, V> currentNode = table[0];
+            if (table[0] == null) {
+                table[0] = new Node<>(key, value);
+            } else {
+                currentNode.value = value;
+                return;
+            }
+        } else {
+            int index = getIndex(key);
+            Node<K, V> currentNode = table[index];
+            if (table[index] == null) {
+                table[index] = new Node<>(key, value);
+            } else if (table[index] != null) {
+                if (Objects.equals(currentNode.key, key)) {
+                    currentNode.value = value;
+                    return;
+                } else {
+                    while (currentNode.next != null) {
+                        if (Objects.equals(currentNode.key, key)) {
+                            currentNode.value = value;
+                            return;
+                        }
+                        currentNode = currentNode.next;
+                    }
+                    currentNode.next = new Node<>(key, value);
+                }
+            }
+        }
+        size++;
+    }
+
+    @Override
+    public V getValue(K key) {
+        if (key == null) {
+            return table[0].value;
+        } else {
+            int index = getIndex(key);
+            Node<K, V> currentNode = table[index];
+            if (currentNode == null) {
+                return null;
+            } else if (Objects.equals(currentNode.key, key)) {
+                return currentNode.value;
+            } else {
+                while (currentNode != null) {
+                    if (Objects.equals(currentNode.key, key)) {
+                        return currentNode.value;
+                    }
+                    currentNode = currentNode.next;
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
 
     private class Node<K, V> {
         private K key;
@@ -17,117 +116,5 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             this.key = key;
             this.value = value;
         }
-
-        public K getKey() {
-            return this.key;
-        }
-
-        public void setKey(K key) {
-            this.key = key;
-        }
-
-        public V getValue() {
-            return this.value;
-        }
-
-        public void setValue(V value) {
-            this.value = value;
-        }
-    }
-
-    private Node<K, V>[] table;
-
-    public MyHashMap() {
-        table = new Node[mapCapacity];
-    }
-
-    public void grow() {
-        Node<K, V> [] tableCopy = Arrays.copyOf(table, mapCapacity);
-        table = new Node[mapCapacity * 2];
-        int oldCapacity = mapCapacity;
-        mapCapacity *= 2;
-        for (int i = 0; i < oldCapacity; i++) {
-            Node<K, V> e = tableCopy[i];
-            if (e != null) {
-                put(e.getKey(), e.getValue());
-                mapSize--;
-                if (e.next != null) {
-                    while (e.next != null) {
-                        put(e.next.getKey(), e.next.getValue());
-                        mapSize--;
-                        e = e.next;
-                    }
-                }
-            }
-        }
-        mapSize--;
-    }
-
-    @Override
-    public void put(K key, V value) {
-        if (key == null) {
-            Node<K, V> e = table[0];
-            if (table[0] == null && mapSize < mapCapacity * loadingFactor) {
-                table[0] = new Node<>(key, value);
-            } else if (table[0] != null) {
-                e.setValue(value);
-                return;
-            } else if (mapSize < mapCapacity * loadingFactor) {
-                grow();
-                put(key, value);
-            }
-        } else {
-            int index = Math.abs(Objects.hash(key) % mapCapacity);
-            Node<K, V> e = table[index];
-            if (table[index] == null && mapSize < mapCapacity * loadingFactor) {
-                table[index] = new Node<>(key, value);
-            } else if (table[index] != null && mapSize < mapCapacity * loadingFactor) {
-                if (Objects.equals(e.getKey(), key)) {
-                    e.setValue(value);
-                    return;
-                } else {
-                    while (e.next != null) {
-                        if (Objects.equals(e.getKey(), key)) {
-                            e.setValue(value);
-                            return;
-                        }
-                        e = e.next;
-                    }
-                    e.next = new Node<>(key, value);
-                }
-            } else if (mapSize == mapCapacity * loadingFactor) {
-                grow();
-                put(key, value);
-            }
-        }
-        mapSize++;
-    }
-
-    @Override
-    public V getValue(K key) {
-        if (key == null) {
-            return table[0].getValue();
-        } else {
-            int index = Math.abs(Objects.hash(key) % mapCapacity);
-            Node<K, V> e = table[index];
-            if (e == null) {
-                return null;
-            } else if (Objects.equals(e.getKey(), key)) {
-                return e.getValue();
-            } else {
-                while (e != null) {
-                    if (Objects.equals(e.getKey(), key)) {
-                        return e.getValue();
-                    }
-                    e = e.next;
-                }
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public int getSize() {
-        return mapSize;
     }
 }
