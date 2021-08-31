d321a41f41b0700c16767a60614cc78a56511811
DmytroPetrushenko <dmytro.petrushenko@gmail.com>
2021-08-31 17:36:53 +0300

    started do put method

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,9 +1,38 @@
 package core.basesyntax;
 
+import java.util.List;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    Node<K, V>[] table = (Node<K, V>[]) new Node[DEFAULT_INITIAL_CAPACITY];
+    int size = 0;
+    private int currentCapacity = DEFAULT_INITIAL_CAPACITY;
+
+    public static class Node<K, V> {
+        K key;
+        V value;
+        int hash;
+        Node<K, V> next;
+
+        public Node(K key, V value, int hash, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.hash = hash;
+            this.next = next;
+        }
+    }
 
     @Override
     public void put(K key, V value) {
+        int hash = key.hashCode();
+        int index = hash / currentCapacity;
+        Node<K, V> newNode = new Node<>(key, value, hash, null);
+        if (table[index] == null) {
+            table[index] = newNode;
+        }
+        if (table[index] != null && table[index].equals(Node.class)) {
+
+        }
+
 
     }
 
--- src/main/java/core/basesyntax/MyMap.java
+++ src/main/java/core/basesyntax/MyMap.java
@@ -1,6 +1,9 @@
 package core.basesyntax;
 
 public interface MyMap<K, V> {
+    static final int DEFAULT_INITIAL_CAPACITY = 16;
+    static final float DEFAULT_LOAD_FACTOR = 0.75f;
+
     void put(K key, V value);
 
     V getValue(K key);
