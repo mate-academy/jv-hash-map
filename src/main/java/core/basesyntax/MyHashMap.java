1adbc65b15a8fd7098f6adcfc596df49b7c4c5d9
Roman Kozhevnikow <roman.kozhevnikow@gmail.com>
2021-09-01 13:30:11 +0300

    initial commit, resize in progress, passed 14 of 15 tests

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,96 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int INITIAL_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+    private static final int RESIZE_MULTIPLY = 2;
+    private int size;
+    private Node<K, V>[] table;
+
+    private int threshhold = (int) (INITIAL_CAPACITY * LOAD_FACTOR);
+
+    @SuppressWarnings({"unchecked"})
+    public MyHashMap() {
+        table = new Node[INITIAL_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
-
+        mapIsFull(getIndex(key));
+        Node<K, V> current = table[getIndex(key)];
+        Node<K, V> node = null;
+        if (current != null) {
+            while (current != null) {
+                if (Objects.equals(current.key, key)) {
+                    current.value = value;
+                    return;
+                }
+                node = current;
+                current = current.next;
+            }
+              node.next = new Node<>(null, key, value);
+        } else {
+            table[getIndex(key)] = new Node<>(null, key, value);
+        }
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        Node<K, V> current = table[getIndex(key)];
+        if (current != null) {
+            do {
+                if (Objects.equals(current.key, key)) {
+                    return current.value;
+                }
+                current = current.next;
+            } while (current != null);
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private boolean mapIsFull(int index) {
+        if (size + 1 > threshhold) {
+            resize();
+            return true;
+        }
+        return false;
+    }
+
+    @SuppressWarnings({"unchecked"})
+    private boolean resize() {
+        Node<K, V>[] newTable = new Node[table.length * RESIZE_MULTIPLY];
+        threshhold = (int) (newTable.length * LOAD_FACTOR);
+        return true;
+    }
+
+    private boolean unlink(Node<K, V> node) {
+        node.next = null;
+        return true;
+    }
+
+    private int getIndex(K key) {
+        int hash = key == null ? 0 :  key.hashCode() % table.length;
+        return hash < 0 ? hash * -1 : hash;
+    }
+
+    private class Node<K, V> {
+       private final K key;
+       private V value;
+       private Node<K, V> next;
+       private int size;
+
+        public Node(Node<K, V> next, K key, V value) {
+            this.next = next;
+            this.key = key;
+            this.value = value;
+        }
     }
 }
