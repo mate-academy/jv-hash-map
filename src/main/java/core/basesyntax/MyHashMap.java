ec6b2e9fd1cf124c7f8c6b61da7d7b659ee6e005
Yuliia Mysova-Shreder <yuliiamysova@gmail.com>
2021-09-01 20:19:39 +0300

    MyHashMap was created

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,101 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+    private float treshold;
+    private int size;
+    private Node<K, V>[] table;
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_CAPACITY];
+        treshold = (int) (LOAD_FACTOR * table.length);
+    }
 
     @Override
     public void put(K key, V value) {
-
+        if (size + 1 > (int) treshold) {
+            resize();
+        }
+        int index = setHash(key);
+        Node<K, V> newNode = new Node<>(key, value, null);
+        size++;
+        Node<K, V> currentNode = table[index];
+        if (table[index] != null) {
+            while (currentNode != null) {
+                if (isKeysIdentical(currentNode.key, key)) {
+                    currentNode.value = value;
+                    size--;
+                    return;
+                }
+                if (currentNode.nextNode == null) {
+                    currentNode.nextNode = newNode;
+                    return;
+                }
+                currentNode = currentNode.nextNode;
+            }
+            return;
+        }
+        table[index] = newNode;
     }
 
     @Override
     public V getValue(K key) {
+        int index = setHash(key);
+        Node<K, V> newNode = table[index];
+
+        if (index < 0 || index > table.length) {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bounds");
+        }
+        while (newNode != null) {
+            if (isKeysIdentical(newNode.key, key)) {
+                return newNode.value;
+            }
+            newNode = newNode.nextNode;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private void resize() {
+        size = 0;
+        treshold *= (int) (LOAD_FACTOR * table.length);
+        Node<K, V>[] oldNode = table;
+        table = new Node[table.length * 2];
+        for (Node<K, V> node : oldNode) {
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.nextNode;
+            }
+        }
+    }
+
+    private boolean isKeysIdentical(K currentNodeKey, K key) {
+        if (Objects.equals(currentNodeKey, key)) {
+            return true;
+        }
+        return false;
+    }
+
+    private int setHash(K key) {
+        return (key == null) ? 0 : Math.abs(key.hashCode()) % table.length;
+    }
+
+    private static class Node<K, V> {
+        private K key;
+        private V value;
+        private Node<K, V> nextNode;
+
+        Node(K key, V value, Node<K, V> nextNode) {
+            this.key = key;
+            this.value = value;
+            this.nextNode = nextNode;
+        }
     }
 }
