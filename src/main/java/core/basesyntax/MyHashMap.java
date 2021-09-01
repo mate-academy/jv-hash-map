853981b49d8193d5cb55e446859d53eddd538728
Dima Petrushenko <dmytro.petrushenko@gmail.com>
2021-09-01 20:56:20 +0300

    Teaher's notices were fixed

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,51 +1,43 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private Node<K, V>[] table = (Node<K, V>[]) new Node[DEFAULT_INITIAL_CAPACITY];
     private int size = 0;
     private int currentCapacity = DEFAULT_INITIAL_CAPACITY;
 
-    public static class Node<K, V> {
-        private K key;
+    private static class Node<K, V> {
+        private final K key;
         private V value;
-        private int hash;
-        private Node<K, V> next;
+        private Node<K, V> next = null;
 
-        public Node(K key, V value, int hash, Node<K, V> next) {
+        public Node(K key, V value) {
             this.key = key;
             this.value = value;
-            this.hash = hash;
-            this.next = next;
         }
     }
 
     @Override
     public void put(K key, V value) {
         resize();
-        int hash = 0;
-        int index = 0;
-        if (key != null) {
-            hash = key.hashCode();
-            hash = hash < 0 ? -hash : hash;
-        }
-        index = hash % currentCapacity;
-        Node<K, V> newNode = new Node<>(key, value, hash, null);
+        int hash = getHash(key);
+        int index = getIndex(hash);
+        Node<K, V> newNode = new Node<>(key, value);
         if (table[index] == null) {
             table[index] = newNode;
+            size++;
         } else {
-            checkingNodes(table[index], newNode);
+            addingValues(table[index], newNode);
         }
     }
 
     @Override
     public V getValue(K key) {
-        int index = 0;
-        if (key != null) {
-            int hash = Math.abs(key.hashCode());
-            index = hash % currentCapacity;
-        }
+        int hash = getHash(key);
+        int index = getIndex(hash);
         Node<K, V> node = table[index];
-        return findingNodes(key, node);
+        return findingValues(key, node);
     }
 
     @Override
@@ -53,24 +45,23 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private void checkingNodes(Node<K, V> tableIndex, Node<K, V> newNode) {
-        if ((tableIndex.key == null && newNode.key == null)
-                || (tableIndex.key != null && tableIndex.key.equals(newNode.key))) {
+    private void addingValues(Node<K, V> tableIndex, Node<K, V> newNode) {
+        if (Objects.equals(tableIndex.key, newNode.key)) {
             tableIndex.value = newNode.value;
-            size--;
         } else if (tableIndex.next == null) {
             tableIndex.next = newNode;
+            size++;
         } else {
-            checkingNodes(tableIndex.next, newNode);
+            addingValues(tableIndex.next, newNode);
         }
     }
 
     private void resize() {
-        if (++size > currentCapacity * DEFAULT_LOAD_FACTOR) {
+        if (size > currentCapacity * DEFAULT_LOAD_FACTOR) {
             currentCapacity = currentCapacity * 2;
             Node<K, V>[] oldTable = table;
             table = (Node<K, V>[]) new Node[currentCapacity];
-            size = 1;
+            size = 0;
             for (Node<K, V> node : oldTable) {
                 if (node != null) {
                     put(node.key, node.value);
@@ -84,13 +75,26 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    private V findingNodes(K key, Node<K, V> node) {
+    private V findingValues(K key, Node<K, V> node) {
         if (node == null) {
             return null;
         }
         if ((key == null && node.key == null) || (key != null && key.equals(node.key))) {
             return node.value;
         }
-        return findingNodes(key, node.next);
+        return findingValues(key, node.next);
+    }
+
+    private int getHash(K key) {
+        int hash = 0;
+        if (key != null) {
+            hash = key.hashCode();
+            hash = hash < 0 ? -hash : hash;
+        }
+        return hash;
+    }
+
+    private int getIndex(int hash) {
+        return hash % currentCapacity;
     }
 }
--- src/main/java/core/basesyntax/MyMap.java
+++ src/main/java/core/basesyntax/MyMap.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 public interface MyMap<K, V> {
-    static final int DEFAULT_INITIAL_CAPACITY = 16;
-    static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    int DEFAULT_INITIAL_CAPACITY = 16;
+    float DEFAULT_LOAD_FACTOR = 0.75f;
 
     void put(K key, V value);
 
