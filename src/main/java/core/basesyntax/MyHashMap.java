f6ab05a7d5ce4a4ec1471c61531c18173306bb32
Vitalii Mykh <vitaliymykh@gmail.com>
2021-09-05 21:17:39 +0300

    just one problem with resize left

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -9,23 +9,21 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int RESIZE_INDEX = 2;
 
     private int threshold;
-    private Node<K,V> [] initialStorage;
-    private float loadFactor;
+    private Node<K, V>[] internalStorage;
     private int size;
 
     /*making constructor*/
     public MyHashMap() {
-        this.loadFactor = DEFAULT_LOAD_FACTOR;
         this.threshold = (int) (DEFAULT_LOAD_FACTOR * DEFAULT_INITIAL_CAPACITY);
-        this.initialStorage = (Node<K, V>[]) new Node [DEFAULT_INITIAL_CAPACITY];
+        internalStorage = new Node[DEFAULT_INITIAL_CAPACITY];
     }
-    
 
-    private class Node<K,V>  {
+
+    private class Node<K, V> {
         private int hash;
         private final K key;
         private V value;
-        Node<K,V> next;
+        Node<K, V> next;
 
         public Node(int hash, K key, V value, Node<K, V> next) {
             this.hash = hash;
@@ -55,91 +53,82 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         @Override
         public int hashCode() {
             int result = 13;
-            return result * ((key != null) ? key.hashCode() : 0);
+            return result * ((key == null) ? 0 : (key.hashCode() > 0) ? key.hashCode() : (key.hashCode() * -1));
         }
     }
 
-   public static final int hash(Object key) {
+    public final int hash(Object key) {
         int hash;
-        return (key == null) ? 0 : (hash = key.hashCode()) ^ (hash >>> 16);
+        return (key == null) ? 0 : Math.abs(key.hashCode());
     }
 
     @Override
     public void put(K key, V value) {
-        Node<K, V> previousNodeInBucket;
-        int bucketNumber = key.hashCode() % initialStorage.length;
-      if (initialStorage[bucketNumber] == null ) {
-          initialStorage[bucketNumber] = newNode(key.hashCode(), key, value, null);
-      } else {
-          previousNodeInBucket = initialStorage[bucketNumber];
-          while(previousNodeInBucket.next != null) {
-              if (Objects.equals(previousNodeInBucket.key, key)) {
-                  previousNodeInBucket.value = value;
-              }
-              previousNodeInBucket = previousNodeInBucket.next;
-          }
-          previousNodeInBucket.next = newNode(key.hashCode(), key, value, null);
-      }
-      if (++size > threshold) {
-          resize();
-      }
+        Node<K, V> oldBucketNode;
+        Node<K, V> inputNode = new Node<>(hash(key), key, value, null);
+        int bucketNumber = hash(key) % internalStorage.length;
+        if (internalStorage[bucketNumber] == null) {
+            internalStorage[bucketNumber] = inputNode;
+            size++;
+            return;
+        } else {
+            oldBucketNode = internalStorage[bucketNumber];
+            while (oldBucketNode != null) {
+                if (Objects.equals(oldBucketNode.key, key)) {
+                    oldBucketNode.value = value;
+                    return;
+                } else if (oldBucketNode.next == null) {
+                    oldBucketNode.next = inputNode;
+                    size++;
+                    return;
+                }
+                oldBucketNode = oldBucketNode.next;
+            }
+            internalStorage[bucketNumber] = inputNode;
+        }
+        resize();
     }
 
-    public Node<K,V>[] transfer(Node<K,V>[] oldTab, int oldCap, Node<K,V>[] newTab) {
+    public Node<K, V>[] transfer(Node<K, V>[] oldTab, int oldCap) {
 
         for (int i = 0; i < oldCap; i++) {
             if (oldTab[i] != null) {
-                if(oldTab[i].next == null) {
-                    newTab[oldTab[i].hashCode() % newTab.length] = oldTab[i];
-                }
-                else {
-                    while (oldTab[i] == null) {
-                        newTab[oldTab[i].hashCode() % newTab.length] = oldTab[i];
+                if (oldTab[i].next == null) {
+                    internalStorage[oldTab[i].hashCode() % internalStorage.length] = oldTab[i];
+                } else {
+                    while (oldTab[i] != null) {
+                        internalStorage[oldTab[i].hashCode() % internalStorage.length] = oldTab[i];
                         oldTab[i] = oldTab[i].next;
                     }
                 }
             }
         }
-        return newTab;
+        return internalStorage;
     }
-    public Node<K,V> [] resize() {
-        Node<K,V>[] oldStorage = initialStorage;
-        int oldCapacity = oldStorage.length;
-        int oldThreshold = threshold;
-        int newCapacity = oldCapacity * RESIZE_INDEX;
-        int newThreshold = (int) (newCapacity * DEFAULT_LOAD_FACTOR);
-        threshold = newThreshold;
-        Node<K, V>[] newStorage = (Node<K, V>[]) new Node[newCapacity];
-        initialStorage = newStorage;
-        if (oldStorage != null) {
-            transfer(oldStorage, oldCapacity, newStorage);
+
+    public void resize() {
+        if (size == threshold) {
+            Node<K, V>[] oldStorage = internalStorage;
+            int oldCapacity = oldStorage.length;
+            int newCapacity = oldCapacity * RESIZE_INDEX;
+            int newThreshold = threshold * RESIZE_INDEX;
+            threshold = newThreshold;
+            internalStorage = (Node<K, V>[]) new Node[newCapacity];
+            if (oldStorage != null) {
+                transfer(oldStorage, oldCapacity);
+            }
         }
-        return newStorage;
     }
 
     @Override
     public V getValue(K key) {
-    Node<K,V> nodeToGetFrom;
-        return (nodeToGetFrom = getNode(key)) == null ? null : nodeToGetFrom.value;
-    }
-
-    public Node<K,V> getNode(Object key) {
-        Node<K,V>[] tab;
-        Node<K,V> first, e;
-        int arrayLength, hash;
-        K k;
-        if ((tab = initialStorage) != null && (arrayLength = tab.length) > 0 &&
-                (first = tab[(arrayLength - 1) & (hash = hash(key))]) != null) {             /*some condition*/
-            if (first.hash == hash && // always check first node
-                    ((k = first.key) == key || (key != null && key.equals(k))))
-                return first;
-            if ((e = first.next) != null) {
-                do {
-                    if (e.hash == hash &&
-                            ((k = e.key) == key || (key != null && key.equals(k))))
-                        return e;
-                } while ((e = e.next) != null);
+        Node<K, V> nodeOnIndex;
+        nodeOnIndex = internalStorage[hash(key) % internalStorage.length];
+        while (nodeOnIndex != null) {
+            if (Objects.equals(nodeOnIndex.key, key)) {
+                return nodeOnIndex.value;
             }
+            nodeOnIndex = nodeOnIndex.next;
         }
         return null;
     }
@@ -148,10 +137,5 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     public int getSize() {
         return size;
     }
-
-    public Node<K,V> newNode(int hash, K key, V value, Node<K,V> next) {
-        return new Node<> (hash, key, value,next);
-    }
-
-
 }
+
