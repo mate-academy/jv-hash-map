51d39504acec8c395f6f9a131dcbe10e4286fe27
Roman Radikhovskiy <radihovskiyroman@gmail.com>
2021-09-05 23:00:20 +0300

    Added constant

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int INCREASE_VALUE_BY_2 = 2;
     private static final int DEFAULT_CAPACITY = 16;
     private static final float LOAD_FACTOR = 0.75f;
     private Node<K, V>[] table;
@@ -66,10 +67,10 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void resize() {
-        threshold *= 2;
+        threshold *= INCREASE_VALUE_BY_2;
         size = 0;
         Node<K, V>[] prev = table;
-        table = new Node[table.length * 2];
+        table = new Node[table.length * INCREASE_VALUE_BY_2];
         for (Node<K, V> node : prev) {
             while (node != null) {
                 put(node.key, node.value);
