3e6b215759d6ca21d88847bd5ac602aa445d9ab7
taras-oleksiuk <taras.oleksiuk@gmail.com>
2021-09-06 16:42:39 +0300

    Added new solution

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,28 +5,30 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int INITIAL_CAPACITY = 16;
     private static final float LOAD_FACTOR = 0.75f;
-    private Node[] table;
+    private Node<K, V>[] table;
     private int size;
     private int threshold;
 
     public MyHashMap() {
-        table = new Node[INITIAL_CAPACITY];
+        table = (Node<K, V>[]) new Node[INITIAL_CAPACITY];
         threshold = (int) (INITIAL_CAPACITY * LOAD_FACTOR);
     }
 
     @Override
     public void put(K key, V value) {
-        Node currentNode = table[getIndex(key)];
+        int bucketIndex = getIndex(key);
+        Node<K, V> currentNode = table[bucketIndex];
         Node<K, V> newNode = new Node<>(key, value, null);
         if (currentNode == null) {
-            table[getIndex(key)] = newNode;
+            table[bucketIndex] = newNode;
             size++;
         }
         while (currentNode != null) {
             if (Objects.equals(key, currentNode.key)) {
                 currentNode.value = value;
                 break;
-            } else if (currentNode.next == null) {
+            }
+            if (currentNode.next == null) {
                 currentNode.next = newNode;
                 size++;
             }
@@ -39,10 +41,10 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public V getValue(K key) {
-        Node currentNode = table[getIndex(key)];
+        Node<K, V> currentNode = table[getIndex(key)];
         while (currentNode != null) {
             if (Objects.equals(key, currentNode.key)) {
-                return (V) currentNode.value;
+                return currentNode.value;
             }
             currentNode = currentNode.next;
         }
@@ -60,28 +62,27 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private void resize() {
         size = 0;
-        threshold = table.length << 1;
-        Node[] oldTable = table;
+        Node<K, V>[] oldTable = table;
         table = new Node[table.length << 1];
         threshold = (int) (table.length * LOAD_FACTOR);
         transfer(oldTable);
     }
 
     private void transfer(Node<K, V>[] table) {
-        for (Node<K, V> kvNode : table) {
-            while (kvNode != null) {
-                put(kvNode.key, kvNode.value);
-                kvNode = kvNode.next;
+        for (Node<K, V> currentBucket : table) {
+            while (currentBucket != null) {
+                put(currentBucket.key, currentBucket.value);
+                currentBucket = currentBucket.next;
             }
         }
     }
 
     private static class Node<K, V> {
-        private final K key;
+        private K key;
         private V value;
         private Node<K, V> next;
 
-        public Node(K key, V value, Node<K,V> next) {
+        public Node(K key, V value, Node<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
