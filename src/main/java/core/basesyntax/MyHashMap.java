ba07e9747682cb548389e232f345c77b700412d6
Lekhter <lehter319@gmail.com>
2021-09-06 12:03:56 +0300

    Deleted redundant if/else. Changed name of transferNodes methood and value of DEFAULT_CAPACITY.

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    private static final int DEFAULT_CAPACITY = 1 << 4;
+    private static final int DEFAULT_CAPACITY = 16;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
     private int threshold;
     private int size;
@@ -35,17 +35,17 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if (table[getIndex(key)] == null) {
             table[getIndex(key)] = newNode;
             size++;
+            return;
+        }
+        Node<K, V> currentNode = table[getIndex(key)];
+        while (currentNode.next != null && !Objects.equals(currentNode.key, key)) {
+            currentNode = currentNode.next;
+        }
+        if (Objects.equals(currentNode.key, key)) {
+            currentNode.value = value;
         } else {
-            Node<K, V> currentNode = table[getIndex(key)];
-            while (currentNode.next != null && !Objects.equals(currentNode.key, key)) {
-                currentNode = currentNode.next;
-            }
-            if (Objects.equals(currentNode.key, key)) {
-                currentNode.value = value;
-            } else {
-                currentNode.next = newNode;
-                size++;
-            }
+            currentNode.next = newNode;
+            size++;
         }
     }
 
@@ -61,17 +61,14 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private int getIndex(Object key) {
-        if (key == null) {
-            return 0;
-        }
-        return Math.abs(key.hashCode() % table.length);
+        return key == null ? 0 : Math.abs(key.hashCode() % table.length);
     }
 
     private void resize() {
         Node<K, V>[] oldArray = table;
         table = new Node[oldArray.length << 1];
         threshold = (int)(table.length * DEFAULT_LOAD_FACTOR);
-        rePlaceNode(oldArray);
+        transferNodes(oldArray);
     }
 
     private Node<K, V> findCurrentNode(K key) {
@@ -85,7 +82,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return Objects.equals(currentNode.key, key) ? currentNode : null;
     }
 
-    private void rePlaceNode(Node<K, V>[] oldArray) {
+    private void transferNodes(Node<K, V>[] oldArray) {
         size = 0;
         for (Node<K, V> element : oldArray) {
             Node<K, V> currentNode = element;
