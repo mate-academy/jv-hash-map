d2a761331467257842fc4e67617d26e359497042
Andruwa808 <legacy112233@gmail.com>
2021-09-06 15:48:38 +0300

    fix all edits

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -3,13 +3,14 @@ package core.basesyntax;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final float LOAD_FACTOR = 0.75f;
+    private static final int DEFAULT_RESIZE = 2;
     private Node<K, V>[] hashTable;
     private int size;
     private float threshold;
 
     public MyHashMap() {
         hashTable = new Node[DEFAULT_CAPACITY];
-        threshold = hashTable.length * LOAD_FACTOR;
+        threshold = DEFAULT_CAPACITY * LOAD_FACTOR;
     }
 
     private static class Node<K, V> {
@@ -29,33 +30,33 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if (size == threshold) {
             resize();
         }
-
-        Node<K, V> addNode = new Node<>(key, value, null);
-        int index = hash(key);
+        int index = calculateIndex(key);
         Node<K, V> indexNode = hashTable[index];
-
+        Node<K, V> addNode = new Node<>(key, value, null);
         if (indexNode == null) {
             hashTable[index] = addNode;
             size++;
             return;
         }
-
         while (indexNode != null) {
             if (key == indexNode.key || (key != null && key.equals(indexNode.key))) {
                 indexNode.value = value;
                 return;
-            } else if (indexNode.next == null) {
+            }
+            if (indexNode.next == null) {
                 indexNode.next = addNode;
                 size++;
                 return;
             }
             indexNode = indexNode.next;
         }
+        hashTable[index] = addNode;
+        size++;
     }
 
     @Override
     public V getValue(K key) {
-        Node<K, V> getNode = hashTable[hash(key)];
+        Node<K, V> getNode = hashTable[calculateIndex(key)];
         while (getNode != null) {
             if (key == getNode.key || (key != null && key.equals(getNode.key))) {
                 return getNode.value;
@@ -70,7 +71,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private int hash(K key) {
+    private int calculateIndex(K key) {
         if (key == null) {
             return 0;
         }
@@ -78,10 +79,10 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void resize() {
-        threshold *= 2;
+        threshold *= DEFAULT_RESIZE;
         size = 0;
         Node<K, V>[] oldHashTable = hashTable;
-        hashTable = new Node[oldHashTable.length * 2];
+        hashTable = new Node[oldHashTable.length * DEFAULT_RESIZE];
         for (Node<K, V> node : oldHashTable) {
             while (node != null) {
                 put(node.key, node.value);
