e91059940e67237b9641e6465d2d8bcd5abab225
Serhii Blakh <s.a.blakh@ukr.net>
2021-09-06 12:01:07 +0300

    deleted hash  from class Entry and changed method to find index

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -8,19 +8,17 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int MULTIPLIER = 2;
     private Entry<K, V>[] table = new Entry[INITIAL_CAPACITY];
     private int size;
-    private  int threshold;
+    private int threshold;
 
     public MyHashMap() {
     }
 
     private static class Entry<K, V> {
-        private final int hash;
         private final K key;
         private V value;
         private Entry<K, V> next;
 
-        Entry(int hash, K key, V value, Entry<K, V> next) {
-            this.hash = hash;
+        Entry(K key, V value, Entry<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
@@ -30,30 +28,27 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public void put(K key, V value) {
         resize();
-        int hash = ourHashCode(key);
-        int index = indexOfPosition(hash, table.length);
-        Entry<K, V> bucket = table[index];
-        if (bucket != null) {
-            while (bucket != null) {
-                if (bucket.hash == hash && Objects.equals(bucket.key, key)) {
-                    bucket.value = value;
-                    return;
-                }
-                bucket = bucket.next;
+        int index = indexPosition(key);
+        Entry<K, V> entry = table[index];
+        while (entry != null) {
+            if (Objects.equals(entry.key, key)) {
+                entry.value = value;
+                return;
             }
+            entry = entry.next;
         }
-        addEntry(hash, key, value, index);
+        addEntry(key, value, index);
     }
 
     @Override
     public V getValue(K key) {
-        int index = indexOfPosition(ourHashCode(key),table.length);
-        Entry<K, V> bucket = table[index];
-        while (bucket != null) {
-            if (Objects.equals(key, bucket.key)) {
-                return bucket.value;
+        int index = indexPosition(key);
+        Entry<K, V> entry = table[index];
+        while (entry != null) {
+            if (Objects.equals(key, entry.key)) {
+                return entry.value;
             }
-            bucket = bucket.next;
+            entry = entry.next;
         }
         return null;
     }
@@ -63,9 +58,9 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private void addEntry(int hash, K key, V value, int index) {
-        Entry<K, V> bucket = table[index];
-        table[index] = new Entry<>(hash, key, value, bucket);
+    private void addEntry(K key, V value, int index) {
+        Entry<K, V> entry = table[index];
+        table[index] = new Entry<>(key, value, entry);
         size++;
     }
 
@@ -75,20 +70,17 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             Entry<K, V>[] newTable = table;
             table = new Entry[table.length * MULTIPLIER];
             size = 0;
-            for (Entry<K, V> bucket : newTable) {
-                while (bucket != null) {
-                    put(bucket.key, bucket.value);
-                    bucket = bucket.next;
+            for (Entry<K, V> entry : newTable) {
+                while (entry != null) {
+                    put(entry.key, entry.value);
+                    entry = entry.next;
                 }
             }
         }
     }
 
-    private int ourHashCode(K key) {
-        return key == null ? 0 : key.hashCode();
-    }
-
-    private int indexOfPosition(int hash, int length) {
-        return Math.abs(hash) % length;
+    private int indexPosition(K key) {
+        int hash = key == null ? 0 : key.hashCode();
+        return Math.abs(hash) % table.length;
     }
 }
