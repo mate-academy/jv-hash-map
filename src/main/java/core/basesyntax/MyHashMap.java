e4ec49c6f9faf68e45d603416da6df78fc29ccdc
Denys Smei <denys.smei@artcodevision.com>
2021-09-06 11:06:35 +0300

    Remarks after mentor review

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -8,41 +8,38 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     static final int ARRAY_SIZE_MULTIPLIER = 2;
     private int size;
     private int threshold;
-    private int capacity;
     private Node<K, V>[] table;
 
     public MyHashMap() {
-        capacity = DEFAULT_INITIAL_CAPACITY;
-        this.table = new Node[DEFAULT_INITIAL_CAPACITY];
-        this.threshold = (int) (DEFAULT_LOAD_FACTOR * capacity);
+        table = new Node[DEFAULT_INITIAL_CAPACITY];
+        threshold = (int) (DEFAULT_LOAD_FACTOR * table.length);
     }
 
     @Override
     public void put(K key, V value) {
-        tableRebuild();
-        Node<K, V> newNode = new Node<>(key, value, null, getHashCodeOfKey(key));
-        if (table[newNode.bucketIndex] == null) {
-            table[newNode.bucketIndex] = newNode;
-        } else {
-            Node<K, V> workingNode = table[newNode.bucketIndex];
-            do {
-                if (Objects.equals(key, workingNode.key)) {
-                    workingNode.value = newNode.value;
-                    return;
-                }
-                if (workingNode.next == null) {
-                    workingNode.next = newNode;
-                    break;
-                }
-                workingNode = workingNode.next;
-            } while (workingNode != null);
+        resize();
+        Node<K, V> newNode = new Node<>(key, value, null, getHashCode(key));
+        int indexOfBucket = newNode.hashCode % table.length;
+        Node<K, V> workingNode = table[indexOfBucket];
+        while (workingNode != null){
+            if (Objects.equals(key, workingNode.key)) {
+                workingNode.value = newNode.value;
+                return;
+            }
+            if (workingNode.next == null) {
+                workingNode.next = newNode;
+                size++;
+                return;
+            }
+            workingNode = workingNode.next;
         }
+        table[indexOfBucket] = newNode;
         size++;
     }
 
     @Override
     public V getValue(K key) {
-        int indexOfKey = getHashCodeOfKey(key);
+        int indexOfKey = getHashCode(key) % table.length;
         Node<K, V> currentNode = table[indexOfKey];
         while (currentNode != null) {
             if (Objects.equals(key, currentNode.key)) {
@@ -58,11 +55,10 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private void tableRebuild() {
+    private void resize() {
         if (size < threshold) {
             return;
         }
-        capacity *= ARRAY_SIZE_MULTIPLIER;
         Node<K, V>[] oldTable = table;
         table = new Node[oldTable.length * ARRAY_SIZE_MULTIPLIER];
         size = 0;
@@ -72,24 +68,24 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                 node = node.next;
             }
         }
-        threshold = (int) (capacity * DEFAULT_LOAD_FACTOR);
+        threshold = (int) (table.length * DEFAULT_LOAD_FACTOR);
     }
 
-    private int getHashCodeOfKey(K key) {
-        return key == null ? 0 : Math.abs(key.hashCode() % capacity);
+    private int getHashCode(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode());
     }
 
     private class Node<K, V> {
-        private int bucketIndex;
+        private int hashCode;
         private final K key;
         private V value;
         private Node<K, V> next;
 
-        Node(K key, V value, Node<K, V> next,int bucketIndex) {
+        Node(K key, V value, Node<K, V> next, int bucketIndex) {
             this.key = key;
             this.value = value;
             this.next = next;
-            this.bucketIndex = bucketIndex;
+            this.hashCode = bucketIndex;
         }
 
         public final String toString() {
