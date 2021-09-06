c462991c049d744bcd76a5f1ffcb1f33229b9af5
Yuliia Mysova-Shreder <yuliiamysova@gmail.com>
2021-09-06 11:54:18 +0300

    Updated new method

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -83,6 +83,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             }
         }
     }
+
     private int getIndex(K key) {
         return (key == null) ? 0 : Math.abs(key.hashCode()) % table.length;
     }
