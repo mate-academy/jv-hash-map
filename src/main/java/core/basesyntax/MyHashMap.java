a75eae7b0ddded0fece0add00ada6b0678c736d6
Mykola Skrypka <mykolaskrypka@gmail.com>
2021-10-11 16:49:05 +0300

    implemented methods: put(K key, V value); getValue();getSize(); was override methods hashCode() in used Key classes

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,30 +1,28 @@
 package core.basesyntax;
 
-import java.util.Objects;
-
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    Node<K, V>[] table;
-    public int DEFAULT_CAPACITY = 16;
-    public int CAPACITY;
-    public int size = 0;
-    public double LOAD_FACTOR = 0.75;
-    public int threshold = 0;
+    private Node<K, V>[] table;
+    private int defCapacity = 16;
+    private int capacity;
+    private int size = 0;
+    private double loadFactor = 0.75;
+    private int threshold = 0;
 
-    public void resize() {
-        if (table == null || CAPACITY == 0) {
-            CAPACITY = DEFAULT_CAPACITY;
-            table = new Node[DEFAULT_CAPACITY];
-            threshold = (int) (CAPACITY * LOAD_FACTOR);
+    private void resize() {
+        if (table == null || capacity == 0) {
+            capacity = defCapacity;
+            table = new Node[defCapacity];
+            threshold = (int) (capacity * loadFactor);
         } else {
-            Node<K, V>[] oldTable = table;
-            CAPACITY = CAPACITY * 2;
-            table = new Node[CAPACITY];
-            threshold = (int) (CAPACITY * LOAD_FACTOR);
+            capacity = capacity * 2;
+            threshold = (int) (capacity * loadFactor);
             size = 0;
+            Node<K, V>[] oldTable = table;
+            table = new Node[capacity];
             for (Node<K, V> node : oldTable) {
                 while (node != null) {
-                    put(node.key, node.value);
-                    node = node.next;
+                    put(node.getKey(), node.getValue());
+                    node = node.getNext();
                 }
             }
         }
@@ -32,52 +30,52 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public void put(K key, V value) {
-        if (CAPACITY == 0 || size + 1 > threshold) {
+        if (capacity == 0 || size + 1 > threshold) {
             resize();
         }
         Node<K, V> node = new Node<>();
-        node.hash = key == null ? 0 : key.hashCode();
-        node.key = key;
-        node.value = value;
+        node.setHash(key == null ? 0 : key.hashCode());
+        node.setKey(key);
+        node.setValue(value);
 
-        Node<K, V> curNode = table[getBucketIndex(node.hash)];
+        Node<K, V> curNode = table[getBucketIndex(node.getHash())];
         if (curNode == null) {
-            table[Math.abs(node.hash) % CAPACITY] = node;
+            table[Math.abs(node.getHash()) % capacity] = node;
             size++;
         } else {
-            while (curNode.next != null && !keysAreEquals(curNode.key, key)) {
-                curNode = curNode.next;
+            while (curNode.getNext() != null && !keysAreEquals(curNode.getKey(), key)) {
+                curNode = curNode.getNext();
             }
-            if (keysAreEquals(curNode.key, key)) {
-                curNode.value = value;
+            if (keysAreEquals(curNode.getKey(), key)) {
+                curNode.setValue(value);
             } else {
-                curNode.next = node;
+                curNode.setNext(node);
                 size++;
             }
         }
     }
 
     private int getBucketIndex(int hash) {
-        return Math.abs(hash) % CAPACITY;
+        return Math.abs(hash) % capacity;
     }
 
     private boolean keysAreEquals(K key1, K key2) {
-        return key1 == key2 || key1 != null && key1.equals(key2);
+        return key1 == key2 || (key1 != null && key1.equals(key2));
     }
 
     @Override
     public V getValue(K key) {
-        if (CAPACITY == 0 || size == 0) {
+        if (capacity == 0 || size == 0) {
             return null;
         }
         Node<K, V> curNode = table[key == null ? 0 : getBucketIndex(key.hashCode())];
         if (curNode == null) {
             return null;
         }
-        while (curNode.next != null && !keysAreEquals(curNode.key, key)) {
-            curNode = curNode.next;
+        while (curNode.getNext() != null && !keysAreEquals(curNode.getKey(), key)) {
+            curNode = curNode.getNext();
         }
-        return curNode.value;
+        return curNode.getValue();
     }
 
     @Override
--- src/main/java/core/basesyntax/Node.java
+++ src/main/java/core/basesyntax/Node.java
@@ -1,8 +1,40 @@
 package core.basesyntax;
 
 public class Node<K, V> {
-    public int hash;
-    public K key;
-    public V value;
-    Node<K, V> next;
+    private int hash;
+    private K key;
+    private V value;
+    private Node<K, V> next;
+
+    public int getHash() {
+        return hash;
+    }
+
+    public void setHash(int hash) {
+        this.hash = hash;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public Node<K, V> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<K, V> next) {
+        this.next = next;
+    }
 }
