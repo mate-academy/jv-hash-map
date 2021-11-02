9ddd2238403fb8ffec1bc19817f2b686baf73189
SonyaPakhomova <88778908+SonyaPakhomova@users.noreply.github.com>
2021-11-02 21:50:26 +0200

    implemented all methods of HM

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,109 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private static final int GROW_COEFFICIENT = 2;
+    private Node<K, V>[] table;
+    private int size;
+    private int index;
+    private int threshold;
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_INITIAL_CAPACITY];
+        threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
+    }
 
     @Override
     public void put(K key, V value) {
-
+        if (size >= threshold) {
+            resize();
+        }
+        index = getNumberOfBucket(key);
+        Node<K, V> node = table[index];
+        while (node != null) {
+            if (Objects.equals(key, node.key)) {
+                node.value = value;
+                return;
+            }
+            if (node.next == null) {
+                node.next = new Node<>(key, value, null);
+                size++;
+                return;
+            }
+            node = node.next;
+        }
+        table[index] = new Node<>(key, value, null);
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        index = getNumberOfBucket(key);
+        Node<K, V> node = table[index];
+        while (node != null) {
+            if (Objects.equals(key, node.key)) {
+                return node.value;
+            }
+            node = node.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof MyHashMap)) {
+            return false;
+        }
+        MyHashMap<?, ?> myHashMap = (MyHashMap<?, ?>) o;
+        return size == myHashMap.size && threshold == myHashMap.threshold
+                && Arrays.equals(table, myHashMap.table);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(size, threshold);
+        result = 31 * result + Arrays.hashCode(table);
+        return result;
+    }
+
+    private void resize() {
+        size = 0;
+        threshold *= GROW_COEFFICIENT;
+        Node<K, V>[] oldTable = table;
+        table = new Node[table.length * GROW_COEFFICIENT];
+        for (Node<K, V> arr : oldTable) {
+            while (arr != null) {
+                put(arr.key, arr.value);
+                arr = arr.next;
+            }
+        }
+    }
+
+    private int getNumberOfBucket(K key) {
+        return key == null ? 0 : Math.abs(table.length % DEFAULT_INITIAL_CAPACITY);
+    }
+
+    private static class Node<K,V> {
+        private final K key;
+        private Node<K, V> next;
+        private V value;
+
+        private Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
