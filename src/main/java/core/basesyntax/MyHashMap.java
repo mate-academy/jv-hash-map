a2212d3c8dd8292d25c7b9f2584e15cbbbcd01f6
Roman Polulikh <polilikh.r@gmail.com>
2021-11-03 09:52:27 +0200

    implemented basic behaviour with stub methods

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,6 +1,11 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+    private Node<K, V>[] buckets = new Node[DEFAULT_CAPACITY];
+    private int size;
+    private int threshold;
 
     @Override
     public void put(K key, V value) {
@@ -14,6 +19,26 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public int getSize() {
+        return size;
+    }
+
+    private int hash() {
         return 0;
+    }
+
+    private void grow(){}
+
+    private static class Node<K, V> {
+        private int hash;
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        private Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
