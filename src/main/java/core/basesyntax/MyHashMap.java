ae96b868c8749b5a79eb55f20c4b727bea759a08
Oleg Shiriaev <mystidreamer@gmail.com>
2021-11-03 17:07:41 +0200

    empty line was deleted

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,7 +5,6 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final float LOAD_FACTOR = 0.75f;
-
     private Node<K, V>[] nodes;
     private int size;
 
