7d4878a8f83db1ea6bbe40b135d93500986856a0
nishchenkokateryna <kateryna.nishchenko@gmail.com>
2021-11-03 12:48:51 +0200

    Removed redcurrant code and added a constant after 1st student review

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,15 +5,14 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final int RESIZE_FACTOR = 2;
+    private static final float LOAD_FACTOR = 0.75F;
     private Node<K, V>[] table;
     private int size;
     private int threshold;
-    private float loadFactor;
 
     public MyHashMap() {
         table = new Node[DEFAULT_CAPACITY];
-        loadFactor = 0.75F;
-        threshold = (int) (table.length * loadFactor);
+        threshold = (int) (table.length * LOAD_FACTOR);
     }
 
     @Override
@@ -24,8 +23,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if (table[index] == null) {
             table[index] = newNode;
             size++;
-        } else if (Objects.equals(table[index].key, key)) {
-            table[index].value = value;
         } else {
             Node<K, V> tempNode = table[index];
             while (tempNode != null) {
@@ -46,14 +43,12 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public V getValue(K key) {
         int index = getIndexFor(key);
-        if (table[index] != null) {
-            Node<K, V> tempNode = table[index];
-            while (tempNode != null) {
-                if (Objects.equals(tempNode.key, key)) {
-                    return tempNode.value;
-                }
-                tempNode = tempNode.next;
+        Node<K, V> tempNode = table[index];
+        while (tempNode != null) {
+            if (Objects.equals(tempNode.key, key)) {
+                return tempNode.value;
             }
+            tempNode = tempNode.next;
         }
         return null;
     }
@@ -71,7 +66,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if (threshold == size) {
             size = 0;
             int newSize = table.length * RESIZE_FACTOR;
-            threshold = (int) (newSize * loadFactor);
+            threshold = (int) (newSize * LOAD_FACTOR);
             transferToNewTable(newSize);
         }
     }
