ed56514becb0360f5bf5b0d382c42633acc500ca
Sergii Svystun <sergiisvystun@gmail.com>
2021-11-05 10:14:03 +0200

    can"t run tests

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,98 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int INITIAL_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private static final int MULTIPLICATION_FACTOR = 2;
+
+    private Node<K, V> next;
+    private Node<K, V>[] table;
+    private int size;
+
+    public MyHashMap() {
+        table = (Node<K, V>[]) new Node[INITIAL_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
-
+        checkSize();
+        addNode(key, value);
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        int hash = key.hashCode();
+        int index = indexOfMapArray(hash);
+        Node<K, V> nodeSearch = table[index];
+        while (nodeSearch != null) {
+            if (Objects.equals(nodeSearch.key, key)) {
+                return nodeSearch.value;
+            }
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private void checkSize() {
+        if (size >= (int) (table.length * DEFAULT_LOAD_FACTOR)) {
+            Node<K, V>[] copiedTable = table;
+            table = (Node<K, V>[]) new Node[copiedTable.length * MULTIPLICATION_FACTOR];
+            for (int i = 0; i < copiedTable.length; i++) {
+                while (copiedTable[i] != null) {
+                    addNode(copiedTable[i].key, copiedTable[i].value);
+                    copiedTable[i] = copiedTable[i].next;
+                }
+            }
+        }
+    }
+
+    private void addNode(K key, V value) {
+        int hash = key.hashCode();
+        int index = indexOfMapArray(hash);
+        Node<K, V> node = new Node<>(hash, key, value, null);
+        if (table[index] == null) {
+            table[index] = node;
+            node.next = null;
+        } else {
+            Node<K, V> nodeSearch = table[index];
+            while (nodeSearch.next != null) {
+                if (Objects.equals(nodeSearch.key, key)) {
+                    nodeSearch.value = value;
+                    return;
+                }
+                nodeSearch = nodeSearch.next;
+            }
+            nodeSearch.next = node;
+        }
+    }
+
+    private int indexOfMapArray(int hash) {
+        int index = hash % table.length;
+        return index;
+    }
+
+    private static class Node<K, V> {
+        private Node<K, V> next;
+        private int hash;
+        private K key;
+        private V value;
+
+        public Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        public int hashCode() {
+            return 31 * 17 + (key == null ? 0 : key.hashCode());
+        }
     }
 }
