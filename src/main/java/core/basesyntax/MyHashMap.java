d16e613dc3bd55ea5a706f075ab6437b990e1067
Vladyslav Chyver <lione2009lione@gmail.com>
2021-11-07 20:20:18 +0200

    code was optimised

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -3,16 +3,18 @@ package core.basesyntax;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
     private static final int DEFAULT_CAPACITY = 16;
+    private static final int DEFAULT_CAPACITY_GROWTH_MULTIPLIER = 2;
 
     private int capacity = DEFAULT_CAPACITY;
     private float loadFactor = DEFAULT_LOAD_FACTOR;
     private int threshold;
+    private int capacityGrowthMultiplier = DEFAULT_CAPACITY_GROWTH_MULTIPLIER;
     private int size;
     private Node<K, V> [] table;
 
     {
         table = new Node[capacity];
-        threshold = computeThreshold();
+        threshold = (int) (DEFAULT_CAPACITY * DEFAULT_LOAD_FACTOR);
     }
 
     @Override
@@ -27,22 +29,21 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             while (true) {
                 if (node.key == key || node.key != null && node.key.equals(key)) {
                     node.value = value;
-                    break;
-                } else if (node.next != null) {
-                    node = node.next;
-                } else {
+                    return;
+                }
+                if (node.next == null) {
                     node.next = new Node<>(key, value, null);
                     size++;
-                    break;
+                    return;
                 }
+                node = node.next;
             }
         }
     }
 
     @Override
     public V getValue(K key) {
-        int index = computeIndex(key);
-        Node<K, V> node = table[index];
+        Node<K, V> node = table[computeIndex(key)];
         while (node != null) {
             if (node.key == key || (node.key != null && node.key.equals(key))) {
                 return node.value;
@@ -58,17 +59,13 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private int computeThreshold() {
-        return (int) (capacity * loadFactor);
-    }
-
     private void checkCapacity() {
-        if (size >= threshold) {
-            setDoubleCapacity();
+        if (size == threshold) {
+            setCapacity();
         }
     }
 
-    private void setDoubleCapacity() {
+    private void setCapacity() {
         Node<K, V>[] nodes = new Node[size];
         int index = 0;
         for (Node<K, V> node : table) {
@@ -77,10 +74,10 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                 node = node.next;
             }
         }
-        capacity = capacity * 2;
+        capacity = capacity * capacityGrowthMultiplier;
         table = new Node[capacity];
         size = 0;
-        threshold = computeThreshold();
+        threshold = (int) (capacity * loadFactor);;
         for (Node<K, V> node : nodes) {
             put(node.key, node.value);
         }
