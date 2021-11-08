82094fa79c756a979a9b816ea11cb671a362cbbf
Pavlo_Martsiniv <imitator2010@gmail.com>
2021-11-08 16:11:09 +0200

    My solution v1.2 added minor fix

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -3,18 +3,23 @@ package core.basesyntax;
 import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    private static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
     private static final int MAGNIFICATION_FACTOR = 2;
     private int size;
-    private int threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
-    private Node<K, V>[] table = new Node[DEFAULT_INITIAL_CAPACITY];
+    private int threshold;
+    private Node<K, V>[] table;
+
+    public MyHashMap() {
+        threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
+        table = new Node[DEFAULT_INITIAL_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
         int index = hash(key);
         if (size == threshold) {
-            toGrow();
+            resize();
         }
         Node<K, V> node = table[index];
         while (node != null) {
@@ -59,7 +64,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return key == null ? 0 : Math.abs(key.hashCode() % table.length);
     }
 
-    private void toGrow() {
+    private void resize() {
         threshold *= MAGNIFICATION_FACTOR;
         size = 0;
         Node<K, V>[] oldTable = table;
