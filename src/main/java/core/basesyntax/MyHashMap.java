87cb9c335ece59e70f15374b4153c1ee6a9e5f5f
Andriy-dz <andriydzdz@gmail.com>
2021-11-08 18:47:40 +0200

    implemented class 'MyHashMap'

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,96 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private MyHashMap.Node<K, V>[] table;
+    private int loadFactor;
+    private int capacity;
+    private int size;
 
     @Override
     public void put(K key, V value) {
-
+        if (table == null || table.length == 0 || size + 1 > loadFactor) {
+            resize();
+        }
+        int indexArrayNode = (key == null) ? 0
+                : (key.hashCode() < 0) ? key.hashCode() % capacity * -1
+                : key.hashCode() % capacity;
+        Node<K, V> newNode = table[indexArrayNode];
+        if (newNode == null) {
+            table[indexArrayNode] = new Node<>(key, value, null);
+        } else {
+            while (true) {
+                if (Objects.equals(newNode.key, key)) {
+                    newNode.value = value;
+                    return;
+                }
+                if (newNode.next == null) {
+                    newNode.next = new Node<>(key, value, null);
+                    break;
+                }
+                newNode = newNode.next;
+            }
+        }
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        if (size == 0) {
+            return null;
+        }
+        int indexArrayNode = (key == null) ? 0
+                : (key.hashCode() < 0) ? key.hashCode() % capacity * -1
+                : key.hashCode() % capacity;
+        Node<K, V> searchNode = table[indexArrayNode];
+        while (searchNode != null) {
+            if (Objects.equals(searchNode.key, key)) {
+                return searchNode.value;
+            }
+            searchNode = searchNode.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    static class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        Node(K key, V value, MyHashMap.Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+    }
+
+    public void resize() {
+        if (table == null || table.length == 0) {
+            loadFactor = (int) (DEFAULT_LOAD_FACTOR * DEFAULT_INITIAL_CAPACITY);
+            capacity = DEFAULT_INITIAL_CAPACITY;
+            table = new MyHashMap.Node[capacity];
+        } else {
+            capacity = capacity * 2;
+            loadFactor = (int) (capacity * DEFAULT_LOAD_FACTOR);
+            Node<K, V>[] oldTable = table;
+            table = new MyHashMap.Node[capacity];
+            size = 0;
+            for (int i = 0; i < oldTable.length; i++) {
+                Node<K, V> newNode = oldTable[i];
+                while (newNode != null) {
+                    put(newNode.key, newNode.value);
+                    newNode = newNode.next;
+                }
+            }
+        }
     }
 }
