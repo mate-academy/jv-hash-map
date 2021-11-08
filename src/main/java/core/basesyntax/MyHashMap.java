1351c36bbc7ce8381a701803fc8b59174f6196ec
KostyNikiforov <www.kosta.kon@gmail.com>
2021-11-08 16:12:23 +0200

    '=' was replaced with '>' in resize method.

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -64,7 +64,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void resize() {
-        if (++size == threshold()) {
+        if (++size > threshold()) {
             grow();
         }
     }
