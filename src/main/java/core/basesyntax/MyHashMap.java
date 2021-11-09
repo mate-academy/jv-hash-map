65fc0d7e70f01c439dbcf281666b6ea182e792b0
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2021-11-09 15:13:34 +0200

    better solution was implemented

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -9,37 +9,34 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private int size;
     private Node<K, V>[] table;
 
-    private static class Node<K, V> {
-        private K key;
-        private V value;
-        private Node<K, V> next;
-
-        public Node(K key, V value, Node<K, V> next) {
-            this.key = key;
-            this.value = value;
-            this.next = next;
-        }
+    public MyHashMap() {
+        table = new Node[DEFAULT_INITIAL_CAPACITY];
+        threshold = (int) (table.length * DEFAULT_LOAD_FACTOR);
     }
 
     @Override
     public void put(K key, V value) {
-        if (table == null) {
+        if (size == threshold) {
             resize();
         }
-        if (size + 1 == threshold) {
-            resize();
+        int index = getIndex(key);
+        for (Node<K, V> node = table[index]; node != null; node = node.next) {
+            if (Objects.equals(key, node.key)) {
+                node.value = value;
+                return;
+            }
+            if (node.next == null) {
+                node.next = new Node<>(key, value, null);
+                size++;
+                return;
+            }
         }
-        putVal(key, value);
+        table[index] = new Node<>(key, value, null);
+        size++;
     }
 
     @Override
     public V getValue(K key) {
-        if (table == null) {
-            return null;
-        }
-        if (key == null) {
-            return getByNullKey(key);
-        }
         int index = getIndex(key);
         for (Node<K, V> node = table[index]; node != null; node = node.next) {
             if (Objects.equals(node.key, key)) {
@@ -55,76 +52,31 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void resize() {
-        if (table == null) {
-            table = new Node[DEFAULT_INITIAL_CAPACITY];
-            threshold = (int) (table.length * DEFAULT_LOAD_FACTOR);
-            return;
-        }
         threshold = (int) (table.length * DEFAULT_LOAD_FACTOR);
         Node<K, V>[] oldTable = table;
         table = new Node[table.length * 2];
         size = 0;
         for (Node<K, V> node : oldTable) {
             while (node != null) {
-                putVal(node.key, node.value);
+                put(node.key, node.value);
                 node = node.next;
             }
         }
     }
 
-    private void putVal(K key, V value) {
-        if (key == null) {
-            putForNullKey(value);
-            return;
-        }
-        int index = getIndex(key);
-        if (table[index] == null) {
-            table[index] = new Node<>(key, value, null);
-            size++;
-            return;
-        }
-        for (Node<K, V> node = table[index]; node != null; node = node.next) {
-            if (key.equals(node.key)) {
-                node.value = value;
-                return;
-            }
-            if (node.next == null) {
-                node.next = new Node<>(key, value, null);
-                size++;
-                return;
-            }
-        }
-    }
-
-    private void putForNullKey(V value) {
-        if (table[0] == null) {
-            table[0] = new Node<>(null, value, null);
-            size++;
-            return;
-        }
-        for (Node<K, V> node = table[0]; node != null; node = node.next) {
-            if (Objects.equals(node.key, null)) {
-                node.value = value;
-                return;
-            }
-            if (node.next == null) {
-                node.next = new Node<>(null, value, null);
-                size++;
-                return;
-            }
-        }
-    }
-
-    private V getByNullKey(K key) {
-        for (Node<K, V> node = table[0]; node != null; node = node.next) {
-            if (node.key == key) {
-                return node.value;
-            }
-        }
-        return null;
-    }
-
     private int getIndex(K key) {
         return key == null ? 0 : key.hashCode() & table.length - 1;
+    }
+
+    private static class Node<K, V> {
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
