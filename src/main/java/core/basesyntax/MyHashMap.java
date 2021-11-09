483b016a85b1d14ba5ffbe5c81dae62db05b8672
Zachar Chorny <ufumoveldar@gmail.com>
2021-11-09 08:47:25 +0200

    edits made(3)

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,20 +5,20 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
-    private static final int GROW_CAPACITY = 2;
+    private static final int GROW_COEFFICIENT = 2;
     private Node<K, V>[] table;
     private int size;
     private int threshold;
 
     public MyHashMap() {
-        table = (Node<K, V>[]) new Node[DEFAULT_INITIAL_CAPACITY];
+        table = new Node[DEFAULT_INITIAL_CAPACITY];
         threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
     }
 
     @Override
     public void put(K key, V value) {
         if (size == threshold) {
-            reSize();
+            resize();
         }
         int cell = hash(key);
         Node<K, V> newNode = new Node<K, V>(value, key, null);
@@ -60,9 +60,9 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private void reSize() {
+    private void resize() {
         Node<K, V>[] oldArray = table;
-        table = (Node<K, V>[]) new Node[oldArray.length * GROW_CAPACITY];
+        table = new Node[oldArray.length * GROW_COEFFICIENT];
         threshold = (int) (table.length * DEFAULT_LOAD_FACTOR);
         for (Node<K, V> tempNode : oldArray) {
             while (tempNode != null) {
@@ -74,8 +74,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private int hash(Object key) {
-        int cell;
-        return (key == null) ? 0 : (Math.abs(cell = key.hashCode() % table.length));
+        return (key == null) ? 0 : (Math.abs(key.hashCode() % table.length));
     }
 
     private static class Node<K, V> {
