d11d43dcba5d2b2c3403c30301b1722b479e7182
Mykola Voronov <www.kolyavoronov@gmail.com>
2021-11-09 16:22:41 +0200

    Threshold was added to the constructor, minor changes in getValue and put methods. Node class and it`s constructor are private now

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,12 +5,13 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
-    private int threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
+    private int threshold;
     private Node<K, V>[] table;
     private int size;
 
     public MyHashMap() {
         table = new Node[DEFAULT_INITIAL_CAPACITY];
+        threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
     }
 
     @Override
@@ -22,36 +23,31 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         Node<K, V> currentNode = table[index];
         if (currentNode == null) {
             table[index] = new Node<>(key, value, null);
-            size++;
         } else {
             do {
                 if (Objects.equals(currentNode.key, key)) {
                     currentNode.value = value;
-                    break;
+                    return;
                 } else if (currentNode.next == null) {
                     currentNode.next = new Node<>(key, value, null);
-                    size++;
                     break;
                 }
                 currentNode = currentNode.next;
             } while (currentNode != null);
         }
+        size++;
     }
 
     @Override
     public V getValue(K key) {
         int index = calculateIndex(key);
         Node<K, V> currentNode = table[index];
-        if (currentNode == null) {
-            return null;
-        }
-        do {
+        while (currentNode != null) {
             if (Objects.equals(currentNode.key, key)) {
                 return currentNode.value;
             }
             currentNode = currentNode.next;
-        } while (currentNode != null);
-
+        }
         return null;
     }
 
@@ -78,12 +74,12 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return key == null ? 0 : Math.abs(key.hashCode() % table.length);
     }
 
-    static class Node<K, V> {
+    private static class Node<K, V> {
         private final K key;
         private V value;
         private Node<K, V> next;
 
-        Node(K key, V value, Node<K, V> next) {
+        private Node(K key, V value, Node<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
