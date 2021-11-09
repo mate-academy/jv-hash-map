39ada2fe676f6170fde702aee27a6610c7595b8d
Maryna Blank <marinablank96@gmail.com>
2021-11-09 13:25:06 +0200

    HashMap implementation - not working.

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,87 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+    private int threshold = (int) (DEFAULT_CAPACITY * LOAD_FACTOR);
+    private Node<K, V>[] table = new Node[DEFAULT_CAPACITY];
+    private int size = 0;
 
     @Override
     public void put(K key, V value) {
-
+        if (size == threshold) {
+            resize();
+        }
+        Node<K, V> newNode = new Node<>(key, value, null);
+        if (table[getIndex(key)] == null) {
+            table[getIndex(key)] = newNode;
+        } else {
+            Node<K, V> oldNode;
+            for (oldNode = table[getIndex(key)]; oldNode.next != null; oldNode = oldNode.next) {
+                if (isEqual(key, oldNode.key)) {
+                    oldNode.value = value;
+                    break;
+                }
+            }
+            oldNode.next = newNode;
+        }
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        Node<K, V> node = table[getIndex(key)];
+        if (node == null) {
+            return null;
+        }
+        for (node = table[getIndex(key)]; node != null; node = node.next) {
+            if (isEqual(key, node.key)) {
+                return node.value;
+            }
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private static class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private int getIndex(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode() % table.length);
+    }
+
+    private boolean isEmpty() {
+        return size == 0;
+    }
+
+    private boolean isEqual(Object first, Object second) {
+        return Objects.equals(first, second);
+    }
+
+    private void resize() {
+        int newCapacity = table.length * 2;
+        Node<K, V>[] oldTable = table;
+        size = 0;
+        table = new Node[newCapacity];
+
+        for (Node<K, V> node : oldTable) {
+            put(node.key, node.value);
+        }
+        threshold = (int) (newCapacity * LOAD_FACTOR);
     }
 }
