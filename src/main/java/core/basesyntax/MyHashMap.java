84e0ff7b67001117637ffa610cd275bd98f4f5ff
Roman Okhrymovych <okhrymovych11@gmail.com>
2021-12-21 01:49:42 +0200

    private methods below

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -51,32 +51,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    private void resize() {
-        Node<K,V>[] oldNodes = table;
-        Node<K,V>[] newNodes = new Node[oldNodes.length * 2];
-        threshold = (int) (newNodes.length * DEFAULT_LOAD_FACTOR);
-        transfer(oldNodes,newNodes);
-        table = newNodes;
-    }
-
-    private void transfer(Node<K,V>[] oldNodes, Node<K,V>[] newNodes) {
-        for (Node<K,V> node:oldNodes) {
-            if (node == null) {
-                continue;
-            }
-            if (newNodes[getIndexNode(node)] != null) {
-                Node<K,V> current = newNodes[getIndexNode(node)];
-                while (current != null) {
-                    if (Objects.equals(current.key,node.key)) {
-                        current.value = node.value;
-                    }
-                    current = current.next;
-                }
-            }
-            newNodes[getIndexNode(node)] = node;
-        }
-    }
-
     @Override
     public V getValue(K key) {
         V value = null;
@@ -101,6 +75,32 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     public int getIndexNode(Node<K,V> node) {
         return hash(node.key) % table.length;
+    }
+
+    private void resize() {
+        Node<K,V>[] oldNodes = table;
+        Node<K,V>[] newNodes = new Node[oldNodes.length * 2];
+        threshold = (int) (newNodes.length * DEFAULT_LOAD_FACTOR);
+        transfer(oldNodes,newNodes);
+        table = newNodes;
+    }
+
+    private void transfer(Node<K,V>[] oldNodes, Node<K,V>[] newNodes) {
+        for (Node<K,V> node:oldNodes) {
+            if (node == null) {
+                continue;
+            }
+            if (newNodes[getIndexNode(node)] != null) {
+                Node<K,V> current = newNodes[getIndexNode(node)];
+                while (current != null) {
+                    if (Objects.equals(current.key,node.key)) {
+                        current.value = node.value;
+                    }
+                    current = current.next;
+                }
+            }
+            newNodes[getIndexNode(node)] = node;
+        }
     }
 
     private int hash(Object key) {
