a7cee0f27ee9bec265474c973aeaeadeee044a16
Roman Novikov <novikovr121281@gmail.com>
2022-01-04 20:54:38 +0200

    Changes are added

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -2,13 +2,9 @@ package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
     static final int DEFAULT_INITIAL_CAPACITY = 16;
-
     static final float LOAD_FACTOR = 0.75f;
-
     private int threshold = (int)(DEFAULT_INITIAL_CAPACITY * LOAD_FACTOR);
-
     private int size;
-
     private Node<K, V>[] table;
 
     static class Node<K, V> {
@@ -31,27 +27,19 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     public void put(K key, V value) {
         fillTable(getIndex(key), key, value);
         if (size > threshold) {
-            size = 0;
             resize();
         }
     }
 
     @Override
     public V getValue(K key) {
-        if (table != null && table[getIndex(key)] != null) {
-            Node<K, V> p = table[getIndex(key)];
-            do {
-                if ((p.key == null && key == null) || (key != null && key.equals(p.key))) {
-                    if (p.key == null) {
-                        return p.value;
-                    }
-                    if (key.equals(p.key)) {
-                        return p.value;
-                    }
-                }
-            } while ((p = p.next) != null);
+        Node<K, V> node = table[getIndex(key)];
+        while (node != null) {
+            if ((node.key == key) || (key != null && key.equals(node.key))) {
+                return node.value;
+            }
+            node = node.next;
         }
-
         return null;
     }
 
@@ -65,57 +53,57 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void resize() {
-        Node<K, V>[] oldTab = table;
-        int oldCap = oldTab.length;
-        int newCap;
+        Node<K, V>[] oldTable = table;
+        int newCapacity;
         int newThreshold;
-        newCap = oldCap << 1;
-        newThreshold = (int) (newCap * LOAD_FACTOR);
+        size = 0;
+        newCapacity = oldTable.length << 1;
+        newThreshold = (int) (newCapacity * LOAD_FACTOR);
 
         threshold = newThreshold;
-        table = (Node<K, V>[]) new Node[newCap];
+        table = (Node<K, V>[]) new Node[newCapacity];
 
-        transfer(oldTab, oldCap);
+        transfer(oldTable);
     }
 
-    private void transfer(Node<K, V>[] oldTab, int oldCap) {
-        for (int i = 0; i < oldCap; i++) {
-            if (oldTab[i] != null) {
-                Node<K, V> e = oldTab[i];
-                if ((e.next) == null) {
-                    int index = getIndex(e.key);
-                    fillTable(index, e.key, e.value);
+    private void transfer(Node<K, V>[] oldTable) {
+        for (Node<K, V> node : oldTable) {
+            if (node != null) {
+                Node<K, V> tempElem = node;
+                if ((tempElem.next) == null) {
+                    int index = getIndex(tempElem.key);
+                    fillTable(index, tempElem.key, tempElem.value);
                 } else {
                     Node<K, V> next;
                     do {
-                        next = e.next;
-                        fillTable(getIndex(e.key), e.key, e.value);
-                    } while ((e = next) != null);
+                        next = tempElem.next;
+                        fillTable(getIndex(tempElem.key), tempElem.key, tempElem.value);
+                    } while ((tempElem = next) != null);
                 }
             }
         }
     }
 
     private void fillTable(int index, K key, V value) {
-        Node<K, V> p;
-        if ((p = table[index]) == null) {
+        Node<K, V> tempElem = table[index];
+        if (tempElem == null) {
             table[index] = new Node<>(key, value, null);
             size++;
         } else {
-            Node<K, V> e;
-            while (true) {
-                e = p.next;
-                if ((p.key == key || (key != null && key.equals(p.key)))) {
-                    p.value = value;
+            Node<K, V> nextNode;
+            do {
+                nextNode = tempElem.next;
+                if ((tempElem.key == key || (key != null && key.equals(tempElem.key)))) {
+                    tempElem.value = value;
                     break;
                 }
-                if (e == null) {
-                    p.next = new Node<>(key, value, null);
+                if (nextNode == null) {
+                    tempElem.next = new Node<>(key, value, null);
                     size++;
                     break;
                 }
-                p = e;
-            }
+                tempElem = nextNode;
+            } while (nextNode != null);
         }
     }
 }
