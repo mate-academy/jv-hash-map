b57030ba2bd35cc0e4e5f05734b66ae6965fa2bf
Yaroslav Boiko <slkawnia@gmail.com>
2022-01-08 00:30:44 +0200

    fixed bugs

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,22 +1,18 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
     private Node<K,V>[] table;
     private int size;
-    private int threshold = 0;
-    private int initialCapacity = 0;
+    private int threshold;
 
     private class Node<K, V> {
-        private int hash;
         private K key;
         private V value;
         private Node<K, V> next;
 
-        public Node(int hash, K key, V value, Node<K, V> next) {
-            this.hash = hash;
+        public Node(K key, V value, Node<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
@@ -26,7 +22,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     public MyHashMap() {
         table = new Node[DEFAULT_INITIAL_CAPACITY];
         threshold = (int) (DEFAULT_LOAD_FACTOR * DEFAULT_INITIAL_CAPACITY);
-        initialCapacity = DEFAULT_INITIAL_CAPACITY;
     }
 
     @Override
@@ -38,42 +33,22 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
 
         if (table[hash] == null) {
-            table[hash] = new Node<>(hash, key, value, null);
+            table[hash] = new Node<>(key, value, null);
             size++;
         } else {
             Node<K, V> searchNode = table[hash];
 
             while (searchNode != null) {
-                if (equals(searchNode.key, key)) {
+                if (isKeysEquals(searchNode.key, key)) {
                     searchNode.value = value;
-                    searchNode = null;
+                    break;
                 } else if (searchNode.next == null) {
-                    searchNode.next = new Node<>(hash, key, value, null);
-                    searchNode = null;
+                    searchNode.next = new Node<>(key, value, null);
                     size++;
+                    break;
                 } else {
                     searchNode = searchNode.next;
                 }
-            }
-        }
-    }
-
-    private void resize() {
-        int newInitialCapacity = table.length * 2;
-        threshold = (int) (DEFAULT_LOAD_FACTOR * newInitialCapacity);
-        Node<K, V>[] oldTable = table; 
-        table = new Node[newInitialCapacity];
-        size = 0;
-        transfer(oldTable);
-    }
-
-    private void transfer(Node<K,V>[] oldTable) {
-        for (int i = 0; i < oldTable.length; i++) {
-            Node<K, V> node = oldTable[i];
-
-            while (node != null) {
-                put(node.key, node.value);
-                node = node.next;
             }
         }
     }
@@ -84,7 +59,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         Node<K, V> searchNode = table[hash];
 
         while (searchNode != null) {
-            if (equals(searchNode.key, key)) {
+            if (isKeysEquals(searchNode.key, key)) {
                 return searchNode.value;
             }
             searchNode = searchNode.next;
@@ -99,10 +74,30 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private int getHash(K key) {
-        return Math.abs((key == null) ? 0 : (key.hashCode() % initialCapacity));
+        return Math.abs((key == null) ? 0 : (key.hashCode() % table.length));
     }
 
-    private boolean equals(K firstKey, K secondKey) {
+    private boolean isKeysEquals(K firstKey, K secondKey) {
         return firstKey == secondKey || (secondKey != null && secondKey.equals(firstKey));
+    }
+
+    private void resize() {
+        int newInitialCapacity = table.length * 2;
+        threshold = (int) (DEFAULT_LOAD_FACTOR * newInitialCapacity);
+        Node<K, V>[] oldTable = table;
+        table = new Node[newInitialCapacity];
+        size = 0;
+        transfer(oldTable);
+    }
+
+    private void transfer(Node<K,V>[] oldTable) {
+        for (int i = 0; i < oldTable.length; i++) {
+            Node<K, V> node = oldTable[i];
+
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.next;
+            }
+        }
     }
 }
