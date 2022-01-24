93ab7e543196d75fc448ba283ff94f5405f0a2a2
maksim <maksim.sokur90@gmail.com>
2022-01-24 21:31:19 +0200

    -myHashMap-Sokur-M - V1.0 1. Create constant value default size of array; 2. Create constant value to calculate load of array; 3. Create size variable; 4. Create array nodes; 5. Create static class node; 6. Create constructor Node; 7. Create constructor MyHashMap class and note array size; 8. Create method hash - calculate int number of bucket; 9. Overriding method's put, get, getSize, isEmpty; 10. Create method resize;

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,97 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float THRESHOLD = 0.75F;
+    private int size;
+    private Node<K, V>[] buckets;
+
+    private static class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public MyHashMap() {
+        buckets = new Node[DEFAULT_CAPACITY];
+    }
+
+    private int hash(K key) {
+        int h;
+        return (key == null) ? 0 : Math.abs((h = key.hashCode()) ^ (h >>> DEFAULT_CAPACITY));
+    }
 
     @Override
     public void put(K key, V value) {
+        if (size + 1 == buckets.length * THRESHOLD) {
+            resize();
+        }
+        Node<K, V> node = new Node<>(key, value, null);
 
+        int bucket = hash(key) % buckets.length;
+
+        Node<K, V> existing = buckets[bucket];
+        if (existing == null) {
+            buckets[bucket] = node;
+            size++;
+        } else {
+            while (existing.next != null) {
+                if (Objects.equals(existing.key, key)) {
+                    existing.value = value;
+                    return;
+                }
+                existing = existing.next;
+            }
+            if (Objects.equals(existing.key, key)) {
+                existing.value = value;
+            } else {
+                existing.next = node;
+                size++;
+            }
+        }
     }
 
     @Override
     public V getValue(K key) {
+        Node<K, V> bucket = buckets[hash(key) % buckets.length];
+        while (bucket != null) {
+            if (Objects.equals(bucket.key, key)) {
+                return bucket.value;
+            }
+            bucket = bucket.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private void resize() {
+        Node[] temp = buckets;
+        buckets = new Node[buckets.length * 2];
+        size = 0;
+        for (Node<K, V> node : temp) {
+            if (buckets != null) {
+                while (node != null) {
+                    put(node.key, node.value);
+                    node = node.next;
+                }
+            }
+        }
     }
 }
--- src/main/java/core/basesyntax/MyMap.java
+++ src/main/java/core/basesyntax/MyMap.java
@@ -6,4 +6,7 @@ public interface MyMap<K, V> {
     V getValue(K key);
 
     int getSize();
+
+    boolean isEmpty();
+
 }
