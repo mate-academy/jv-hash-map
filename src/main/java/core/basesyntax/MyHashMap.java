1805a9b37b81683bcc0d77d89805ff8d3c783ece
Sasha Bachynskyi <allfred12@gmail.com>
2022-01-28 11:23:25 +0200

    added necessary fields into MyHashMap class and inner class Node

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,6 +1,26 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_CAPACITY = 0.75f;
+    private Node<K, V>[] table;
+    private int size;
+    private int threshold;
+    private float loadFactor;
+
+    private static class Node<K, V> {
+        final int hash;
+        final K key;
+        V value;
+        Node<K, V> next;
+
+        public Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
 
     @Override
     public void put(K key, V value) {
