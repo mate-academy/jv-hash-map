6e7b08fee3cf7da1ba427e83257b7e53c157cca0
Roman <dudanecr@gmail.com>
2022-02-03 12:55:11 +0200

    homevork HashMap

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,87 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_SIZE = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private Node<K, V>[] table;
+    private int size;
+    private int threshold;
+
+    private static class Node<K, V> {
+        private K key;
+        private V value;
+        private int hash;
+        private Node<K, V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.hash = hash;
+            this.next = next;
+        }
+    }
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_SIZE];
+        threshold = (int) (DEFAULT_SIZE * DEFAULT_LOAD_FACTOR);
+    }
 
     @Override
     public void put(K key, V value) {
-
+        if (size >= threshold) {
+            resize();
+        }
+        Node<K, V> node = table[hash(key)];
+        while (node != null) {
+            if (Objects.equals(key, node.key)) {
+                node.value = value;
+                return;
+            } else if (node.next == null) {
+                node.next = new Node<K, V>(key, value, null);
+                size++;
+                return;
+            }
+            node = node.next;
+        }
+        table[hash(key)] = new Node<>(key, value, null);
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        Node<K, V> node = table[hash(key)];
+        while (node != null) {
+            if (Objects.equals(key, node.key)) {
+                return node.value;
+            }
+            node = node.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private int hash(K key) {
+        int res = key == null ? 0 : Math.abs(key.hashCode() % table.length);
+        return res;
+    }
+
+    private void resize() {
+        int oldSize = table.length;
+        size = 0;
+        Node<K, V>[] oldTable = table;
+        table = new Node[oldSize * 2];
+        for (Node<K, V> old : oldTable) {
+            while (old != null) {
+                put(old.key, old.value);
+                old = old.next;
+            }
+        }
+        threshold = (int) (table.length * DEFAULT_LOAD_FACTOR);
     }
 }
