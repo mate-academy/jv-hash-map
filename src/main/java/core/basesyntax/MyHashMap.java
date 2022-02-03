626487579c51738f2f32e967627c73beb00f6583
Viktoriia <viksonchik1996@ukr.net>
2022-02-03 16:16:35 +0200

    after students review

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final float DEFAULT_LOAD_FACTOR = 0.75F;
-    private static final int INCREASE = 2;
+    private static final int INCREASE_INDEX = 2;
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
     private Node<K, V>[] table;
     private int threshold;
@@ -64,7 +64,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private void resize() {
         size = 0;
         Node<K, V>[] previousMap = table;
-        table = new Node[table.length * INCREASE];
+        table = new Node[table.length * INCREASE_INDEX];
         threshold = (int) (table.length * DEFAULT_LOAD_FACTOR);
         for (Node<K, V> node: previousMap) {
             while (node != null) {
