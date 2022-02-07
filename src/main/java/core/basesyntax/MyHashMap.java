f179f6d11be5f74f10b3660a31c1e9441af8b802
Ivan Pavluchuk <java.python10@gmail.com>
2022-02-07 06:46:05 +0200

    added solution

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,92 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int INITIAL_CAPACITY = 1 << 4;
+    private static final float LOAD_FACTOR = 0.75f;
+    private Node<K, V>[] nodes;
+    private int index;
+    private int volume;
 
-    @Override
-    public void put(K key, V value) {
-
+    public MyHashMap() {
+        nodes = new Node[INITIAL_CAPACITY];
     }
 
-    @Override
+    private class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        private Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public void put(K key, V value) {
+
+        Node<K, V> currentNode = new Node<>(key, value, null);
+        Node<K, V> temporaryNode;
+        int index = setIndex(key);
+        if (volume == threshold()) {
+            resize();
+        }
+        if (nodes[index] == null) {
+            nodes[index] = currentNode;
+            volume++;
+        } else {
+            temporaryNode = nodes[index];
+            while (temporaryNode != null) {
+                if (Objects.equals(temporaryNode.key, key)) {
+                    temporaryNode.value = value;
+                    break;
+                }
+                if (temporaryNode.next == null) {
+                    temporaryNode.next = currentNode;
+                    volume++;
+                    break;
+                }
+                temporaryNode = temporaryNode.next;
+            }
+        }
+    }
+
+    private void resize() {
+        Node<K, V>[] temporaryArray = nodes;
+        nodes = new Node[nodes.length * 2];
+        volume = 0;
+        for (Node<K, V> node : temporaryArray) {
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.next;
+            }
+        }
+    }
+
+    private int threshold() {
+        return (int) (nodes.length * LOAD_FACTOR);
+    }
+
     public V getValue(K key) {
+        Node<K, V> temporaryNode;
+        index = setIndex(key);
+        temporaryNode = nodes[index];
+        while (temporaryNode != null) {
+            if (Objects.equals(temporaryNode.key, key)) {
+                return temporaryNode.value;
+            }
+            temporaryNode = temporaryNode.next;
+        }
         return null;
     }
 
-    @Override
+    private int setIndex(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode() % nodes.length);
+    }
+
     public int getSize() {
-        return 0;
+        return volume;
     }
 }
