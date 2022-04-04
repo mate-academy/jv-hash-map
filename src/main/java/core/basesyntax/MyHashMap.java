c66b507a80b07149ec3ea18914d4a1e2f4b00945
AndrewLinskii <tolstuy1996@gmail.com>
2022-04-04 15:52:29 +0300

    Implements HashMap

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,105 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final double LOAD_FACTOR = 0.75d;
+    private int threshold;
+    private int capacity;
+    private int size;
+    private Node<K, V>[] buckets;
+
+    public static class Node<K, V> {
+        private int hash;
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public MyHashMap() {
+        buckets = new Node[DEFAULT_CAPACITY];
+        size = 0;
+        capacity = DEFAULT_CAPACITY;
+        threshold = (int) (DEFAULT_CAPACITY * LOAD_FACTOR);
+    }
+
+    private int makeHashCode(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode());
+    }
+
+    private int toAssignIndex(K key) {
+        return key == null ? 0 : makeHashCode(key) % capacity;
+    }
+
+    private void transfer(Node<K, V>[] oldBuckets) {
+        for (Node<K, V> enrty : oldBuckets) {
+            while (enrty != null) {
+                put(enrty.key, enrty.value);
+                enrty = enrty.next;
+            }
+        }
+    }
+
+    private void resize() {
+        if (size == threshold) {
+            capacity = capacity * 2;
+            final Node<K, V>[] oldBuckets = buckets;
+            buckets = new Node[capacity];
+            size = 0;
+            threshold = (int) (capacity * LOAD_FACTOR);
+            transfer(oldBuckets);
+        }
+    }
+
+    private boolean checkKeys(K firstKey, K secondKey) {
+        return firstKey == null && secondKey == null
+                || firstKey != null && firstKey.equals(secondKey);
+    }
 
     @Override
     public void put(K key, V value) {
-
+        Node<K, V> newNode = new Node<>(makeHashCode(key), key, value, null);
+        int index = toAssignIndex(key);
+        if (buckets[index] == null) {
+            buckets[index] = newNode;
+            size++;
+        } else {
+            Node<K, V> temp = buckets[index];
+            while (true) {
+                if (checkKeys(key, temp.key)) {
+                    temp.value = value;
+                    break;
+                } else if (temp.next == null) {
+                    temp.next = newNode;
+                    size++;
+                    break;
+                } else {
+                    temp = temp.next;
+                }
+            }
+        }
+        resize();
     }
 
     @Override
     public V getValue(K key) {
+        int index = toAssignIndex(key);
+        for (Node<K, V> temp = buckets[index]; temp != null; temp = temp.next) {
+            if (checkKeys(key, temp.key)) {
+                return temp.value;
+            }
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
     }
 }
