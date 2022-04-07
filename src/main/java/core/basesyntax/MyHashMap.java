d9afb9c7f9532c9354454047f3c5bec74fd5add5
GerasimovTA-92 <olexandr.gerasimov.02@gmail.com>
2022-04-07 16:20:02 +0300

    made code simply and fixed some mistakes

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,46 +5,44 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final float LOAD_FACTOR = 0.75f;
     private static final int DEFAULT_LENGTH = 16;
+    private static final int LENGTH_MULTIPLIER = 2;
     private int size;
     private Node<K, V>[] data;
-    private int threshold;
+
+    public MyHashMap() {
+        data = new Node[DEFAULT_LENGTH];
+    }
 
     @Override
     public void put(K key, V value) {
-        Node<K, V>[] arrayOfData = data;
         int hash = hash(key);
-        if (arrayOfData == null || size >= threshold) {
+        if (size > data.length * LOAD_FACTOR) {
             resize();
-            arrayOfData = data;
         }
-        if (arrayOfData[(arrayOfData.length - 1) & hash] == null) {
-            arrayOfData[(arrayOfData.length - 1) & hash] = new Node<>(hash, key, value, null);
+        int bucketIndex = (data.length - 1) & hash;
+        if (data[bucketIndex] == null) {
+            data[bucketIndex] = new Node<>(key, value, null);
         } else {
-            Node<K, V> currentNode = arrayOfData[(arrayOfData.length - 1) & hash];
+            Node<K, V> currentNode = data[bucketIndex];
             while (currentNode != null) {
                 if (Objects.equals(currentNode.key, key)) {
                     currentNode.value = value;
                     return;
                 }
                 if (currentNode.next == null) {
-                    currentNode.next = new Node<>(hash, key, value, null);
+                    currentNode.next = new Node<>(key, value, null);
                     break;
                 }
                 currentNode = currentNode.next;
             }
         }
         size++;
-        data = arrayOfData;
     }
 
     @Override
     public V getValue(K key) {
         int hash = hash(key);
-        Node<K, V>[] ourData = data;
-        if (ourData == null || ourData[(ourData.length - 1) & hash] == null) {
-            return null;
-        }
-        Node<K, V> current = ourData[(ourData.length - 1) & hash];
+        Node<K, V> current = data[(data.length - 1) & hash];
         while (current != null) {
             if (Objects.equals(current.key, key)) {
                 return current.value;
@@ -60,26 +58,12 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private int hash(K key) {
-        return key == null ? 0 : key.hashCode();
+        return key == null ? 0 : Math.abs(key.hashCode());
     }
 
     private void resize() {
         Node<K, V>[] oldData = data;
-        int oldLength = oldData == null ? 0 : oldData.length;
-        int oldThreshold = threshold;
-        int newLength = 0;
-        int newThreshold = 0;
-        if (oldThreshold == 0) {
-            newLength = DEFAULT_LENGTH;
-            newThreshold = (int)(DEFAULT_LENGTH * LOAD_FACTOR);
-        }
-        if (size > oldThreshold) {
-            newLength = oldLength << 1;
-            newThreshold = (int)(newLength * LOAD_FACTOR);
-        }
-        Node<K, V>[] newData = (Node<K, V>[]) new Node[newLength];
-        threshold = newThreshold;
-        data = newData;
+        data = new Node[data.length * LENGTH_MULTIPLIER];
         if (oldData != null) {
             size = 0;
             for (Node<K, V> node : oldData) {
@@ -92,13 +76,11 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private static class Node<K, V> {
-        private int hash;
         private K key;
         private V value;
         private Node<K, V> next;
 
-        Node(int hash, K key, V value, Node<K, V> next) {
-            this.hash = hash;
+        Node(K key, V value, Node<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
