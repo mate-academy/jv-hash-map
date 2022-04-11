310041d0eef64908058e5e226ad8bf2814b64382
DanyloTiazhev <finckreg@gmail.com>
2022-04-11 16:49:31 +0300

    Custom HashMap implemented

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,86 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    static final int DEFAULT_CAPACITY = 16;
+    static final float LOAD_FACTOR = 0.75f;
+
+    private int size;
+    private Node<K, V>[] table;
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
-
+        if (size > table.length * LOAD_FACTOR) {
+            ensureCapacity();
+        }
+        int keyHash = hash(key);
+        int bucketIndex = Math.abs(keyHash % table.length);
+        Node<K, V> currentNode = table[bucketIndex];
+        if (table[bucketIndex] == null) {
+            table[bucketIndex] = new Node<>(key, value, null);
+        }
+        while (currentNode != null) {
+            if (Objects.equals(currentNode.key, key)) {
+                currentNode.value = value;
+                return;
+            }
+            if (currentNode.next == null) {
+                currentNode.next = new Node<>(key, value, null);
+                break;
+            }
+            currentNode = currentNode.next;
+        }
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        for (Node<K, V> currentNode: table) {
+            while (currentNode != null) {
+                if (Objects.equals(currentNode.key, key)) {
+                    return currentNode.value;
+                }
+                currentNode = currentNode.next;
+            }
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private static class Node<K,V> {
+        private final K key;
+        private V value;
+        private Node<K,V> next;
+
+        Node(K key, V value, Node<K,V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private void ensureCapacity() {
+        Node<K, V> [] oldTable = table;
+        size = 0;
+        table = new Node[oldTable.length << 1];
+        for (Node<K, V> currentNode: oldTable) {
+            while (currentNode != null) {
+                put(currentNode.key, currentNode.value);
+                currentNode = currentNode.next;
+            }
+        }
+    }
+
+    private int hash(K key) {
+        return key == null ? 0 : key.hashCode() * 31;
     }
 }
