45a912a7182a61bff10aa6e1ae62b8ec0093ba85
Vladyslav Fomenko <with.god.victorius@gmail.com>
2022-04-12 22:39:11 +0300

    implemented MyHashMap

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,84 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int INITIAL_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+    private int capacity = INITIAL_CAPACITY;
+    private int threshold;
+    private Node<K, V>[] table = new Node[capacity];
+    private int size;
 
     @Override
     public void put(K key, V value) {
-
+        if (size >= threshold) {
+            resize();
+        }
+        int index = getHashCode(key) % capacity;
+        Node<K, V> node = table[index];
+        if (node == null) {
+            table[index] = new Node<>(key, value, null);
+        }
+        for (; node != null; node = node.next) {
+            if (Objects.equals(node.key, key)) {
+                node.value = value;
+                return;
+            }
+            if (node.next == null) {
+                node.next = new Node<>(key, value, null);
+                break;
+            }
+        }
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        for (Node<K, V> node : table) {
+            for (; node != null; node = node.next) {
+                if (node.key == key || node.key != null && node.key.equals(key)) {
+                    return node.value;
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public int getSize() {
+        return size;
+    }
+
+    private int getHashCode(K key) {
+        if (key != null) {
+            return Math.abs(key.hashCode());
+        }
         return 0;
+    }
+
+    private void resize() {
+        capacity *= 2;
+        threshold = Math.round(capacity * LOAD_FACTOR);
+        Node<K, V>[] table = this.table;
+        this.table = new Node[capacity];
+        size = 0;
+        for (Node<K, V> node : table) {
+            for (; node != null; node = node.next) {
+                put(node.key, node.value);
+            }
+        }
+    }
+
+    private class Node<K,V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
