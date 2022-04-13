937e9b115c1cfe2bdd15d7eb7cf92ade07648536
Oleksander <ghjwtyrj25@gmail.com>
2022-04-13 09:13:58 +0300

    After change #2

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -3,25 +3,23 @@ package core.basesyntax;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final float LOAD_FACTOR = 0.75f;
-    private int currentCap;
     private int size;
     private Node<K, V>[] table;
 
     public MyHashMap() {
-        this.currentCap = DEFAULT_CAPACITY;
         this.table = new Node[DEFAULT_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
-        if (LOAD_FACTOR * currentCap == size) {
+        if (LOAD_FACTOR * table.length == size) {
             resize();
         }
-        Node<K, V> bucket = new Node<>(hash(key), key, value);
-        if (table[bucket.hash] == null) {
-            table[bucket.hash] = bucket;
+        Node<K, V> newNode = new Node<>(key, value);
+        if (table[hash(key)] == null) {
+            table[hash(key)] = newNode;
         } else {
-            Node<K, V> iterator = table[bucket.hash];
+            Node<K, V> iterator = table[hash(key)];
             while (iterator != null) {
                 if (iterator.key == key || iterator.key != null
                         && iterator.key.equals(key)) {
@@ -33,23 +31,20 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                 }
                 iterator = iterator.next;
             }
-            iterator.next = bucket;
+            iterator.next = newNode;
         }
         size++;
     }
 
     @Override
     public V getValue(K key) {
-        if (size == 0) {
-            return null;
-        }
         for (Node<K, V> bucket : table) {
             if (bucket != null) {
                 if (bucket.key == key || bucket.key != null
                         && bucket.key.equals(key)) {
                     return bucket.value;
-                } else if (bucket.hash == hash(key)) {
-                    Node<K, V> iterator = table[bucket.hash];
+                } else if (hash(key) == hash(key)) {
+                    Node<K, V> iterator = table[hash(key)];
                     while (iterator != null) {
                         if (iterator.key == key || iterator.key != null
                                 && iterator.key.equals(key)) {
@@ -70,35 +65,26 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private void resize() {
         size = 0;
-        currentCap = currentCap << 1;
         Node<K, V>[] oldTable = table;
-        table = new Node[currentCap];
+        table = new Node[table.length << 1];
         for (Node<K, V> bucket : oldTable) {
-            if (bucket != null) {
-                while (bucket != null) {
-                    put(bucket.key, bucket.value);
-                    bucket = bucket.next;
-                }
+            while (bucket != null) {
+                put(bucket.key, bucket.value);
+                bucket = bucket.next;
             }
         }
     }
 
     private int hash(Object key) {
-        if (key == null) {
-            return 0;
-        } else {
-            return Math.abs(key.hashCode()) % currentCap;
-        }
+        return key == null ? 0 : Math.abs(key.hashCode()) % table.length;
     }
 
     private class Node<K, V> {
-        private final int hash;
         private final K key;
         private V value;
         private Node<K, V> next;
 
-        Node(int hash, K key, V value) {
-            this.hash = hash;
+        Node(K key, V value) {
             this.key = key;
             this.value = value;
         }
