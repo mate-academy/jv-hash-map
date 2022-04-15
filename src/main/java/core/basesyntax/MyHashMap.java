7d75d9cde716bbb7bb5befdb07d6e838be9e26e6
Serhii Pankov <pankoff.serg@gmail.com>
2022-04-15 14:22:51 +0300

    test commit

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -2,6 +2,10 @@ package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
 
+    public static void main(String[] args) {
+        System.out.println("jjj");
+    }
+
     @Override
     public void put(K key, V value) {
 
