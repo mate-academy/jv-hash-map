d0dffb592a20c714b6f8f7bef89cc46b93281573
Serhii Pankov <pankoff.serg@gmail.com>
2022-04-15 14:31:43 +0300

    3

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,6 +5,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     public static void main(String[] args) {
         System.out.println("jjj");
         System.out.println("test");
+        System.out.println("test");
     }
 
     @Override
