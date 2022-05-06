6f63a1a5b41117aa8aca2377d32dba4001127e28
Vasiliy Sheretko <vasil.sheretko@yahoo.com>
2022-05-06 21:28:57 +0300

    Added a few more changes after review

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,49 +5,48 @@ import java.util.Arrays;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final float LOAD_FACTOR = 0.75f;
     private static final int INCREASE_COEFFICIENT = 2;
-    private int capacity = 16;
+    private static final int INITIAL_CAPACITY = 16;
     private int size;
     private Node<K, V>[] table;
 
     public MyHashMap() {
-        table = new Node[capacity];
+        table = new Node[INITIAL_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
-        if (size > capacity * LOAD_FACTOR) {
+        if (size > table.length * LOAD_FACTOR) {
             resize();
         }
-        int hash = hash(key);
+        int index = getIndex(key);
         Node<K, V> node = new Node<>(key, value, null);
-        if (table[hash] == null) {
-            table[hash] = node;
+        if (table[index] == null) {
+            table[index] = node;
             size++;
             return;
         }
-            Node<K, V> current = table[hash];
-            Node<K, V> tmp = null;
-            while (current != null) {
-                if (current.key == node.key
-                        || current.key != null && current.key.equals(node.key)) {
-                    current.value = node.value;
-                    return;
-                }
-                if (current.next == null) {
-                    tmp = current;
-                }
-                current = current.next;
+        Node<K, V> current = table[index];
+        Node<K, V> tmp = null;
+        while (current != null) {
+            if (current.key == node.key
+                    || current.key != null && current.key.equals(node.key)) {
+                current.value = node.value;
+                return;
             }
-            current = node;
-            tmp.next = current;
-            size++;
+            if (current.next == null) {
+                tmp = current;
+            }
+            current = current.next;
         }
+        current = node;
+        tmp.next = current;
+        size++;
     }
 
     @Override
     public V getValue(K key) {
-        int hash = hash(key);
-        Node<K,V> current = table[hash];
+        int index = getIndex(key);
+        Node<K,V> current = table[index];
         while (current != null) {
             if (current.key == key || current.key != null && current.key.equals(key)) {
                 return current.value;
@@ -67,23 +66,22 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return Arrays.toString(table);
     }
 
-    private int hash(K key) {
+    private int getIndex(K key) {
         return (key == null) ? 0 : Math.abs(key.hashCode()) % table.length;
     }
 
     private void resize() {
         Node<K,V>[] currentTable = table;
-        table = new Node[capacity * INCREASE];
+        table = new Node[table.length * INCREASE_COEFFICIENT];
         size = 0;
         Node<K, V> current;
-        for (int i = 0; i < capacity; i++) {
+        for (int i = 0; i < table.length / 2; i++) {
             current = currentTable[i];
             while (current != null) {
                 put(current.key, current.value);
                 current = current.next;
             }
         }
-        capacity = capacity * INCREASE;
     }
 
     private static class Node<K, V> {
