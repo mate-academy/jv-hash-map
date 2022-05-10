46d1969f91ed6a06c68f518f1c02da987e386bbc
Vadym Datsiuk <lutskvadym@gmail.com>
2022-05-10 12:15:04 +0300

    fixed according to remarks

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,14 +1,14 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    private static final int DEFAULT_LENGTH = 16;
+    private static final int DEFAULT_CAPACITY = 16;
     private static final float LOAD_FACTOR = 0.75F;
     private Node<K, V>[] table;
     private int size;
     private int threshold;
 
     public MyHashMap() {
-        table = new Node[DEFAULT_LENGTH];
+        table = new Node[DEFAULT_CAPACITY];
         threshold = (int) (table.length * LOAD_FACTOR);
     }
 
@@ -17,7 +17,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if (size == threshold) {
             resize();
         }
-        int bucketIndex = getHashByKey(key);
+        int bucketIndex = getIndexByKey(key);
         Node<K, V> currentNode = table[bucketIndex];
 
         if (currentNode == null) {
@@ -31,8 +31,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                 }
                 if (currentNode.next == null) {
                     currentNode.next = new Node<>(key, value, null);
-                    size++;
-                    return;
+                    break;
                 }
                 currentNode = currentNode.next;
             }
@@ -42,7 +41,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public V getValue(K key) {
-        Node<K, V> currentNode = table[getHashByKey(key)];
+        Node<K, V> currentNode = table[getIndexByKey(key)];
         while (currentNode != null) {
             if (key == currentNode.key
                     || (key != null && key.equals(currentNode.key))) {
@@ -55,16 +54,15 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public int getSize() {
-        //return 0;
         return size;
     }
 
     private void resize() {
         size = 0;
-        Node<K, V>[] tableForIncrease = table;
-        table = new Node[tableForIncrease.length * 2];
-        threshold = (int) ((tableForIncrease.length * 2) * LOAD_FACTOR);
-        for (Node<K, V> node : tableForIncrease) {
+        Node<K, V>[] oldTable = table;
+        table = new Node[oldTable.length * 2];
+        threshold = (int) ((oldTable.length * 2) * LOAD_FACTOR);
+        for (Node<K, V> node : oldTable) {
             while (node != null) {
                 put(node.key, node.value);
                 node = node.next;
@@ -72,7 +70,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    private int getHashByKey(K key) {
+    private int getIndexByKey(K key) {
         return key == null ? 0 : Math.abs(key.hashCode() % table.length);
     }
 
