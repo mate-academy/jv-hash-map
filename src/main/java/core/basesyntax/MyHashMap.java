92a6536831a85b11db3adaaf8b1fb6840f159d79
Kate Chernyavska <ccarts.kpi@gmail.com>
2022-05-24 13:19:20 +0300

    changed all methods and fields in accordance with the requirements

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,13 +5,16 @@ import java.util.Objects;
 
 @SuppressWarnings("unchecked")
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    private static final int DEFAULT_CAPACITY = 16;
-    private static final float LOAD_FACTOR = 0.75f;
-    private int threshold = (int) (DEFAULT_CAPACITY * LOAD_FACTOR);
+    private static int DEFAULT_CAPACITY;
+    private static float LOAD_FACTOR;
+    private int threshold;
     private Entry<K, V>[] table;
     private int size;
 
     public MyHashMap() {
+        DEFAULT_CAPACITY = 16;
+        LOAD_FACTOR = 0.75f;
+        threshold = (int) (DEFAULT_CAPACITY * LOAD_FACTOR);
         table = new Entry[DEFAULT_CAPACITY];
     }
 
@@ -41,39 +44,55 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             Map.Entry<K, V> entry = (Map.Entry<K, V>) o;
             Object key1 = getKey();
             Object key2 = entry.getKey();
-            if (Objects.equals(key1, key2)) {
+            if (Objects.equals(key1, key2) && key1 != null) {
                 Object value1 = getValue();
                 Object value2 = entry.getValue();
                 return Objects.equals(value1, value2);
             }
             return false;
         }
+    }
 
-        public final int hashCode() {
-            return (key == null ? 0 : Math.abs(key.hashCode()));
+    @Override
+    public void put(K key, V value) {
+        if (size >= threshold) {
+            resize(2 * table.length);
         }
+        int index = indexFor(key);
+        Entry<K, V> entry = table[index];
+        if (entry == null) {
+            table[index] = new Entry<>(key, value, null);
+        }
+        for (; entry != null; entry = entry.next) {
+            if (entry.key == key || entry.key != null && entry.key.equals(key)) {
+                entry.value = value;
+                return;
+            }
+            if (entry.next == null) {
+                entry.next = new Entry<>(key, value, null);
+                break;
+            }
+        }
+        size++;
+    }
+
+    @Override
+    public V getValue(K key) {
+        for (Entry<K, V> entry = table[indexFor(key)]; entry != null; entry = entry.next) {
+            if (entry.key == key || entry.key != null && entry.key.equals(key)) {
+                return entry.value;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
     }
 
     private int indexFor(K key) {
         return key == null ? 0 : Math.abs(key.hashCode() % DEFAULT_CAPACITY);
-    }
-
-    private void putForNullKey(V value) {
-        for (Entry<K, V> entry = table[0]; entry != null; entry = entry.next) {
-            if (entry.key == null) {
-                entry.value = value;
-                return;
-            }
-        }
-        addEntry(null, value, 0);
-    }
-
-    private void addEntry(K key, V value, int index) {
-        Entry<K, V> entry = table[index];
-        table[index] = new Entry<>(key, value, entry);
-        if (size++ >= threshold) {
-            resize(2 * table.length);
-        }
     }
 
     private void resize(int newCapacity) {
@@ -98,49 +117,5 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                 entry = next;
             }
         }
-    }
-
-    private V getForNullKey() {
-        for (Entry<K, V> entry = table[0]; entry != null; entry = entry.next) {
-            if (entry.key == null) {
-                return entry.value;
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public void put(K key, V value) {
-        if (key == null) {
-            putForNullKey(value);
-        } else {
-            int index = indexFor(key);
-            for (Entry<K, V> entry = table[index]; entry != null; entry = entry.next) {
-                if (key == entry.key || key.equals(entry.key)) {
-                    entry.value = value;
-                    return;
-                }
-            }
-            addEntry(key, value, index);
-        }
-    }
-
-    @Override
-    public V getValue(K key) {
-        if (key == null) {
-            return getForNullKey();
-        } else {
-            for (Entry<K, V> entry = table[indexFor(key)]; entry != null; entry = entry.next) {
-                if (key == entry.key || key.equals(entry.key)) {
-                    return entry.value;
-                }
-            }
-            return null;
-        }
-    }
-
-    @Override
-    public int getSize() {
-        return size;
     }
 }
