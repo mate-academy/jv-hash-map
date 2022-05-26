237ee3715075619855d94c2eec2f6ecf307bdd41
Dima Davydenko <dmytro.davydenko89@gmail.com>
2022-05-26 16:27:17 +0300

    optimized after review

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,60 +5,49 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final double LOAD_FACTOR = 0.75;
-    private int bucket;
     private int size;
     private Node<K, V>[] table = (Node<K,V>[])new Node[DEFAULT_CAPACITY];
 
     @Override
+
     public void put(K key, V value) {
         int threshold = (int) (table.length * LOAD_FACTOR);
-        int hash = Math.abs(hashCode(key));
-        Node<K, V> node = new Node<>(hash, key, value, null);
-        if (threshold >= size) {
-            bucket = getBucket(key, hash);
-            if (table[bucket] == null) {
-                table[bucket] = node;
-            } else {
-                Node<K, V> oldNode = getNode(key, bucket);
-                if (oldNode != null) {
-                    oldNode.value = node.value;
-                    node.next = oldNode.next;
-                    return;
-                } else {
-                    Node<K, V> secondLastNode = table[bucket];
-                    while (secondLastNode.next != null) {
-                        secondLastNode = secondLastNode.next;
-                    }
-                    secondLastNode.next = node;
-                }
-            }
-            size++;
-        } else {
-            table = resize();
-            put(key, value);
+        int hash = getHash(key);
+        int bucket = hash % table.length;
+        Node<K, V> newNode = new Node<>(hash, key, value, null);
+        if (threshold < size) {
+            resize();
         }
+        if (table[bucket] != null) {
+            Node<K, V> oldNode = getNode(key, bucket);
+            if (oldNode != null) {
+                oldNode.value = newNode.value;
+                newNode.next = oldNode.next;
+                return;
+            } else {
+                Node<K, V> node = table[bucket];
+                while (node.next != null) {
+                    node = node.next;
+                }
+                node.next = newNode;
+            }
+        } else {
+            table[bucket] = newNode;
+        }
+        size++;
     }
 
     @Override
     public V getValue(K key) {
-        int hash = Math.abs(hashCode(key));
-        bucket = getBucket(key, hash);
+        int hash = getHash(key);
+        int bucket = hash % table.length;
         Node<K, V> node = getNode(key, bucket);
-        if (node != null) {
-            return node.value;
-        }
-        return null;
+        return node != null ? node.value : null;
     }
 
     @Override
     public int getSize() {
         return size;
-    }
-
-    @Override
-    public int hashCode(Object key) {
-        int h;
-        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
     }
 
     private static class Node<K, V> {
@@ -75,13 +64,8 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    private int getBucket(K key, int hash) {
-        if (key == null) {
-            bucket = 0;
-        } else {
-            bucket = hash % table.length;
-        }
-        return bucket;
+    private int getHash(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode());
     }
 
     private Node<K, V> getNode(K key, int bucket) {
@@ -95,7 +79,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return null;
     }
 
-    private Node<K, V>[] resize() {
+    private void resize() {
         Node<K, V>[] oldTable = table;
         table = (Node<K,V>[]) new Node[oldTable.length * 2];
         size = 0;
@@ -105,6 +89,5 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                 node = node.next;
             }
         }
-        return table;
     }
 }
