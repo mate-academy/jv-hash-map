8adb2704d7e2049d8132d1bd05bc67e72deaa683
Igor <ZIM20093@gmail.com>
2022-05-31 15:12:41 +0300

    added threshold variable, change custom equals to Objects equals

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,15 +1,16 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_SIZE = 16;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
 
     private Node<K,V>[] table;
     private int size;
+    private int threshold;
 
     public MyHashMap() {
-        table = new Node[DEFAULT_SIZE];
-        size = 0;
     }
 
     @Override
@@ -22,7 +23,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         } else {
             Node<K, V> curNode = table[keyHash % table.length];
             while (curNode != null) {
-                if (key == null ? curNode.key == null : key.equals(curNode.key)) {
+                if (Objects.equals(key, curNode.key)) {
                     curNode.value = value;
                     return;
                 }
@@ -38,9 +39,12 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public V getValue(K key) {
+        if (table == null) {
+            return null;
+        }
         Node<K, V> curNode = table[getHash(key) % table.length];
         while (curNode != null) {
-            if (key == null ? curNode.key == null : key.equals(curNode.key)) {
+            if (Objects.equals(key, curNode.key)) {
                 return curNode.value;
             }
             curNode = curNode.next;
@@ -59,22 +63,33 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void grow() {
-        if (size >= table.length * DEFAULT_LOAD_FACTOR) {
-            size = 0;
-            Node<K, V>[] oldTable = table;
-            table = new Node[table.length << 1];
-            for (Node<K, V> node : oldTable) {
-                while (node != null) {
-                    put(node.key, node.value);
-                    node = node.next;
+        if (table == null) {
+            initializateMap();
+        } else {
+            if (size >= threshold) {
+                size = 0;
+                Node<K, V>[] oldTable = table;
+                table = new Node[table.length << 1];
+                threshold = threshold << 1;
+                for (Node<K, V> node : oldTable) {
+                    while (node != null) {
+                        put(node.key, node.value);
+                        node = node.next;
+                    }
                 }
             }
         }
     }
 
+    private void initializateMap() {
+        size = 0;
+        table = new Node[DEFAULT_SIZE];
+        threshold = (int) (DEFAULT_SIZE * DEFAULT_LOAD_FACTOR);
+    }
+
     static class Node<K, V> {
         private final int hashcode;
-        private K key;
+        private final K key;
         private V value;
         private Node<K, V> next;
 
