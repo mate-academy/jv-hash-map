99b4257a6c2b456174f07f6b6cd78a8b22bbde49
Алена <258258>
2022-06-02 19:49:43 +0300

    All methods are added.

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -53,7 +53,7 @@ public class MyHashMap<K,V> implements MyMap<K,V> {
     }
 
     private int getHashCode(K key) {
-        return (key == null) ? 0 : (17 * 31 + key.hashCode()) >>> 2;
+        return (key == null) ? 0 : (17 * 31 + key.hashCode() >>> 2);
     }
 
     @Override
