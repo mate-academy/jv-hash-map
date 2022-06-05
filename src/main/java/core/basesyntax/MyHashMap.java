dd1c508afb799154323bdcb2e2716d2922c64cd8
Nikita Semisoshenko <jaderussell197@gmail.com>
2022-06-05 21:41:20 +0300

    build success

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,7 +1,5 @@
 package core.basesyntax;
 
-import java.util.Objects;
-
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
@@ -9,26 +7,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private int threshold;
     private Node<K,V>[] table;
 
-    private void iterateNodes(Node<K,V> node, K key, V value) {
-        boolean isNewNode = true;
-        Node<K,V> prevNode = null;
-        do {
-            if (node.key == key || (node.key != null && node.key.equals(key))) {
-                node.value = value;
-                isNewNode = false;
-                break;
-            } else {
-                prevNode = node;
-                node = node.next;
-            }
-        } while (node != null);
-        if (isNewNode) {
-            prevNode.next = new Node<>(key, value, null);
-            size++;
-        }
-    }
     private void resize() {
-
         if (size == threshold) {
             size = 0;
             int newCapacity = table.length << 1;
@@ -45,13 +24,33 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             }
         }
     }
+
+    private void iterateNodes(Node<K,V> node, K key, V value) {
+        boolean isNewNode = true;
+        Node<K,V> prevNode = null;
+        do {
+            if (node.key == key || node.key != null && node.key.equals(key)) {
+                node.value = value;
+                isNewNode = false;
+                break;
+            } else {
+                prevNode = node;
+                node = node.next;
+            }
+        } while (node != null);
+        if (isNewNode) {
+            prevNode.next = new Node<>(key, value, null);
+            size++;
+        }
+    }
+
     @Override
     public void put(K key, V value) {
         isEmpty();
         int keyHash = key == null ? 0 : key.hashCode();
         int index = Math.abs(keyHash % table.length);
         if (table[index] == null) {
-            table[index] = new Node<>(key, value, null);;
+            table[index] = new Node<>(key, value, null);
             size++;
         } else {
             iterateNodes(table[index], key, value);
@@ -66,7 +65,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         Node<K,V> node = table[index];
         if (node != null) {
             do {
-                if (Objects.equals(key, node.key)) {
+                if (key == node.key || key != null && key.equals(node.key)) {
                     return node.value;
                 } else {
                     node = node.next;
