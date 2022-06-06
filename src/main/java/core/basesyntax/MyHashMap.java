65f9ef8053043c0156e50b7d972ad7bd485518da
olyaklipa <olyaklipa@gmail.com>
2022-06-06 22:47:25 +0300

    reviewer suggestions were taken into account

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -10,9 +10,8 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     public MyHashMap() {
         this.table = new Node[DEFAULT_INITIAL_CAPACITY];
-        this.size = 0;
         this.capacity = DEFAULT_INITIAL_CAPACITY;
-        this.threshold = (int) (capacity * DEFAULT_LOAD_FACTOR);
+        this.threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
     }
 
     private static class Node<K,V> {
@@ -38,20 +37,46 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             table[index] = new Node(key, value, null);
             size++;
         } else {
-            Node<K, V> previousNode = null;
-            boolean replaced = false;
-            while (node != null) {
-                if (node.key == key || (key != null && key.equals(node.key))) {
-                    node.value = value;
-                    replaced = true;
-                    break;
-                }
-                previousNode = node;
-                node = node.next;
-            }
-            if (previousNode != null && !replaced) {
-                previousNode.next = new Node(key, value, null);
+            Node<K, V> sameNode = findNodeInBucket(node, key);
+            if (sameNode != null) {
+                sameNode.value = value;
+            } else {
+                Node<K, V> tail = getTail(node);
+                tail.next = new Node(key, value, null);
                 size++;
+            }
+        }
+    }
+
+    @Override
+    public V getValue(K key) {
+        int index = getIndex(key);
+        Node<K,V> sameNode = findNodeInBucket(table[index], key);
+        if (sameNode != null) {
+            return sameNode.value;
+        }
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    private void resize() {
+        capacity *= 2;
+        threshold = (int) (capacity * DEFAULT_LOAD_FACTOR);
+        size = 0;
+        Node<K,V>[] oldTable = table;
+        table = new Node[capacity];
+        for (int i = 0; i < oldTable.length; i++) {
+            Node<K,V> currNode = oldTable[i];
+            if (currNode == null) {
+                continue;
+            }
+            while (currNode != null) {
+                put(currNode.key, currNode.value);
+                currNode = currNode.next;
             }
         }
     }
@@ -63,53 +88,20 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return Math.abs(key.hashCode()) % capacity;
     }
 
-    private void resize() {
-        int newCapacity = capacity * 2;
-        capacity = newCapacity;
-        threshold = (int) (newCapacity * DEFAULT_LOAD_FACTOR);
-        Node<K,V>[] newTable = new Node[capacity];
-        for (int i = 0; i < table.length; i++) {
-            Node<K,V> currNode = table [i];
-            if (currNode == null) {
-                continue;
+    private Node<K, V> findNodeInBucket(Node<K, V> node, K key) {
+        while (node != null) {
+            if (node.key == key || (key != null && key.equals(node.key))) {
+                return node;
             }
-            while (currNode != null) {
-                K currKey = currNode.key;
-                int newIndex = getIndex(currKey);
-                if (newTable[newIndex] == null) {
-                    newTable[newIndex] = currNode;
-                } else {
-                    Node<K,V> presentNode = newTable[newIndex];
-                    while (presentNode.next != null) {
-                        presentNode = presentNode.next;
-                    }
-                    presentNode.next = currNode;
-                }
-                Node<K, V> nextNode = currNode.next;
-                currNode.next = null;
-                currNode = nextNode;
-            }
-        }
-        table = newTable;
-    }
-
-    @Override
-    public V getValue(K key) {
-        int index = getIndex(key);
-        Node<K,V> currNode = table [index];
-        while (currNode != null) {
-            if (currNode.key == key || (key != null && key.equals(currNode.key))) {
-                V value = currNode.value;
-                return value;
-            }
-            currNode = currNode.next;
+            node = node.next;
         }
         return null;
     }
 
-    @Override
-    public int getSize() {
-        return size;
+    private Node<K, V> getTail(Node<K, V> head) {
+        while (head.next != null) {
+            head = head.next;
+        }
+        return head;
     }
 }
-
