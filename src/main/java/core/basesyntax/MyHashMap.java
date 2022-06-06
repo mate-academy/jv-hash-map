d9215c187616391f8fe1cbacd87ccb332487a94a
Anton Karpov <antonkarpov547@gmail.com>
2022-06-06 11:24:19 +0200

    objects.equals used, fixes

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,9 +1,11 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_LENGTH = 16;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
-    private static final int INCREASE_COEFFITIENT = 2;
+    private static final int INCREASE_COEFFICIENT = 2;
     private Node<K, V>[] array;
     private int size;
     private int threshold;
@@ -16,15 +18,14 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public void put(K key, V value) {
         increase();
-        putNode(key, value);
+        insert(key, value);
     }
 
     @Override
     public V getValue(K key) {
         Node<K, V> currentNode = array[getIndex(key)];
         if (currentNode != null) {
-            while (currentNode.next != null
-                    && !(currentNode.key == key || key != null && key.equals(currentNode.key))) {
+            while (currentNode.next != null && !(Objects.equals(key, currentNode.key))) {
                 currentNode = currentNode.next;
             }
             return currentNode.value;
@@ -52,38 +53,36 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return key == null ? 0 : Math.abs(key.hashCode()) % array.length;
     }
 
-    private void putNode(K key, V value) {
+    private void insert(K key, V value) {
         final int index = getIndex(key);
         final Node<K, V> newNode = new Node<>(key, value);
         Node<K, V> currentNode = array[index];
-        if (currentNode != null) {
-            while (currentNode.next != null
-                    && !(key == currentNode.key || key != null && key.equals(currentNode.key))) {
-                currentNode = currentNode.next;
-            }
-            if (key == currentNode.key || key != null && key.equals(currentNode.key)) {
-                currentNode.key = key;
-                currentNode.value = value;
-                return;
-            }
-            currentNode.next = newNode;
-            size++;
-
-        } else {
+        if (currentNode == null) {
             array[index] = newNode;
             size++;
+            return;
         }
+        while (currentNode.next != null && !(Objects.equals(key, currentNode.key))) {
+            currentNode = currentNode.next;
+        }
+        if (Objects.equals(key, currentNode.key)) {
+            currentNode.key = key;
+            currentNode.value = value;
+            return;
+        }
+        currentNode.next = newNode;
+        size++;
     }
 
     private void increase() {
         if (size >= threshold) {
             Node<K, V>[] buffer = array;
-            array = new Node[array.length * INCREASE_COEFFITIENT];
+            array = new Node[array.length * INCREASE_COEFFICIENT];
             size = 0;
-            for (Node<K, V> node : buffer) {
-                while (node != null) {
-                    putNode(node.key, node.value);
-                    node = node.next;
+            for (Node<K, V> bucket : buffer) {
+                while (bucket != null) {
+                    insert(bucket.key, bucket.value);
+                    bucket = bucket.next;
                 }
             }
             threshold = (int) (array.length * DEFAULT_LOAD_FACTOR);
