c8658cb4e48d2fa95801f99560f9d1adb911fcac
Oleksandr Petrunovskyi <o.petrunovskyi@gmail.com>
2022-07-09 08:52:15 +0300

    the new-hw-has-map task is completed and ready for check

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,14 +1,83 @@
 package core.basesyntax;
 
-import java.util.Objects;
-
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    final static int INITIAL_CAPACITY = 16;
-    final static float LOAD_FACTOR = 0.75f;
+    private static final int INITIAL_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+    private static final int DEFAULT_MULTIPLIER = 2;
+    private Node<K, V>[] table;
     private int threshold;
     private int capacity;
-    private Node<K, V>[] table;
     private int size;
+
+    public MyHashMap() {
+        table = new Node[INITIAL_CAPACITY];
+        capacity = INITIAL_CAPACITY;
+        threshold = calculateThreshold(INITIAL_CAPACITY);
+    }
+
+    @Override
+    public void put(K key, V value) {
+        if (size >= threshold) {
+            resize();
+        }
+        Node currentNode = table[getBucketIndex(hash(key))];
+        while (currentNode != null) {
+            if (hash((K) currentNode.key) == (hash(key))
+                    && (currentNode.key == key
+                    || (currentNode.key != null && currentNode.key.equals(key)))) {
+                currentNode.value = value;
+                return;
+            }
+            if (currentNode.next == null) {
+                currentNode.next = newNode(key, value);
+                size++;
+                return;
+            }
+            currentNode = currentNode.next;
+        }
+        table[getBucketIndex(hash(key))] = newNode(key, value);
+        size++;
+    }
+
+    @Override
+    public V getValue(K key) {
+        int hash = hash(key);
+        int index = getBucketIndex(hash);
+        Node currentNode = table[index];
+        while (currentNode != null) {
+            if (hash((K) currentNode.key) == (hash(key))
+                    && (currentNode.key == key
+                    || (currentNode.key != null && currentNode.key.equals(key)))) {
+                return (V) currentNode.value;
+            }
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    private void resize() {
+        size = 0;
+        capacity = table.length * DEFAULT_MULTIPLIER;
+        Node<K, V>[] newTable = table;
+        table = new Node[capacity];
+        threshold = calculateThreshold(capacity);
+        transfer(newTable);
+    }
+
+    private Node<K, V>[] transfer(Node<K, V>[] newTable) {
+        for (Node<K, V> node : newTable) {
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.next;
+            }
+        }
+        return newTable;
+    }
 
     private int hash(K key) {
         return Math.abs((key == null) ? 0 : key.hashCode());
@@ -18,94 +87,12 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return hash % capacity;
     }
 
-    @Override
-    public void put(K key, V value) {
-        resize();
-        int hash = hash(key);
-        int index = getBucketIndex(hash);
-        Node currentNode = table[index];
-        if (bucketConditionIsNull(currentNode)) {
-            table[index] = newNode(key, value);
-            size++;
-        } else {
-            // якщо хеши ключів однакові, колізія
-            if (hash((K) currentNode.key) == (hash(key))) {
-                // перевіряємо за equals
-                while (currentNode != null) {
-                    if (currentNode.key == key || currentNode.key.equals(key)) {
-                        // співпали - перезаписуєм ноду
-                        currentNode.value = value;
-                    } else {
-                        // не співпали за equals, переходимо до наступної ноди (якщо вона є)
-                        if (currentNode.next != null) {
-                            currentNode = currentNode.next;
-                        } else {
-                            currentNode.next = newNode(key, value);
-                            size++;
-                        }
-                    }
-                    currentNode = currentNode.next;
-                }
-            }
-            // якщо хеши ключів різні
-            else {
-                while (currentNode.next != null) {
-                    currentNode = currentNode.next;
-                }
-                currentNode.next = newNode(key, value);
-                size++;
-            }
-        }
-    }
-
-    @Override
-    public V getValue(K key) {
-        return table[hash(key)].value;
-    }
-
-    @Override
-    public int getSize() {
-        return size;
-    }
-
-    private Node<K, V>[] resize() {
-        if (table == null) {
-            capacity = INITIAL_CAPACITY;
-            threshold = (int) (INITIAL_CAPACITY * LOAD_FACTOR);
-            table = (Node<K, V>[]) new Node[INITIAL_CAPACITY];
-        }
-        if (size + 1 > threshold) {
-            capacity = capacity * 2;
-            setThreshold(threshold * 2);
-            table = (Node<K, V>[]) new Node[capacity];
-            // transfer(newTable);
-        }
-        return table;
-    }
-
     private Node<K, V> newNode(K key, V value) {
         return new Node<>(hash(key), key, value, null);
     }
 
-
-    private boolean bucketConditionIsNull(Node currentNode) {
-        return currentNode == null ? true : false;
-    }
-
-    private Node<K, V>[] transfer(Node<K, V>[] newTable) {
-        for (Node<K, V> node : table) {
-            if (node != null) {
-                hash(node.key);
-            }
-
-        }
-        table = newTable;
-        return table;
-    }
-
-    public int setThreshold(int threshold) {
-        this.threshold = threshold;
-        return threshold;
+    private int calculateThreshold(int capacity) {
+        return (int) (capacity * LOAD_FACTOR);
     }
 
     private static class Node<K, V> {
