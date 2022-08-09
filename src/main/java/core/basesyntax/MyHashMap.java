9cc3cb91d68926b6592d09c45beed0231c937e26
Vyacheslav Verbanov <v.verbanov@gmail.com>
2022-08-09 14:57:51 +0200

    make mvn clean package)

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,7 +1,5 @@
 package core.basesyntax;
 
-import java.util.Objects;
-
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
