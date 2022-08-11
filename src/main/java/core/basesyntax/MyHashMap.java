b25dd584e4d42d5c9ec902d05aad33c42a14144c
asha <ashajanebrown@gmail.com>
2022-08-11 13:50:10 +0300

    created MyHashMap

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,98 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+
+    private Bucket<K, V>[] table;
+    private int size;
+    private int capacity;
+    private int threshold;
+
+    public MyHashMap() {
+        table = new Bucket[DEFAULT_CAPACITY];
+        capacity = DEFAULT_CAPACITY;
+        threshold = (int) (DEFAULT_CAPACITY * LOAD_FACTOR);
+    }
+
+    private class Bucket<K, V> {
+        private final K key;
+        private V value;
+        private Bucket<K, V> next;
+
+        public Bucket(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 
     @Override
     public void put(K key, V value) {
+        if (size >= threshold) {
+            increaseCapacity();
+        }
+        int index = getBucketIndex(key);
+        Bucket<K, V> bucket = table[index];
 
+        if (bucket == null) {
+            table[index] = new Bucket<>(key, value);
+        } else {
+            while (bucket.next != null) {
+                if (Objects.equals(bucket.key, key)) {
+                    bucket.value = value;
+                    return;
+                }
+                bucket = bucket.next;
+            }
+            if (Objects.equals(bucket.key, key)) {
+                bucket.value = value;
+                return;
+            }
+            bucket.next = new Bucket<>(key, value);
+        }
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        int index = getBucketIndex(key);
+        Bucket<K, V> bucket = table[index];
+
+        if (bucket == null) {
+            return null;
+        }
+        while (bucket != null) {
+            if (Objects.equals(bucket.key, key)) {
+                return bucket.value;
+            }
+            bucket = bucket.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private void increaseCapacity() {
+        capacity *= 2;
+        Bucket<K, V>[] oldTable = table;
+        table = new Bucket[capacity];
+        size = 0;
+        for (Bucket<K, V> bucket : oldTable) {
+            Bucket<K, V> current = bucket;
+            while (current != null) {
+                put(current.key, current.value);
+                current = current.next;
+            }
+        }
+        threshold = (int) (capacity * LOAD_FACTOR);
+    }
+
+    private int getBucketIndex(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode()) % capacity;
     }
 }
