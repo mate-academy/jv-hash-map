1d4e4797966777403f0648c99cdc0e5ad28e7443
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-08-12 13:20:19 +0300

    refactor getValue method

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -40,13 +40,30 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if (storage == null) {
             return null;
         }
+        return findNode(key) == null ? null : findNode(key).value;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    private int getHash(K key) {
+        return key == null ? 0 : key.hashCode();
+    }
+
+    private int getIndex(K key) {
+        return Math.abs(getHash(key)) % capacity;
+    }
+
+    private Node<K, V> findNode(K key) {
         Node<K, V> node = storage[getIndex(key)];
         if (node != null) {
             K nodeKey;
             do {
                 nodeKey = node.key;
                 if ((nodeKey == key || nodeKey != null && nodeKey.equals(key))) {
-                    return node.value;
+                    return node;
                 }
                 node = node.next;
             } while (node != null);
@@ -54,9 +71,16 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return null;
     }
 
-    @Override
-    public int getSize() {
-        return size;
+    private boolean add(K key, V value) {
+        Node<K, V> node = findNode(key);
+        if (node == null) {
+            int index = getIndex(key);
+            storage[index] = new Node<>(getHash(key), key, value, storage[index]);
+            return true;
+        } else {
+            node.value = value;
+        }
+        return false;
     }
 
     private void resizeIfNeed() {
@@ -74,40 +98,5 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                 }
             }
         }
-    }
-
-    private int getHash(K key) {
-        return key == null ? 0 : key.hashCode();
-    }
-
-    private int getIndex(K key) {
-        return Math.abs(getHash(key)) % capacity;
-    }
-
-    private boolean add(K key, V value) {
-        Node<K, V> node = findNode(key);
-        if (node == null) {
-            int index = getIndex(key);
-            storage[index] = new Node<>(getHash(key), key, value, storage[index]);
-            return true;
-        } else {
-            node.value = value;
-        }
-        return false;
-    }
-
-    private Node<K, V> findNode(K key) {
-        Node<K, V> node = storage[getIndex(key)];
-        if (node != null) {
-            K nodeKey;
-            do {
-                nodeKey = node.key;
-                if ((nodeKey == key || nodeKey != null && nodeKey.equals(key))) {
-                    return node;
-                }
-                node = node.next;
-            } while (node != null);
-        }
-        return null;
     }
 }
