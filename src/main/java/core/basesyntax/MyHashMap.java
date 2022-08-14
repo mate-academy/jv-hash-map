a9d50732784a319b111428d20c8b1f902f38d027
Ilja Nekrituk <Nekrit99@gmail.com>
2022-08-14 17:52:45 +0300

    implement get method and done checkstyle

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -3,13 +3,13 @@ package core.basesyntax;
 import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    private static final int INITIAL_CAPACITY = 16;
     static final int DEFAULT_CAPACITY = 1 << 4;
     static final float DEFAULT_RESIZE_FACTOR = 0.75f;
-    Node<K, V>[] table;
-    private int capacity;  // length of masiv
-    private int threshold; // length for nuw length
-    int size; // how much elements Node in it
+    static final int DEFAULT_SIZE = 0;
+    private Node<K, V>[] table;
+    private int capacity;
+    private int threshold;
+    private int size;
 
     public MyHashMap() {
         table = new Node[DEFAULT_CAPACITY];
@@ -35,37 +35,22 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                 if (currentNode.next == null) {
                     currentNode.next = newNode;
                     size++;
+
                 }
                 currentNode = currentNode.next;
             }
         }
     }
 
-
-    public Node<K, V>[] resize() {
-        Node<K, V>[] newTab = new Node[capacity * 2];
-        return newTab;
-    }
-
-    private void checkSize() {
-        if (size == threshold) {
-            resize();
-        }
-    }
-
-    private int getBucketIndex(K key) {
-        return (key == null) ? 0 : hash(key) % capacity;
-    }
-
-
-    private static int hash(Object key) {
-        int h;
-        return (key == null) ? 0 : (h = key.hashCode());
-    }
-
-
     @Override
     public V getValue(K key) {
+        Node<K, V> node = table[getBucketIndex(key)];
+        while (node != null) {
+            if (Objects.equals(node.key, key)) {
+                return node.value;
+            }
+            node = node.next;
+        }
         return null;
     }
 
@@ -75,15 +60,43 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     static class Node<K, V> {
-        final int hash;
-        final K key;
-        V value;
-        Node<K, V> next;
+        private final int hash;
+        private final K key;
+        private V value;
+        private Node<K, V> next;
 
         public Node(int hash, K key, V value) {
             this.hash = hash;
             this.key = key;
             this.value = value;
+        }
+    }
+
+    private static int hash(Object key) {
+        return (key == null) ? 0 : key.hashCode();
+    }
+
+    private int getBucketIndex(K key) {
+        return (key == null) ? 0 : Math.abs(key.hashCode() % table.length);
+    }
+
+    private void checkSize() {
+        if (size >= threshold) {
+            resize();
+        }
+    }
+
+    private void resize() {
+        size = DEFAULT_SIZE;
+        capacity = capacity << 1;
+        threshold = (int) (capacity * DEFAULT_RESIZE_FACTOR);
+        Node<K, V>[] oldTab = table;
+        table = new Node[capacity];
+        for (Node<K, V> node : oldTab) {
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.next;
+            }
         }
     }
 }
