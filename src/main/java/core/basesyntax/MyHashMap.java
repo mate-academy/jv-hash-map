175ba2921fcb48fb0df25fb82cfb63e6d83805f8
IraHurko <iragurko19@gmail.com>
2022-08-14 15:26:40 +0300

    Created my own HashMap

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,110 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private Node<K, V>[] table;
+    private final float loadFactor;
+    private int size;
+    private int capacity;
+    private int threshold;
+
+    public MyHashMap() {
+        this.capacity = DEFAULT_INITIAL_CAPACITY;
+        this.loadFactor = DEFAULT_LOAD_FACTOR;
+        this.threshold = (int) (capacity * loadFactor);
+        this.table = new Node[capacity];
+    }
+
+    static class Node<K, V> {
+        private int hash;
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
 
     @Override
     public void put(K key, V value) {
-
+        int hash = getHash(key);
+        Node<K, V> newNode = new Node<>(hash, key, value, null);
+        Node<K, V> presentNode = table[hash];
+        if (presentNode == null) {
+            table[hash] = newNode;
+            size++;
+        } else {
+            Node<K, V> lastNode = null;
+            Node<K, V> equalNode = null;
+            for (Node<K, V> node = presentNode; node != null; node = node.next) {
+                if (node.key == key || node.key != null && node.key.equals(key)) {
+                    equalNode = node;
+                    node.value = newNode.value;
+                }
+                lastNode = node;
+            }
+            if (equalNode == null) {
+                lastNode.next = newNode;
+                size++;
+            }
+        }
+        if (size >= threshold) {
+            resize();
+        }
     }
 
     @Override
     public V getValue(K key) {
-        return null;
+        Node<K, V> node = getNode(key);
+        return node == null ? null : node.value;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    final Node<K,V> getNode(K key) {
+        for (Node<K, V> newNode : table) {
+            if (newNode == null) {
+                continue;
+            }
+            for (Node<K, V> node = newNode; node != null; node = node.next) {
+                if (node.key == key || node.key != null && node.key.equals(key)) {
+                    return node;
+                }
+            }
+        }
+        return null;
+    }
+
+    final void resize() {
+        final Node<K,V>[] oldTable = table;
+        final int oldCapacity = (oldTable == null) ? 0 : oldTable.length;
+        final int newCapacity;
+        final int newThreshold;
+        newCapacity = oldCapacity * 2;
+        capacity = newCapacity;
+        newThreshold = (int) (capacity * loadFactor);
+        threshold = newThreshold;
+        Node<K, V>[] newTable = (Node<K, V>[]) new Node[capacity];
+        table = newTable;
+        if (oldTable != null) {
+            for (int i = 0; i < oldTable.length; i++) {
+                Node<K, V> element = oldTable[i];
+                if (element != null) {
+                    newTable[getHash(element.key)] = element;
+                }
+            }
+        }
+    }
+
+    final int getHash(Object key) {
+        return (key == null) ? 0 : (Math. abs(key.hashCode()) % capacity);
     }
 }
