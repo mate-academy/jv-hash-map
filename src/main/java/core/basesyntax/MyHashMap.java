5776d2df7843758feaa732b7bdd90f7630d21302
Vasyl Pankiv <wasilius2@gmail.com>
2022-08-15 17:37:29 +0300

    add constructor to MyHashMap, implement getValue(Key) method

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
@@ -7,14 +9,18 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private int threshole;
     private Node<K, V>[] buckets;
 
+    public MyHashMap() {
+        buckets = new Node[DEFAULT_CAPACITY];
+        threshole = (int) (DEFAULT_CAPACITY * DEFAULT_LOAD_FACTOR);
+    }
 
     @Override
     public void put(K key, V value) {
-        int index  = Math.abs(key.hashCode() % buckets.length);
-        Node<K, V> currNode = buckets[index];
-        if (currNode == null) {
+        int index = getIndex(key);
+        if (buckets[index] == null) {
             buckets[index] = new Node<>(key, value, null);
         } else {
+            Node<K, V> currNode = buckets[index];
             while (currNode.next != null) {
                 currNode = currNode.next;
             }
@@ -25,12 +31,19 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    private void resize() {
-
-    }
-
     @Override
     public V getValue(K key) {
+        int index = getIndex(key);
+        if (buckets[index] == null) {
+            return null;
+        } else {
+            Node<K, V> currNode = buckets[index];
+            while (currNode.next != null) {
+                if(Objects.equals(currNode.key,key)) {
+                    return currNode.value;
+                }
+            }
+        }
         return null;
     }
 
@@ -39,7 +52,16 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
+    private int getIndex(K key) {
+        return Math.abs(key.hashCode() % buckets.length);
+    }
+
+    private void resize() {
+
+    }
+
     private class Node<K, V> {
+
         private K key;
         private V value;
         private Node<K, V> next;
