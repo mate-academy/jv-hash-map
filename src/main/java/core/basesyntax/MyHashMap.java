b493ff78d742a5a279c13a75a4592194395efc20
LubusUzh <lubus.uzh@gmail.com>
2022-08-15 14:23:31 +0300

    Add: getValue class and MyHashMap constructor

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -7,15 +7,24 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
     private static final int RESIZE_VALUE = 2;
     private Node<K, V>[] table;
-    private int threshold;
+    private int currentCapacity;
     private int size;
+    private int threshold;
+
+    public MyHashMap() {
+        this.table = new Node[DEFAULT_CAPACITY];
+        this.currentCapacity = DEFAULT_CAPACITY;
+        this.threshold = (int) (DEFAULT_CAPACITY * DEFAULT_LOAD_FACTOR);
+    }
 
     private class Node<K, V> {
+        final int hash;
         final K key;
         V value;
         Node<K, V> next;
 
-        public Node(K key, V value, Node<K, V> next) {
+        public Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
             this.key = key;
             this.value = value;
             this.next = next;
@@ -29,6 +38,19 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public V getValue(K key) {
+        int index;
+        if (key == null) {
+            index = 0;
+        } else {
+            index = Math.abs(key.hashCode()) % currentCapacity;
+        }
+        Node<K, V> currentNode = table[index];
+        while (currentNode != null) {
+            if (Objects.equals(currentNode.key, key)) {
+                return currentNode.value;
+            }
+            currentNode = currentNode.next;
+        }
         return null;
     }
 
