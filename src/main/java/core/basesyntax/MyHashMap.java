f5d6b2372b2631b517a102ba954e401b2b023d19
Alexandr Semashko <semashko123456789@gmail.com>
2022-08-15 07:51:32 +0300

    combine two method

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -16,7 +16,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public void put(K key, V value) {
-        if (checkSize(size)) {
+        if (size >= threshold) {
             resize();
         }
         if (putValue(key, value, table, getIndex(key, table.length))) {
@@ -44,12 +44,12 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private boolean putValue(K key, V value, Node<K, V>[] nodes, int index) {
         Node<K, V> currentNode = table[index];
         if (nodes[index] == null) {
-            nodes[index] = new Node<>(hashCode(key), key, value);
+            nodes[index] = new Node<>(key, value);
             return true;
         }
         while (!Objects.equals(currentNode.key, key)) {
             if (currentNode.next == null) {
-                currentNode.next = new Node<>(hashCode(key), key, value);
+                currentNode.next = new Node<>(key, value);
                 return true;
             }
             currentNode = currentNode.next;
@@ -61,7 +61,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private void resize() {
         Node<K, V>[] oldTable = table;
         int oldCapacity = table.length;
-        int oldThreshold = threshold;
         int newCapacity = oldCapacity * 2;
         threshold = (int) (newCapacity * DEFAULT_LOAD_FACTOR);
 
@@ -76,26 +75,16 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         table = newNodes;
     }
 
-    public boolean checkSize(int size) {
-        return size >= threshold;
-    }
-
     private int getIndex(K key, int capacity) {
-        return hashCode(key) % capacity;
-    }
-
-    private int hashCode(K key) {
-        return key == null ? 0 : Math.abs(key.hashCode());
+        return key == null ? 0 : Math.abs(key.hashCode()) % capacity;
     }
 
     private static class Node<K,V> {
-        private final int hash;
         private final K key;
         private V value;
         private Node<K, V> next;
 
-        public Node(int hash, K key, V value) {
-            this.hash = hash;
+        public Node(K key, V value) {
             this.key = key;
             this.value = value;
         }
