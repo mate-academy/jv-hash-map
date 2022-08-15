5d611c7b768ba7df1ad2728b592da17d80e0ec77
Andriy Linkov <linkoff.as@gmail.com>
2022-08-15 15:05:35 +0300

    Optimized code

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,57 +1,43 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
     private Node<K,V>[] table;
-    private int capacity;
     private int threshold;
     private int size;
 
+    public MyHashMap() {
+        table = new Node[DEFAULT_CAPACITY];
+        threshold = (int) (DEFAULT_LOAD_FACTOR * DEFAULT_CAPACITY);
+    }
+
     @Override
     public void put(K key, V value) {
-        if (table == null) {
+        if (size == threshold) {
             resize();
         }
         int bucket = getBucket(key);
-        Node<K, V> current = table[bucket];
-        if (current != null) {
-            while (current.next != null) {
-                if (isEqualsKeys(current.key, key)) {
+        if (table[bucket] == null) {
+            table[bucket] = new Node<>(key, value);
+            size++;
+        } else {
+            Node<K, V> current = table[bucket];
+            while (current != null) {
+                if (Objects.equals(current.key, key)) {
                     current.value = value;
+                    return;
+                }
+                if (current.next == null) {
+                    current.next = new Node<>(key, value);
+                    size++;
                     return;
                 }
                 current = current.next;
             }
-            if (isEqualsKeys(current.key, key)) {
-                current.value = value;
-                return;
-            }
-            if (size == threshold) {
-                resize();
-                bucket = getBucket(key);
-                current = table[bucket];
-                if (current != null) {
-                    while (current.next != null) {
-                        current = current.next;
-                    }
-                    current.next = new Node<>(key, value, null);
-                } else {
-                    current.next = new Node<>(key, value, null);
-                }
-            } else {
-                current.next = new Node<>(key, value, null);
-            }
-        } else {
-            if (size == threshold) {
-                resize();
-                bucket = getBucket(key);
-                table[bucket] = new Node<>(key, value, null);
-            } else {
-                table[bucket] = new Node<>(key, value, null);
-            }
         }
-        size++;
     }
 
     @Override
@@ -62,7 +48,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         int bucket = getBucket(key);
         Node<K, V> current = table[bucket];
         while (current != null) {
-            if (isEqualsKeys(current.key, key)) {
+            if (Objects.equals(current.key, key)) {
                 return current.value;
             }
             current = current.next;
@@ -80,47 +66,35 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         private V value;
         private Node<K, V> next;
 
-        private Node(K key, V value, Node<K, V> next) {
+        private Node(K key, V value) {
             this.key = key;
             this.value = value;
-            this.next = next;
         }
     }
 
     private int getHash(K key) {
-        return (key == null) ? 0 : key.hashCode();
+        return key == null ? 0 : key.hashCode();
     }
 
     private int getBucket(K key) {
         int hash = getHash(key);
-        return Math.abs(hash % capacity);
+        return Math.abs(hash % table.length);
     }
 
     private void resize() {
-        if (table == null) {
-            table = new Node[DEFAULT_CAPACITY];
-            capacity = DEFAULT_CAPACITY;
-            threshold = (int) (DEFAULT_LOAD_FACTOR * DEFAULT_CAPACITY);
-        } else {
-            capacity = capacity << 1;
-            threshold = threshold << 1;
-            Node<K,V>[] oldTable = table;
-            table = new Node[capacity];
-            size = 0;
-            transfer(oldTable);
-        }
+        Node<K,V>[] oldTable = table;
+        table = new Node[oldTable.length << 1];
+        threshold = threshold << 1;
+        size = 0;
+        transfer(oldTable);
     }
 
-    private void transfer(Node<K,V>[] src) {
-        for (Node<K, V> node : src) {
+    private void transfer(Node<K,V>[] table) {
+        for (Node<K, V> node : table) {
             while (node != null) {
                 put(node.key, node.value);
                 node = node.next;
             }
         }
-    }
-
-    private boolean isEqualsKeys(K firstKey, K secondKey) {
-        return firstKey == secondKey || (firstKey != null && firstKey.equals(secondKey));
     }
 }
