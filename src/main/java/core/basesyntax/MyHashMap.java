c797a99cb7891367e2f350df4c728f6ed6ea0a9b
Sofiia <sofiakazzz@gmail.com>
2022-08-16 15:53:03 +0300

    renamed some fields

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -6,18 +6,16 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final float LOAD_FACTOR = 0.75f;
     private static final int INCREASE = 2;
-    private int currentCapacity;
     private Node<K, V>[] table;
     private int size;
 
     public MyHashMap() {
         table = (Node<K, V>[]) new Node[DEFAULT_CAPACITY];
-        currentCapacity = DEFAULT_CAPACITY;
     }
 
     @Override
     public void put(K key, V value) {
-        increaseCapacity();
+        checkCapacity();
         Node<K, V> node = new Node<>(key, value, null);
         int index = getBucketIndex(key);
         if (table[index] == null) {
@@ -25,17 +23,18 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             size++;
             return;
         }
-        Node<K, V> currentNode = table[index];
-        while (currentNode != null) {
-            if (Objects.equals(currentNode.key, key)) {
-                currentNode.value = value;
+        Node<K, V> current = table[index];
+        while (current != null) {
+            if (Objects.equals(current.key, key)) {
+                current.value = value;
                 return;
             }
-            if (currentNode.next == null) {
-                currentNode.next = node;
+            if (current.next == null) {
+                current.next = node;
                 size++;
+                return;
             }
-            currentNode = currentNode.next;
+            current = current.next;
         }
     }
 
@@ -63,12 +62,11 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return Math.abs(key.hashCode() % table.length);
     }
 
-    private void increaseCapacity() {
-        if (size == currentCapacity * LOAD_FACTOR) {
+    private void checkCapacity() {
+        if (size == table.length * LOAD_FACTOR) {
             size = 0;
-            currentCapacity = currentCapacity * INCREASE;
             Node<K, V>[] oldTable = table;
-            table = new Node[currentCapacity];
+            table = new Node[oldTable.length * INCREASE];
             for (Node<K, V> node : oldTable) {
                 while (node != null) {
                     put(node.key, node.value);
