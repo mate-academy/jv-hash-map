1ac4612e974379b5e9552eb5fb2aaba78a80a785
Myroslav_Voskoboinyk <maestro.of.pain@gmail.com>
2022-08-16 10:36:24 +0300

    bucketIndex field was removed from Node class

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -78,7 +78,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private static class Node<K, V> {
-        private int bucketIndex;
         private K key;
         private V value;
         private Node<K, V> next;
