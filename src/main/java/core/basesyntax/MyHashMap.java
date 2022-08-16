f76a87ff90cc0fd5ce681659ac1b58b8039ddc92
KosatskiyKirill <kirillkosatskiy@gmail.com>
2022-08-16 12:40:49 +0300

    Created own hash map

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,106 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+    private Node<K, V>[] table;
+    private int capacity;
+    private int size;
+    private int threshold;
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_CAPACITY];
+        threshold = (int) (DEFAULT_CAPACITY * LOAD_FACTOR);
+        capacity = DEFAULT_CAPACITY;
+    }
 
     @Override
     public void put(K key, V value) {
-
+        resize();
+        Node<K, V> node = new Node<>(key, value);
+        int index = searchNodePosition(key);
+        if (checkBucketIsEmpty(index)) {
+            table[index] = node;
+            size++;
+        } else {
+            putCollisionNode(table[index], node);
+        }
     }
 
     @Override
     public V getValue(K key) {
-        return null;
+        if (size <= 0) {
+            return null;
+        }
+        int index = searchNodePosition(key);
+        return searchValueByKey(table[index], key);
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private void resize() {
+        if (size >= threshold) {
+            size = 0;
+            Node<K, V>[] oldTable = table;
+            table = new Node[capacity * 2];
+            threshold = threshold * 2;
+            for (Node<K, V> node : oldTable) {
+                while (node != null) {
+                    put(node.key, node.value);
+                    node = node.next;
+                }
+            }
+        }
+    }
+
+    private int searchNodePosition(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode()) % table.length;
+    }
+
+    private boolean checkBucketIsEmpty(int index) {
+        return table[index] == null;
+    }
+
+    private void putCollisionNode(Node<K, V> existNode, Node<K, V> newNode) {
+        if (existNode.key == null && existNode.key == newNode.key) {
+            existNode.value = newNode.value;
+            return;
+        } else if (existNode.key != null && existNode.key.equals(newNode.key)) {
+            existNode.value = newNode.value;
+            return;
+        }
+
+        if (existNode.next == null) {
+            existNode.next = newNode;
+            size++;
+        } else {
+            putCollisionNode(existNode.next, newNode);
+        }
+    }
+
+    private V searchValueByKey(Node<K, V> node, K key) {
+        if (key == null && node.key == null) {
+            return node.value;
+        } else if (node.key != null && node.key.equals(key)) {
+            return node.value;
+        } else if (node.next != null) {
+            return searchValueByKey(node.next, key);
+        } else {
+            return null;
+        }
+    }
+
+    private static class Node<K, V> {
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
