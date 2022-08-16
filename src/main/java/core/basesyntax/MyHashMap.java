2d09b0ccf81390fd4580a08497aa5a3707f24b73
mr-Claster <67782747+mr-Claster@users.noreply.github.com>
2022-08-16 09:48:05 +0300

    first version done

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,96 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int START_CAPACITY = 16;
+    private static final int ARRAY_EXPANSION = 2;
+    private static final double LOAD_FACTOR = 0.75;
+
+    private Node<K, V>[] heads;
+    private int capacity;
+    private int size;
+
+    public MyHashMap() {
+        capacity = START_CAPACITY;
+        heads = new Node[START_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
+        increaseCapacity();
+        int index = getIndex(key);
+        Node<K, V> newNode = new Node<>(key, value);
+        if (heads[index] == null) {
+            heads[index] = newNode;
+            size++;
+            return;
+        }
+        Node<K, V> tmp = heads[index];
+        if (Objects.equals(tmp.key, key)) {
+            tmp.value = value;
+            return;
+        }
+        Node prev = null;
+        while (tmp != null) {
+            if (Objects.equals(tmp.key, key)) {
+                tmp.value = newNode.value;
+                return;
+            }
+            prev = tmp;
+            tmp = tmp.next;
+        }
+        prev.next = newNode;
+        tmp = newNode;
+        size++;
 
     }
 
     @Override
     public V getValue(K key) {
+        Node<K, V> tmp = heads[getIndex(key)];
+        while (tmp != null) {
+            if (Objects.equals(key, tmp.key)) {
+                return tmp.value;
+            }
+            tmp = tmp.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private void increaseCapacity() {
+        if (((double)size / (double)capacity) >= LOAD_FACTOR) {
+            Node<K, V>[] tmp = heads;
+            capacity *= ARRAY_EXPANSION;
+            size = 0;
+            Node<K, V>[] newHeads = new Node[capacity];
+            heads = newHeads;
+            for (int i = 0; i < heads.length / ARRAY_EXPANSION; i++) {
+                while (tmp[i] != null) {
+                    put(tmp[i].key, tmp[i].value);
+                    tmp[i] = tmp[i].next;
+                }
+            }
+        }
+    }
+
+    private int getIndex(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode() % capacity);
+    }
+
+    private class Node<K, V> {
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
