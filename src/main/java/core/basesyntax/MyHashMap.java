5132e5be422808e66194425c1c068f876518651f
Ira Zatsnova <a.ir@outlook.com>
2022-08-17 23:32:46 +0300

    Done refactoring of code

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -2,9 +2,9 @@ package core.basesyntax;
 
 @SuppressWarnings("unchecked")
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    static final int DEFAULT_INITIAL_CAPACITY = 16;
-    static final float DEFAULT_LOAD_FACTOR = 0.75f;
-    static final int RESIZE = 2;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private static final int GROWTH_COEFFICIENT = 2;
     private Node<K,V>[] table;
     private int size;
     private int threshold;
@@ -12,25 +12,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     public MyHashMap() {
         table = (Node<K, V>[]) new Node[DEFAULT_INITIAL_CAPACITY];
         threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
-    }
-
-    private static class Node<K,V> {
-        private final int hash;
-        private final K key;
-        private V value;
-        private Node<K,V> next;
-
-        Node(int hash, K key, V value, Node<K,V> next) {
-            this.hash = hash;
-            this.key = key;
-            this.value = value;
-            this.next = next;
-        }
-
-        @Override
-        public final String toString() {
-            return key + "=" + value;
-        }
     }
 
     @Override
@@ -69,6 +50,25 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
+    private static class Node<K,V> {
+        private final int hash;
+        private final K key;
+        private V value;
+        private Node<K,V> next;
+
+        Node(int hash, K key, V value, Node<K,V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        @Override
+        public final String toString() {
+            return key + "=" + value;
+        }
+    }
+
     private int hash(K key) {
         return key == null ? 0 : key.hashCode();
     }
@@ -93,8 +93,8 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private void resize() {
         size = 0;
         Node<K, V>[] oldTable = table;
-        table = (Node<K, V>[]) new Node[table.length * RESIZE];
-        threshold = threshold * RESIZE;
+        table = (Node<K, V>[]) new Node[table.length * GROWTH_COEFFICIENT];
+        threshold = threshold * GROWTH_COEFFICIENT;
         for (Node<K, V> node : oldTable) {
             while (node != null) {
                 put(node.key, node.value);
