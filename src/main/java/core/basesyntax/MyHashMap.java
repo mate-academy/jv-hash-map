4f9a448c91b9e00a565076bc8de91bf2ce9a38c1
Kostiantyn Sacheiev <dynamofan1927@gmail.com>
2022-09-06 12:13:49 +0200

    Solution has been added

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -77,9 +77,9 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     class Node<K,V> {
-    private K key;
-    private V value;
-    private Node<K,V> next;
+        private K key;
+        private V value;
+        private Node<K,V> next;
 
         public Node(K key, V value, Node<K,V> next) {
             this.key = key;
