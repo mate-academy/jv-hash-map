ca6e0ab819ab44d4ebb23349177dfa9937a359c7
Volodymyr Sibiruk <arthes46@gmail.com>
2022-09-24 23:18:37 +0300

    added fixes after review: "size++" moved from method "put(K key, V value)" to method "add(K key, V value)", added "size = 0" to method "resize()", "size--" deleted; class "Node<K, V> is made private; all private methods moved down below public methods; fixed method "resize()"; fixed methods "add(K key, V value)" and "getValue(K key)" with Objects.equals(); deleted method "addNullKey(V value); created method "getIndex(K key).

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,7 +1,42 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    static class Node<K, V> {
+    private static final int INITIAL_CAPACITY = 1 << 4;
+    private static final float LOAD_FACTOR = 0.75f;
+    private int size;
+    private Node<K, V>[] table;
+
+    public MyHashMap() {
+        table = new Node[INITIAL_CAPACITY];
+    }
+
+    @Override
+    public void put(K key, V value) {
+        if (size > table.length * LOAD_FACTOR) {
+            resize();
+        }
+        add(key, value);
+    }
+
+    @Override
+    public V getValue(K key) {
+        int index = getIndex(key);
+        for (Node<K, V> node = table[index]; node != null; node = node.next) {
+            if (Objects.equals(key, node.key)) {
+                return node.value;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    private static class Node<K, V> {
         private int hash;
         private K key;
         private V value;
@@ -15,47 +50,33 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    private static final int INITIAL_CAPACITY = 1 << 4;
-    private static final float LOAD_FACTOR = 0.75f;
-    private int size;
-    private Node<K, V>[] table;
-
-    public MyHashMap() {
-        table = new Node[INITIAL_CAPACITY];
-    }
-
     private int hash(K key) {
         int h;
         return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
     }
 
+    private int getIndex(K key) {
+        return (table.length - 1) & hash(key);
+    }
+
     private void add(K key, V value) {
         Node<K, V> newNode = new Node<>(hash(key), key, value, null);
-        int number = ((table.length - 2) & hash(key)) + 1;
+        int number = getIndex(key);
         if (table[number] == null) {
             table[number] = newNode;
+            size++;
         } else {
             for (Node<K, V> node = table[number]; node != null; node = node.next) {
-                if (key != null && key.equals(node.key)) {
+                if (Objects.equals(key, node.key)) {
                     node.value = value;
-                    size--;
                     return;
                 }
                 if (node.next == null) {
                     node.next = newNode;
+                    size++;
                     break;
                 }
             }
-        }
-    }
-
-    private void addNullKey(V value) {
-        Node<K, V> newNullKeyNode = new Node<>(0, null, value, null);
-        if (table[0] == null) {
-            table[0] = newNullKeyNode;
-        } else {
-            table[0].value = value;
-            size--;
         }
     }
 
@@ -64,52 +85,12 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         int oldCapacity = oldTable.length;
         int newCapacity = oldCapacity * 2;
         table = (Node<K, V>[]) new Node[newCapacity];
-        for (Node<K, V> tableNode : oldTable) {
-            if (tableNode != null) {
-                add(tableNode.key, tableNode.value);
-            }
-            if (tableNode != null && tableNode.next != null) {
-                for (Node<K, V> node = tableNode.next; node != null; node = node.next) {
-                    add(node.key, node.value);
-                }
+        size = 0;
+        for (Node<K, V> currentNode : oldTable) {
+            while (currentNode != null) {
+                add(currentNode.key, currentNode.value);
+                currentNode = currentNode.next;
             }
         }
-    }
-
-    @Override
-    public void put(K key, V value) {
-        if (size > table.length * LOAD_FACTOR) {
-            resize();
-        }
-        if (key == null) {
-            addNullKey(value);
-        } else {
-            add(key, value);
-        }
-        size++;
-    }
-
-    @Override
-    public V getValue(K key) {
-        if (key == null) {
-            return table[0].value;
-        } else {
-            int number = ((table.length - 2) & hash(key)) + 1;
-            if (table[number] != null && table[number].next == null) {
-                return table[number].value;
-            } else {
-                for (Node<K, V> node = table[number]; node != null; node = node.next) {
-                    if (key.equals(node.key)) {
-                        return node.value;
-                    }
-                }
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public int getSize() {
-        return size;
     }
 }
