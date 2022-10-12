97ee11846fbb86ed2bcd01e3d31c4c5e6c13293a
Yura Hryniv <superyura444@gmail.com>
2022-10-12 14:29:51 +0300

    Rewrote my code and now it's working just fine

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,17 +1,104 @@
 package core.basesyntax;
 
-import java.util.Objects;
-
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int INITIAL_CAPACITY = 16;
     private static final float LOAD_FACTOR = 0.75f;
-    private Node<K,V>[] table = (Node<K, V>[]) new Node[INITIAL_CAPACITY];
+    private Node<K,V>[] table;
     private int capacity = INITIAL_CAPACITY;
     private int size = 0;
     private int threshold = (int) (capacity * LOAD_FACTOR);
 
-    private int getHash(Object key) {
-        return Math.abs(key == null ? 0 : key.hashCode() % capacity);
+    MyHashMap() {
+        table = (Node<K, V>[]) new Node[capacity];
+    }
+
+    private int getHash(Object key, int capacity) {
+        return key == null ? 0 : key.hashCode() & (capacity - 1);
+    }
+
+    @Override
+    public void put(K key, V value) {
+        if (size > threshold) {
+            resize();
+        }
+        int bucket = getHash(key,capacity);
+        Node<K,V> insertedElement = new Node<>(bucket, key, value, null);
+        Node<K,V> currentElement = table[bucket];
+
+        if (currentElement == null) {
+            table[bucket] = insertedElement;
+            size++;
+        } else {
+            while (currentElement.next != null) {
+                if (currentElement.key == insertedElement.key
+                        || currentElement.key != null
+                        && currentElement.key.equals(insertedElement.key)) {
+                    currentElement.value = insertedElement.value;
+                    return;
+                }
+                currentElement = currentElement.next;
+            }
+            if ((currentElement.key == insertedElement.key
+                    || currentElement.key != null
+                    && currentElement.key.equals(insertedElement.key))) {
+                currentElement.value = insertedElement.value;
+            } else {
+                currentElement.next = insertedElement;
+                size++;
+            }
+        }
+    }
+
+    @Override
+    public V getValue(K key) {
+        int hash = getHash(key,capacity);
+        Node<K, V> element = table[hash];
+        if (element == null) {
+            return null;
+        }
+        if (element.key == key || element.key != null && element.key.equals(key)) {
+            return table[hash].getValue();
+        } else {
+            while (element != null) {
+                if (element.key == key || element.key != null && element.key.equals(key)) {
+                    return element.getValue();
+                }
+                element = element.next;
+            }
+            return null;
+        }
+    }
+
+    private void resize() {
+        int newCapacity = capacity << 1;
+        threshold = threshold << 1;
+        Node<K,V> [] newTable = (Node<K, V>[]) new Node [newCapacity];
+        transfer(table, newTable);
+        table = newTable;
+        capacity = newCapacity;
+    }
+
+    private void transfer(Node<K,V> [] oldTable, Node<K,V> [] newTable) {
+        int index = 0;
+        while (index < oldTable.length) {
+            Node<K,V> oldNode = oldTable[index];
+            if (oldNode != null) {
+                oldTable[index] = null;
+                do {
+                    Node<K,V> next = oldNode.next;
+                    int hash = getHash(oldNode.key, newTable.length);
+                    oldNode.next = newTable[hash];
+                    newTable[hash] = oldNode;
+                    oldNode = next;
+                } while (oldNode != null);
+            }
+            index++;
+        }
+    }
+
+    @Override
+    public int getSize() {
+        return size;
     }
 
     private static class Node<K,V> {
@@ -31,10 +118,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             return value;
         }
 
-        public K getKey() {
-            return key;
-        }
-
         @Override
         public boolean equals(Object o) {
             if (this == o) {
@@ -44,9 +127,9 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                 return false;
             }
             Node<?, ?> node = (Node<?, ?>) o;
-            return hash == node.hash && Objects.equals(key, node.key)
-                    && Objects.equals(value, node.value)
-                    && Objects.equals(next, node.next);
+            return hash == node.hash && key == node.key || key != null && key.equals(node.key)
+                    && value == node.value || value != null && value.equals(node.value)
+                    && next == node.next || next != null && next.equals(node.next);
         }
 
         @Override
@@ -58,129 +141,5 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             return result;
         }
     }
-
-    private void putInResize(Node<K,V>[] newTable, Node<K,V> oldNode, int hash) {
-        boolean isReplaced = false;
-        Node<K,V> target;
-        Node<K,V> temp;
-
-        if (newTable[hash] != null && !newTable[hash].getKey().equals(oldNode.getKey())) {
-            target = newTable[hash];
-            if (target.next == null) {
-                target.next = oldNode;
-            } else {
-                while (target.next != null) {
-                    if (target.next.getKey().equals(oldNode.getKey())) {
-                        oldNode.next = target.next.next;
-                        target.next = oldNode;
-                        isReplaced = true;
-                        break;
-                    }
-                    if (!target.getKey().equals(oldNode.getKey())) {
-                        target = target.next;
-                    }
-                }
-                if (!isReplaced) {
-                    target.next = oldNode;
-                }
-            }
-        } else {
-            temp = oldNode;
-            newTable[hash] = temp;
-            newTable[hash].next = null;
-        }
-    }
-
-    private Node<K,V> [] resize() {
-        threshold = threshold * 2;
-        capacity = capacity * 2;
-        Node<K,V> [] newTable = (Node<K, V>[]) new Node[capacity];
-
-        int elementInOld = 0;
-        while (elementInOld < table.length) {
-            Node<K,V> oldNode = table[elementInOld];
-            if (oldNode != null && oldNode.next == null) {
-                Node<K,V> newNode = new Node<>(getHash(oldNode.getKey()),
-                        oldNode.getKey(), oldNode.getValue(), null);
-                //newTable[newNode.hash] = oldNode;
-                putInResize(newTable, oldNode, newNode.hash);
-            }
-            if (oldNode != null && oldNode.next != null) {
-                while (oldNode != null) {
-                    Node<K,V> next = oldNode.next;
-                    Node<K,V> newNode = new Node<>(getHash(oldNode.getKey()),
-                            oldNode.getKey(), oldNode.getValue(), null);
-                    //newTable[newNode.hash] = oldNode;
-                    putInResize(newTable, oldNode, newNode.hash);
-                    oldNode = next;
-                }
-            }
-            elementInOld++;
-        }
-        return newTable;
-    }
-
-    @Override
-    public void put(K key, V value) {
-        if (size > threshold) {
-            table = resize();
-        }
-        boolean isReplaced = false;
-        Node<K,V> element = new Node<>(getHash(key), key, value, null);
-        Node<K,V> target;
-
-        if (table[element.hash] != null && (table[element.hash].getKey() == element.getKey()
-                || table[element.hash].getKey() != null
-                && table[element.hash].getKey().equals(element.getKey()))) {
-            element.next = table[element.hash].next;
-            table[element.hash] = element;
-
-        } else if (table[element.hash] != null
-                && !table[element.hash].getKey().equals(element.getKey())) {
-            target = table[element.hash];
-
-            while (target.next != null) {
-                if (target.next.key == element.key
-                            || target.next.key != null && target.next.key.equals(element.key)) {
-                    element.next = target.next.next;
-                    target.next = element;
-                    isReplaced = true;
-                    break;
-                }
-                target = target.next;
-            }
-            if (!isReplaced) {
-                target.next = element;
-                size++;
-            }
-        } else {
-            table[element.hash] = element;
-            size++;
-        }
-    }
-
-    @Override
-    public V getValue(K key) {
-        Node<K, V> target = table[getHash(key)];
-        if (target == null) {
-            return null;
-        }
-        if (target.getKey() == key || target.getKey() != null && target.getKey().equals(key)) {
-            return table[getHash(key)].getValue();
-        } else {
-            while (target != null) {
-                if (target.getKey() == key || target.getKey() != null
-                        && target.getKey().equals(key)) {
-                    return target.getValue();
-                }
-                target = target.next;
-            }
-            return null;
-        }
-    }
-
-    @Override
-    public int getSize() {
-        return size;
-    }
 }
+
