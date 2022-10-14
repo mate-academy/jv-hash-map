fcfbb700ca8ccabf47d517e7e8fdf4d6563d64ac
Volodymyr Barasiuk <barmalej23@gmail.com>
2022-10-14 21:50:20 +0300

    all tests passed

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,16 +1,15 @@
 package core.basesyntax;
 
-import java.util.Map;
 import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
 
-    static final int DEFAULT_INITIAL_CAPACITY = 16;
-    static final float LOAD_FACTOR = 0.75f;
-    static int size;
-    static int capacity = DEFAULT_INITIAL_CAPACITY;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+    private int size;
+    private int capacity = DEFAULT_INITIAL_CAPACITY;
 
-    private final Entry<K, V>[] table;
+    private Entry<K, V>[] table;
 
     @SuppressWarnings("unchecked")
     public MyHashMap() {
@@ -18,9 +17,9 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private static class Entry<K, V> {
-        K key;
-        V value;
-        Entry<K, V> next;
+        private final K key;
+        private V value;
+        private Entry<K, V> next;
 
         Entry(K key, V value) {
             this.key = key;
@@ -39,24 +38,33 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             this.value = value;
         }
 
-        public final boolean equals(Object o) {
-            if (o == this)
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
                 return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
 
-            return o instanceof Map.Entry<?, ?> e
-                    && Objects.equals(key, e.getKey())
-                    && Objects.equals(value, e.getValue());
+            Entry<?, ?> entry = (Entry<?, ?>) o;
+
+            return Objects.equals(key, entry.key);
+        }
+
+        @Override
+        public int hashCode() {
+            return key != null ? key.hashCode() : 0;
         }
     }
 
-
     @Override
     public void put(K key, V value) {
+        if ((size + 1) > (capacity * LOAD_FACTOR)) {
+            resize();
+        }
         int hash = getIndexByTheKey(key);
         Entry<K, V> e = table[hash];
-        /*if (++size > (capacity * LOAD_FACTOR)) {
-            resize();
-        }*/
         if (e == null) {
             table[hash] = new Entry<>(key, value);
             size++;
@@ -70,7 +78,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                     e.setValue(value);
                     return;
                 }
-
                 e = e.next;
             }
             if ((e.getKey() != null) && (e.getKey().equals(key))) {
@@ -90,9 +97,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     public V getValue(K key) {
         int hash = getIndexByTheKey(key);
         Entry<K, V> e = table[hash];
-        /*if (key == null && table[0].getKey() == null) {
-            return table[0].getValue();
-        }*/
         if (e == null) {
             return null;
         }
@@ -114,57 +118,54 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    /*
     @SuppressWarnings("unchecked")
-    private Entry<K, V>[] resize() {
-        Entry<K, V>[]newTable = new Entry[capacity * 2];
-        capacity *= 2;
-
-    }
-
-    private void transfer(Entry<K, V>[]newTable) {
-        for (Entry<K,V> e: table) {
+    private void resize() {
+        Entry<K, V>[] newTable = new Entry[capacity * 2];
+        for (Entry<K, V> e : table) {
             if (e != null) {
                 while (e.next != null) {
-                    Entry<K, V> newEntry = new Entry<>(e.getKey(), e.getValue());
-                    int hash = getIndexByTheKey(newEntry.getKey());
-                    if (newTable[hash] == null) {
-                        newTable[hash] = newEntry;
-                    }
+                    transfer(e.getKey(), e.getValue(), newTable);
+                    e = e.next;
                 }
+                transfer(e.getKey(), e.getValue(), newTable);
             }
+        }
+        table = newTable;
+        capacity *= 2;
+    }
 
+    private void transfer(K key, V value, Entry<K, V>[] newTable) {
+        int hash = (key == null) ? 0 : (key.hashCode() % (capacity * 2));
+        hash = hash < 0 ? -hash : hash;
+        Entry<K, V> e = newTable[hash];
+        if (e == null) {
+            newTable[hash] = new Entry<>(key, value);
+        } else {
+            while (e.next != null) {
+                if ((e.getKey() != null) && (e.getKey().equals(key))) {
+                    e.setValue(value);
+                    return;
+                }
+                if (e.getKey() == null && key == null) {
+                    e.setValue(value);
+                    return;
+                }
+                e = e.next;
+            }
+            if ((e.getKey() != null) && (e.getKey().equals(key))) {
+                e.setValue(value);
+                return;
+            }
+            if (e.getKey() == null && key == null) {
+                e.setValue(value);
+                return;
+            }
+            e.next = new Entry<>(key, value);
         }
     }
-    */
+
     private int getIndexByTheKey(K key) {
         int hash = (key == null) ? 0 : (key.hashCode() % capacity);
         return hash < 0 ? -hash : hash;
-    }
-
-    public Entry<K, V> remove(K key) {
-        int hash = getIndexByTheKey(key);
-        Entry<K, V> e = table[hash];
-        if (e == null) {
-            return null;
-        }
-        if (e.getKey() == key) {
-            table[hash] = e.next;
-            e.next = null;
-            return e;
-        }
-        Entry<K, V> prev = e;
-        e = e.next;
-
-        while (e != null) {
-            if (e.getKey() == key) {
-                prev.next = e.next;
-                e.next = null;
-                return e;
-            }
-            prev = e;
-            e = e.next;
-        }
-        return null;
     }
 }
