4b47e4ce319293a9efcd33b589d5edcf8bb287a9
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2022-10-16 15:00:06 +0300

    Node class

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,6 +1,21 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private transient Node<K, V>[] table;
+
+    static class Node<K, V> {
+        final int hash;
+        K key;
+        V value;
+        Node<K, V> next;
+
+        public Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
 
     @Override
     public void put(K key, V value) {
