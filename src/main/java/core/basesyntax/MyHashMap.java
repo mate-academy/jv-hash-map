536770b8bcae83d37e21ac44e1eabdec66f73793
Zoreslav Todorenko <wzxzzx@gmail.com>
2022-10-18 13:57:17 +0300

    1.Raplaced capacity by elements.length 2.Deleted chek by hash(key)

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -6,18 +6,17 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
     private static final int RESIZE_INDEX = 2;
     private static final float DEFAULT_LOAD_FACTOR = 0.75F;
-    private Node<K,V>[] elements;
+    private Node[] elements;
     private int size;
-    private int capacity = DEFAULT_INITIAL_CAPACITY;
     private int threshold;
 
     @Override
     public void put(K key, V value) {
         resize();
         int hashValue = hash(key);
-        int index = getIndex(key, capacity);
+        int index = getIndex(key, elements.length);
         for (Node<K,V> node = elements[index]; node != null; node = node.next) {
-            if (node.hash == hashValue && Objects.equals(node.key, key)) {
+            if (Objects.equals(node.key, key)) {
                 node.value = value;
                 return;
             }
@@ -34,9 +33,9 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if (size == 0) {
             return null;
         }
-        int index = getIndex(key, capacity);
+        int index = getIndex(key, elements.length);
         for (Node<K, V> node = elements[index]; node != null; node = node.next) {
-            if (Objects.equals(node.key, key) && node.hash == hash(key)) {
+            if (Objects.equals(node.key, key)) {
                 return node.value;
             }
         }
@@ -50,7 +49,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private void putValue(K key, V value, int hashvalue, int index) {
         Node<K,V> nextNode = elements[index];
-        elements[index] = new Node<>(key, value, hashvalue, nextNode);
+        elements[index] = new Node<K,V>(key, value, hashvalue, nextNode);
         if (++size >= threshold) {
             resize();
         }
@@ -58,7 +57,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private int hash(K key) {
         int hashCode = key == null ? 0 : key.hashCode();
-        return (hashCode < 0) ? -hashCode : hashCode;
+        return hashCode < 0 ? -hashCode : hashCode;
     }
 
     private int getIndex(K key, int capacity) {
@@ -70,11 +69,10 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             elements = new Node[DEFAULT_INITIAL_CAPACITY];
             threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
         } else if (size >= threshold) {
-            Node<K,V>[] newElements = new Node[capacity * RESIZE_INDEX];
-            capacity *= RESIZE_INDEX;
+            Node[] newElements = new Node[elements.length * RESIZE_INDEX];
             transfer(newElements, elements);
             elements = newElements;
-            threshold = (int) (capacity * DEFAULT_LOAD_FACTOR);
+            threshold = (int) (elements.length * DEFAULT_LOAD_FACTOR);
         }
     }
 
@@ -84,7 +82,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             if (node != null) {
                 do {
                     Node<K, V> nextNoda = node.next;
-                    int index = getIndex(node.key, capacity);
+                    int index = getIndex(node.key, newElements.length);
                     node.next = newElements[index];
                     newElements[index] = node;
                     node = nextNoda;
