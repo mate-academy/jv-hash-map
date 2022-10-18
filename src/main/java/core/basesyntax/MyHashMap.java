a0763da4056f71fd4e339be08f1b51d8b1e3a399
Artem Kurinniy <artem.kyrinniy@gmail.com>
2022-10-18 18:03:46 +0300

    created my own hashmap

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -4,38 +4,33 @@ import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;
-    //private static final int MAXIMUM_CAPACITY = 1 << 30;
-    static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private static final int INCREASE_THE_LENGTH = 2;
     private Node<K,V>[] table = new Node[DEFAULT_INITIAL_CAPACITY];
     private int currentLength = table.length;
     private int size;
-    private Node<K, V> node;
-    Node<K, V> last = null;
+
     @Override
     public void put(K key, V value) {
         resizeIfNeed();
-        int index = Node.hashCode(key);
-        index = index >= 1 ? index : index * -1;
-        node = new Node<>(index, key, value, null);
-        Node<K, V> node1 = table[index];
+        int index = getIndex(key);
+        Node<K, V> node = new Node<>(index, key, value, null);
+        Node<K, V> currentNode;
+        index = index % currentLength;
+        currentNode = table[index];
         if (node.hash == 0) {
-            if (table[0] == null) {
-                table[0] = node;
-                size++;
-            } else {
-                table[0] = node;
-            }
+            setInNullBucket(node);
             return;
         }
-        index = index % currentLength;
         if (table[index] != null) {
             if (!table[index].key.equals(node.key)) {
-                if (table[index].next == null) {
-                    table[index].next = node;
-                    last = node;
-                } else {
-                    last.next = node;
-                    last = node;
+                while (true) {
+                    if (currentNode.next == null) {
+                        currentNode.next = node;
+                        size++;
+                        return;
+                    }
+                    currentNode = currentNode.next;
                 }
             } else {
                 table[index].value = node.value;
@@ -47,21 +42,15 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         size++;
     }
 
-
     @Override
     public V getValue(K key) {
-        int index = Node.hashCode(key) >= 1 ? Node.hashCode(key) : Node.hashCode(key) * -1;
-        index = index % currentLength;
-        Node<K,V> node = table[index];
-        if (node != null && node.next == null) {
-            return node.value;
-        } else {
-            while (node != null && node != null) {
-                if (Objects.equals(key, node.key)) {
-                    return node.value;
-                }
-                node = node.next;
+        int index = getIndex(key);
+        Node<K, V> node = table[index];
+        while (node != null) {
+            if (Objects.equals(key, node.key)) {
+                return node.value;
             }
+            node = node.next;
         }
         return null;
     }
@@ -70,40 +59,57 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     public int getSize() {
         return size;
     }
-    public void resizeIfNeed() {
-        if (size >= currentLength * DEFAULT_LOAD_FACTOR) {
-            Node<K,V>[] newTab = new Node[currentLength * 2];
+
+    private void resizeIfNeed() {
+        if (size == currentLength * DEFAULT_LOAD_FACTOR) {
             Node<K,V>[] oldTab = table;
-            table = newTab;
-            currentLength *= 2;
-            for (Node<K,V> nd : oldTab){
-                if (nd != null) {
-                    while (nd != null) {
-                        //System.out.println(nd.key + " "  + nd.value);
-                        put(nd.key, nd.value);
-                        nd = nd.next;
-                        size--;
+            table = new Node[currentLength *= INCREASE_THE_LENGTH];
+            size = 0;
+            for (Node<K,V> current : oldTab) {
+                if (current != null) {
+                    while (current != null) {
+                        put(current.key, current.value);
+                        current = current.next;
                     }
                 }
             }
         }
     }
 
-    static class Node<K, V> {
-    final int hash;
-    final K key;
-    V value;
-    Node<K,V> next;
+    private int getIndex(K key) {
+        int index = Node.hashCode(key);
+        index = (index >= 1 ? index : index * -1) % currentLength;
+        return index;
+    }
 
-    Node(int hash, K key, V value, Node<K,V> next) {
-        this.hash = hash;
-        this.key = key;
-        this.value = value;
-        this.next = next;
+    private void setInNullBucket(Node<K,V> node) {
+        if (table[0] == null) {
+            table[0] = node;
+            size++;
+        } else {
+            table[0] = node;
+        }
     }
-    public static int hashCode(Object key) {
-        return (key == null) ? 0 : key.hashCode() * 17 + 1;
-    }
-    public final String toString() { return key + "=" + value; }
+
+    static class Node<K, V> {
+        private final int hash;
+        private final K key;
+        private V value;
+        private Node<K,V> next;
+
+        Node(int hash, K key, V value, Node<K,V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        public static int hashCode(Object key) {
+            return (key == null) ? 0 : key.hashCode() * 17 + 1;
+        }
+
+        public final String toString() {
+            return key + "=" + value;
+        }
     }
 }
