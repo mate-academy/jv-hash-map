22f0269b49881fb57a1948ddb53b9fb6ee0236e7
Luka Lozovyk <lukalozovyk@gmail.com>
2022-10-19 14:30:11 +0300

    Local variable for keyHash created, naming improved.

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -6,21 +6,22 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final float LOAD_FACTOR = 0.75f;
     private Node<K, V>[] table;
-    private int loaded;
+    private int threshold;
     private int size;
 
     public MyHashMap() {
         this.table = new Node[DEFAULT_CAPACITY];
-        this.loaded = (int) (DEFAULT_CAPACITY * LOAD_FACTOR);
+        this.threshold = (int) (DEFAULT_CAPACITY * LOAD_FACTOR);
     }
 
     @Override
     public void put(K key, V value) {
         resizeTable();
-        Node<K,V> newNode = new Node<>(keyHash(key), key, value, null);
-        Node<K,V> bucketToPut = table[keyHash(key)];
+        int hash = keyHash(key);
+        Node<K,V> newNode = new Node<>(hash, key, value, null);
+        Node<K,V> bucketToPut = table[hash];
         if (bucketToPut == null) {
-            table[keyHash(key)] = newNode;
+            table[hash] = newNode;
         } else {
             while (bucketToPut != null) {
                 if (Objects.equals(bucketToPut.key, key)) {
@@ -39,7 +40,8 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public V getValue(K key) {
-        Node<K,V> bucketToFind = table[keyHash(key)];
+        int hash = keyHash(key);
+        Node<K,V> bucketToFind = table[hash];
         while (bucketToFind != null) {
             if (Objects.equals(bucketToFind.key, key)) {
                 return bucketToFind.value;
@@ -55,11 +57,11 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void resizeTable() {
-        if (loaded == size) {
+        if (threshold == size) {
             size = 0;
             Node<K,V>[] oldTable = table;
             table = new Node[table.length << 1];
-            loaded = loaded << 1;
+            threshold = threshold << 1;
             for (Node<K,V> nodes : oldTable) {
                 while (nodes != null) {
                     put(nodes.key, nodes.value);
