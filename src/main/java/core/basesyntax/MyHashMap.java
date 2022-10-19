337b23f4d3cfb098e6049f34e5a029d97c7bc56f
Anastasiia Romanova <ana.roma.vi@gmail.com>
2022-10-19 18:55:19 +0300

    changed put(), getValue() and resize() methods

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,34 +5,35 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final float LOAD_FACTOR = 0.75f;
-    private static final int PRIME_NUMBER = 11;
     private Node<K, V>[] table;
     private int threshold;
     private int size;
 
+    public MyHashMap() {
+        threshold = (int) (DEFAULT_CAPACITY * LOAD_FACTOR);
+        table = (Node<K, V>[]) new Node[DEFAULT_CAPACITY];
+    }
+
     @Override
     public void put(K key, V value) {
-        if (size == 0 && table == null) {
-            threshold = (int) (DEFAULT_CAPACITY * LOAD_FACTOR);
-            table = (Node<K, V>[]) new Node[DEFAULT_CAPACITY];
-        }
         Node<K, V> newNode = new Node<>(key, value, null);
         int index = bucketIndex(key);
-        if (table[index] == null) {
+        Node<K, V> node = table[index];
+        if (node == null) {
             table[index] = newNode;
-        } else if (Objects.equals(table[index].key, key)) {
-            table[index].value = value;
-            size--;
-        } else if (table[index].next == null) {
-            table[index].next = newNode;
+            size++;
         } else {
-            Node<K, V> node = table[index];
-            while (node.next != null) {
+            while (node != null) {
+                if (Objects.equals(node.key, key)) {
+                    node.value = value;
+                    break;
+                } else if (node.next == null) {
+                    node.next = newNode;
+                    size++;
+                }
                 node = node.next;
             }
-            node.next = newNode;
         }
-        size++;
         if (size == threshold) {
             resize();
         }
@@ -44,12 +45,13 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             return null;
         }
         int index = bucketIndex(key);
-        while (table[index] != null) {
-            if (table[index].key == key
-                    || key != null && table[index].key.equals(key)) {
-                return table[index].value;
+        Node<K, V> node = table[index];
+
+        while (node != null) {
+            if (Objects.equals(node.key, key)) {
+                return node.value;
             }
-            table[index] = table[index].next;
+            node = node.next;
         }
         return null;
     }
@@ -72,8 +74,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private int bucketIndex(K key) {
-        return key == null ? 0 : Math.abs((key.hashCode()
-                * PRIME_NUMBER) + PRIME_NUMBER) % table.length;
+        return key == null ? 0 : Math.abs(key.hashCode()) % table.length;
     }
 
     private void resize() {
@@ -83,12 +84,9 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         table = (Node<K, V>[]) new Node[newCapacity];
         size = 0;
         for (Node<K, V> bucket : oldTable) {
-            if (bucket != null) {
+            while (bucket != null) {
                 put(bucket.key, bucket.value);
-                while (bucket.next != null) {
-                    put(bucket.next.key, bucket.next.value);
-                    bucket = bucket.next;
-                }
+                bucket = bucket.next;
             }
         }
     }
