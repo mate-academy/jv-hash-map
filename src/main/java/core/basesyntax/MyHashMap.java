fb2aa82dda1ddb08b13bc5cc67337e511e5bd2fb
AndriySichniy <sichniy.andriy@gmail.com>
2022-11-03 22:31:25 +0200

    adding logic

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,97 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float LOAD_FACTORY = .75f;
+    private int size;
+    private int capacity;
+    private Node<K, V>[] dataTable;
+
+    public MyHashMap() {
+        capacity = DEFAULT_CAPACITY;
+        dataTable = new Node[capacity];
+    }
 
     @Override
     public void put(K key, V value) {
+        if (size + 1 > capacity * LOAD_FACTORY) {
+            resize();
+        }
+        if (checkKey(key, value)) {
+            putNewNode(getHash(key), key, value, dataTable);
+            ++size;
+        }
+    }
 
+    private boolean checkKey(K key, V value) {
+        Node<K, V> node = dataTable[getIndex(key)];
+        while (node != null) {
+            if (Objects.equals(node.key, key)) {
+                node.value = value;
+                return false;
+            }
+            node = node.next;
+        }
+        return true;
     }
 
     @Override
     public V getValue(K key) {
+        Node<K, V> node = dataTable[getIndex(key)];
+        while (node != null) {
+            if (Objects.equals(node.key, key)) {
+                return node.value;
+            }
+            node = node.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private int getHash(K key) {
+        return key != null ? key.hashCode() : 0;
+    }
+
+    private int getIndex(K key) {
+        int hash = getHash(key);
+        return (hash < 0 ? hash * -1 : hash) % capacity;
+    }
+
+    private void putNewNode(int hash, K key, V value, Node<K, V>[] data) {
+        int index = getIndex(key);
+        data[index] = new Node<>(hash, key, value,
+                (data[index] == null ? null : data[index]));
+    }
+
+    private void resize() {
+        capacity <<= 1;
+        Node<K, V>[] newDataTable = new Node[capacity];
+        for (Node<K, V> node: dataTable) {
+            while (node != null) {
+                putNewNode(node.hash, node.key, node.value, newDataTable);
+                node = node.next;
+            }
+        }
+        dataTable = newDataTable;
+    }
+
+    private static class Node<K, V> {
+        private final int hash;
+        private final K key;
+        private V value;
+        private final Node<K, V> next;
+
+        private Node(int hash, K k, V v, Node<K, V> next) {
+            this.hash = hash;
+            key = k;
+            value = v;
+            this.next = next;
+        }
     }
 }
