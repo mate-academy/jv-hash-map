5bcfffc12878ecb2976c42df77cf5a52d65707dc
sasha <radovietsoleksandr@gmail.com>
2022-12-02 14:34:12 +0200

    hashMap32

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,7 +5,9 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final float LOAD_FACTOR = 0.75f;
+    private static final int INCREASE_RATE = 2;
     private Node<K, V>[] tables;
+    private int dynamicSize;
     private int threshold;
     private int size;
 
@@ -16,6 +18,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public void put(K key, V value) {
+        dynamicSize = tables.length;
         Node<K, V> newNode;
         int index;
         if (size > threshold) {
@@ -27,12 +30,12 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
         if (tables[index] == null) {
             newNode = new Node<>(index, key, value, null);
-            tables[index(key)] = newNode;
+            tables[index] = newNode;
             size++;
             return;
         }
         newNode = new Node<>(index, key, value, null);
-        if (newNode.hash == tables[index].hash) {
+        if (index == tables[index].index) {
             Node<K, V> tmpTables = tables[index];
             while (tmpTables != null) {
                 if (newNode.key.equals(tmpTables.key)) {
@@ -49,6 +52,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public V getValue(K key) {
+        dynamicSize = tables.length;
         if (key == null) {
             key = (K) Integer.valueOf(0);
         }
@@ -70,48 +74,42 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private Node<K, V>[] resize() {
         Node<K, V>[] oldTable = tables;
-        Node<K, V>[] newTable = new Node[oldTable.length * 2];
-
+        Node<K, V>[] newTable = new Node[oldTable.length * INCREASE_RATE];
+        dynamicSize = newTable.length;
         int index;
+
         for (int i = 0; i < oldTable.length; ++i) {
             Node<K, V> tmp;
             if ((tmp = oldTable[i]) != null) {
                 oldTable[i] = null;
                 if (tmp.next == null) {
-                    index = tmp.key.hashCode() % newTable.length;
-                    if (index < 0) {
-                        index = index * (-1);
-                    }
+                    index = index(tmp.key);
                     newTable[index] = tmp;
                 }
                 Node<K, V> current = tmp;
                 while (current != null) {
-                    index = current.key.hashCode() % newTable.length;
-                    if (index < 0) {
-                        index = index * (-1);
-                    }
+                    index = index(current.key);
                     newTable[index] = tmp;
                     current = current.next;
                 }
             }
         }
-
         threshold = (int) (newTable.length * LOAD_FACTOR);
         return tables = newTable;
     }
 
     private int index(K key) {
-        return key == null ? 0 : Math.abs(key.hashCode() % tables.length);
+        return key == null ? 0 : Math.abs(key.hashCode() % dynamicSize);
     }
 
     private static class Node<K, V> {
-        private final int hash;
         private final K key;
+        private final int index;
         private Node<K, V> next;
         private V value;
 
-        public Node(int hash, K key, V value, Node<K, V> next) {
-            this.hash = hash;
+        public Node(int index, K key, V value, Node<K, V> next) {
+            this.index = index;
             this.key = key;
             this.value = value;
             this.next = next;
@@ -137,14 +135,14 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                 return false;
             }
             Node<?, ?> node = (Node<?, ?>) o;
-            return hash == node.hash && key.equals(node.key)
+            return index == node.index && key.equals(node.key)
                     && Objects.equals(value, node.value)
                     && Objects.equals(next, node.next);
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(hash, key, value, next);
+            return Objects.hash(index, key, value, next);
         }
     }
 }
