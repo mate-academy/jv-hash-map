1843738888d0448491f6b575b4339975cb9d84ca
Kateryna Takhtai <ktakhtay22@gmail.com>
2022-12-04 18:31:28 +0100

    HashMap task is done

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -80,7 +80,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         private V value;
         private Node<K,V> next;
 
-          Node(K key, V value) {
+        Node(K key, V value) {
             this.key = key;
             this.value = value;
             this.next = next;
