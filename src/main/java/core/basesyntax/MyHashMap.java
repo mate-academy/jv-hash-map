698aebbfb25e88df150637028ba970b6135deba6
Bogdan Pokhilko <bogdanp1701@gmail.com>
2022-12-23 15:43:46 +0200

    Constant values were created; Sub class Node was created with constructor; Method resize was created. Where we are increasing table array length by 2 and loop over table array to transfer values into new, increased array; Method getTableIndex was created, where we return 0 if our key equals 0, otherwise we are returning the remainder of key hashcode / table.length; Method put was updated. First we are checking if we need to resize our table array, then we are creating a new node and equals it to the node in the array and checking if this node is empty, then we are putting in a bucket first node. If there is already a node in the bucket, then we are checking if the node values are the same as the values we want to put, if so, we just break the loop and finish method, if not, then we are assigning the node we want to put into the end of node list in the bucket; increasing the size accordingly; Method getValue was improved. we are checking if size equals null, then return 0, otherwise crating an index of the key, then creating the new node with the value of the node in the array according to the index that we just calculated. while this new node is not equal null, we are checking if the new node equals to the key we want value from, we are returning the value of that node, otherwise there is no match; Method getSize was improved. We're simply returning our size value;

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,92 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private int size;
+    private Node<K, V>[] table;
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_INITIAL_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
-
+        if (size + 1 > table.length * DEFAULT_LOAD_FACTOR) {
+            resize();
+        }
+        int arrayIndexNode = getTableIndex(key);
+        Node<K, V> nodeByIndex = table[arrayIndexNode];
+        if (nodeByIndex == null) {
+            table[arrayIndexNode] = new Node<>(key, value, null);
+        } else {
+            while (nodeByIndex != null) {
+                if (Objects.equals(nodeByIndex.key, key)) {
+                    nodeByIndex.value = value;
+                    return;
+                }
+                if (nodeByIndex.next == null) {
+                    nodeByIndex.next = new Node<>(key, value,null);
+                    break;
+                }
+                nodeByIndex = nodeByIndex.next;
+            }
+        }
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        if (size == 0) {
+            return null;
+        }
+        int arrayIndexNode = getTableIndex(key);
+        Node<K, V> tempNode = table[arrayIndexNode];
+        while (tempNode != null) {
+            if (Objects.equals(tempNode.key, key)) {
+                return tempNode.value;
+            }
+            tempNode = tempNode.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private static class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        private Node(K key, V value, Node<K, V> next) {
+
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+    }
+
+    private void resize() {
+        Node<K, V>[] oldTab = table;
+        table = new Node[oldTab.length * 2];
+        size = 0;
+        for (int i = 0; i < oldTab.length; i++) {
+            Node<K, V> newNode = oldTab[i];
+            while (newNode != null) {
+                put(newNode.key, newNode.value);
+                newNode = newNode.next;
+            }
+        }
+
+    }
+
+    private int getTableIndex(K key) {
+        return (key == null) ? 0 : Math.abs(key.hashCode()) % table.length;
     }
 }
