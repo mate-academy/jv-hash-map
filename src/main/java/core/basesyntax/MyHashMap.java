94a0a8de943e37c5025c9295bad5a9497ae044e5
Vasyl Gladkyi <vasyl.gladky@gmail.com>
2022-12-25 20:02:32 +0200

    little improvements

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -6,26 +6,21 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
     private static final int ARRAY_GROW = 2;
-    private Node<K, V>[] table;
+    private Node<K, V>[] values;
     private int size;
 
     public MyHashMap() {
-        table = new Node[DEFAULT_INITIAL_CAPACITY];
+        values = new Node[DEFAULT_INITIAL_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
-        if (size > table.length * DEFAULT_LOAD_FACTOR) {
+        if (size > values.length * DEFAULT_LOAD_FACTOR) {
             resize();
         }
         int index = getIndex(key);
         Node<K, V> newNode = new Node<>(key, value);
-        if (table[index] == null) {
-            table[index] = newNode;
-            size++;
-            return;
-        }
-        for (Node<K,V> newTab = table[index]; newTab != null; newTab = newTab.next) {
+        for (Node<K,V> newTab = values[index]; newTab != null; newTab = newTab.next) {
             if (Objects.equals(key, newTab.key)) {
                 newTab.value = value;
                 return;
@@ -36,12 +31,16 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                 return;
             }
         }
+        if (values[index] == null) {
+            values[index] = newNode;
+            size++;
+        }
     }
 
     @Override
     public V getValue(K key) {
         int index = getIndex(key);
-        for (Node<K,V> node = table[index]; node != null; node = node.next) {
+        for (Node<K,V> node = values[index]; node != null; node = node.next) {
             if (Objects.equals(key, node.key)) {
                 return node.value;
             }
@@ -55,8 +54,8 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void resize() {
-        Node<K, V>[] oldTable = table;
-        table = new Node[table.length * ARRAY_GROW];
+        Node<K, V>[] oldTable = values;
+        values = new Node[values.length * ARRAY_GROW];
         size = 0;
         for (Node<K, V> newTab : oldTable) {
             while (newTab != null) {
@@ -78,6 +77,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private int getIndex(K key) {
-        return key == null ? 0 : Math.abs(key.hashCode()) % table.length;
+        return key == null ? 0 : Math.abs(key.hashCode()) % values.length;
     }
 }
