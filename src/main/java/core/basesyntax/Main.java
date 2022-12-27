162f116b3fb08b016401af2d59bb09a7f02c5938
Denis Kalyakin <denis.kalyakin999@gmail.com>
2022-12-27 23:07:38 +0200

    in process 2

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,35 +1,36 @@
 package core.basesyntax;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        //System.out.println(getNewCapacity());
-       /** MyHashMap<Integer, String> myHashMap = new MyHashMap<>();
+        Map<Integer, String> map = new HashMap<>();
+        MyHashMap<Integer, String> myHashMap = new MyHashMap<>();
         myHashMap.put(30, "qwerty");
-        myHashMap.put(23, "asdfgh");
-        myHashMap.put(30, "qwe");
-        myHashMap.put(14, "zxcvbn");
-        myHashMap.put(14, "xvb");
+        myHashMap.put(16, "asdfgh");
+        //myHashMap.put(30, "qwe");
+        //myHashMap.put(14, "zxcvbn");
+        //myHashMap.put(14, "xvb");
         myHashMap.put(null, "nulll");
-        myHashMap.put(null, "NULLL"); */
+        //myHashMap.put(null, "NULLL");
 
-        Bus firstBus = new Bus("FirstBus", "white");
-        Bus secondBus = new Bus("SecondBus", "white");
-        Bus thirdBus = new Bus("ThirdBus", "grey");
-
-        MyMap<Bus, Integer> myHashMap1 = new MyHashMap<>();
-        myHashMap1.put(firstBus, 3);
-        myHashMap1.put(null, 4);
-        myHashMap1.put(secondBus, 5);
-        myHashMap1.put(null, 10);
-        myHashMap1.put(thirdBus, 1);
-
-        //myHashMap.print();
-        System.out.println("Size: " + myHashMap1.getSize());
+        myHashMap.print();
+        System.out.println("Size: " + myHashMap.getSize());
         System.out.println("==============================");
-        System.out.println(myHashMap1.getValue(null));
-    }
-    private static int getNewCapacity() {
-        int[] arrayInt = new int[16];
-        return arrayInt.length << 1;
+        myHashMap.put(1, "q1");
+        myHashMap.put(2, "q2");
+        myHashMap.put(3, "q3");
+        myHashMap.put(4, "q4");
+        myHashMap.put(5, "q5");
+        myHashMap.put(6, "q6");
+        myHashMap.put(7, "q7");
+        myHashMap.put(8, "q8");
+        myHashMap.put(9, "w9");
+        myHashMap.put(10, "www10");
+        //myHashMap.put(11, "w");
+
+        myHashMap.print();
+        System.out.println("Size: " + myHashMap.getSize());
     }
 }
--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,25 +1,33 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
 
-    private static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;
+    private static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;  //16
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
     private Node<K, V>[] table;
     private int size;
 
+    private int threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
+
     public MyHashMap() {
+
         table = new Node[DEFAULT_INITIAL_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
+        if (size + 1 > threshold) {
+            resize();
+        }
         Node<K, V> currentNode = table[getBucketNumber(key)];
         if (currentNode == null) {
             table[getBucketNumber(key)] = new Node<>(hash(key), key, value, null);
             size++;
         } else {
             while (true) {
-                if (key == null || key.equals(currentNode.key)) {
+                if (key == null && currentNode.key == null || Objects.equals(key, currentNode.key)) {
                     currentNode.value = value;
                     return;
                 } else if (currentNode.next == null) {
@@ -36,7 +44,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     public V getValue(K key) {
         Node<K, V> currentNode = table[getBucketNumber(key)];
         while (currentNode != null) {
-            if (key == null || currentNode.key.equals(key)) {
+            if (key == null && currentNode.key == null || Objects.equals(key, currentNode.key)) {
                 return currentNode.value;
             }
             currentNode = currentNode.next;
@@ -54,6 +62,29 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void resize() {
+        Node<K, V>[] oldTable = table;
+        int oldCapacity = oldTable.length;
+        int newCapacity = oldCapacity << 1;
+        threshold = threshold << 1;
+        Node<K, V>[] newTable = (Node<K, V>[]) new Node[newCapacity];
+        table = newTable;
+        size = 0;
+        for (int i = 0; i < oldTable.length; i++) {
+            Node<K, V> currentNode = oldTable[i];
+            if (currentNode != null) {
+                if (currentNode.next == null) {
+                    put(currentNode.key, currentNode.value);
+                } else {
+                    while (currentNode != null) {
+                        put(currentNode.key, currentNode.value);
+                        currentNode = currentNode.next;
+                    }
+                }
+            }
+        }
+    }
+
+    private void transfer() {
 
     }
 
@@ -62,15 +93,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             return hash(key) % table.length * -1;
         }
         return hash(key) % table.length;
-    }
-
-    private float getThreshold() {
-        return table.length * DEFAULT_LOAD_FACTOR;
-    }
-
-    private Node<K, V>[] expandTable() {
-        Node<K, V>[] expandTable = new Node[table.length << 1];
-        return expandTable;
     }
 
     ///////////// DELETE PRINT ////////////////////
