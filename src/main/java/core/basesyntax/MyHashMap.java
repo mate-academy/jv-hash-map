d1bf420ac4d7042e246e1f36825038efc1896825
Gleb Shevchenko <glieb.shevchenko@gmail.com>
2022-12-29 16:44:10 +0200

    Maven checked

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,34 +5,31 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final double DEFAULT_LOADFACTOR = 0.75d;
-    private int currentCapacity;
-    private int currentThreshold;
     private int size;
     private Node<K, V>[] elements;
 
     public MyHashMap() {
         this.elements = new Node[DEFAULT_CAPACITY];
-        currentCapacity = DEFAULT_CAPACITY;
-        currentThreshold = (int) (currentCapacity * DEFAULT_LOADFACTOR);
     }
 
     @Override
     public void put(K key, V value) {
         Node<K, V> node = new Node<>(key, value);
         int index = hash(node.key);
-        Node<K, V> temp = elements[index];
-        if (size > currentThreshold) {
+        if (size > elements.length * DEFAULT_LOADFACTOR) {
             resize();
         }
-        if (temp == null) {
+        if (elements[index] == null) {
             elements[index] = node;
             size++;
         } else {
+            Node<K, V> temp = elements[index];
             while (temp != null) {
                 if (Objects.equals(node.key, temp.key)) {
-                    temp.value = node.value;
+                    temp.value = value;
                     return;
-                } else if (temp.next == null) {
+                }
+                if (temp.next == null) {
                     temp.next = node;
                     size++;
                     return;
@@ -74,20 +71,19 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private int hash(K key) {
         int h;
-        return (key == null) ? 0 : Math.abs(h = key.hashCode()) % currentCapacity;
+        return (key == null) ? 0 : Math.abs(h = key.hashCode()) % elements.length;
     }
 
-    private void resize() {
-        currentCapacity = currentCapacity * 2;
-        currentThreshold = currentThreshold * 2;
-        Node<K, V>[] temp = elements;
-        elements = (Node<K, V>[]) new Node[currentCapacity];
+    private Node<K, V>[] resize() {
+        Node<K, V>[] oldElements = elements;
+        elements = new Node[elements.length * 2];
         size = 0;
-        for (Node<K, V> node : temp) {
+        for (Node<K, V> node : oldElements) {
             while (node != null) {
                 put(node.key, node.value);
                 node = node.next;
             }
         }
+        return elements;
     }
 }
