8e55f23f62a1fda480d001d80e38f299ddb52618
Denis Pugovkin <den.pugovkin@gmail.com>
2023-01-02 15:32:11 +0200

    1. Private method put was deleted. 2. Resize method was remade.

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -3,24 +3,18 @@ package core.basesyntax;
 import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    private final int initialCapacity = 16;
-    private final double loadFactor = 0.75;
-    private Node<K, V>[] table = new Node[initialCapacity];
+    private static final int INITIAL_CAPACITY = 16;
+    private static final double LOAD_FACTOR = 0.75;
+    private Node<K, V>[] values = new Node[INITIAL_CAPACITY];
     private int size;
-    private int capacity = initialCapacity;
 
     @Override
     public void put(K key, V value) {
-        put(key, value,table);
-
-    }
-
-    private void put(K key, V value, Node<K, V>[] table) {
-        Node<K, V> node = new Node<>(key, value, null);
+        Node<K, V> node = new Node<>(key, value);
         int bucketId = getBucketId(key);
-        Node<K, V> nodeTemp = table[bucketId];
+        Node<K, V> nodeTemp = values[bucketId];
         if (nodeTemp == null) {
-            table[bucketId] = node;
+            values[bucketId] = node;
             size++;
             return;
         }
@@ -35,15 +29,15 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             nodeTemp = nodeTemp.next;
         }
         nodeTemp.next = node;
-        if (++size >= capacity * loadFactor) {
+        if (++size >= values.length * LOAD_FACTOR) {
             resize();
         }
     }
 
     @Override
     public V getValue(K key) {
-        int bucket = getBucketId(key);
-        Node<K, V> nodeTemp = table[bucket];
+        int index = getBucketId(key);
+        Node<K, V> nodeTemp = values[index];
         while (nodeTemp != null) {
             if (Objects.equals(nodeTemp.key,key)) {
                 return nodeTemp.value;
@@ -63,30 +57,26 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         private V value;
         private Node<K, V> next;
 
-        private Node(K key, V value, Node<K, V> next) {
+        private Node(K key, V value) {
             this.key = key;
             this.value = value;
-            this.next = next;
-
         }
     }
 
     private void resize() {
-        capacity *= 2;
         size = 0;
-        Node<K, V>[] nodes = new Node[capacity];
-        for (Node element : table) {
+        Node<K, V>[] oldBuckets = values;
+        values = new Node[values.length * 2];
+        for (Node element : oldBuckets) {
             Node<K, V> nodeTemp = element;
             while (nodeTemp != null) {
-                put(nodeTemp.key, nodeTemp.value,nodes);
+                put(nodeTemp.key, nodeTemp.value);
                 nodeTemp = nodeTemp.next;
             }
         }
-        table = nodes;
     }
 
     private int getBucketId(K key) {
-        return key == null ? 0 : Math.abs(key.hashCode() % capacity);
+        return key == null ? 0 : Math.abs(key.hashCode() % values.length);
     }
-
 }
