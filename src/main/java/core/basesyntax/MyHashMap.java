03be108cfe32e4e2d9d9780d304a93037d312378
Vladyslav Fesenko <vlad.11.fesenko@gmail.com>
2023-01-07 20:00:23 +0200

    comments added

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -29,12 +29,13 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public V getValue(K key) {
+        // check for null table
         if (table == null) {
             return null;
         }
-
+        // calculate index based on provided key
         int index = calculateIndex(key, table);
-
+        // check calculated index for collision
         for (Node<K,V> node = table[index]; node != null; node = node.next) {
             if (node.key != null && node.key.equals(key) || node.key == key) {
                 return node.value;
@@ -50,16 +51,16 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void checkTable() {
-        // check if the basket is null
+        // check if the bucket is null
         if (table == null) {
             // create new array of nodes with default capacity
             table = new Node[DEFAULT_INITIAL_CAPACITY];
             // calculate initial threshold
             threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
         }
-        // check if the basket has more than 25% of free space available
+        // check if the bucket has more than 25% of free space available
         if (size + 1 > threshold) {
-            // resize the basket if there is less than 25% of free space available
+            // resize the bucket if there is less than 25% of free space available
             resize();
         }
     }
@@ -70,55 +71,63 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         int newCapacity = currentCapacity << 1;
         // copy current data into a separate variable
         Node<K, V>[] oldTable = table;
-        // create a new basket
+        // create a new bucket
         Node<K, V>[] newTable = new Node[newCapacity];
-        // move old data to the new basket
+        // move old data to the new bucket
         transform(oldTable, newTable);
         // calculate new threshold
         threshold = (int) (DEFAULT_LOAD_FACTOR * newCapacity);
     }
 
     private void transform(Node<K, V>[] fromTable, Node<K, V>[] toTable) {
+        // loop over all the elements in bucket
         for (Node<K, V> tableNode : fromTable) {
+            // check if element is null
             if (tableNode == null) {
                 continue;
             }
-
+            // move node to new bucket
             do {
                 insertValue(tableNode.key, tableNode.value, toTable, true);
                 tableNode = tableNode.next;
             } while (tableNode != null);
         }
-
+        // assign new bucket to hashmap
         table = toTable;
     }
 
-    private int calculateIndex(K key, Node<K, V>[] basket) {
+    private int calculateIndex(K key, Node<K, V>[] bucket) {
+        // check for null
         if (key == null) {
             return 0;
         }
+        // calculate hashCode
         int hash = key.hashCode();
-        return (hash < 0) ? -hash % basket.length : hash % basket.length;
+        return (hash < 0) ? -hash % bucket.length : hash % bucket.length;
     }
 
     private void insertValue(K key, V value, Node<K, V>[] toTable, boolean transformation) {
+        // calculate index for provided key
         int index = calculateIndex(key, toTable);
+        // create new Node with provided key and value
         Node<K, V> newNode = new Node<>(key, value);
-
+        // check if there is no element stored at the same index
         if (toTable[index] == null) {
+            // insert the new node at calculated index
             toTable[index] = newNode;
             if (!transformation) {
                 size++;
             }
             return;
         }
-
+        // in case of collision - check key for every node stored at the same index
         for (Node<K,V> node = toTable[index]; node != null; node = node.next) {
             if (node.key != null && node.key.equals(newNode.key) || node.key == newNode.key) {
+                // update value of the node, if the same key is found
                 node.value = value;
                 return;
             }
-
+            // if there are the same keys - link new node to the end of linked list, stored at calculated index
             if (node.next == null) {
                 node.next = newNode;
                 if (!transformation) {
