98e7821eb55da49a501d828677ef0030a1d408ee
Stepan Peknyi <stepan_peknyy@ukr.net>
2023-01-09 20:36:29 +0200

    Reworked implementation of MyHashMap 1) Method put resizes hashMap if needed. Chooses number of a bucket, if the bucket has this key, just rewrite, else - puts new node. 2) Method get chooses number of a bucket, if the bucket has this key - returns it, if not - returns null. 3) Method resize increases capacity of hashMap twice when 75% is filled and copies all nodes to a new bigger hashMap

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,44 +1,68 @@
 package core.basesyntax;
 
-import java.util.Arrays;
+import static java.lang.Math.abs;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final double LOAD_FACTOR = 0.75;
     private int size;
-    private Node<K, V>[] values;
+    private Node<K, V>[] table;
 
     public MyHashMap() {
-        values = new Node[DEFAULT_CAPACITY];
+        table = new Node[DEFAULT_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
-        boolean put = true;
-        for (int i = 0; i < size; i++) {
-            if (values[i].key != null && values[i].key.equals(key)
-                    || key == null && values[i].key == null) {
-                values[i].value = value;
-                put = false;
+        resize();
+        int bucketNumber = getBucketNumber(key);
+        Node<K, V> newNode = new Node<>(key, value);
+        Node<K, V> currentNode = table[bucketNumber];
+        if (table[bucketNumber] == null) {
+            table[bucketNumber] = newNode;
+            size++;
+        } else {
+            while (currentNode != null) {
+                if (currentNode.key == null && key == null
+                        || currentNode.key != null && currentNode.key.equals(key)) {
+                    currentNode.value = value;
+                    return;
+                }
+                currentNode = currentNode.next;
             }
+            newNode.next = table[bucketNumber].next;
+            table[bucketNumber].next = newNode;
+            size++;
         }
-        if (put) {
-            if (size >= LOAD_FACTOR * values.length) {
-                int newSize = values.length * 2;
-                values = Arrays.copyOf(values, newSize);
-            }
-            values[size++] = new Node<>(key, value);
-        }
+    }
 
+    private void resize() {
+        if (size >= LOAD_FACTOR * table.length) {
+            Node<K, V>[] oldTable = table;
+            table = new Node[oldTable.length * 2];
+            size = 0;
+            for (Node<K, V> node: oldTable) {
+                while (node != null) {
+                    put(node.key, node.value);
+                    node = node.next;
+                }
+            }
+        }
     }
 
     @Override
     public V getValue(K key) {
-        for (int i = 0; i < size; i++) {
-            if (values[i].key != null && values[i].key.equals(key)
-                    || values[i].key == null && key == null) {
-                return values[i].value;
+        int bucketNumber = getBucketNumber(key);
+        if (table[bucketNumber] == null) {
+            return null;
+        }
+        Node node = table[bucketNumber];
+        while (node != null) {
+            if (node.key == null && key == null
+                    || node.key != null && node.key.equals(key)) {
+                return (V) node.value;
             }
+            node = node.next;
         }
         return null;
     }
@@ -51,10 +75,24 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static class Node<K, V> {
         private final K key;
         private V value;
+        private Node<K, V> next;
 
         private Node(K key, V value) {
             this.key = key;
             this.value = value;
         }
+
+        @Override
+        public int hashCode() {
+            return super.hashCode();
+        }
+    }
+
+    private int getBucketNumber(K key) {
+        int bucketNumber = 0;
+        if (key != null) {
+            bucketNumber = abs(key.hashCode() % table.length);
+        }
+        return bucketNumber;
     }
 }
