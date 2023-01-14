12a2cc368a973e5b935a8f8e703fe411b260601d
iskamele <iskamele@gmail.com>
2023-01-14 20:48:32 +0200

    Cosmetic changes, transfer of putValue() logic to put(), table initialization in the constructor, and removal of unnecessary getters and setters.

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -6,33 +6,47 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
-    private Node<K, V>[] hashTable;
-    private int threshold = DEFAULT_INITIAL_CAPACITY * (int) DEFAULT_LOAD_FACTOR;
+    private Node<K, V>[] table;
+    private int threshold;
     private int size;
 
     public MyHashMap() {
+        table = new Node[DEFAULT_INITIAL_CAPACITY];
+        threshold = (int) DEFAULT_LOAD_FACTOR * DEFAULT_INITIAL_CAPACITY;
     }
 
     @Override
     public void put(K key, V value) {
-        if (size > threshold || hashTable == null) {
+        if (size > threshold) {
             resize();
         }
-        putValue(indexFromHash(key), key, value);
-        size++;
+        int index = indexFromHash(key);
+        if (table[index] == null) {
+            table[index] = new Node<>(key, value);
+            size++;
+        } else {
+            for (Node<K, V> node = table[index]; node != null; node = node.next) {
+                if (Objects.equals(key, node.key)) {
+                    node.value = value;
+                    return;
+                }
+                if (node.next == null) {
+                    node.next = new Node<>(key, value);
+                    size++;
+                }
+            }
+        }
     }
 
     @Override
     public V getValue(K key) {
-        if (hashTable != null) {
-            int index = indexFromHash(key);
-            Node<K, V> currentNode = hashTable[index];
-            while (currentNode != null) {
-                if (Objects.equals(key, currentNode.getKey())) {
-                    return currentNode.getValue();
-                }
-                currentNode = currentNode.next;
+        int index = indexFromHash(key);
+        Node<K, V> currentNode = table[index];
+        while (currentNode != null) {
+            if (Objects.equals(key, currentNode.key)) {
+                return currentNode.value;
             }
+            currentNode = currentNode.next;
         }
         return null;
     }
@@ -51,48 +65,14 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             this.key = key;
             this.value = value;
         }
-
-        public K getKey() {
-            return key;
-        }
-
-        public V getValue() {
-            return value;
-        }
-
-        public void setValue(V value) {
-            this.value = value;
-        }
-    }
-
-    private void putValue(int index, K key, V value) {
-        if (hashTable[index] == null) {
-            hashTable[index] = new Node<>(key, value);
-        } else {
-            Node<K, V> currentNode = hashTable[index];
-            while (currentNode.next != null) {
-                if (Objects.equals(key, currentNode.getKey())) {
-                    break;
-                }
-                currentNode = currentNode.next;
-            }
-            if (Objects.equals(key, currentNode.getKey())) {
-                currentNode.setValue(value);
-                size--;
-            } else {
-                currentNode.next = new Node<>(key, value);
-            }
-        }
     }
 
     private void resize() {
-        if (hashTable == null) {
-            hashTable = new Node[DEFAULT_INITIAL_CAPACITY];
-        }
-        int newCapacity = hashTable.length << 1;
+        size = 0;
+        int newCapacity = table.length << 1;
         threshold = (int) (DEFAULT_LOAD_FACTOR * newCapacity);
-        Node<K, V>[] oldHashTable = hashTable;
-        hashTable = new Node[newCapacity];
+        Node<K, V>[] oldHashTable = table;
+        table = new Node[newCapacity];
         transfer(oldHashTable);
     }
 
@@ -100,13 +80,13 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         for (Node<K, V> node : oldHashTable) {
             Node<K, V> newNode = node;
             while (newNode != null) {
-                putValue(indexFromHash(newNode.getKey()), newNode.getKey(), newNode.getValue());
+                put(newNode.key, newNode.value);
                 newNode = newNode.next;
             }
         }
     }
 
     private int indexFromHash(K key) {
-        return key == null ? 0 : Math.abs(key.hashCode() % hashTable.length);
+        return key == null ? 0 : Math.abs(key.hashCode() % table.length);
     }
 }
