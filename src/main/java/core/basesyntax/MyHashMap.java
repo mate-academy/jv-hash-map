91feae7ae1063353c755b1196d38d4c4f254ada6
Mr.GvvD <mr.gvvd@gmail.com>
2023-01-25 22:38:13 +0200

    jv-hash-map solution was created. There were implemented override methods with a few new methods.

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,89 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_SIZE = 16;
+    private static final int RELOAD_INDEX = 2;
+    private static final float RESIZE_COEFFICIENT = 0.75f;
+    private Node<K, V>[] buckets;
+    private int currentLoad;
+    private int size;
+
+    public MyHashMap() {
+        buckets = new Node[DEFAULT_SIZE];
+        currentLoad = (int) (DEFAULT_SIZE * RESIZE_COEFFICIENT);
+    }
 
     @Override
     public void put(K key, V value) {
-
+        resize();
+        int index = getIndex(key);
+        Node<K, V> newElement = new Node<>(key, value, null);
+        Node<K, V> element = buckets[index];
+        if (element == null) {
+            buckets[index] = newElement;
+        }
+        while (element != null) {
+            if (element.key == key || element.key != null && element.key.equals(key)) {
+                element.value = value;
+                return;
+            }
+            if (element.next == null) {
+                element.next = newElement;
+                break;
+            }
+            element = element.next;
+        }
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        int index = getIndex(key);
+        if (buckets[index] != null) {
+            Node<K, V> node = buckets[index];
+            while (node != null) {
+                if (node.key == key || node.key != null && node.key.equals(key)) {
+                    return node.value;
+                }
+                node = node.next;
+            }
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private void resize() {
+        if (currentLoad == size) {
+            size = 0;
+            Node<K, V>[] oldBuckets = buckets;
+            buckets = new Node[buckets.length * RELOAD_INDEX];
+            currentLoad *= RELOAD_INDEX;
+            for (Node<K, V> node : oldBuckets) {
+                while (node != null) {
+                    put(node.key, node.value);
+                    node = node.next;
+                }
+            }
+        }
+    }
+
+    public int getIndex(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode()) % buckets.length;
+    }
+
+    private class Node<K, V> {
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
