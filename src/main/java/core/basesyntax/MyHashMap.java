9c5a08b74e6c1a763b77e1b751d8314f41c32387
Igor Puhach <igorpuhach21@gmail.com>
2023-01-27 12:12:40 +0200

    I know there is a default_capacity error, but I can't find it

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,101 @@
 package core.basesyntax;
 
+import static java.lang.Math.abs;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 32;
+    private static final double LOAD_FACTOR = 0.75;
+    private int size;
+    private Node<K,V>[] table;
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
 
+        int number = getNubmer(key);
+        Node<K,V> newNode = new Node<>(key,value);
+        Node<K,V> currentNode = table[number];
+        if (table[number] == null) {
+            table[number] = newNode;
+            size++;
+        } else {
+            while (currentNode != null) {
+                if (currentNode.key == null
+                        && key == null || currentNode.key != null
+                        && currentNode.key.equals(key)) {
+                    currentNode.value = value;
+                    return;
+                }
+                currentNode = currentNode.next;
+            }
+            newNode.next = table[number].next;
+            table[number].next = newNode;
+            size++;
+        }
     }
 
     @Override
     public V getValue(K key) {
+        int number = getNubmer(key);
+        if (table[number] == null) {
+            return null;
+        }
+        Node newNode = table[number];
+        while (newNode != null) {
+            if (newNode.key == null && key == null
+                    || newNode.key != null
+                    && newNode.key.equals(key)) {
+                return (V) newNode.value;
+            }
+            newNode = newNode.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+
+        return size;
     }
+
+    @Override
+    public int hashCode() {
+
+        return super.hashCode();
+    }
+
+    private static class Node<K,V> {
+        private K key;
+        private V value;
+        private Node<K,V> next;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private void resize() {
+        Node<K, V>[] oldTable = table;
+        table = new Node[oldTable.length * 2];
+        size = 0;
+        for (Node<K, V> node : oldTable) {
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.next;
+            }
+        }
+    }
+
+    private int getNubmer(K key) {
+        int number = 0;
+        if (key != null) {
+            number = abs(key.hashCode() % table.length);
+        }
+        return number;
+    }
+
 }
