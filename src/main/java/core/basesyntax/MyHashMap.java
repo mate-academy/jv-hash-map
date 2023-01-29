0bf9dea744cc3f9ee9c4558731cf55fa78674897
Igor Puhach <igorpuhach21@gmail.com>
2023-01-29 10:54:37 +0200

    fixed bugs

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -3,8 +3,9 @@ package core.basesyntax;
 import static java.lang.Math.abs;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    private static final int DEFAULT_CAPACITY = 32;
+    private static final int DEFAULT_CAPACITY = 16;
     private static final double LOAD_FACTOR = 0.75;
+    private static final int GROW_FACTOR = 2;
     private int size;
     private Node<K,V>[] table;
 
@@ -14,10 +15,10 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public void put(K key, V value) {
-
-        int number = getNubmer(key);
-        Node<K,V> newNode = new Node<>(key,value);
-        Node<K,V> currentNode = table[number];
+        resize();
+        int number = getNumber(key);
+        Node<K, V> newNode = new Node<>(key, value);
+        Node<K, V> currentNode = table[number];
         if (table[number] == null) {
             table[number] = newNode;
             size++;
@@ -39,16 +40,16 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public V getValue(K key) {
-        int number = getNubmer(key);
+        int number = getNumber(key);
         if (table[number] == null) {
             return null;
         }
-        Node newNode = table[number];
+        Node<K,V> newNode = table[number];
         while (newNode != null) {
             if (newNode.key == null && key == null
                     || newNode.key != null
                     && newNode.key.equals(key)) {
-                return (V) newNode.value;
+                return newNode.value;
             }
             newNode = newNode.next;
         }
@@ -57,7 +58,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public int getSize() {
-
         return size;
     }
 
@@ -68,19 +68,22 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private static class Node<K,V> {
-        private K key;
+        private final K key;
         private V value;
         private Node<K,V> next;
 
-        public Node(K key, V value) {
+        private Node(K key, V value) {
             this.key = key;
             this.value = value;
         }
     }
 
     private void resize() {
+        if (size < table.length * LOAD_FACTOR) {
+            return;
+        }
         Node<K, V>[] oldTable = table;
-        table = new Node[oldTable.length * 2];
+        table = new Node[oldTable.length * GROW_FACTOR];
         size = 0;
         for (Node<K, V> node : oldTable) {
             while (node != null) {
@@ -90,7 +93,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    private int getNubmer(K key) {
+    private int getNumber(K key) {
         int number = 0;
         if (key != null) {
             number = abs(key.hashCode() % table.length);
