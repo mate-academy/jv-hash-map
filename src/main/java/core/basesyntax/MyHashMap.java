e72c7d36f5249c8802113248b71c3c34ef299d55
Artem Bordichuk <artyombordichuk@gmail.com>
2023-02-05 22:25:07 +0200

    Commit 3: Initialization of size filed in MyHasMap() constructor was removed.

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -13,7 +13,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     public MyHashMap() {
         this.table = (Node<K,V>[]) new Node[DEFAULT_INITIAL_CAPACITY];
         this.threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
-        this.size = 0;
     }
 
     @Override
