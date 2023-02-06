e720c52c25efc6f6a96ed7fe0b6ce5a1ef4b001e
Mik911 <911Sokol@gmail.com>
2023-02-06 11:20:26 +0200

    Good afternoon. For the first check. jv-hash-map 06/02/23

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,105 @@
 package core.basesyntax;
 
+import static java.lang.Math.abs;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final double LOAD_FACTOR = 0.75;
+    private static final int RESIZE_FACTOR = 2;
+    private int size;
+    private Node<K, V>[] table;
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
+        resize();
+        putInSize(key, value);
+    }
 
+    private void putInSize(K key, V value) {
+        int bucketNumber = getBucketNumber(key);
+        Node<K, V> newNode = new Node<>(key, value);
+        Node<K, V> currentNode = table[bucketNumber];
+        if (table[bucketNumber] == null) {
+            table[bucketNumber] = newNode;
+            size++;
+        } else {
+            while (currentNode != null) {
+                if ((currentNode.key == null && key == null)
+                        || (currentNode.key != null && currentNode.key.equals(key))) {
+                    currentNode.value = value;
+                    return;
+                }
+                currentNode = currentNode.next;
+            }
+            newNode.next = table[bucketNumber].next;
+            table[bucketNumber].next = newNode;
+            size++;
+        }
     }
 
     @Override
     public V getValue(K key) {
+        int bucketNumber = getBucketNumber(key);
+        if (table[bucketNumber] == null) {
+            return null;
+        }
+        Node node = table[bucketNumber];
+        while (node != null) {
+            if (node.key == null && key == null || node.key
+                    != null && node.key.equals(key)) {
+                return (V) node.value;
+            }
+            node = node.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    private static class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        private Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private int getBucketNumber(K key) {
+        int bucketNumber = 0;
+        if (key != null) {
+            bucketNumber = abs(key.hashCode() % table.length);
+        }
+        return bucketNumber;
+    }
+
+    private void resize() {
+        if (table.length * LOAD_FACTOR > size) {
+            return;
+        }
+        Node<K, V>[] oldTable = table;
+        table = new Node[oldTable.length * RESIZE_FACTOR];
+        size = 0;
+        for (Node<K, V> node : oldTable) {
+            while (node != null) {
+                putInSize(node.key, node.value);
+                node = node.next;
+            }
+        }
     }
 }
+
