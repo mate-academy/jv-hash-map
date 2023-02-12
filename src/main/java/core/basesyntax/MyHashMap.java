b297f370aed69d2c2c6efd0295bf0000961d6ee6
mykoladymchenko <mykola.dymchenko@insightsoftware.com>
2023-02-12 18:49:37 +0100

    added second fixes after review

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -4,56 +4,58 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
     private Node<K, V>[] table;
-    private int threshold;
     private int size;
 
     public MyHashMap() {
-        this.threshold = 12;
         this.table = new Node[DEFAULT_INITIAL_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
+        int threshold = getThreshold(table.length);
         if (size > threshold) {
             resize();
         }
 
-        int indexOfBucket = hash(key);
+        int indexOfBucket = getIndex(key);
+        Node<K, V> pointer = table[indexOfBucket];
 
         if (table[indexOfBucket] == null) {
             table[indexOfBucket] = new Node(key, value);
             size++;
-        }
-
-        Node<K, V> pointer = table[indexOfBucket];
-
-        while (pointer != null) {
+        } else {
+            pointer = foundNode(pointer, key);
             if (pointer.key == key || key != null && key.equals(pointer.key)) {
                 pointer.value = value;
-                return;
-            }
-            if (pointer.next == null) {
-                pointer.next = new Node(key, value);
+            } else {
+                pointer.next = new Node<>(key, value);
                 size++;
-                return;
             }
-            pointer = pointer.next;
         }
     }
 
     @Override
     public V getValue(K key) {
-        int indexOfBucket = hash(key);
+        int indexOfBucket = getIndex(key);
         Node<K, V> pointer = table[indexOfBucket];
+        if (pointer != null) {
+            pointer = foundNode(pointer, key);
+            return pointer.value;
+        } else {
+            return null;
+        }
+    }
 
+    public Node<K, V> foundNode(Node<K, V> pointer, K key) {
         while (pointer != null) {
             if (pointer.key == key || key != null && key.equals(pointer.key)) {
-                return pointer.value;
-            } else {
-                pointer = pointer.next;
+                return pointer;
+            } else if (pointer.next == null) {
+                return pointer;
             }
+            pointer = pointer.next;
         }
-        return null;
+        return pointer;
     }
 
     @Override
@@ -61,13 +63,13 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private int hash(K key) {
+    private int getIndex(K key) {
         return key == null ? 0 : Math.abs(key.hashCode() % table.length);
     }
 
     private void resize() {
         final Node<K, V>[] oldTable = table;
-        threshold = (int) (DEFAULT_LOAD_FACTOR * (table.length << 1));
+        int threshold = getThreshold(table.length);
         table = (Node<K, V>[]) new Node[table.length << 1];
         size = 0;
         for (Node<K, V> node : oldTable) {
@@ -75,6 +77,14 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                 put(node.key, node.value);
                 node = node.next;
             }
+        }
+    }
+
+    private int getThreshold(int length) {
+        if (length == 16) {
+            return (int) (DEFAULT_LOAD_FACTOR * (table.length));
+        } else {
+            return (int) (DEFAULT_LOAD_FACTOR * (table.length << 1));
         }
     }
 
