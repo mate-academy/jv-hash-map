10a2cefca6b38b0fa679c49ddf6b791b01099fcb
“Ymishok321 <kkoiku3@gmail.com>
2023-02-18 15:19:49 +0200

    Refactored MyHashMap

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -19,24 +19,25 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if (size + 1 > threshold) {
             resize();
         }
-        putVal(key, value);
+        if (key == null || key.hashCode() % table.length == 0) {
+            zeroIndexPutHandler(key, value);
+        } else if (table[getIndexFromKey(key)] != null) {
+            nonEmptyBucketPutHandler(key, value);
+        } else {
+            table[getIndexFromKey(key)] = new Node<K, V>(key, value, null);
+            size++;
+        }
     }
 
     @Override
     public V getValue(K key) {
         if (key == null && table[0] != null) {
-            return readZeroPosition(key);
+            return zeroIndexReadHandler();
         }
-
-        int currentIndex = getIndexFromKey(key);
-
-        if (table[currentIndex] == null) {
-            return null;
+        if (table[getIndexFromKey(key)] != null) {
+            return nonEmptyBucketReadHandler(key);
         }
-        if (table[currentIndex].getKey() != key) {
-            return readCollisionBucket(key, currentIndex);
-        }
-        return table[currentIndex].getValue();
+        return null;
     }
 
     @Override
@@ -44,45 +45,26 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private V readCollisionBucket(K key, int currentIndex) {
-        Node<K, V> localCurrent = table[currentIndex];
-        while (localCurrent != null) {
-            if (Objects.equals(localCurrent.getKey(), key)) {
-                return localCurrent.getValue();
+    private V nonEmptyBucketReadHandler(K key) {
+        Node<K, V> current = table[getIndexFromKey(key)];
+        while (current != null) {
+            if (Objects.equals(current.key, key)) {
+                return current.value;
             }
-            localCurrent = localCurrent.getNext();
+            current = current.next;
         }
         return null;
     }
 
-    private V readZeroPosition(K key) {
-        if (table[0].getKey() == null) {
-            return table[0].getValue();
-        }
-        if (table[0].getNext() != null) {
-            Node<K, V> current = table[0].getNext();
-            while (current.getNext() != null) {
-                if (current.getKey() == null) {
-                    return current.getValue();
-                }
-                current = current.getNext();
+    private V zeroIndexReadHandler() {
+        Node<K, V> current = table[0];
+        while (current != null) {
+            if (current.key == null) {
+                return current.value;
             }
+            current = current.next;
         }
         return null;
-    }
-
-    private void putVal(K key, V value) {
-        if (key == null || key.hashCode() % table.length == 0) {
-            zeroIndexPutHandler(key, value);
-            return;
-        }
-        int currentIndex = getIndexFromKey(key);
-        if (table[currentIndex] != null) {
-            nonEmptyBucketPutHandler(key, value, currentIndex);
-            return;
-        }
-        table[currentIndex] = new Node<K, V>(key, value, null);
-        size++;
     }
 
     private void zeroIndexPutHandler(K key, V value) {
@@ -92,44 +74,41 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             size++;
             return;
         }
-        if (localCurrent.getKey() == key) {
-            localCurrent.setValue(value);
+        if (localCurrent.key == key) {
+            localCurrent.value = value;
             return;
         }
-        while (localCurrent.getNext() != null) {
-            if (localCurrent.getKey() == key) {
-                localCurrent.setValue(value);
+        while (localCurrent.next != null) {
+            if (localCurrent.key == key) {
+                localCurrent.value = value;
                 return;
             }
-            localCurrent = localCurrent.getNext();
+            localCurrent = localCurrent.next;
         }
-        localCurrent.setNext(new Node<K, V>(key, value, null));
+        localCurrent.next = new Node<K, V>(key, value, null);
         size++;
     }
 
-    private void nonEmptyBucketPutHandler(K key, V value, int currentIndex) {
-        if (table[currentIndex] != null) {
-            if (Objects.equals(table[currentIndex].getKey(), key)) {
-                table[currentIndex].setValue(value);
+    private void nonEmptyBucketPutHandler(K key, V value) {
+        Node<K, V> current = table[getIndexFromKey(key)];
+        if (Objects.equals(current.key, key)) {
+            current.value = value;
+            return;
+        }
+        while (current.next != null) {
+            current = current.next;
+            if (Objects.equals(current.key, key)) {
+                current.value = value;
                 return;
             }
-            Node<K, V> localCurrent = table[currentIndex];
-            while (localCurrent.getNext() != null) {
-                localCurrent = localCurrent.getNext();
-                if (Objects.equals(localCurrent.getKey(), key)) {
-                    localCurrent.setValue(value);
-                    return;
-                }
-            }
-            localCurrent.setNext(new Node<K, V>(key, value, null));
-            size++;
         }
+        current.next = new Node<K, V>(key, value, null);
+        size++;
     }
 
     private int getIndexFromKey(K key) {
         int currentIndex = key.hashCode() % table.length;
-        currentIndex = currentIndex < 0 ? -1 * currentIndex : currentIndex;
-        return currentIndex;
+        return currentIndex < 0 ? -1 * currentIndex : currentIndex;
     }
 
     private void resize() {
@@ -139,10 +118,10 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         table = (Node<K, V>[]) new Node[table.length << 1];
         for (Node<K, V> kvNode : temp) {
             if (kvNode != null) {
-                putVal(kvNode.getKey(), kvNode.getValue());
-                while (kvNode.getNext() != null) {
-                    kvNode = kvNode.getNext();
-                    put(kvNode.getKey(), kvNode.getValue());
+                put(kvNode.key, kvNode.value);
+                while (kvNode.next != null) {
+                    kvNode = kvNode.next;
+                    put(kvNode.key, kvNode.value);
                 }
             }
         }
@@ -153,34 +132,10 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         private V value;
         private Node<K, V> next;
 
-        public Node(K key, V value, Node<K, V> next) {
+        private Node(K key, V value, Node<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
-        }
-
-        public void setValue(V value) {
-            this.value = value;
-        }
-
-        public void setKey(K key) {
-            this.key = key;
-        }
-
-        public void setNext(Node<K, V> next) {
-            this.next = next;
-        }
-
-        public K getKey() {
-            return key;
-        }
-
-        public V getValue() {
-            return value;
-        }
-
-        public Node<K, V> getNext() {
-            return next;
         }
     }
 }
