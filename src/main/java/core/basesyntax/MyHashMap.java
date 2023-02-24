64d4672f05e9b2e116b804908d5cde0ba5e5d653
Hryhorii Chernolutskyi <iamchgi1976@gmail.com>
2023-02-24 22:36:42 +0200

    code simplified

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -6,23 +6,19 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int MAXIMUM_CAPACITY = 1 << 30;
 
     private int size;
-    private int threshold;
     private Node<K, V>[] table;
 
     public MyHashMap() {
+        table = (Node<K, V>[]) new Node[DEFAULT_INITIAL_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
-        if (table == null) {
-            initialMyHash();
-        }
-        if (size >= threshold) {
+        if (size >= table.length * DEFAULT_LOAD_FACTOR) {
             resize();
         }
-        int currentKeyHash = hash(key);
-        int i = getIndex(currentKeyHash);
-        Node<K, V> newNode = new Node<>(currentKeyHash, key, value, null);
+        int i = getIndex(key);
+        Node<K, V> newNode = new Node<>(key, value, null);
         if ((table[i]) == null) {
             table[i] = newNode;
             size++;
@@ -36,13 +32,12 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if ((size == 0)) {
             return null;
         }
-        int currentKeyHash = hash(key);
-        int i = getIndex(currentKeyHash);
+        int i = getIndex(key);
         Node<K, V> currentNode;
         if ((currentNode = table[i]) != null) {
             do {
-                if ((currentNode.hash == currentKeyHash) && ((currentNode.key == key)
-                        || ((key != null) && key.equals(currentNode.key)))) {
+                if ((currentNode.key == key) || ((key != null)
+                        && key.equals(currentNode.key))) {
                     return currentNode.value;
                 }
                 currentNode = currentNode.next;
@@ -57,8 +52,8 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void putValueInTree(Node<K, V> rootNode, Node<K, V> growNode) {
-        if ((rootNode.hash == growNode.hash) && ((growNode.key == rootNode.key)
-                || ((growNode.key != null) && growNode.key.equals(rootNode.key)))) {
+        if ((growNode.key == rootNode.key) || ((growNode.key != null)
+                && growNode.key.equals(rootNode.key))) {
             rootNode.value = growNode.value;
             return;
         }
@@ -71,15 +66,9 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         putValueInTree(rootNode, growNode);
     }
 
-    private void initialMyHash() {
-        table = (Node<K, V>[]) new Node[DEFAULT_INITIAL_CAPACITY];
-        threshold = (int) (table.length * DEFAULT_LOAD_FACTOR);
-    }
-
     private void resize() {
         int newSize;
         if ((newSize = table.length << 1) <= MAXIMUM_CAPACITY) {
-            threshold = (int) (newSize * DEFAULT_LOAD_FACTOR);
             Node<K, V>[] tempTable = table;
             table = (Node<K, V>[]) new Node[newSize];
             size = 0;
@@ -91,23 +80,16 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    private static int hash(Object key) {
-        int h;
-        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
-    }
-
-    private int getIndex(int hash) {
-        return ((table.length - 1) & hash);
+    private int getIndex(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode() % table.length);
     }
 
     private static class Node<K, V> {
-        private final int hash;
         private final K key;
         private V value;
         private Node<K, V> next;
 
-        Node(int hash, K key, V value, Node<K, V> next) {
-            this.hash = hash;
+        Node(K key, V value, Node<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
