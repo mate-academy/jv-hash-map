7e8865a6573f175b3a89d6dc66699c080db83e0c
Enver Ibraimov <apcyk015@gmail.com>
2023-02-26 21:42:55 +0200

    implemented MyHashMap

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,91 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int INITIAL_LENGTH = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+    private int size;
+    private Node<K, V>[] table;
+
+    public MyHashMap() {
+        table = new Node[INITIAL_LENGTH];
+    }
 
     @Override
     public void put(K key, V value) {
-
+        grow();
+        int hash = hash(key);
+        Node<K, V> inputNode = new Node<>(key, value, null);
+        Node<K, V> node = table[hash];
+        if (node == null) {
+            table[hash] = inputNode;
+        } else {
+            while (node != null) {
+                if (Objects.equals(key, node.key)) {
+                    node.value = value;
+                    return;
+                }
+                if (node.next == null) {
+                    break;
+                }
+                node = node.next;
+            }
+            node.next = inputNode;
+        }
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        Node<K, V> node = table[hash(key)];
+        while (node != null) {
+            if (Objects.equals(key, node.key)) {
+                return node.value;
+            }
+            node = node.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private static class Node<K, V> {
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        private Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private int hash(K key) {
+        if (key == null) {
+            return 0;
+        } else {
+            return Math.abs(key.hashCode() % table.length);
+        }
+    }
+
+    private void grow() {
+        if (size >= (table.length * LOAD_FACTOR)) {
+            size = 0;
+            int newLength = table.length << 1;
+            Node<K, V>[] oldTable = table;
+            table = new Node[newLength];
+            for (Node<K, V> element: oldTable) {
+                while (element != null) {
+                    put(element.key, element.value);
+                    element = element.next;
+                }
+            }
+        }
     }
 }
+
