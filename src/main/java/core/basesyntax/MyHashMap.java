68b997a40125023c100f5b9cecc0e2c9a5055ff0
Olena Matus <matus_ak16@nuwm.edu.ua>
2023-02-26 23:58:27 +0200

    added solution hw-12-jv-hash-map

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,85 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+    private Node<K, V>[] table;
+    private int size;
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
-
+        resize();
+        int position = hashCode(key);
+        Node<K, V> currentNode = table[position];
+        if (currentNode == null) {
+            table[position] = new Node(key, value, null);
+            size++;
+            return;
+        } else {
+            while (currentNode != null) {
+                if (Objects.equals(currentNode.key, key)) {
+                    currentNode.value = value;
+                    return;
+                }
+                currentNode = currentNode.next;
+            }
+        }
+        table[position] = new Node(key, value, table[position]);
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        int position = hashCode(key);
+        Node<K, V> nodeByKey = table[position];
+        while (nodeByKey != null) {
+            if (Objects.equals(nodeByKey.key, key)) {
+                return nodeByKey.value;
+            }
+            nodeByKey = nodeByKey.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private int hashCode(Object key) {
+        return key == null ? 0 : Math.abs(key.hashCode()) % table.length;
+    }
+
+    private void resize() {
+        if (size > LOAD_FACTOR * table.length) {
+            Node<K, V>[] newTable = table;
+            int newCapacity = table.length << 1;
+            table = new Node[newCapacity];
+            for (Node<K, V> oldNode : newTable) {
+                while (oldNode != null) {
+                    put(oldNode.key, oldNode.value);
+                    oldNode = oldNode.next;
+                    size--;
+                }
+            }
+        }
+    }
+
+    private static class Node<K, V> {
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
