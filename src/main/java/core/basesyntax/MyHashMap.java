e12cbdf4cfc9d96d7fd3f52f3863ea6be98c0c78
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2023-02-27 11:44:35 +0200

    moved nested class in the bottom, added checkThreshold()

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,31 +5,16 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
-
     private Node<K,V>[] table;
-    private int threshold;
     private int size;
 
-    private static class Node<K,V> {
-        private final K key;
-        private V value;
-        private Node<K,V> next;
-
-        Node(K key, V value, Node<K, V> next) {
-            this.key = key;
-            this.value = value;
-            this.next = next;
-        }
-    }
-
     public MyHashMap() {
-        threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
         table = new Node[DEFAULT_INITIAL_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
-        resize();
+        checkThreshold();
         int index = getIndex(key);
         Node<K, V> node = table[index];
         while (node != null) {
@@ -66,17 +51,32 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void resize() {
-        if (size == threshold) {
-            size = 0;
-            Node<K, V>[] tmp = table;
-            table = new Node[table.length << 1];
-            for (Node<K, V> node : tmp) {
-                while (node != null) {
-                    put(node.key, node.value);
-                    node = node.next;
-                }
+        Node<K, V>[] tmp = table;
+        table = new Node[table.length << 1];
+        size = 0;
+        for (Node<K, V> node : tmp) {
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.next;
             }
-            threshold <<= 1;
+        }
+    }
+
+    private void checkThreshold() {
+        if (size > table.length * DEFAULT_LOAD_FACTOR) {
+            resize();
+        }
+    }
+
+    private static class Node<K,V> {
+        private final K key;
+        private V value;
+        private Node<K,V> next;
+
+        Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
         }
     }
 
