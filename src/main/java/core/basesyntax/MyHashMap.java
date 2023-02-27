9d8a06b5a209d62ab45aec9449f3ca4396e95412
Andrew kluchnik <andrey.kluchnik.10@gmail.com>
2023-02-27 20:04:00 +0200

    resolved issues

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -12,20 +12,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         table = new Node[DEFAULT_CAPACITY];
     }
 
-    static class Node<K, V> {
-        private int hash;
-        private K key;
-        private V value;
-        private Node<K, V> next;
-
-        public Node(int hash, K key, V value, Node<K, V> next) {
-            this.hash = hash;
-            this.key = key;
-            this.value = value;
-            this.next = next;
-        }
-    }
-
     @Override
     public void put(K key, V value) {
         if (size >= LOAD_FACTOR * table.length) {
@@ -37,7 +23,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if (node == null) {
             table[hash] = newNode;
         } else {
-            while (true) {
+            while (node != null) {
                 if (Objects.equals(key, node.key)) {
                     node.value = value;
                     return;
@@ -55,13 +41,11 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public V getValue(K key) {
         Node<K, V> node = table[hash(key)];
-        if (node != null) {
-            while (true) {
-                if (Objects.equals(key, node.key)) {
-                    return node.value;
-                }
-                node = node.next;
+        while (node != null) {
+            if (Objects.equals(key, node.key)) {
+                return node.value;
             }
+            node = node.next;
         }
         return null;
     }
@@ -77,10 +61,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         Node<K, V>[] oldTable = table;
         table = (Node<K, V>[]) new Node[newSize];
         for (Node<K, V> node : oldTable) {
-            while (true) {
-                if (node == null) {
-                    break;
-                }
+            while (node != null) {
                 put(node.key, node.value);
                 node = node.next;
             }
@@ -89,5 +70,19 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private int hash(K key) {
         return Math.abs(key == null ? 0 : key.hashCode() % table.length);
+    }
+
+    private static class Node<K, V> {
+        private int hash;
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
