bcede57f19475ede0b2019bfa065ef9ac677292f
Nikita <nsalogub96@gmail.com>
2023-02-27 15:28:42 +0200

    Implementation hashmap3

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,8 +1,11 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
     public static final int DEFAULT_CAPASITY = 16;
     public static final double LOAD_FACTOR = 0.75;
+    private static final int DOUBLE_SIZE = 2;
     private int size;
     private Node<K, V>[] table;
 
@@ -13,11 +16,11 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public void put(K key, V value) {
         resize();
-        int index = hash(key);
+        int index = getIndex(key);
         if (table[index] != null) {
             Node<K, V> current = table[index];
             while (current != null) {
-                if ((current.key == key) || (current.key != null) && current.key.equals(key)) {
+                if (Objects.equals(current.key, key)) {
                     current.value = value;
                     return;
                 } else if (current.next == null) {
@@ -47,7 +50,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private void resize() {
         if (size >= table.length * LOAD_FACTOR) {
             size = 0;
-            int newCapacity = table.length * 2;
+            int newCapacity = table.length * DOUBLE_SIZE;
             Node<K, V>[] oldTable = table;
             table = new Node[newCapacity];
             for (Node<K, V> node : oldTable) {
@@ -59,15 +62,15 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    private int hash(K key) {
+    private int getIndex(K key) {
         return (key == null) ? 0 : Math.abs(key.hashCode() % table.length);
     }
 
     private Node<K, V> getNode(K key) {
-        int index = hash(key);
+        int index = getIndex(key);
         Node<K, V> current = table[index];
         while (current != null) {
-            if ((current.key == key) || (current.key != null) && (current.key.equals(key))) {
+            if (Objects.equals(current.key, key)) {
                 return current;
             }
             current = current.next;
