3da254a3676cee51d318903dbd46eb301618caea
Oleksandr Koval <0993079960a@gmail.com>
2023-02-27 15:36:41 +0200

    fixed access modifiers

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -73,12 +73,12 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return new Node<>(key, value, null);
     }
 
-    static class Node<K, V> {
+    private static class Node<K, V> {
         private final K key;
         private V value;
         private Node<K, V> next;
 
-        public Node(K key, V value, Node<K, V> next) {
+        private Node(K key, V value, Node<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
