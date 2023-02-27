a43f0bd52eac8a00ca32042f0fe66ddcefbfc743
Artem Ponomarenko <artemponomar98@gmail.com>
2023-02-27 12:24:20 +0200

    Implemented custom hash map according to MA guide

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,7 +5,6 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     public static final int DEFAULT_SIZE = 16;
     public static final double RESIZE_FACTOR = 0.75;
-    public static final double EXPANSION_COEFFICIENT = 2;
     private Node<K, V>[] storage;
     private int size;
 
@@ -15,68 +14,33 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public void put(K key, V value) {
-        checkSize();
-        if (key == null) {
-            if (storage[0] == null) {
-                storage[0] = new Node<>(null, value);
-                size++;
-                return;
-            }
-            if (storage[0].key == null) {
-                storage[0].value = value;
-                return;
-            }
-            System.arraycopy(storage, 0, storage, 1, storage.length - 1);
-            storage[0] = new Node<>(null, value);
-            size++;
+        Node<K, V> existingNode = getNodeAt(key);
+        if (existingNode != null) {
+            existingNode.value = value;
             return;
         }
-        for (Node<K, V> node : storage) {
-            if (node == null) {
-                break;
-            }
-            if (node.key == null) {
-                continue;
-            }
-            if (Objects.equals(node.key.hashCode(), key.hashCode())) {
-                for (Node<K, V> nextNode = node; true; nextNode = nextNode.next) {
-                    if (Objects.equals(node.key, key)) {
-                        nextNode.value = value;
-                        return;
-                    }
-                    if (nextNode.next == null) {
-                        nextNode.next = new Node<>(key, value);
-                        size++;
-                        return;
-                    }
-                }
-
-            }
-        }
-        storage[size++] = new Node<>(key, value);
+        resize();
+        insertNode(new Node<>(key, value), storage);
+        size++;
     }
 
     @Override
     public V getValue(K key) {
-        for (Node<K, V> node : storage) {
-            if (node == null) {
-                break;
-            }
-            if (key == null) {
-                if (storage[0].key == null) {
-                    return storage[0].value;
-                }
-                return null;
-            }
-            if (node.key == null) {
-                continue;
-            }
-            if (Objects.equals(node.key.hashCode(), key.hashCode())) {
-                for (Node<K, V> nextNode = node; nextNode != null; nextNode = nextNode.next) {
-                    if (Objects.equals(nextNode.key, key)) {
-                        return nextNode.value;
-                    }
-                }
+        final Node<K, V> existingNode = getNodeAt(key);
+        if (existingNode == null) {
+            return null;
+        }
+        return existingNode.value;
+    }
+
+    private Node<K, V> getNodeAt(K key) {
+        Node<K, V> starterNode = storage[getBucket(key)];
+        if (starterNode == null) {
+            return null;
+        }
+        for (Node<K, V> current = starterNode; current != null; current = current.next) {
+            if (Objects.equals(current.key, key)) {
+                return current;
             }
         }
         return null;
@@ -87,16 +51,31 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private void checkSize() {
+    private int getBucket(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode()) % storage.length;
+    }
+
+    private void resize() {
         if (size / (double) storage.length > RESIZE_FACTOR) {
-            int newCapacity = (int) (storage.length * EXPANSION_COEFFICIENT);
-            Node<K, V>[] newStorage = (Node<K, V>[]) new Node[newCapacity];
-            System.arraycopy(storage, 0, newStorage, 0, storage.length);
-            storage = newStorage;
+            Node<K, V>[] oldData = storage;
+            storage = (Node<K, V>[]) new Node[storage.length << 1];
+            for (Node<K, V> rootNode : oldData) {
+                for (Node<K, V> innerNode = rootNode;
+                        innerNode != null;
+                        innerNode = innerNode.next) {
+                    insertNode(innerNode.clone(), storage);
+                }
+            }
         }
     }
 
-    private static class Node<K, V> {
+    private void insertNode(Node<K, V> node, Node<K, V>[] storage) {
+        int bucket = getBucket(node.key);
+        node.next = storage[bucket];
+        storage[bucket] = node;
+    }
+
+    private static class Node<K, V> implements Cloneable {
         private final K key;
         private V value;
         private Node<K, V> next;
@@ -104,6 +83,11 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         private Node(K key, V value) {
             this.key = key;
             this.value = value;
+        }
+
+        @Override
+        protected Node<K, V> clone() {
+            return new Node<K, V>(key, value);
         }
     }
 }
