3956dfdbe32c61b134c2447e494e7306d75b27ee
Enver Ibraimov <123152797+en4ik99@users.noreply.github.com>
2023-02-27 13:33:54 +0200

    Update src/main/java/core/basesyntax/MyHashMap.java
    Co-authored-by: Mykyta Oleksin <32339836+nacenik@users.noreply.github.com>

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -58,7 +58,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void grow() {
-        if (size >= (table.length * LOAD_FACTOR)) {
+        if (size >= table.length * LOAD_FACTOR) {
             size = 0;
             int newLength = table.length << 1;
             Node<K, V>[] oldTable = table;
