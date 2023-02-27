36f9634cece038000797c8f8b7a91ca3c0506208
Vitalii Fedychkanych <fraugerschoh@gmail.com>
2023-02-27 16:55:35 +0200

    add decision hashMap 1.0

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,87 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private int size;
+    private Node<K, V>[] nodes;
+
+    public MyHashMap() {
+        nodes = new Node[DEFAULT_INITIAL_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
-
+        if (size >= (nodes.length * DEFAULT_LOAD_FACTOR)) {
+            resize();
+        }
+        int hash = hash(key);
+        Node<K, V> nodeToPut = new Node<K, V>(key, value, null);
+        Node<K, V> node = nodes[hash];
+        if (node == null) {
+            nodes[hash] = nodeToPut;
+        } else {
+            while (node != null) {
+                if (Objects.equals(key, node.key)) {
+                    node.value = value;
+                    return;
+                }
+                if (node.next == null) {
+                    break;
+                }
+                node = node.next;
+            }
+            node.next = nodeToPut;
+        }
+        ++size;
     }
 
     @Override
     public V getValue(K key) {
+        Node<K, V> node = nodes[hash(key)];
+        while (node != null) {
+            if (Objects.equals(key, node.key)) {
+                return node.value;
+            }
+            node = node.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private void resize() {
+        size = 0;
+        int capacity = nodes.length;
+        Node<K, V>[] tempTable = nodes;
+        capacity = capacity * 2;
+        nodes = new Node[capacity];
+        for (Node<K, V> element : tempTable) {
+            while (element != null) {
+                put(element.key, element.value);
+                element = element.next;
+            }
+        }
+    }
+
+    private int hash(K key) {
+        return (key == null) ? 0 : Math.abs(key.hashCode() % nodes.length);
+    }
+
+    private static class Node<K, V> {
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        private Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
