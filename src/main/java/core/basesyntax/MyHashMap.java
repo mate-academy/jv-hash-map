8b40e961c8637667e239a55c163ddb2ae3753299
Volodymyr Sakalosh <sva2005@ukr.net>
2023-03-05 21:52:51 +0200

    Fixed lines : 4, 5

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    private final static int DEFAULT_CAPACITY = 16;
-    private final static float LOAD_FOLDER = 0.75f;
+    private final int DEFAULT_CAPACITY = 16;
+    private final float LOAD_FOLDER = 0.75f;
     private int size = 0;
     private Node<K,V>[] table;
 
