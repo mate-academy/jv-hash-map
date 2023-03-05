6313c315eab6cd3aa1b7419922d7bd84e8217a57
Volodymyr Sakalosh <sva2005@ukr.net>
2023-03-05 21:05:44 +0200

    Created constants and fields

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,6 +1,10 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private final static int DEFAULT_CAPACITY = 16;
+    private final static float LOAD_FOLDER = 0.75f;
+    private int size = 0;
+    private Node<K,V>[] table;
 
     @Override
     public void put(K key, V value) {
