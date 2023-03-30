3928782c29e572c11bc8fc57919d3acd4166266b
antonsablev <live.incntr@gmail.com>
2023-03-30 14:34:23 +0200

    add fixes

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -7,50 +7,51 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final float THRESHOLD_COEFFICIENT = 0.75f;
     private Node<K, V>[] table;
     private int threshold;
-    private int capacity;
     private int size;
-    private Node<K, V> tail;
 
     public MyHashMap() {
-        threshold = (int) (INITIAL_CAPACITY * THRESHOLD_COEFFICIENT);
-        capacity = INITIAL_CAPACITY;
-        table = (Node<K, V>[]) new Node[INITIAL_CAPACITY];
+        table = new Node[INITIAL_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
-        int hash = hash(key);
-        Node<K, V> newNode = new Node<>(key, value, null);
-
-        if (table[hash] == null) {
+        Node<K, V> node = new Node<>(key, value, null);
+        checkSize();
+        int index = getIndex(node.key);
+        if (table[index] == null) {
+            table[index] = node;
             size++;
-            resizeCheck();
-            table[hash] = newNode;
         } else {
-            Node<K, V> node = checkKey(key);
-            if (node != null) {
-                node.value = value;
-            } else {
-                size++;
-                tail.next = newNode;
-                tail = newNode;
-                resizeCheck();
+            Node<K, V> newNode = table[index];
+            while (newNode != null) {
+                if (Objects.equals(node.key, newNode.key)) {
+                    newNode.value = value;
+                    return;
+                }
+                if (newNode.next == null) {
+                    newNode.next = node;
+                    size++;
+                    return;
+                }
+                newNode = newNode.next;
             }
+        }
+    }
+
+    public void checkSize() {
+        if (size > threshold) {
+            resize();
         }
     }
 
     @Override
     public V getValue(K key) {
-        int hash = hash(key);
-        if (table[hash] == null) {
-            System.out.println("the key is absent in the map");
-        } else {
-            Node<K, V> node = checkKey(key);
-
-            if (node != null) {
-                return node.value;
-            } else {
-                throw new RuntimeException("the key is absent: " + key);
+        for (Node<K, V> node : table) {
+            while (node != null) {
+                if (Objects.equals(node.key, key)) {
+                    return node.value;
+                }
+                node = node.next;
             }
         }
         return null;
@@ -61,73 +62,31 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private int hash(Object key) {
-        return (key == null) ? 0 : Math.abs(Objects.hashCode(key)) % capacity;
+    private int getIndex(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode() % table.length);
     }
 
-    private Node<K, V>[] resize() {
-        capacity = capacity << 1;
-        threshold = threshold << 1;
-        Node<K, V>[] newTable = (Node<K, V>[]) new Node[capacity];
-        for (int i = 0; i < table.length; i++) {
-            Node<K, V> tempNode;
-            int hash;
-            if ((tempNode = table[i]) != null) {
-                table[i] = null;
+    private void resize() {
+        int newCapacity = table.length << 1;
+        threshold = (int) (newCapacity * THRESHOLD_COEFFICIENT);
+        Node<K, V>[] previousTable = table;
+        table = new Node[newCapacity];
+        size = 0;
 
-                while (tempNode != null) {
-                    Node<K, V> nodeForSaveNext = tempNode.next;
-                    hash = hash(tempNode.key);
-                    if (newTable[hash] == null) {
-                        newTable[hash] = tempNode;
-                        newTable[hash].next = null;
-                    } else {
-                        Node<K, V> next = newTable[hash];
-                        while (next.next != null) {
-                            next = next.next;
-                        }
-                        next.next = tempNode;
-                        next.next.next = null;
-                    }
-                    tempNode = nodeForSaveNext;
-                }
-            }
-        }
-        return newTable;
-    }
-
-    private void resizeCheck() {
-        if (size > threshold) {
-            table = resize();
-        }
-    }
-
-    private boolean isSameKey(K key, K newKey) {
-        return (hash(key) == hash(newKey) && key == newKey) || (key != null && key.equals(newKey));
-    }
-
-    private Node<K, V> checkKey(K key) {
-        Node<K, V> node = table[hash(key)];
-        Node<K, V> temp = null;
-
-        while (node != null) {
-            if (isSameKey(node.key, key)) {
-                return node;
-            } else {
-                temp = node;
+        for (Node<K, V> node : previousTable) {
+            while (node != null) {
+                put(node.key, node.value);
                 node = node.next;
             }
         }
-        tail = temp;
-        return null;
     }
 
-    private static class Node<K, V> {
-        private final K key;
+    private class Node<K, V> {
+        private K key;
         private V value;
         private Node<K, V> next;
 
-        public Node(K key, V value, Node<K, V> next) {
+        private Node(K key, V value, Node<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
