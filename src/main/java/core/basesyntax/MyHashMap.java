1b74831b1059b15fd9fb8d23f639ca0fa46f3c1a
Bakhmet Ivan <bahmetvanya1290@gmail.com>
2023-04-04 18:39:15 +0300

    extra fields removed and styling bugs fixed

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,53 +5,30 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final float LOAD_FACTOR = 0.75f;
     private Node<K, V> [] table;
     private int size;
-    private int capacity;
-    private int threshold;
-
-    private class Node<K, V> {
-        private int hash;
-        private K key;
-        private V value;
-        private Node<K,V> next;
-
-        public Node(int hash, K key, V value, Node<K, V> next) {
-            this.key = key;
-            this.value = value;
-            this.hash = hash;
-            this.next = next;
-        }
-    }
 
     public MyHashMap() {
         this.size = 0;
-        this.capacity = INITIAL_CAPACITY;
-        this.threshold = (int)(capacity * LOAD_FACTOR);
-        this.table = new Node[capacity];
-    }
-
-    private int hash(Object key) {
-        return key == null ? 0 : key.hashCode() ^ (key.hashCode() >>> 16);
+        this.table = new Node[INITIAL_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
-        int hash = hash(key);
-        int index = indexFor(hash, table.length);
+        int index = getIndex(key);
         for (Node<K, V> i = table[index]; i != null; i = i.next) {
-            if (i.hash == hash && (i.key == key || (i.key != null && i.key.equals(key)))) {
+            if (i.key == key || i.key != null && i.key.equals(key)) {
                 i.value = value;
                 return;
             }
         }
-        addNode(hash, key, value, index);
+        addNode(key, value, index);
     }
 
     @Override
     public V getValue(K key) {
         int hash = hash(key);
-        int index = indexFor(hash, table.length);
+        int index = getIndex(key);
         for (Node<K, V> i = table[index]; i != null; i = i.next) {
-            if (i.hash == hash && (i.key == key || (i.key != null && i.key.equals(key)))) {
+            if (i.key == key || i.key != null && i.key.equals(key)) {
                 return i.value;
             }
         }
@@ -64,31 +41,44 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private void resize(int newCapacity) {
+    private void resize(int newLength) {
         Node<K, V>[] oldTable = table;
-        table = new Node[newCapacity];
-        final int oldCapacity = capacity;
-        capacity = newCapacity;
-        threshold = (int) (capacity * LOAD_FACTOR);
+        final int oldLength = table.length;
+        table = new Node[newLength];
         size = 0;
-        for (int i = 0; i < oldCapacity; i++) {
+        for (int i = 0; i < oldLength; i++) {
             for (Node<K, V> j = oldTable[i]; j != null; j = j.next) {
-                addNode(j.hash, j.key, j.value, indexFor(j.hash, newCapacity));
+                addNode(j.key, j.value, getIndex(j.key));
             }
         }
     }
 
-    private void addNode(int hash, K key, V value, int index) {
+    private void addNode(K key, V value, int index) {
         Node<K, V> tempNode = table[index];
-        table[index] = new Node<K, V>(hash, key, value, tempNode);
+        table[index] = new Node<K, V>(key, value, tempNode);
         size++;
-        if (size >= threshold) {
-            resize(2 * capacity);
+        if (size >= (int)(table.length * LOAD_FACTOR)) {
+            resize(2 * table.length);
         }
     }
 
-    private int indexFor(int hash, int length) {
-        return hash & (length - 1);
+    private int getIndex(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode() % table.length);
     }
 
+    private int hash(Object key) {
+        return key == null ? 0 : key.hashCode() ^ (key.hashCode() >>> 16);
+    }
+
+    private class Node<K, V> {
+        private K key;
+        private V value;
+        private Node<K,V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
 }
