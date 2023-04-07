06d56c376d810ed4f95aec4c98e676ee1409510d
Artem Fedoseev <afedoseev359@gmail.com>
2023-04-07 20:09:42 +0300

    Removed unnecessary empty lines

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -12,22 +12,17 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public void put(K key, V value) {
         Node<K, V> node = new Node<K, V>(key, value);
-
         V valueByNewKey = getValue(key);
-
         if (valueByNewKey != null) {
             if (Objects.equals(value, valueByNewKey)) {
                 return;
             }
-
             putIntoTable(node, table);
             return;
         }
-
         if (table == null || size == threshold) {
             resize();
         }
-
         putIntoTable(node, table);
         size++;
     }
@@ -37,25 +32,19 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if (table == null || size == 0) {
             return null;
         }
-
         int index = key == null ? 0 : Math.abs(key.hashCode()) % table.length;
-
         Node<K, V> current = table[index];
-
         if (current == null) {
             return null;
         }
 
         while (current != null) {
             boolean areKeysEqual = Objects.equals(current.key, key);
-
             if (areKeysEqual) {
                 return current.value;
             }
-
             current = current.next;
         }
-
         return null;
     }
 
@@ -75,70 +64,55 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         } else {
             int newCapacity = table.length * 2;
             threshold = threshold * 2;
-
             Node<K, V>[] newTable = new Node[newCapacity];
-
             for (int i = 0; i < newTable.length; i++) {
                 newTable[i] = null;
             }
 
             Node<K, V>[] nodesArray = new Node[size];
             int index = 0;
-
             for (Node<K, V> element: table) {
                 Node<K, V> node = element;
-
                 while (node != null) {
                     nodesArray[index] = node;
                     index++;
                     node = node.next;
                 }
             }
-
             for (Node<K, V> nodeFromArray: nodesArray) {
                 nodeFromArray.next = null;
                 putIntoTable(nodeFromArray, newTable);
             }
-
             table = newTable;
         }
     }
 
     private void putIntoTable(Node<K, V> node, Node<K, V>[] useTable) {
         int index = node.hash % useTable.length;
-
         Node<K, V> nodeByIndex = useTable[index];
-
         if (nodeByIndex == null) {
             useTable[index] = node;
-
             return;
         }
 
         Node<K, V> current = nodeByIndex;
         Node<K, V> prev = null;
-
         while (current != null) {
             boolean areKeysEqual = Objects.equals(current.key, node.key);
-
             if (areKeysEqual) {
                 node.next = current.next;
-
                 if (prev != null) {
                     prev.next = node;
                 } else {
                     useTable[index] = node;
                 }
-
                 return;
             } else {
                 if (current.next == null) {
                     current.next = node;
-
                     return;
                 }
             }
-
             prev = current;
             current = current.next;
         }
