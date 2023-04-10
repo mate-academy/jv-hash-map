6d07fb9c6ddc371a4d95e82ecae86e5ee7708f7e
Artem Fedoseev <afedoseev359@gmail.com>
2023-04-10 13:37:41 +0300

    Simplified resize, improved put, removed putIntoTable

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -4,43 +4,53 @@ import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private static final int GROW_FACTOR = 2;
     private static final double LOAD_FACTOR = 0.75;
-    private int threshold;
     private Node<K, V>[] table;
     private int size;
 
+    public MyHashMap() {
+        this.table = (Node<K, V>[]) new Node[DEFAULT_INITIAL_CAPACITY];
+    }
+
     @Override
     public void put(K key, V value) {
-        Node<K, V> node = new Node<K, V>(key, value);
-        V valueByNewKey = getValue(key);
-        if (valueByNewKey != null) {
-            if (Objects.equals(value, valueByNewKey)) {
-                return;
+        Node<K, V> existingNode = getHeadNodeInPosition(key);
+
+        if (existingNode != null) {
+            Node<K, V> current = existingNode;
+            while (current != null) {
+                if (Objects.equals(current.key, key)) {
+                    current.value = value;
+                    return;
+                }
+                current = current.next;
             }
-            putIntoTable(node, table);
-            return;
         }
-        if (table == null || size == threshold) {
+
+        int threshold = (int)(table.length * LOAD_FACTOR);
+        if (size == threshold) {
             resize();
+            existingNode = getHeadNodeInPosition(key);
         }
-        putIntoTable(node, table);
+        Node<K, V> node = new Node<K, V>(key, value);
+        if (existingNode != null) {
+            while (existingNode.next != null) {
+                existingNode = existingNode.next;
+            }
+            existingNode.next = node;
+        } else {
+            table[calculateIndex(key)] = node;
+        }
         size++;
     }
 
     @Override
     public V getValue(K key) {
-        if (table == null || size == 0) {
-            return null;
-        }
-        int index = key == null ? 0 : Math.abs(key.hashCode()) % table.length;
-        Node<K, V> current = table[index];
-        if (current == null) {
-            return null;
-        }
+        Node<K, V> current = getHeadNodeInPosition(key);
 
         while (current != null) {
-            boolean areKeysEqual = Objects.equals(current.key, key);
-            if (areKeysEqual) {
+            if (Objects.equals(current.key, key)) {
                 return current.value;
             }
             current = current.next;
@@ -54,68 +64,26 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void resize() {
-        if (table == null) {
-            Node<K, V>[] newTable = new Node[DEFAULT_INITIAL_CAPACITY];
-            for (int i = 0; i < newTable.length; i++) {
-                newTable[i] = null;
-            }
-            table = newTable;
-            threshold = (int)(DEFAULT_INITIAL_CAPACITY * LOAD_FACTOR);
-        } else {
-            int newCapacity = table.length * 2;
-            threshold = threshold * 2;
-            Node<K, V>[] newTable = new Node[newCapacity];
-            for (int i = 0; i < newTable.length; i++) {
-                newTable[i] = null;
-            }
+        Node<K, V>[] oldTable = table;
+        table = new Node[table.length * GROW_FACTOR];
+        size = 0;
 
-            Node<K, V>[] nodesArray = new Node[size];
-            int index = 0;
-            for (Node<K, V> element: table) {
-                Node<K, V> node = element;
-                while (node != null) {
-                    nodesArray[index] = node;
-                    index++;
-                    node = node.next;
-                }
+        for (Node<K, V> node : oldTable) {
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.next;
             }
-            for (Node<K, V> nodeFromArray: nodesArray) {
-                nodeFromArray.next = null;
-                putIntoTable(nodeFromArray, newTable);
-            }
-            table = newTable;
         }
     }
 
-    private void putIntoTable(Node<K, V> node, Node<K, V>[] useTable) {
-        int index = node.hash % useTable.length;
-        Node<K, V> nodeByIndex = useTable[index];
-        if (nodeByIndex == null) {
-            useTable[index] = node;
-            return;
-        }
+    private Node<K, V> getHeadNodeInPosition(K key) {
+        int index = calculateIndex(key);
 
-        Node<K, V> current = nodeByIndex;
-        Node<K, V> prev = null;
-        while (current != null) {
-            boolean areKeysEqual = Objects.equals(current.key, node.key);
-            if (areKeysEqual) {
-                node.next = current.next;
-                if (prev != null) {
-                    prev.next = node;
-                } else {
-                    useTable[index] = node;
-                }
-                return;
-            } else {
-                if (current.next == null) {
-                    current.next = node;
-                    return;
-                }
-            }
-            prev = current;
-            current = current.next;
-        }
+        return table[index];
+    }
+
+    private int calculateIndex(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode()) % table.length;
     }
 
     private static class Node<K, V> {
