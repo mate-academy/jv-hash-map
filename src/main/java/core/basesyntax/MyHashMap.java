977685d60282b2b17a99739dd4dd71ef0c33f9ab
Artem <artempharm83@gmail.com>
2023-04-12 18:55:18 +0300

    added Node class implemented public void put added private void resize() getBucketIndex(K key) method for bucketIndex public V getValue to get all values public void put to put value under key

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,82 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;// aka 16
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private static final int INCREASED_RESIZE = 2;
+    private int size;
+    private Node<K, V> [] table;
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_INITIAL_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
-
+        if (size == table.length * DEFAULT_LOAD_FACTOR) {
+            resize();
+        }
+        Node<K, V> node = new Node(key, value, null);
+        int bucket = getBucketIndex(node.key);
+        Node<K, V> currentNode = table[bucket];
+        while (currentNode != null) {
+            if (currentNode.key == node.key
+                    || (currentNode.key != null && currentNode.key.equals(node.key))) {
+                currentNode.value = node.value;
+                return;
+            } else if (currentNode.next == null) {
+                currentNode.next = node;
+                size++;
+                return;
+            }
+            currentNode = currentNode.next;
+        }
+        table[bucket] = node;
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        Node<K, V> currentNode = table[getBucketIndex(key)];
+        while (currentNode != null) {
+            if (key == currentNode.key || (key != null && key.equals(currentNode.key))) {
+                return currentNode.value;
+            }
+            currentNode = currentNode.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private class Node<K, V> {
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private void resize() {
+        Node<K, V> [] oldTable = table;
+        table = new Node [table.length * INCREASED_RESIZE];
+        size = 0;
+        for (Node<K, V> oldTableNode : oldTable) {
+            while (oldTableNode != null) {
+                put(oldTableNode.key, oldTableNode.value);
+                oldTableNode = oldTableNode.next;
+            }
+        }
+    }
+
+    private int getBucketIndex(K key) {
+        return (key == null) ? 0 : Math.abs(key.hashCode() % table.length);
     }
 }
