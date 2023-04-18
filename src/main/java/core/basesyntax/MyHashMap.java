5eeec3d058493a6bf71cdadd903202b2fa21aa1e
Victor Rudkovsky <vpcomtek@gmail.com>
2023-04-18 14:10:45 +0300

    HW MyHashMap v1.2

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -3,15 +3,14 @@ package core.basesyntax;
 import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
-    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+    private static final int INITIAL_CAPACITY = 16;
     private static final int RESIZE_MULTIPLY = 2;
     private Node[] table;
     private int size;
-    private int threshold = (int) (DEFAULT_LOAD_FACTOR * DEFAULT_INITIAL_CAPACITY);
 
     public MyHashMap() {
-        table = new Node[DEFAULT_INITIAL_CAPACITY];
+        table = new Node[INITIAL_CAPACITY];
     }
 
     private static class Node<K, V> {
@@ -55,7 +54,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private void addNode(K key, V value) {
         int index = getIndex(key);
-        Node<K, V> node = new Node<>(hash(key), key, value, null);;
+        Node<K, V> node = new Node<>(hash(key), key, value, null);
         Node<K,V> currentNode = table[index];
         while (currentNode != null) {
             if (Objects.equals(currentNode.key, key)) {
@@ -82,16 +81,16 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void resize() {
-        if (size == threshold) {
-            Node<K, V>[] oldTable = table;
-            table = new Node[oldTable.length * RESIZE_MULTIPLY];
-            threshold = (int) (table.length * DEFAULT_LOAD_FACTOR);
-            size = 0;
-            for (Node<K, V> kvNode : oldTable) {
-                while (kvNode != null) {
-                    put(kvNode.key, kvNode.value);
-                    kvNode = kvNode.next;
-                }
+        if (size < table.length * LOAD_FACTOR) {
+            return;
+        }
+        Node<K, V>[] oldTable = table;
+        table = new Node[oldTable.length * RESIZE_MULTIPLY];
+        size = 0;
+        for (Node<K, V> kvNode : oldTable) {
+            while (kvNode != null) {
+                put(kvNode.key, kvNode.value);
+                kvNode = kvNode.next;
             }
         }
     }
