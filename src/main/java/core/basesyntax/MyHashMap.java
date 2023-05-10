98d393df678fcecaf7502860317c904262bacadf
Petro Kobevko <kobevkopi@gmail.com>
2023-05-10 22:46:23 +0200

    Added HashMap realization

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,91 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private int size;
+    private Node<K, V>[] table;
+    private int threshold;
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_INITIAL_CAPACITY];
+        threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
+    }
 
     @Override
     public void put(K key, V value) {
-
+        if (size > threshold) {
+            resize();
+        }
+        putIntoTableOrReplaceValue(key, value);
     }
 
     @Override
     public V getValue(K key) {
+        int index = calculateIndex(key);
+        Node<K, V> node = table[index];
+        while (node != null) {
+            if (elementsAreEqual(node.key, key)) {
+                return node.value;
+            }
+            node = node.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private void putIntoTableOrReplaceValue(K key, V value) {
+        int index = calculateIndex(key);
+        Node<K, V> node = table[index];
+        while (node != null) {
+            if (elementsAreEqual(node.key, key)) {
+                node.value = value;
+                return;
+            }
+            node = node.next;
+        }
+        table[index] = new Node<>(key, value, table[index]);
+        size++;
+    }
+
+    private boolean elementsAreEqual(K first, K second) {
+        return first == second || first != null && first.equals(second);
+    }
+
+    private int calculateIndex(K key) {
+        return Math.abs(hash(key) % table.length);
+    }
+
+    private int hash(Object key) {
+        return key == null ? 0 : key.hashCode();
+    }
+
+    private void resize() {
+        Node[] oldTable = table;
+        table = new Node[oldTable.length * 2];
+        size = 0;
+        for (Node<K, V> node : oldTable) {
+            while (node != null) {
+                putIntoTableOrReplaceValue(node.key, node.value);
+                node = node.next;
+            }
+        }
+        threshold = (int) (table.length * DEFAULT_LOAD_FACTOR);
+    }
+
+    private static class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
