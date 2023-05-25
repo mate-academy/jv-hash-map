e9e53ae42b61c6ce556f0f2372a22be60fec136d
pochtalon <pochtalon1010@gmail.com>
2023-05-25 22:43:30 +0300

    Created inner class Node with constructor; Added 2 constants for capacity and loadFactor; Added private fields threshold, size and array; Created constructor for MyHashMap class; Created private method getIndex; Created private methods getPresentNode() and getLastNode(); Implemented methods;

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,106 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final double DEFAULT_LOAD_FACTOR = 0.75;
+    private int threshold;
+    private int size;
+    private Node<K, V>[] array;
+
+    public MyHashMap() {
+        array = new Node[DEFAULT_CAPACITY];
+        threshold = (int) (array.length * DEFAULT_LOAD_FACTOR);
+    }
 
     @Override
     public void put(K key, V value) {
-
+        resize();
+        int index = getIndex(key);
+        Node<K, V> newNode = new Node<>(key, value);
+        if (array[index] == null) {
+            array[index] = newNode;
+            size++;
+            return;
+        }
+        Node<K, V> present = getPresentNode(key);
+        if (present != null) {
+            present.value = value;
+        } else {
+            getLastNode(key).next = newNode;
+            size++;
+        }
     }
 
     @Override
     public V getValue(K key) {
+        Node<K, V> result = getPresentNode(key);
+        if (result != null) {
+            return result.value;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private int getIndex(K key) {
+        return (key == null) ? 0 : Math.abs(key.hashCode()) % array.length;
+    }
+
+    private Node<K, V> getLastNode(K key) {
+        int index = getIndex(key);
+        Node<K, V> currentNode = array[index];
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+        }
+        return currentNode;
+    }
+
+    private Node<K, V> getPresentNode(K key) {
+        int index = getIndex(key);
+        Node<K, V> currentNode = array[index];
+        while (currentNode != null) {
+            if (Objects.equals(currentNode.key, key)) {
+                return currentNode;
+            }
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
+
+    private void resize() {
+        if (size > threshold) {
+            Node<K, V>[] oldArray = array;
+            array = new Node[array.length * 2];
+            threshold = (int) (array.length * DEFAULT_LOAD_FACTOR);
+            size = 0;
+            for (Node<K, V> node : oldArray) {
+                if (node != null) {
+                    Node<K, V> currentNode = node;
+                    while (currentNode != null) {
+                        put(currentNode.key, currentNode.value);
+                        currentNode = currentNode.next;
+                    }
+                }
+            }
+        }
+    }
+
+    private static class Node<K, V> {
+        final int hash;
+        final K key;
+        V value;
+        Node<K, V> next;
+
+        public Node(K key, V value) {
+            this.hash = (key == null) ? 0 : key.hashCode();
+            this.key = key;
+            this.value = value;
+        }
     }
 }
+
