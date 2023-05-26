c78cad231e7907c7d7c311d5b96a7afa2019bab7
pochtalon <pochtalon1010@gmail.com>
2023-05-26 19:35:15 +0300

    Deleted threshold field; Deleted hash field in inner class; Removed static keyword in Node class; Changed logic in resize() method;

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -4,14 +4,13 @@ import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
+    private static final int RESIZE_ARRAY = 2;
     private static final double DEFAULT_LOAD_FACTOR = 0.75;
-    private int threshold;
     private int size;
     private Node<K, V>[] array;
 
     public MyHashMap() {
         array = new Node[DEFAULT_CAPACITY];
-        threshold = (int) (array.length * DEFAULT_LOAD_FACTOR);
     }
 
     @Override
@@ -73,31 +72,25 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void resize() {
-        if (size > threshold) {
+        if (size > array.length * DEFAULT_LOAD_FACTOR) {
             final Node<K, V>[] oldArray = array;
-            array = new Node[array.length * 2];
-            threshold = (int) (array.length * DEFAULT_LOAD_FACTOR);
+            array = new Node[array.length * RESIZE_ARRAY];
             size = 0;
             for (Node<K, V> node : oldArray) {
-                if (node != null) {
-                    Node<K, V> currentNode = node;
-                    while (currentNode != null) {
-                        put(currentNode.key, currentNode.value);
-                        currentNode = currentNode.next;
-                    }
+                while (node != null) {
+                    put(node.key, node.value);
+                    node = node.next;
                 }
             }
         }
     }
 
-    private static class Node<K, V> {
-        private final int hash;
+    private class Node<K, V> {
         private final K key;
         private V value;
         private Node<K, V> next;
 
         public Node(K key, V value) {
-            this.hash = (key == null) ? 0 : key.hashCode();
             this.key = key;
             this.value = value;
         }
