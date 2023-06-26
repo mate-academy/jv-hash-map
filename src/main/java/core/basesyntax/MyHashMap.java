0abd9025faebabb6b78d6398ea8ced3162385c14
nikita <saschenko7@gmail.com>
2023-06-26 17:47:25 +0300

    remove imports

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,44 +1,41 @@
 package core.basesyntax;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
 
-    private List<LinkedList<Node<K,V>>> buckets;
+    private Node<K, V>[] buckets;
     private int size;
-    private Object[] arrayField;
 
     public MyHashMap() {
         this(DEFAULT_CAPACITY);
     }
 
     public MyHashMap(int initialCapacity) {
-        buckets = new ArrayList<>(initialCapacity);
-        for (int i = 0; i < initialCapacity; i++) {
-            buckets.add(new LinkedList<>());
-        }
-        arrayField = new Object[0];
+        buckets = new Node[initialCapacity];
+        size = 0;
     }
 
     @Override
     public void put(K key, V value) {
         int index = getIndex(key);
-        LinkedList<Node<K, V>> list = buckets.get(index);
+        Node<K, V> node = buckets[index];
 
-        for (Node<K, V> node : list) {
-            if (node.key == null && key == null || node.key != null && node.key.equals(key)) {
+        while (node != null) {
+            if (Objects.equals(node.key, key)) {
                 node.value = value;
                 return;
             }
+            node = node.next;
         }
 
-        list.add(new Node<>(key, value));
+        Node<K, V> newNode = new Node<>(key, value);
+        newNode.next = buckets[index];
+        buckets[index] = newNode;
         size++;
+
         if (needsResize()) {
             resize();
         }
@@ -47,12 +44,15 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public V getValue(K key) {
         int index = getIndex(key);
-        LinkedList<Node<K, V>> list = buckets.get(index);
-        for (Node<K, V> node : list) {
+        Node<K, V> node = buckets[index];
+
+        while (node != null) {
             if (Objects.equals(node.key, key)) {
                 return node.value;
             }
+            node = node.next;
         }
+
         return null;
     }
 
@@ -61,41 +61,41 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    public int getIndex(K key) {
+    private int getIndex(K key) {
         if (key == null) {
             return 0;
         }
-        return Math.abs(key.hashCode() % buckets.size());
+        return Math.abs(key.hashCode() % buckets.length);
     }
 
     private boolean needsResize() {
-        float loadFactor = (float) size / buckets.size();
+        float loadFactor = (float) size / buckets.length;
         return loadFactor > DEFAULT_LOAD_FACTOR;
     }
 
     private void resize() {
-        List<LinkedList<Node<K, V>>> oldBuckets = buckets;
-        int newCapacity = buckets.size() * 2;
-        buckets = new ArrayList<>(newCapacity);
-        for (int i = 0; i < newCapacity; i++) {
-            buckets.add(new LinkedList<>());
-        }
-        for (LinkedList<Node<K, V>> list : oldBuckets) {
-            for (Node<K, V> node : list) {
-                int index = getIndex(node.key);
-                buckets.get(index).add(node);
+        Node<K, V>[] oldBuckets = buckets;
+        int newCapacity = buckets.length * 2;
+        buckets = new Node[newCapacity];
+        size = 0;
+
+        for (Node<K, V> node : oldBuckets) {
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.next;
             }
         }
-        arrayField = new Object[newCapacity];
     }
 
     private static class Node<K, V> {
         private final K key;
         private V value;
+        private Node<K, V> next;
 
         public Node(K key, V value) {
             this.key = key;
             this.value = value;
+            this.next = null;
         }
     }
 }
