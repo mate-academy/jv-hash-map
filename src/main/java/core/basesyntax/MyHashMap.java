e6a885b97dc9ab358b370b3ef49118a00ff7f760
ANDREY <andrey.kiyas@gmail.com>
2023-06-27 23:22:27 +0300

    hw-jv-hash-map-solution-andrey-kiyas

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,95 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int INCREASE_FACTOR = 2;
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final double LOAD_FACTOR = 0.75;
+    private Node<K, V>[] table;
+    private int size = 0;
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
-
+        if (size >= table.length * LOAD_FACTOR) {
+            resize();
+        }
+        int index = getIndexHash(key);
+        Node<K, V> newNode = new Node<>(key, value);
+        if (table[index] == null) {
+            table[index] = newNode;
+            size++;
+        } else {
+            Node<K, V> current = table[index];
+            while (current != null) {
+                if (isEqualKeys(current.key, key)) {
+                    current.value = value;
+                    return;
+                } else if (current.next == null) {
+                    current.next = newNode;
+                    size++;
+                    return;
+                } else {
+                    current = current.next;
+                }
+            }
+        }
     }
 
     @Override
     public V getValue(K key) {
+        if (table == null) {
+            return null;
+        }
+        int index = getIndexHash(key);
+        Node<K, V> current = table[index];
+        while (current != null) {
+            if (isEqualKeys(current.key, key)) {
+                return current.value;
+            }
+            current = current.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private int getIndexHash(K key) {
+        return (key == null) ? 0 : Math.abs(key.hashCode() % table.length);
+    }
+
+    private boolean isEqualKeys(K key1, K key2) {
+        if (key1 == null) {
+            return key2 == null;
+        }
+        return key1.equals(key2);
+    }
+
+    private void resize() {
+        Node<K, V>[] previousTable = table;
+        table = new Node[table.length * INCREASE_FACTOR];
+        size = 0;
+        for (Node<K, V> node : previousTable) {
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.next;
+            }
+        }
     }
 }
