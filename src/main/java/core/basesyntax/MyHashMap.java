f344967b54664f7079bb099d717bfc19037d18bf
happytroya <happytroya2001@gmail.com>
2023-07-04 17:08:27 +0300

    mu hashmap implementation

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,125 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int INITIAL_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+    private int factCapacity;
+
+    private Node<K, V>[] table;
+    private int size;
+
+    public MyHashMap() {
+        table = new Node[INITIAL_CAPACITY];
+        factCapacity = INITIAL_CAPACITY;
+    }
+
+    private static class Node<K, V> {
+        private final K key;
+        private int hash;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(int hash, K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.hash = hash;
+            this.value = value;
+            this.next = next;
+        }
+    }
 
     @Override
     public void put(K key, V value) {
+        checkSize();
+        int hash = hash(key);
+        int bucketIndex = hash % factCapacity;
 
+        if (table[bucketIndex] == null) {
+            table[bucketIndex] = new Node<>(hash, key, value, null);
+        } else {
+            Node<K, V> tmp = table[bucketIndex];
+
+            if (tmp.hash == hash && Objects.equals(tmp.key, key)) {
+                tmp.value = value;
+                return;
+            }
+
+            while (tmp.next != null) {
+                if (tmp.hash == hash && Objects.equals(tmp.key, key)) {
+                    tmp.value = value;
+                    return;
+                }
+                tmp = tmp.next;
+            }
+
+            if (tmp.hash == hash && Objects.equals(tmp.key, key)) {
+                tmp.value = value;
+                return;
+            }
+
+            tmp.next = new Node<>(hash, key, value, null);
+        }
+        size++;
     }
 
     @Override
     public V getValue(K key) {
-        return null;
+        int hash = hash(key);
+        int bucketIndex = hash % factCapacity;
+        Node<K, V> tmp = table[bucketIndex];
+
+        if (tmp == null) {
+            return null;
+        } else {
+            while (tmp.next != null) {
+                if (Objects.equals(tmp.key, key)) {
+                    return tmp.value;
+                }
+                tmp = tmp.next;
+            }
+        }
+        return tmp.value;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private int hash(K key) {
+        int hash = 0;
+        if (key == null) {
+            return hash;
+        }
+        hash = key.hashCode();
+        if (hash < 0) {
+            hash *= -1;
+        }
+        return hash;
+    }
+
+    private void checkSize() {
+        if (size >= factCapacity * LOAD_FACTOR) {
+            resize();
+        }
+    }
+
+    private void resize() {
+        int newCapacity = factCapacity * 2;
+        Node<K, V>[] newTable = new Node[newCapacity];
+
+        for (Node<K, V> node : table) {
+            while (node != null) {
+                int newBucketIndex = node.hash % newCapacity;
+                Node<K, V> nextNode = node.next;
+                node.next = newTable[newBucketIndex];
+                newTable[newBucketIndex] = node;
+                node = nextNode;
+            }
+        }
+
+        table = newTable;
+        factCapacity = newCapacity;
     }
 }
