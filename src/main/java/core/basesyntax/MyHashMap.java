139e7cf6a2cd103517258f0f1e85ceace746cd81
Denis Moroz <moroz06denis@gmail.com>
2023-07-13 14:13:20 +0200

    implemented MyHashMap class

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,100 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;
+    private static final float LOAD_FACTOR = 0.75f;
+    private static final int RESIZE_FACTOR = 2;
+    private Node<K, V>[] buckets;
+    private int size;
+
+    public MyHashMap() {
+        buckets = new Node[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    private static class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 
     @Override
     public void put(K key, V value) {
+        checkCapacity();
+        int index = getIndex(key);
+        if (buckets[index] == null) {
+            Node<K, V> newNode = new Node<>(key, value);
+            buckets[index] = newNode;
+            size++;
+            return;
+        }
+        Node<K, V> node = buckets[index];
+        while (node != null) {
+            if (Objects.equals(node.key, key)) {
+                node.value = value;
+                return;
+            }
+            node = node.next;
+        }
 
+        Node<K, V> newNode = new Node<>(key, value);
+        newNode.next = buckets[index];
+        buckets[index] = newNode;
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        int index = getIndex(key);
+        Node<K, V> node = buckets[index];
+        while (node != null) {
+            if (Objects.equals(node.key, key)) {
+                return node.value;
+            }
+            node = node.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private void checkCapacity() {
+        if (size == buckets.length * LOAD_FACTOR) {
+            increaseCapacity();
+        }
+    }
+
+    private void increaseCapacity() {
+        int newCapacity = buckets.length * RESIZE_FACTOR;
+        Node<K, V>[] oldBucket = buckets;
+        buckets = (Node<K, V>[]) new Node[newCapacity];
+        size = 0;
+
+        for (Node<K, V> node : oldBucket) {
+            while (node != null) {
+                final Node<K, V> nextNode = node.next;
+                int index = getIndex(node.key);
+                node.next = buckets[index];
+                buckets[index] = node;
+                size++;
+                node = nextNode;
+            }
+        }
+    }
+
+    private int getIndex(K key) {
+        if (key == null) {
+            return 0;
+        }
+        return Math.abs(key.hashCode()) % buckets.length;
     }
 }
