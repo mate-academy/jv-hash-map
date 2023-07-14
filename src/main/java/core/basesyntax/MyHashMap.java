62bc713bafda04c751cc8ece6df2d0aca6daff29
Visqe <veetee@tutanota.com>
2023-07-14 17:29:26 +0300

    style improvements

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -4,23 +4,23 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int STOCK_ARR_LENGTH = 16;
     private static final float MAX_FILL = 0.75f;
     private static final int GROWTH_COEFICIENT = 2;
-    private Node[] arr;
+    private Node<K,V>[] table;
     private int size;
 
     public MyHashMap() {
-        this.arr = new Node[STOCK_ARR_LENGTH];
+        this.table = (Node<K, V>[]) new Node[STOCK_ARR_LENGTH];
     }
 
     @Override
     public void put(K key, V value) {
         int pos = bucketFor(key);
-        if (arr[pos] == null) {
-            arr[pos] = new Node<>(key, value);
+        if (table[pos] == null) {
             increaseSize();
+            table[pos] = new Node<>(key, value);
             return;
         }
-        Node tempNode = findNode(key);
-        if (keysAreEqual(key, (K)tempNode.key)) {
+        Node<K,V> tempNode = closestNode(key);
+        if (keysAreEqual(key, tempNode.key)) {
             tempNode.value = value;
             return;
         }
@@ -30,9 +30,11 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public V getValue(K key) {
-        Node tempNode = findNode(key);
-        return arr[bucketFor(key)] == null || !keysAreEqual(key, (K)findNode(key).key)
-                ? null : (V)tempNode.value;
+        Node<K,V> tempNode = closestNode(key);
+        return tempNode != null
+                && tempNode.hash == hashOf(key)
+                && (keysAreEqual(tempNode.key, key))
+                ? tempNode.value : null;
     }
 
     @Override
@@ -41,36 +43,36 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private int bucketFor(K key) {
-        return key == null ? 0 : Math.abs(key.hashCode()) % arr.length;
+        return key == null ? 0 : Math.abs(key.hashCode()) % table.length;
     }
 
-    private Node findNode(K key) {
-        Node currNode = arr[bucketFor(key)];
+    private Node<K,V> closestNode(K key) {
+        Node<K,V> currNode = table[bucketFor(key)];
         if (currNode == null) {
             return null;
         }
         while (currNode.next != null) {
-            if (keysAreEqual(key, (K)currNode.key)) {
+            if (currNode.hash == hashOf(key) && keysAreEqual(currNode.key, key)) {
                 return currNode;
             }
             currNode = currNode.next;
         }
-        return currNode; //last node in bucket
+        return currNode;
     }
 
     private void increaseSize() {
-        if (++size >= arr.length * MAX_FILL) {
+        if (++size >= table.length * MAX_FILL) {
             growArr();
         }
     }
 
     private void growArr() {
-        Node[] oldArrCash = arr;
-        arr = new Node[arr.length * GROWTH_COEFICIENT];
+        Node<K,V>[] oldArrCash = table;
+        table = (Node<K, V>[])new Node[table.length * GROWTH_COEFICIENT];
         size = 0;
-        for (Node node : oldArrCash) {
+        for (Node<K,V> node : oldArrCash) {
             while (node != null) {
-                this.put((K)node.key, (V)node.value);
+                this.put(node.key, node.value);
                 node = node.next;
             }
         }
@@ -80,14 +82,24 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return key1 == key2 || key1 != null && key1.equals(key2);
     }
 
-    static class Node<K, V> {
+    private static int hashOf(Object key) {
+        if (key == null) {
+            return 0;
+        } else {
+            return key.hashCode();
+        }
+    }
+
+    private static class Node<K, V> {
+        private final int hash;
         private final K key;
         private V value;
-        private Node next;
+        private Node<K, V> next;
 
         public Node(K key, V value) {
             this.key = key;
             this.value = value;
+            hash = hashOf(key);
         }
     }
 }
