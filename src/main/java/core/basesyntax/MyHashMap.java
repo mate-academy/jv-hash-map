8cdcc19f29db7ee262aa7fa33acefb48c8fde59c
Влад <dakadofordisluv@gmail.com>
2023-07-14 11:16:44 +0300

    added 'threshold' instance

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -8,9 +8,11 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int TABLE_MAGNIFICATION_FACTOR = 2;
     private Node<K,V>[] table;
     private int size;
+    private int threshold;
 
     public MyHashMap() {
         table = new Node[DEFAULT_INITIAL_CAPACITY];
+        threshold = (int)(DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
     }
 
     @Override
@@ -67,14 +69,15 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private void increment() {
         size++;
-        if (size >= table.length * DEFAULT_LOAD_FACTOR) {
+        if (size >= threshold) {
             resize();
         }
     }
 
     private void resize() {
-        Node<K, V>[] oldTable = table;
+        final Node<K, V>[] oldTable = table;
         int newCapacity = table.length * TABLE_MAGNIFICATION_FACTOR;
+        threshold = (int)(newCapacity * DEFAULT_LOAD_FACTOR);
         table = new Node[newCapacity];
         size = 0;
         for (Node<K, V> node : oldTable) {
@@ -85,7 +88,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private int keyHash(K key) {
-        return key == null ? 0 : key.hashCode() < 0 ? key.hashCode() * -1 : key.hashCode();
+        return key == null ? 0 : Math.abs(key.hashCode());
     }
 
     private int indexByHash(int hash) {
