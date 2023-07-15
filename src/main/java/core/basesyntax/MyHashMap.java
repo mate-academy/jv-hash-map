b721fa0c84462589e376f86090ce0d4d3931b23b
satori1111 <romakuchmii@gmail.com>
2023-07-15 17:29:45 +0300

    added solution

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,106 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private static final double LOAD_FACTOR = 0.75;
+    private int size;
+    private Node<K, V>[] table;
+    private int threshold;
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_INITIAL_CAPACITY];
+        threshold = (int) (LOAD_FACTOR * DEFAULT_INITIAL_CAPACITY);
+    }
 
     @Override
     public void put(K key, V value) {
-
+        if (size == threshold) {
+            resize();
+        }
+        Node<K, V> node = new Node<>(getHash(key), key, value, null);
+        int index = getIndex(node.key);
+        if (table[index] == null) {
+            table[index] = node;
+        } else if (putIfKeyContainsInHashMap(node, index)) {
+            return;
+        } else {
+            Node<K, V> currentNode = table[index];
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = node;
+        }
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        int index = getIndex(key);
+        Node<K, V> currentNode = table[index];
+        while (currentNode != null) {
+            if (currentNode.key == key
+                    || currentNode.key != null && currentNode.key.equals(key)) {
+                return currentNode.value;
+            }
+            currentNode = currentNode.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private int getHash(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode());
+    }
+
+    private int getIndex(K key) {
+        return key == null ? 0 : getHash(key) % table.length;
+    }
+
+    private void resize() {
+        Node<K, V>[] oldTable = table;
+        int newLength = oldTable.length * 2;
+        table = new Node[newLength];
+        threshold = (int) (LOAD_FACTOR * table.length);
+        size = 0;
+        for (Node<K, V> node : oldTable) {
+            if (node != null) {
+                put(node.key, node.value);
+                while (node.next != null) {
+                    put(node.next.key, node.next.value);
+                    node = node.next;
+                }
+            }
+        }
+    }
+
+    private boolean putIfKeyContainsInHashMap(Node<K, V> node, int index) {
+        Node<K, V> currentNode = table[index];
+        while (currentNode != null) {
+            if (currentNode.key == node.key
+                    || currentNode.key != null && currentNode.key.equals(node.key)) {
+                currentNode.value = node.value;
+                return true;
+            }
+            currentNode = currentNode.next;
+        }
+        return false;
+    }
+
+    private class Node<K, V> {
+        private final int hash;
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
