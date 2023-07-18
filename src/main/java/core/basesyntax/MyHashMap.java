284b28f2588099592844105187a791f891fcf079
Ivan Diatliuk <dyatlyukk.ivan@gmail.com>
2023-07-18 15:09:02 +0300

    fixed all mistakes after the review

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,27 +5,28 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final float LOAD_FACTOR = 0.75f;
     private static final int RESIZE_COEFFICIENT = 2;
     private Node<K, V>[] table;
-    private int capacity;
     private int threshold;
     private int size;
 
     public MyHashMap() {
         table = new Node[DEFAULT_CAPACITY];
-        capacity = DEFAULT_CAPACITY;
         threshold = (int) (LOAD_FACTOR * DEFAULT_CAPACITY);
     }
 
     @Override
     public void put(K key, V value) {
-        putValue(hash(key), key, value);
+        int index = calculateIndexByKey(key);
+        putValue(index, key, value);
     }
 
     @Override
     public V getValue(K key) {
-        int index = hash(key);
+        int index = calculateIndexByKey(key);
         Node<K, V> node = table[index];
+
         while (node != null) {
-            if ((node.key != null && node.key.equals(key)) || node.key == key) {
+            K currentKey = node.key;
+            if (compareKeys(currentKey, key)) {
                 return node.value;
             }
             node = node.next;
@@ -38,20 +39,24 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private int hash(K key) {
-        return (key == null) ? 0 : Math.abs(key.hashCode()) % capacity;
+    private int getHashForKey(K key) {
+        return (key == null) ? 0 : Math.abs(key.hashCode());
     }
 
-    private void putValue(int hash, K key, V value) {
+    private int calculateIndexByKey(K key) {
+        return getHashForKey(key) % table.length;
+    }
+
+    private void putValue(int index, K key, V value) {
         if (size + 1 > threshold) {
             resize();
         }
-        int index = hash;
         Node<K, V> node = table[index];
-        Node<K, V> newNode = new Node<>(hash, key, value, null);
+        Node<K, V> newNode = new Node<>(key, value, null);
 
         while (node != null) {
-            if (node.key == key || (node.key != null && node.key.equals(key))) {
+            K currentKey = node.key;
+            if (compareKeys(currentKey, key)) {
                 node.value = value;
                 return;
             } else if (node.next == null) {
@@ -65,12 +70,16 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         size++;
     }
 
+    private boolean compareKeys(K fistKey, K secondKey) {
+        return fistKey == secondKey || (fistKey != null && fistKey.equals(secondKey));
+    }
+
     private void resize() {
-        capacity = capacity * RESIZE_COEFFICIENT;
-        threshold = (int) (LOAD_FACTOR * capacity);
+        int newLength = table.length * RESIZE_COEFFICIENT;
+        threshold = (int) (LOAD_FACTOR * newLength);
 
         Node<K, V>[] oldTable = table;
-        table = new Node[capacity];
+        table = new Node[newLength];
         size = 0;
         transferNodesFromOldTable(oldTable);
     }
@@ -86,13 +95,11 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private class Node<K, V> {
-        private final int hash;
         private final K key;
         private V value;
         private Node<K, V> next;
 
-        public Node(int hash, K key, V value, Node<K, V> next) {
-            this.hash = hash;
+        public Node(K key, V value, Node<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
