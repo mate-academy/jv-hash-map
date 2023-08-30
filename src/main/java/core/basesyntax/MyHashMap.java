5dbabb7562f7067d0347a7298fdd3dcb17d64fe2
Mykyta Pavlov <pavlov2021mf12@student.karazin.ua>
2023-08-30 11:15:50 +0300

    implemented custom HashMap class

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,96 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private int size;
+    private int threshold;
+    private Node<K, V>[] table;
+
+    public MyHashMap() {
+        table = (Node<K, V>[]) new Node[DEFAULT_CAPACITY];
+        threshold = (int) (DEFAULT_CAPACITY * DEFAULT_LOAD_FACTOR);
+    }
 
     @Override
     public void put(K key, V value) {
-
+        increaseCapacity();
+        int index = getIndex(key);
+        Node<K, V> current = table[index];
+        if (current == null) {
+            table[index] = new Node<>(key, value, null);
+            size++;
+        } else {
+            putOrSet(key, value, current);
+        }
     }
 
     @Override
     public V getValue(K key) {
+        int index = getIndex(key);
+        Node<K, V> current = table[index];
+        while (current != null) {
+            if (current.key == key || key != null && key.equals(current.key)) {
+                return current.value;
+            }
+            current = current.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private void increaseCapacity() {
+        if (size >= threshold) {
+            resize();
+        }
+    }
+
+    private void resize() {
+        int newCapacity = table.length << 1;
+        threshold = threshold << 1;
+        Node<K, V>[] oldTable = table;
+        table = (Node<K, V>[]) new Node[newCapacity];
+        size = 0;
+        for (Node<K, V> current : oldTable) {
+            while (current != null) {
+                put(current.key, current.value);
+                current = current.next;
+            }
+        }
+    }
+
+    private void putOrSet(K key, V value, Node<K, V> current) {
+        while (current != null) {
+            if (current.key == key || key != null && key.equals(current.key)) {
+                current.value = value;
+                return;
+            }
+            if (current.next == null) {
+                break;
+            }
+            current = current.next;
+        }
+        current.next = new Node<>(key, value, null);
+        size++;
+    }
+
+    private int getIndex(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode() % table.length);
+    }
+
+    private static class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        private Node(K key, V value, Node<K, V> node) {
+            this.key = key;
+            this.value = value;
+            this.next = node;
+        }
     }
 }
