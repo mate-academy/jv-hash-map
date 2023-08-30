8b561bcbd5f311eb02638206d082dd679e346bab
Salnikov Markiian <salnikov.markiyan@gmail.com>
2023-08-30 18:28:41 +0300

    fixed all comments

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,44 +1,33 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;
-    static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
     private static final int COEFFICIENT_FOR_INCREASE = 2;
+    private Node<K, V>[] table;
+    private int threshold;
     private int size;
-    private Node<K, V>[] tableOfNodes;
 
     public MyHashMap() {
-        tableOfNodes = new Node[DEFAULT_INITIAL_CAPACITY];
-    }
-
-    private void increaseSize() {
-        size = 0;
-        Node<K, V>[] oldBuckets = tableOfNodes;
-        tableOfNodes = new Node[tableOfNodes.length * COEFFICIENT_FOR_INCREASE];
-        for (Node<K, V> node : oldBuckets) {
-            while (node != null) {
-                put(node.key, node.value);
-                node = node.next;
-            }
-        }
+        table = new Node[DEFAULT_INITIAL_CAPACITY];
+        threshold = (int) (table.length * DEFAULT_LOAD_FACTOR);
     }
 
     @Override
     public void put(K key, V value) {
         Node<K, V> newNode = new Node<>(key, value);
-        int threshold = (int) (tableOfNodes.length * DEFAULT_LOAD_FACTOR);
         if (threshold == size) {
             increaseSize();
         }
 
         int index = calculateIndex(key);
-        if (tableOfNodes[index] == null) {
-            tableOfNodes[index] = newNode;
+        if (table[index] == null) {
+            table[index] = newNode;
             size++;
             return;
         }
 
-        Node<K, V> node = tableOfNodes[index];
+        Node<K, V> node = table[index];
         while (node != null) {
             if (node.key == key
                     || node.key != null && node.key.equals(key)) {
@@ -47,15 +36,15 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             }
             node = node.next;
         }
-        newNode.next = tableOfNodes[index];
-        tableOfNodes[index] = newNode;
+        newNode.next = table[index];
+        table[index] = newNode;
         size++;
     }
 
     @Override
     public V getValue(K key) {
         int index = calculateIndex(key);
-        Node<K, V> node = tableOfNodes[index];
+        Node<K, V> node = table[index];
         while (node != null) {
             if (node.key == key
                     || node.key != null && node.key.equals(key)) {
@@ -71,8 +60,20 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
+    private void increaseSize() {
+        size = 0;
+        Node<K, V>[] oldBuckets = table;
+        table = new Node[table.length * COEFFICIENT_FOR_INCREASE];
+        for (Node<K, V> node : oldBuckets) {
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.next;
+            }
+        }
+    }
+
     private int calculateIndex(K key) {
-        return (key == null) ? 0 : Math.abs(key.hashCode()) % tableOfNodes.length;
+        return key == null ? 0 : Math.abs(key.hashCode()) % table.length;
     }
 
     private static class Node<K, V> {
