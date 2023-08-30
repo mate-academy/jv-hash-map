7a17d56e00f6b6e4c1dea39c1fea085cdf3f7839
Oleksandr Tymoshenko <aleksandrtimosenko089@gmail.com>
2023-08-30 16:40:35 +0300

    improved solution

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,17 +1,16 @@
 package core.basesyntax;
 
-import java.util.Map;
 import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
+    private static final int SIZE_INCREASE = 2;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
     private Node<K, V>[] table;
     private int size;
     private int capacity;
     private int threshold;
 
-    @SuppressWarnings("unchecked")
     public MyHashMap() {
         table = (Node<K, V>[]) new Node[DEFAULT_CAPACITY];
         capacity = DEFAULT_CAPACITY;
@@ -25,11 +24,11 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
         Node<K, V> newNode;
         if (key == null) {
-            newNode = new Node<>(0, key, value, null);
+            newNode = new Node<>(key, value, null);
         } else {
-            newNode = new Node<>(key.hashCode(), key, value, null);
+            newNode = new Node<>(key, value, null);
         }
-        int position = hash(key);
+        int position = getPosition(key);
         if (table[position] == null) {
             table[position] = newNode;
         } else if (Objects.equals(table[position].key, key)) {
@@ -51,20 +50,16 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public V getValue(K key) {
-        int position = hash(key);
+        int position = getPosition(key);
         if (table[position] == null) {
             return null;
         }
-        if (Objects.equals(table[position].key, key)) {
-            return table[position].value;
-        } else {
-            Node<K, V> itemInTable = table[position];
-            while (itemInTable.next != null) {
-                itemInTable = itemInTable.next;
-                if (Objects.equals(itemInTable.key, key)) {
-                    return itemInTable.value;
-                }
+        Node<K, V> itemInTable = table[position];
+        while (itemInTable != null) {
+            if (Objects.equals(itemInTable.key, key)) {
+                return itemInTable.value;
             }
+            itemInTable = itemInTable.next;
         }
         return null;
     }
@@ -74,57 +69,33 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    @SuppressWarnings("unchecked")
     private void resize() {
         size = 0;
-        capacity *= 2;
+        capacity *= SIZE_INCREASE;
         threshold = (int) (capacity * DEFAULT_LOAD_FACTOR);
         Node<K, V>[] oldTab = table;
         table = (Node<K, V>[]) new Node[capacity];
-        for (Node<K, V> kvNode : oldTab) {
-            if (kvNode != null) {
-                Node<K, V> nextItem = kvNode.next;
-                put(kvNode.key, kvNode.value);
-                while (nextItem != null) {
-                    put(nextItem.key, nextItem.value);
-                    nextItem = nextItem.next;
-                }
+        for (Node<K, V> oldNode : oldTab) {
+            while (oldNode != null) {
+                put(oldNode.key, oldNode.value);
+                oldNode = oldNode.next;
             }
         }
     }
 
-    private int hash(K key) {
+    private int getPosition(K key) {
         return key == null ? 0 : Math.abs(key.hashCode() % capacity);
     }
 
-    private static class Node<K, V> implements Map.Entry<K, V> {
-        protected int hash;
-        protected K key;
-        protected V value;
-        protected Node<K, V> next;
+    private static class Node<K, V> {
+        private K key;
+        private V value;
+        private Node<K, V> next;
 
-        public Node(int hash, K key, V value, Node<K, V> next) {
-            this.hash = hash;
+        public Node(K key, V value, Node<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
-        }
-
-        @Override
-        public K getKey() {
-            return key;
-        }
-
-        @Override
-        public V getValue() {
-            return value;
-        }
-
-        @Override
-        public V setValue(V value) {
-            V oldValue = this.value;
-            this.value = value;
-            return oldValue;
         }
     }
 }
