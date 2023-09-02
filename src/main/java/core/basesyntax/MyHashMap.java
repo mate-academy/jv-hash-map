b4f677c2a1d37d86fbc40e8dc5f9a613e4fb556c
Denis Bohusevich <bogusevich03@gmail.com>
2023-09-03 02:27:59 +0300

    map

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -6,24 +6,24 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static int MULTYPLYCATION = 2;
     private static double LOAD_FACTOR = 0.75;
-    private int threshold = (int) (DEFAULT_CAPACITY * LOAD_FACTOR);
+    private int threshold;
     private int size;
-    private int capacity;
-    private Node<K, V>[] hashMap;
+
+    private Node<K, V>[] table;
 
     public MyHashMap() {
-        hashMap = new Node[DEFAULT_CAPACITY];
-        this.capacity = DEFAULT_CAPACITY;
+        table = new Node[DEFAULT_CAPACITY];
+        threshold = (int) (DEFAULT_CAPACITY * LOAD_FACTOR);
     }
 
     @Override
     public void put(K key, V value) {
         increaseCapacity();
         int index = getIndex(key);
-        Node<K, V> node = hashMap[index];
+        Node<K, V> node = table[index];
         Node<K, V> newNode = new Node<>(key, value);
         if (node == null) {
-            hashMap[index] = newNode;
+            table[index] = newNode;
         } else {
             Node<K, V> prev = node;
             while (node != null) {
@@ -42,8 +42,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public V getValue(K key) {
         int index = getIndex(key);
-        Node<K, V> node = hashMap[index];
-        Node<K, V> prev = node;
+        Node<K, V> node = table[index];
         while (node != null) {
             if (Objects.equals(key, node.key)) {
                 return node.value;
@@ -59,26 +58,21 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private int getIndex(K key) {
-        return hashCode(key) % capacity;
+        return hashCode(key) % table.length;
     }
 
     private void increaseCapacity() {
         if (size >= threshold) {
-            int newCapacity = capacity * MULTYPLYCATION;
-            Node<K, V>[] newHashMap = new Node[newCapacity];
-            capacity = newCapacity;
-            threshold = (int) (capacity * LOAD_FACTOR);
-            for (Node singleNode : hashMap) {
-                Node<K, V> node = singleNode;
-                while (node != null) {
-                    Node<K, V> next = node.next;
-                    int index = getIndex(node.key);
-                    node.next = newHashMap[index];
-                    newHashMap[index] = node;
-                    node = next;
+            size = 0;
+            Node<K, V>[] oldTable = table;
+            table = new Node[table.length * MULTYPLYCATION];
+            threshold = (int) (table.length * LOAD_FACTOR);
+            for (Node<K, V> singleNode : oldTable) {
+                while (singleNode != null) {
+                    put(singleNode.key, singleNode.value);
+                    singleNode = singleNode.next;
                 }
             }
-            hashMap = newHashMap;
         }
     }
 
