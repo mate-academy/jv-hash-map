39895cd618e230cea3a1ead451e7000ebac56fcc
Andreyepam <epameducation@gmail.com>
2023-09-04 16:48:51 +0300

    put

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -13,23 +13,50 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         buckets = new Node[DEFAULT_CAPACITY];
     }
 
-    @Override
     public void put(K key, V value) {
-        resizeIfNeeded();
-        int index = getIndex(key);
-        Node<K, V> node = buckets[index];
-        while (node != null) {
-            if (Objects.equals(key, node.key)) {
-                node.value = value;
-                return;
+        if (key == null) {
+            resize();
+            int index = 0;
+            Node<K, V> node = buckets[index];
+            while (node != null) {
+                if (Objects.equals(key, node.key)) {
+                    node.value = value;
+                    return;
+                }
+                node = node.next;
             }
-            node = node.next;
+            buckets[index] = new Node<>(key, value, buckets[index]);
+            size++;
+        } else {
+            int index = getIndex(key);
+            Node<K, V> newNode = new Node<>(key, value, null);
+            if (buckets[index] == null) {
+                buckets[index] = newNode;
+                size++;
+            } else {
+                Node<K, V> currentNode = buckets[index];
+                Node<K, V> prevNode = null;
+                while (currentNode != null) {
+                    if (Objects.equals(key, currentNode.key)) {
+                        currentNode.value = value;
+                        return;
+                    }
+                    prevNode = currentNode;
+                    currentNode = currentNode.next;
+                }
+                if (prevNode != null) {
+                    Node<K, V> newNextNode = prevNode.next;
+                    prevNode.next = newNode;
+                    newNode.next = newNextNode;
+                    size++;
+                }
+            }
+            if ((float) size / buckets.length >= LOAD_FACTOR) {
+                resize();
+            }
         }
-        buckets[index] = new Node<>(hash(key), key, value, buckets[index]);
-        size++;
     }
 
-    @Override
     public V getValue(K key) {
         for (Node<K, V> node : buckets) {
             while (node != null) {
@@ -51,9 +78,27 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return index > 0 ? index : Math.abs(index);
     }
 
-    private void resizeIfNeeded() {
+    private void resize() {
         if ((float) size / buckets.length >= LOAD_FACTOR) {
             int newCapacity = buckets.length * 2;
+            Node<K, V>[] newBuckets = new Node[newCapacity];
+            int newSize = 0;
+
+            for (Node<K, V> node : buckets) {
+                while (node != null) {
+                    Node<K, V> nextNode = node.next;
+                    int newIndex = getIndex(node.key);
+                    node.next = newBuckets[newIndex];
+                    newBuckets[newIndex] = node;
+                    node = nextNode;
+                    newSize++;
+                }
+            }
+
+            buckets = newBuckets;
+            size = newSize;
+        } else if (size < buckets.length / 2) {
+            int newCapacity = buckets.length / 2;
             Node<K, V>[] newBuckets = new Node[newCapacity];
             int newSize = 0;
 
@@ -73,18 +118,12 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    private int hash(K key) {
-        return (key == null) ? 0 : (key.hashCode() ^ (key.hashCode() >>> 16));
-    }
-
     private static class Node<K, V> {
-        private final int hash;
         private final K key;
         private V value;
         private Node<K, V> next;
 
-        public Node(int hash, K key, V value, Node<K, V> next) {
-            this.hash = hash;
+        public Node(K key, V value, Node<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
