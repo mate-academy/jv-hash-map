db828b5562e7baa53fe7ce41fc57187f33af977d
Epikano <epikano@gmail.com>
2023-09-09 02:06:06 +0300

    Added jv-hash-map solution.

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,88 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private Node<K,V>[] table;
+    private int size;
+    private int threshold;
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_INITIAL_CAPACITY];
+        threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
+    }
 
     @Override
     public void put(K key, V value) {
-
+        if (size >= threshold) {
+            resize();
+        }
+        int hashOfKey = getHash(key);
+        Node<K, V> newNode = new Node<>(key, value, null);
+        if (table[hashOfKey] == null) {
+            table[hashOfKey] = newNode;
+        } else {
+            Node<K, V> currentNode = table[hashOfKey];
+            while (true) {
+                if (Objects.equals(currentNode.key, newNode.key)) {
+                    currentNode.value = newNode.value;
+                    return;
+                }
+                if (currentNode.next == null) {
+                    currentNode.next = newNode;
+                    break;
+                }
+                currentNode = currentNode.next;
+            }
+        }
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        Node<K, V> currentNode = table[getHash(key)];
+        while (currentNode != null) {
+            if (Objects.equals(currentNode.key, key)) {
+                return currentNode.value;
+            }
+            currentNode = currentNode.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private int getHash(K key) {
+        return (key == null) ? 0 : Math.abs((key.hashCode() % table.length));
+    }
+
+    private void resize() {
+        threshold = threshold << 1;
+        Node<K,V>[] oldTable = table;
+        table = new Node[table.length << 1];
+        size = 0;
+        for (Node<K, V> nodeValue : oldTable) {
+            while (nodeValue != null) {
+                put(nodeValue.key, nodeValue.value);
+                nodeValue = nodeValue.next;
+            }
+        }
+    }
+
+    private static class Node<K,V> {
+        private final K key;
+        private V value;
+        private Node<K,V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
