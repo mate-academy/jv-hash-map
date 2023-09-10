469c6b7408c8db8cda443d40630d3b1df9fbd866
Andrei_Bondarenko <bondorol007@gmail.com>
2023-09-10 14:19:32 +0300

    I don't know, what I should do with the third remark... I removed magic number and renamed some fields and methods. I do general method putKey. I can't create not private fields in class Node, because Maven throw : "Error:  /home/runner/work/jv-hash-map/jv-hash-map/src/main/java/core/basesyntax/MyHashMap.java:14:19: Variable 'hash' must be private and have accessor methods. [VisibilityModifier]"

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -11,10 +11,10 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private static class Node<K, V> {
-        final int hash;
-        final K key;
-        V value;
-        Node<K, V> next;
+        private final int hash;
+        private final K key;
+        private V value;
+        private Node<K, V> next;
 
         public Node(int hash, K key, V value, Node<K, V> next) {
             this.hash = hash;
