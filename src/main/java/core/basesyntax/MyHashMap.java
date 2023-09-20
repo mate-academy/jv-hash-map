d5bf423d8a7381be1f2ab06789553b430a2fa3b3
Oleksandr Pidhaiko <iviviv1999@gmail.com>
2023-09-21 00:41:01 +0300

    simplified code,private class and methods moved after public ones,put() used in resize()

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private static final int INCREASE_RATIO = 2;
     private int threshold;
     private final float loadFactor;
     private Node<K, V>[] buckets;
@@ -26,21 +27,18 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         threshold = (int) (initialCapacity * loadFactor);
     }
 
-    private static class Node<K, V> {
-        private final K key;
-        private V value;
-        private Node<K, V> next;
-
-        Node(K key, V value) {
-            this.key = key;
-            this.value = value;
-        }
-
-    }
-
     @Override
     public void put(K key, V value) {
         int index = computeIndex(key);
+        Node<K, V> nodeToBeAdded = new Node<>(key, value);
+        if (size == threshold) {
+            resize();
+        }
+        if (buckets[index] == null) {
+            buckets[index] = nodeToBeAdded;
+            size++;
+            return;
+        }
         Node<K, V> current = buckets[index];
         while (current != null) {
             if (Objects.equals(key, current.key)) {
@@ -50,11 +48,8 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                 current = current.next;
             }
         }
-        if (size > threshold) {
-            resize();
-            index = computeIndex(key);
-        }
-        addNode(key, value, index);
+        nodeToBeAdded.next = buckets[index];
+        buckets[index] = nodeToBeAdded;
         size++;
     }
 
@@ -76,32 +71,31 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private int computeIndex(K key) {
-        return Math.abs(getHash(key) % buckets.length);
-    }
+    private static class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
 
-    private void resize() {
-        Node<K, V>[] oldBuckets = buckets;
-        int newCapacity = buckets.length * 2;
-        threshold = (int) (newCapacity * loadFactor);
-        buckets = (Node<K, V>[]) new Node[newCapacity];
-        for (Node<K, V> oldBucket : oldBuckets) {
-            Node<K, V> current = oldBucket;
-            while (current != null) {
-                int index = computeIndex(current.key);
-                addNode(current.key, current.value, index);
-                current = current.next;
-            }
+        Node(K key, V value) {
+            this.key = key;
+            this.value = value;
         }
     }
 
-    private void addNode(K key, V value, int index) {
-        Node<K, V> nodeToBeAdded = new Node<>(key, value);
-        nodeToBeAdded.next = buckets[index];
-        buckets[index] = nodeToBeAdded;
+    private int computeIndex(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode() % buckets.length);
     }
 
-    private static int getHash(Object key) {
-        return key == null ? 0 : key.hashCode();
+    private void resize() {
+        size = 0;
+        Node<K, V>[] oldBuckets = buckets;
+        int newCapacity = buckets.length * INCREASE_RATIO;
+        buckets = (Node<K, V>[]) new Node[newCapacity];
+        for (Node<K, V> current : oldBuckets) {
+            while (current != null) {
+                put(current.key, current.value);
+                current = current.next;
+            }
+        }
     }
 }
