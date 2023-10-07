ee21b1a0fedf078ea900703e3587d73c39357519
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2023-10-07 22:08:05 +0300

    changed the code

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,20 +1,27 @@
 package core.basesyntax;
+import
+import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
-    private Node<K,V> [] table;
+    private static final float LOAD_FACTOR = 0.75f;
     private int size;
-    private int newThr = 12;
-    private int capacity = DEFAULT_CAPACITY;
-    private class Node <K,V> {
+    private Node<K, V>[] table;
+
+    public MyHashMap() {
+        this.table = new Node[DEFAULT_CAPACITY];
+    }
+
+    private class Node<K, V> {
         private final int hashCode;
         private final K key;
         private V value;
-        private Node<K,V> next;
-        public Node(K key, V value, Node<K,V> next) {
+        private Node<K, V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
             this.key = key;
             this.value = value;
-            this.hashCode = key.hashCode();
+            this.hashCode = key == null ? 0 : key.hashCode();
             this.next = next;
         }
 
@@ -23,35 +30,28 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public void put(K key, V value) {
-        if (key == null && table[0] != null) {
-            table[0] = new Node<>(key, value, null);
+        if (size >= LOAD_FACTOR * table.length) {
+            resize();
+        }
+        Node<K, V> currentNode = table[calculateIndex(key)];
+        Node<K, V> newNode = new Node<>(key, value, null);
+        if (currentNode == null) {
+            table[calculateIndex(key)] = newNode;
             size++;
             return;
         }
-        if (++size > newThr) {
-            resize();
-        }
-        int index = (int) hashCode(key) % capacity;
-        if (table[index] == null) {
-            table[index] = new Node<>(key, value, null);
-            size++;
-        } else {
-            addToBucket(key, value, index, table);
-            size++;
-        }
+        connectNode(currentNode, newNode);
     }
+
 
     @Override
     public V getValue(K key) {
-        if (table == null) {
-            return null;
-        }
-        for (Node<K,V> node : table) {
-            if (node.key.equals(key)) {
-                return node.value;
-            } else if (node.key == null) {
-                return table[0].value;
+        Node<K, V> currentNode = table[calculateIndex(key)];
+        while (currentNode != null) {
+            if (Objects.equals(currentNode.key, key)) {
+                return currentNode.value;
             }
+            currentNode = currentNode.next;
         }
         return null;
     }
@@ -61,38 +61,39 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private int hashCode(K key) {
-        int result = 31;
-        result = 31 * (key == null? 0 : key.hashCode());
-        return result;
-    }
 
     private void resize() {
-        capacity = capacity * 2;
-        Node<K, V> [] newTable = new Node[capacity];
-        newThr = newThr * 2;
-        for (Node<K, V> node : table) {
-            int indexResize = (int) node.key.hashCode() % capacity;
-            if (newTable[indexResize] == null) {
-                newTable[indexResize] = new Node<>(node.key, node.value, null);
-            } else {
-               addToBucket(node.key, node.value, indexResize, newTable);
+        Node<K, V>[] oldTable = table;
+        size = 0;
+        table = new Node[oldTable.length << 1];
+        for (Node<K, V> node : oldTable) {
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.next;
             }
         }
-        table = newTable;
     }
 
-    private void addToBucket(K key, V value, int index, Node<K,V> [] typeTable) {
-        Node<K,V> resultNode = new Node<>(key, value, null);
-        Node<K,V> currentNode = typeTable[index];
-        while (currentNode.next != null || currentNode.key != key) {
+    private int calculateIndex(K key) {
+        int hashCode = key == null ? 0 : key.hashCode();
+        int index = Math.abs(hashCode) % table.length;
+        return index;
+    }
+
+    private void connectNode(Node<K, V> currentNode, Node<K, V> newNode) {
+        while (currentNode != null) {
+            if (Objects.equals(currentNode.key, newNode.key)) {
+                currentNode.value = newNode.value;
+                return;
+            }
+            if (currentNode.next == null) {
+                currentNode.next = newNode;
+                size++;
+                return;
+            }
             currentNode = currentNode.next;
         }
-        if (currentNode.key == key) {
-            currentNode.value = value;
-            return;
-        }
-        resultNode = currentNode.next;
-        currentNode.next = resultNode;
     }
+
 }
+
