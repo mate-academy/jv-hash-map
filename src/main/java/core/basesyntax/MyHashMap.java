421584ad415a87bb3821695024a27a6638d18856
Dima Semeniuk <semeniuk.dima@ukr.net>
2023-10-07 10:59:26 +0300

    implemented MyHashMap with some methods

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,96 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+    private Node<K, V>[] table;
+    private int threshold;
+    private int size;
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_CAPACITY];
+        threshold = (int) (LOAD_FACTOR * DEFAULT_CAPACITY);
+    }
+
+    static class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
 
     @Override
     public void put(K key, V value) {
-
+        resizeIfNeeded();
+        if (table[hash(key)] == null) {
+            table[hash(key)] = new Node<>(key, value, null);
+            size++;
+        } else {
+            putIfCollision(key, value);
+        }
     }
 
     @Override
     public V getValue(K key) {
+        Node<K, V> node = table[hash(key)];
+        while (node != null) {
+            if (key == node.key || (key != null && key.equals(node.key))) {
+                return node.value;
+            }
+            node = node.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private int hash(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode()) % table.length;
+    }
+
+    private void resizeIfNeeded() {
+        if (size == threshold) {
+            int oldCapacity = table.length;
+            int newCapacity = oldCapacity << 1;
+            threshold = (int) (LOAD_FACTOR * table.length);
+            Node<K, V>[] oldTable = table;
+            table = new Node[newCapacity];
+            size = 0;
+            for (Node<K, V> node : oldTable) {
+                while (node != null) {
+                    put(node.key, node.value);
+                    node = node.next;
+                }
+            }
+        }
+    }
+
+    private void putIfCollision(K key, V value) {
+        Node<K, V> node = table[hash(key)];
+        while (node != null) {
+            if (key == node.key || key != null && key.equals(node.key)) {
+                node.value = value;
+                return;
+            }
+            if (node.next == null) {
+                node.next = new Node<K, V>(key, value, null);
+                size++;
+                return;
+            }
+            node = node.next;
+        }
     }
 }
