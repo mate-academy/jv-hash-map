a9d8db09715552db547e1df0187fccd825fee5b3
Volodymyr Ozemblovskyi <Vladimir.ozem.ps@gmail.com>
2023-10-16 19:39:52 +0300

    (refactored) Solve tasks from Readme file

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,34 +5,33 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final double DEFAULT_LOAD_FACTOR = 0.75;
-    private Node<K,V>[] table = (Node<K,V>[]) new Node[DEFAULT_CAPACITY];
+    private static final int GROWTH_COEFFICIENT = 2;
+    private Node<K,V>[] table = new Node[DEFAULT_CAPACITY];
     private int size;
 
     @Override
     public void put(K key, V value) {
         resizeIfNeeded();
-        int index = hashIndex(key);
-        Node<K, V> targetNode = table[index];
-        while (targetNode != null) {
-            if (Objects.equals(targetNode.key, key)) {
-                targetNode.value = value;
+        Node<K, V> node = table[hash(key)];
+        while (node != null) {
+            if (Objects.equals(node.key, key)) {
+                node.value = value;
                 return;
             }
-            if (targetNode.next == null) {
-                targetNode.next = new Node<>(key, value, null);
+            if (node.next == null) {
+                node.next = new Node<>(key, value, null);
                 size++;
                 return;
             }
-            targetNode = targetNode.next;
+            node = node.next;
         }
-        table[index] = new Node<>(key, value, null);
+        table[hash(key)] = new Node<>(key, value, null);
         size++;
     }
 
     @Override
     public V getValue(K key) {
-        int index = hashIndex(key);
-        Node<K, V> node = table[index];
+        Node<K, V> node = table[hash(key)];
         while (node != null) {
             if (Objects.equals(node.key, key)) {
                 return node.value;
@@ -49,23 +48,22 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    static class Node<K,V> {
-        protected final K key;
-        protected V value;
-        protected Node<K,V> next;
+    private static class Node<K,V> {
+        private final K key;
+        private V value;
+        private Node<K,V> next;
 
-        Node(K key, V value, Node<K,V> next) {
+        private Node(K key, V value, Node<K,V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
         }
     }
 
-    private int hashIndex(K key) {
-        int keyHash = (key == null) ? 0 : key.hashCode();
-        if (keyHash < 0) {
-            keyHash = keyHash * (-1);
-        }
+    private int hash(K key) {
+        int keyHash = (key == null) ? 0
+                : (key.hashCode() > 0) ? key.hashCode()
+                : (-1) * key.hashCode();
         return keyHash % table.length;
     }
 
@@ -73,15 +71,12 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         double loadFactor = (double) (size) / table.length;
         if (loadFactor >= DEFAULT_LOAD_FACTOR) {
             Node<K,V>[] oldTable = table;
-            table = (Node<K,V>[]) new Node[table.length * 2];
+            table = new Node[table.length * GROWTH_COEFFICIENT];
             size = 0;
             for (Node<K,V> node: oldTable) {
-                if (node != null) {
+                while (node != null) {
                     put(node.key, node.value);
-                    while (node.next != null) {
-                        node = node.next;
-                        put(node.key, node.value);
-                    }
+                    node = node.next;
                 }
             }
         }
