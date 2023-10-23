c900f656952878d708c48b328a83bd4a89f9cc5f
Nataliia Shtykh <forstudying93@gmail.com>
2023-10-23 10:59:20 +0300

    changed access modifiers

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -73,16 +73,15 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    static class Node<K, V> {
+    private static class Node<K, V> {
         private final K key;
         private V value;
         private Node<K, V> next;
 
-        public Node(K key, V value, Node<K, V> next) {
+        private Node(K key, V value, Node<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
         }
-
     }
 }
