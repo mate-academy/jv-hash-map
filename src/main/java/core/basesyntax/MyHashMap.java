e804d5e16240d67b5885473e8bb29204e2c26feb
Bohdan Bilko <rapsbodik@gmail.com>
2023-10-23 15:49:15 +0300

    implements new resize() method

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -6,7 +6,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
 
-    private Node[] table;
+    private Node<K, V>[] table;
     private int size;
     private int threshold;
 
@@ -66,18 +66,15 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private void resize() {
         int newCapacity = table.length << 1;
-        Node<K, V>[] oldTable = table;
-        threshold = (int) (newCapacity * DEFAULT_LOAD_FACTOR);
+        final Node<K, V>[] oldTable = table;
         table = new Node[newCapacity];
+        threshold = (int) (newCapacity * DEFAULT_LOAD_FACTOR);
+        size = 0;
 
         for (Node<K, V> node : oldTable) {
             while (node != null) {
-                Node<K, V> nextNode = node.next;
-                node.next = null;
-                int index = getIndex(node.key);
-                node.next = table[index];
-                table[index] = node;
-                node = nextNode;
+                put(node.key, node.value);
+                node = node.next;
             }
         }
     }
@@ -87,7 +84,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         private V value;
         private Node<K, V> next;
 
-        public Node(K key, V value, Node<K, V> next) {
+        private Node(K key, V value, Node<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
