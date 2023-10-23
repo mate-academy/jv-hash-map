e1701d24e60c9f4cd6943d7c5f56b0fa84381d80
Bohdan Bilko <rapsbodik@gmail.com>
2023-10-23 15:03:01 +0300

    fix all mistakes

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -6,13 +6,12 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
 
-    private Node<K, V>[] table;
+    private Node[] table;
     private int size;
     private int threshold;
 
     public MyHashMap() {
         this.table = new Node[DEFAULT_CAPACITY];
-        this.size = 0;
         this.threshold = (int) (table.length * DEFAULT_LOAD_FACTOR);
     }
 
@@ -21,17 +20,12 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if (size == threshold) {
             resize();
         }
-
-        if (key == null) {
-            putNullValue(value);
-            return;
-        }
-        int index = hash(key);
+        int index = getIndex(key);
         Node<K, V> newNode = new Node<>(key, value, null);
         if (table[index] != null) {
             Node<K, V> currentNode = table[index];
             while (currentNode != null) {
-                if (currentNode.key.equals(key)) {
+                if (key == currentNode.key || (key != null && key.equals(currentNode.key))) {
                     currentNode.value = value;
                     return;
                 }
@@ -50,13 +44,10 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public V getValue(K key) {
-        if (key == null) {
-            getNullValue();
-        }
-        int index = hash(key);
+        int index = getIndex(key);
         Node<K, V> currentNode = table[index];
         while (currentNode != null) {
-            if (currentNode.key.equals(key)) {
+            if (key == currentNode.key || (key != null && key.equals(currentNode.key))) {
                 return currentNode.value;
             }
             currentNode = currentNode.next;
@@ -69,49 +60,21 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private V putNullValue(V value) {
-        if (table[0] != null) {
-            Node<K, V> currentNode = table[0];
-            while (currentNode != null) {
-                if (currentNode.next == null) {
-                    currentNode.value = value;
-                    return currentNode.value;
-                }
-                currentNode = currentNode.next;
-            }
-        } else {
-            table[0] = new Node<>(null, value, null);
-            size++;
-        }
-        return null;
-    }
-
-    private V getNullValue() {
-        Node<K, V> currentNode = table[0];
-        while (currentNode != null) {
-            if (currentNode.key == null) {
-                return currentNode.value;
-            }
-            currentNode = currentNode.next;
-        }
-        return null;
-    }
-
-    private int hash(K key) {
+    private int getIndex(K key) {
         return (key == null) ? 0 : Math.abs(key.hashCode() % table.length);
     }
 
     private void resize() {
         int newCapacity = table.length << 1;
-        Node<K,V>[] oldTable = table;
+        Node<K, V>[] oldTable = table;
         threshold = (int) (newCapacity * DEFAULT_LOAD_FACTOR);
         table = new Node[newCapacity];
 
-        for (Node<K,V> node : oldTable) {
+        for (Node<K, V> node : oldTable) {
             while (node != null) {
-                Node<K,V> nextNode = node.next;
+                Node<K, V> nextNode = node.next;
                 node.next = null;
-                int index = hash(node.key);
+                int index = getIndex(node.key);
                 node.next = table[index];
                 table[index] = node;
                 node = nextNode;
@@ -119,7 +82,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    static class Node<K, V> {
+    private static class Node<K, V> {
         private final K key;
         private V value;
         private Node<K, V> next;
