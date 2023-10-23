6a09000943b9ad8348362a30bb77541ecbd26912
Mateusz Rostek <17rostek17@gmail.com>
2023-10-23 23:48:43 +0200

    First draft

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,6 +1,22 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static double LOAD_FACTOR = 0.75;
+
+    private Node<K, V>[] table;
+    private int size;
+    private int currentCapacity;
+    private double threshold;
+
+    @SuppressWarnings("unchecked")
+    public MyHashMap() {
+        this.table = (Node<K, V>[]) new Node[DEFAULT_CAPACITY];
+        currentCapacity = DEFAULT_CAPACITY;
+        threshold = DEFAULT_CAPACITY * LOAD_FACTOR;
+    }
 
     @Override
     public void put(K key, V value) {
@@ -14,6 +30,29 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private int getBucket(K key) {
+        if (key == null) {
+            return 0;
+        }
+        return key.hashCode() % currentCapacity;
+    }
+
+    private void resize() {
+
+    }
+
+    private static class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
