59e57e9564df2442e91b0d658e3dd18b61e4fdf3
Andrii Laver <laver.andriy@gmail.com>
2023-10-25 16:42:43 +0300

    changed resize

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -7,28 +7,29 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private Node<K, V>[] table;
     private int threshold;
 
+    public MyHashMap() {
+        threshold = (int) (INITIAL_CAPACITY * LOAD_FACTOR);
+        table = (Node<K, V>[]) new Node[INITIAL_CAPACITY];
+    }
+
     @Override
     public void put(K key, V value) {
-        if (table == null || table.length == 0) {
-            table = resize();
-        }
         int index = (table.length - 1) & hash(key);
         Node<K, V> tempNode = table[index];
         if (table[index] == null) {
             table[index] = new Node<>(hash(key), key, value, null);
         } else {
-            while (true) {
+            while (tempNode != null) {
                 if (tempNode.key == null ? key == null : tempNode.key.equals(key)) {
                     tempNode.value = value;
                     return;
                 }
                 if (tempNode.next == null) {
+                    tempNode.next = new Node<>(hash(key), key, value, null);
                     break;
                 }
                 tempNode = tempNode.next;
             }
-            tempNode.next = new Node<>(hash(key), key, value, null);
-
         }
 
         if (++size > threshold) {
@@ -38,15 +39,13 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public V getValue(K key) {
-        if (table != null && table.length > 0) {
-            int index = (table.length - 1) & hash(key);
-            Node<K, V> temp = table[index];
-            while (temp != null) {
-                if (temp.key == null ? key == null : temp.key.equals(key)) {
-                    return temp.value;
-                }
-                temp = temp.next;
+        int index = (table.length - 1) & hash(key);
+        Node<K, V> temp = table[index];
+        while (temp != null) {
+            if (temp.key == null ? key == null : temp.key.equals(key)) {
+                return temp.value;
             }
+            temp = temp.next;
         }
         return null;
     }
@@ -74,66 +73,55 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return key == null ? 0 : key.hashCode();
     }
 
-    private Node<K, V>[] resize() {
+    private void resize() {
         final Node<K, V>[] oldTab = table;
         final int oldCap = table == null ? 0 : table.length;
-        final int oldThr = threshold;
-        int newCap;
-        int newThr;
-        if (oldCap > 0) {
-            newCap = oldCap << 1;
-            newThr = oldThr << 1;
-        } else {
-            newCap = INITIAL_CAPACITY;
-            newThr = (int) (INITIAL_CAPACITY * LOAD_FACTOR);
-        }
+        int newCap = oldCap << 1;
+        threshold = threshold << 1;
+        table = (Node<K, V>[]) new Node[newCap];
+        transferNode(oldTab, oldCap);
+    }
 
-        threshold = newThr;
-        Node<K, V>[] newTab = (Node<K, V>[]) new Node[newCap];
-        table = newTab;
-        if (oldTab != null) {
-            for (int i = 0; i < oldTab.length; i++) {
-                Node<K, V> tempNode = oldTab[i];
-                if (tempNode != null) {
-                    if (tempNode.next == null) {
-                        table[(table.length - 1) & hash(tempNode.key)] = tempNode;
-                    } else {
-                        Node<K,V> loHead = null;
-                        Node<K,V> loTail = null;
-                        Node<K,V> hiHead = null;
-                        Node<K,V> hiTail = null;
-                        Node<K,V> next;
-                        do {
-                            next = tempNode.next;
-                            if ((tempNode.hash & oldCap) == 0) {
-                                if (loTail == null) {
-                                    loHead = tempNode;
-                                } else {
-                                    loTail.next = tempNode;
-                                }
-                                loTail = tempNode;
+    private void transferNode(Node<K, V>[] oldTab, int oldCap) {
+        for (int i = 0; i < oldTab.length; i++) {
+            Node<K, V> tempNode = oldTab[i];
+            if (tempNode != null) {
+                if (tempNode.next == null) {
+                    table[(table.length - 1) & tempNode.hash] = tempNode;
+                } else {
+                    Node<K,V> loHead = null;
+                    Node<K,V> loTail = null;
+                    Node<K,V> hiHead = null;
+                    Node<K,V> hiTail = null;
+                    Node<K,V> next;
+                    do {
+                        next = tempNode.next;
+                        if ((tempNode.hash & oldCap) == 0) {
+                            if (loTail == null) {
+                                loHead = tempNode;
                             } else {
-                                if (hiTail == null) {
-                                    hiHead = tempNode;
-                                } else {
-                                    hiTail.next = tempNode;
-                                }
-                                hiTail = tempNode;
+                                loTail.next = tempNode;
                             }
-                        } while ((tempNode = next) != null);
-                        if (loTail != null) {
-                            loTail.next = null;
-                            newTab[i] = loHead;
+                            loTail = tempNode;
+                        } else {
+                            if (hiTail == null) {
+                                hiHead = tempNode;
+                            } else {
+                                hiTail.next = tempNode;
+                            }
+                            hiTail = tempNode;
                         }
-                        if (hiTail != null) {
-                            hiTail.next = null;
-                            newTab[i + oldCap] = hiHead;
-                        }
+                    } while ((tempNode = next) != null);
+                    if (loTail != null) {
+                        loTail.next = null;
+                        table[i] = loHead;
+                    }
+                    if (hiTail != null) {
+                        hiTail.next = null;
+                        table[(table.length - 1) & hiHead.hash] = hiHead;
                     }
                 }
-
             }
         }
-        return table;
     }
 }
