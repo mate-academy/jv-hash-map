ccd375f3445d948018bd1e94d4566e2a79f6c0b3
Bohdan Sosunovych <bohdan.sosunovych@gmail.com>
2023-10-28 10:13:27 +0300

    jv-hash-map_solution

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,96 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75F;
+    private int size;
+    private Node<K, V>[] table;
+
+    public MyHashMap() {
+        this.table = new Node[DEFAULT_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
+        checkSize();
+        int position = getPosition(key);
+        Node<K, V> currentNode = table[position];
+        Node<K, V> newNode = new Node<>(key, value, null);
 
+        if (table[position] == null) {
+            table[position] = newNode;
+            size++;
+        } else {
+            while (true) {
+                if (Objects.equals(key, currentNode.key)) {
+                    currentNode.value = value;
+                    return;
+                }
+                if (currentNode.next == null) {
+                    currentNode.next = newNode;
+                    size++;
+                    return;
+                }
+                currentNode = currentNode.next;
+            }
+        }
     }
 
     @Override
     public V getValue(K key) {
+        int position = getPosition(key);
+        Node<K, V> currentNode = table[position];
+        while (currentNode != null) {
+            if (Objects.equals(key, currentNode.key)) {
+                return currentNode.value;
+            }
+            currentNode = currentNode.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private void checkSize() {
+        int capacity = table.length;
+        int threshold = (int) (capacity * DEFAULT_LOAD_FACTOR);
+        if (getSize() >= threshold) {
+            capacity = capacity << 1;
+            transferNodes(capacity);
+        }
+    }
+
+    private void transferNodes(int capacity) {
+        Node<K, V>[] oldTable = table;
+        Node<K, V>[] newTable = new Node[capacity];
+        table = newTable;
+        size = 0;
+        for (Node<K, V> node : oldTable) {
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.next;
+            }
+        }
+    }
+
+    private int getPosition(K key) {
+        return key == null ? 0 : (Math.abs(key.hashCode()) % table.length);
+    }
+
+    private static class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
