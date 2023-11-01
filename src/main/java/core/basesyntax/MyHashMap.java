2c8891e9e15dbcc56314421bd774314b3c13ba24
alukavenko <a.lukavenko@gmail.com>
2023-11-01 20:12:30 +0200

    Extracted method getIndex(), get rid of storing hash as it's never used

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -23,11 +23,10 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public V getValue(K key) {
-        int hash = getHash(key);
-        int position = hash % nodes.length;
+        int position = getIndex(key);
         Node<K, V> currentNode = nodes[position];
         while (currentNode != null) {
-            if (currentNode.hash == hash && Objects.equals(currentNode.key, key)) {
+            if (Objects.equals(currentNode.key, key)) {
                 return currentNode.value;
             }
             currentNode = currentNode.next;
@@ -44,6 +43,11 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return key == null ? 0 : Math.abs(key.hashCode());
     }
 
+    private int getIndex(K key) {
+        int hash = getHash(key);
+        return hash % nodes.length;
+    }
+
     private void resize() {
         Node<K, V>[] temp = nodes;
         nodes = new Node[nodes.length * GROWTH_FACTOR];
@@ -57,38 +61,34 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void putValue(K key, V value) {
-        int hash = getHash(key);
-        int position = hash % nodes.length;
+        int position = getIndex(key);
         Node<K, V> currentNode = nodes[position];
         if (currentNode == null) {
-            nodes[position] = new Node<>(key, value, hash);
+            nodes[position] = new Node<>(key, value);
             size++;
         } else {
-            while (currentNode.next != null) {
-                if (currentNode.hash == hash && Objects.equals(currentNode.key, key)) {
+            while (currentNode != null) {
+                if (Objects.equals(currentNode.key, key)) {
+                    currentNode.value = value;
                     break;
                 }
+                if (currentNode.next == null) {
+                    currentNode.next = new Node<>(key, value);
+                    size++;
+                }
                 currentNode = currentNode.next;
-            }
-            if (currentNode.hash == hash && Objects.equals(currentNode.key, key)) {
-                currentNode.value = value;
-            } else {
-                currentNode.next = new Node<>(key, value, hash);
-                size++;
             }
         }
     }
 
     private static class Node<K, V> {
-        private final int hash;
         private final K key;
         private V value;
         private Node<K, V> next;
 
-        public Node(K key, V value, int hash) {
+        public Node(K key, V value) {
             this.key = key;
             this.value = value;
-            this.hash = hash;
         }
     }
 }
