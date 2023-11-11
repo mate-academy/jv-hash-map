7f4e7dc4281b06a732b81b3467043ad83586331b
Vladyslav Druhov <space.drugov@gmail.com>
2023-11-11 11:58:39 +0200

    Redesigned the method, improved readability and logic

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -7,50 +7,45 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final float DEFAULT_LOAD_FACTOR = 0.75f;
     private static final int INCREASE_VALUE = 2;
     private int size;
-    private float loadFactor;
+    private float threshold;
     private Node<K, V>[] table;
 
     public MyHashMap() {
         table = new Node[DEFAULT_INITIAL_CAPACITY];
+        threshold = DEFAULT_LOAD_FACTOR;
     }
 
     @Override
     public void put(K key, V value) {
         resizeIfNeeded();
-        Node<K, V> node = new Node<>(key, value);
+        Node<K, V> newNode = new Node<>(key, value);
         int indexBucket = getIndex(key);
         if (table[indexBucket] == null) {
-            table[indexBucket] = node;
+            table[indexBucket] = newNode;
+            size++;
         } else {
             Node<K, V> current = table[indexBucket];
-            Node<K, V> prev = null;
             while (current != null) {
-                if (Objects.equals(current.key, node.key)) {
-                    current.value = node.value;
+                if (Objects.equals(current.key, key)) {
+                    current.value = value;
                     return;
                 }
-                prev = current;
+                if (current.next == null) {
+                    break;
+                }
                 current = current.next;
             }
-            prev.next = node;
+            current.next = newNode;
+            size++;
         }
-        size++;
     }
 
     @Override
     public V getValue(K key) {
         int indexBucket = getIndex(key);
-        if (table[indexBucket] != null) {
-            Node<K, V> current = table[indexBucket];
-
-            while (current != null) {
-                if (Objects.equals(current.key, key)) {
-                    return current.value;
-                }
-                current = current.next;
-            }
-        }
-        return null;
+        Node<K, V> current = table[indexBucket];
+        Node<K, V> node = findNodeByKey(indexBucket, key);
+        return (node != null) ? node.value : null;
     }
 
     @Override
@@ -62,9 +57,20 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return (key == null) ? 0 : Math.abs(key.hashCode());
     }
 
+    private Node<K, V> findNodeByKey(int indexBucket, K key) {
+        Node<K, V> current = table[indexBucket];
+        while (current != null) {
+            if (Objects.equals(current.key, key)) {
+                return current;
+            }
+            current = current.next;
+        }
+        return null;
+    }
+
     private void resizeIfNeeded() {
-        loadFactor = (float) size / table.length;
-        if (loadFactor > DEFAULT_LOAD_FACTOR) {
+        threshold = (float) size / table.length;
+        if (threshold > DEFAULT_LOAD_FACTOR) {
             Node<K, V>[] oldTable = table;
             table = new Node[table.length * INCREASE_VALUE];
             size = 0;
