07899341343a4f4bf7479b6511081679c7a7286b
Bohdan Chupika <boroda4436@users.noreply.github.com>
2023-11-13 13:07:15 +0200

    HashMap

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,28 @@
 package core.basesyntax;
 
+import java.util.HashMap;
+
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+  private final HashMap<K,V> map;
+
+    public MyHashMap() {
+        this.map = new HashMap<>();
+    }
+
 
     @Override
     public void put(K key, V value) {
-
+        map.put(key, value);
     }
 
     @Override
     public V getValue(K key) {
-        return null;
+        return map.get(key);
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return map.size();
     }
 }
