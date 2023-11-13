57edf28aeb71c40ca51167d096a570bfd184a579
Maksym Musiienko <maxymvm@gmail.com>
2023-11-13 09:51:04 +0200

    refactored HashMap. Added .next field in Entry<K, V> class and now using usual array instead of array of linked lists.

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,16 @@
 package core.basesyntax;
 
-import java.util.LinkedList;
-
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final double LOAD_FACTOR = 0.75;
-    private static final int INCREASING_NUM = 2;
+    private static final int GROW_FACTOR = 2;
     private static final int ZERO_HASH = 0;
 
-    private LinkedList<Entry<K, V>>[] table;
+    private Entry<K, V>[] table;
     private int size;
 
     public MyHashMap() {
-        table = new LinkedList[DEFAULT_CAPACITY];
-        size = 0;
+        table = new Entry[DEFAULT_CAPACITY];
     }
 
     @Override
@@ -26,21 +23,18 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         int hash = key.hashCode();
         int index = getIndex(hash);
 
-        if (table[index] == null) {
-            table[index] = new LinkedList<>();
-            table[index].add(new Entry<>(key, value));
-            size++;
-            return;
-        }
-
-        for (Entry<K, V> entry : table[index]) {
-            if (entry.key != null && entry.key.equals(key)) {
-                entry.value = value;
+        Entry<K, V> current = table[index];
+        while (current != null) {
+            if (current.key != null && current.key.equals(key)) {
+                current.value = value;
                 return;
             }
+            current = current.next;
         }
 
-        table[index].add(new Entry<>(key, value));
+        Entry<K, V> newEntry = new Entry<>(key, value);
+        newEntry.next = table[index];
+        table[index] = newEntry;
         size++;
 
         if ((double) size / table.length > LOAD_FACTOR) {
@@ -51,18 +45,18 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public V getValue(K key) {
         if (key == null) {
-            return getKeyNullVal();
+            return getNullVal();
         }
 
         int hash = key.hashCode();
         int index = getIndex(hash);
 
-        if (table[index] != null) {
-            for (Entry<K, V> entry : table[index]) {
-                if (entry.key != null && entry.key.equals(key)) {
-                    return entry.value;
-                }
+        Entry<K, V> current = table[index];
+        while (current != null) {
+            if (current.key != null && current.key.equals(key)) {
+                return current.value;
             }
+            current = current.next;
         }
 
         return null;
@@ -76,10 +70,12 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static class Entry<K, V> {
         private K key;
         private V value;
+        private Entry<K, V> next;
 
         public Entry(K key, V value) {
             this.key = key;
             this.value = value;
+            this.next = null;
         }
     }
 
@@ -92,59 +88,54 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void resizeTable() {
-        int newCapacity = table.length * INCREASING_NUM;
-        LinkedList<Entry<K, V>>[] newTable = new LinkedList[newCapacity];
+        int newCapacity = table.length * GROW_FACTOR;
+        Entry<K, V>[] newTable = new Entry[newCapacity];
 
-        for (LinkedList<Entry<K, V>> bucket : table) {
-            if (bucket != null) {
-                for (Entry<K, V> entry : bucket) {
-                    int newHash = entry.key.hashCode();
-                    int newIndex = getIndex(newHash, newCapacity);
+        for (Entry<K, V> current : table) {
+            while (current != null) {
+                Entry<K, V> next = current.next;
+                int newIndex = getIndex(current.key.hashCode(), newCapacity);
 
-                    if (newTable[newIndex] == null) {
-                        newTable[newIndex] = new LinkedList<>();
-                    }
+                current.next = newTable[newIndex];
+                newTable[newIndex] = current;
 
-                    newTable[newIndex].add(entry);
-                }
+                current = next;
             }
         }
+
         table = newTable;
     }
 
     private void putNullKey(V value) {
         int index = getIndex(ZERO_HASH);
 
-        if (table[index] == null) {
-            table[index] = new LinkedList<>();
-            table[index].add(new Entry<>(null, value));
-            size++;
-            if ((double) size / table.length > LOAD_FACTOR) {
-                resizeTable();
-            }
-            return;
-        }
-
-        for (Entry<K, V> entry : table[index]) {
-            if (entry.key == null) {
-                entry.value = value;
+        Entry<K, V> current = table[index];
+        while (current != null) {
+            if (current.key == null) {
+                current.value = value;
                 return;
             }
+            current = current.next;
         }
 
-        table[index].add(new Entry<>(null, value));
+        Entry<K, V> newEntry = new Entry<>(null, value);
+        newEntry.next = table[index];
+        table[index] = newEntry;
         size++;
+
         if ((double) size / table.length > LOAD_FACTOR) {
             resizeTable();
         }
     }
 
-    private V getKeyNullVal() {
-        if (table[0] != null) {
-            for (Entry<K, V> entry : table[0]) {
-                if (entry.key == null) {
-                    return entry.value;
+    private V getNullVal() {
+        if (table[ZERO_HASH] != null) {
+            Entry<K, V> current = table[ZERO_HASH];
+            while (current != null) {
+                if (current.key == null) {
+                    return current.value;
                 }
+                current = current.next;
             }
         }
         return null;
