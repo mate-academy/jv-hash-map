8668761ea8d5887e8c1164d612f3bb728c9b1e5c
Dmytro Sokolovskyi <dmytro.sokolovskyi@icloud.com>
2023-11-15 17:00:49 -0500

    added my HashMap solution

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,110 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final float LOAD_FACTOR = 0.75f;
+    private static final int DEFAULT_CAPACITY = 1 << 4;
+    private int capacity;
+    private int threshold;
+    private int size;
+    private Node<K, V>[] table;
+
+    public MyHashMap() {
+        this.capacity = DEFAULT_CAPACITY;
+        this.threshold = (int) (DEFAULT_CAPACITY * LOAD_FACTOR);
+        table = new Node[DEFAULT_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
-
+        int hash = hash(key);
+        int index = getIndex(key);
+        Node<K, V> lastPrev = table[index];
+        Node<K, V> newNode = new Node<>(hash, key, value, null);
+        if (lastPrev == null) {
+            table[index] = newNode;
+        } else {
+            if (lastPrev.next == null) {
+                if ((key != null && key.equals(lastPrev.key))
+                        || key == lastPrev.key) {
+                    lastPrev.value = value;
+                    return;
+                }
+                lastPrev.next = newNode;
+            } else {
+                while (lastPrev.next != null) {
+                    if ((key != null && key.equals(lastPrev.key))
+                            || key == lastPrev.key) {
+                        lastPrev.value = value;
+                        return;
+                    }
+                    lastPrev = lastPrev.next;
+                }
+                if ((key != null && key.equals(lastPrev.key))
+                        || key == lastPrev.key) {
+                    lastPrev.value = value;
+                    return;
+                }
+                lastPrev.next = newNode;
+            }
+        }
+        if (++size > threshold) {
+            resize();
+        }
     }
 
     @Override
     public V getValue(K key) {
+        Node<K, V> currentNode = table[getIndex(key)];
+        for (;currentNode != null; currentNode = currentNode.next) {
+            if ((currentNode.key == null && key == null)
+                    || (currentNode.key != null && currentNode.key.equals(key))) {
+                return currentNode.value;
+            }
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private int getIndex(K key) {
+        return (key == null) ? 0 : Math.abs(hash(key) % capacity);
+    }
+
+    private int hash(K key) {
+        return (key == null) ? 0 : key.hashCode();
+    }
+
+    private void resize() {
+        capacity = capacity << 1;
+        threshold = (int) (capacity * LOAD_FACTOR);
+        size = 0;
+        Node<K, V>[] oldTable = table;
+        Node<K, V>[] newTable = new Node[capacity];
+        table = newTable;
+        for (Node<K, V> bucket: oldTable) {
+            if (bucket != null) {
+                while (bucket != null) {
+                    put(bucket.key, bucket.value);
+                    bucket = bucket.next;
+                }
+            }
+        }
+    }
+
+    private static class Node<K, V> {
+        private final int hashCode;
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        private Node(int hashCode, K key, V value, Node<K, V> next) {
+            this.hashCode = hashCode;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
