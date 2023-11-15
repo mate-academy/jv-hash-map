cc61e0ab0d806a11e7f07b5cefeadb2e13c8d000
gulini32 <gulini32@gmail.com>
2023-11-15 17:55:05 +0200

    Hw-HashMap

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,14 +1,16 @@
 package core.basesyntax;
 
+
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final double LOAD_FACTOR = 0.75;
 
-    private Node<K, V>[] table;
+    private Entry<K, V>[] table;
     private int size;
 
     public MyHashMap() {
-        this.table = new Node[DEFAULT_CAPACITY];
+        this.table = new Entry[DEFAULT_CAPACITY];
         this.size = 0;
     }
 
@@ -16,19 +18,19 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     public void put(K key, V value) {
         int hash = hash(key);
         int index = indexFor(hash, table.length);
-        Node<K, V> node = table[index];
-        while (node != null) {
-            if ((key == null && node.getKey() == null)
-                    || (key != null && key.equals(node.getKey()))) {
-                node.setValue(value);
+        Entry<K, V> entry = table[index];
+        while (entry != null) {
+            if ((key == null && entry.getKey() == null)
+                     || (key != null && key.equals(entry.getKey()))) {
+                entry.setValue(value);
                 return;
             }
-            node = node.getNext();
+            entry = entry.getNext();
         }
 
-        Node<K, V> newNode = new Node<>(key, value);
-        newNode.setNext(table[index]);
-        table[index] = newNode;
+        Entry<K, V> newEntry = new Entry<>(key, value);
+        newEntry.setNext(table[index]);
+        table[index] = newEntry;
         size++;
         if ((double) size / table.length > LOAD_FACTOR) {
             resize();
@@ -39,13 +41,13 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     public V getValue(K key) {
         int hash = hash(key);
         int index = indexFor(hash, table.length);
-        Node<K, V> node = table[index];
-        while (node != null) {
-            if ((key == null && node.getKey() == null)
-                    || (key != null && key.equals(node.getKey()))) {
-                return node.getValue();
+        Entry<K, V> entry = table[index];
+        while (entry != null) {
+            if ((key == null && entry.getKey() == null)
+                    || (key != null && key.equals(entry.getKey()))) {
+                return entry.getValue();
             }
-            node = node.getNext();
+            entry = entry.getNext();
         }
         return null;
     }
@@ -57,14 +59,14 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private void resize() {
         int newCapacity = table.length * 2;
-        Node[] newTable = new Node[newCapacity];
-        for (Node<K, V> node : table) {
-            while (node != null) {
-                int index = indexFor(hash(node.getKey()), newCapacity);
-                Node<K, V> next = node.getNext();
-                node.setNext(newTable[index]);
-                newTable[index] = node;
-                node = next;
+        Entry[] newTable = new Entry[newCapacity];
+        for (Entry<K, V> entry : table) {
+            while (entry != null) {
+                int index = indexFor(hash(entry.getKey()), newCapacity);
+                Entry<K, V> next = entry.getNext();
+                entry.setNext(newTable[index]);
+                newTable[index] = entry;
+                entry = next;
             }
         }
         table = newTable;
@@ -78,12 +80,12 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return hash & (tableLength - 1);
     }
 
-    private static class Node<K, V> {
+    private static class Entry<K, V> {
         private final K key;
         private V value;
-        private Node<K, V> next;
+        private Entry<K, V> next;
 
-        public Node(K key, V value) {
+        public Entry(K key, V value) {
             this.key = key;
             this.value = value;
             this.next = null;
@@ -101,11 +103,11 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             this.value = value;
         }
 
-        public Node<K, V> getNext() {
+        public Entry<K, V> getNext() {
             return next;
         }
 
-        public void setNext(Node<K, V> next) {
+        public void setNext(Entry<K, V> next) {
             this.next = next;
         }
     }
