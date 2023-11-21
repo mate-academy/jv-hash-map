16ab23f1068b3552c729c500190afaac34f60892
Vadim <waterfallkadas@gmail.com>
2023-11-22 00:42:15 +0200

    correction 22/11

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,28 +1,25 @@
 package core.basesyntax;
 
-public class MyHashMap<K,V> implements MyMap<K,V> {
-    private static final int DEFAULT_ARRAY_LENGTH = 16;
+public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final double LOAD_FACTOR = 0.75;
-    private int elementsAmount;
-    private Node<K,V> [] valuesArray;
-    private int arrayIndex;
+    private static final int DEFAULT_ARRAY_LENGTH = 16;
+    private int size;
+    private Node<K, V> [] values;
+
+    public MyHashMap() {
+        values = new Node[DEFAULT_ARRAY_LENGTH];
+    }
 
     @Override
     public void put(K key, V value) {
-        if (elementsAmount == 0) {
-            valuesArray = new Node[DEFAULT_ARRAY_LENGTH];
-        }
-        if (elementsAmount != 0 && elementsAmount > (valuesArray.length * LOAD_FACTOR)) {
-            resize();
-        }
-        arrayIndex = (key == null ? 0 : Math.abs(key.hashCode() % valuesArray.length));
-        if (valuesArray[arrayIndex] == null) {
+        resizeIfNeeded();
+        if (values[getIndexFromKey(key)] == null) {
             Node<K, V> newNode = new Node<>(key, value, null);
-            valuesArray[arrayIndex] = newNode;
-            elementsAmount++;
+            values[getIndexFromKey(key)] = newNode;
+            size++;
         } else {
-            Node<K,V> currentNode = valuesArray[arrayIndex];
-            while (true) {
+            Node<K, V> currentNode = values[getIndexFromKey(key)];
+            while (currentNode != null) {
                 if (currentNode.nodeKey != null && currentNode.nodeKey.equals(key)
                         || currentNode.nodeKey == null && key == null) {
                     currentNode.nodeValue = value;
@@ -30,7 +27,7 @@ public class MyHashMap<K,V> implements MyMap<K,V> {
                 } else {
                     if (currentNode.next == null) {
                         currentNode.next = new Node<>(key,value,null);
-                        elementsAmount++;
+                        size++;
                         break;
                     } else {
                         currentNode = currentNode.next;
@@ -42,76 +39,59 @@ public class MyHashMap<K,V> implements MyMap<K,V> {
 
     @Override
     public V getValue(K key) {
-        if (elementsAmount == 0) {
+        if (size == 0) {
             return null;
         }
-        arrayIndex = (key == null ? 0 : Math.abs(key.hashCode() % valuesArray.length));
-        Node<K,V> currentNode = valuesArray[arrayIndex];
+        Node<K, V> currentNode = values[getIndexFromKey(key)];
         if (currentNode == null) {
             return null;
         }
-        while (true) {
+        while (currentNode != null) {
             if (currentNode.nodeKey != null && currentNode.nodeKey.equals(key)
-                    || currentNode.nodeKey == null && key == null) {
+                    || currentNode.nodeKey == key) {
                 return currentNode.nodeValue;
-            } else if (currentNode.next != null) {
-                currentNode = currentNode.next;
-            } else {
-                return null;
             }
+            currentNode = currentNode.next;
         }
+        return null;
     }
 
     @Override
     public int getSize() {
-        return elementsAmount;
+        return size;
     }
 
-    private void resize() {
-        Node<K,V>[] newValuesArray = new Node[valuesArray.length * 2];
-        arrayCopy(newValuesArray);
-    }
-
-    private void arrayCopy(Node<K,V>[] newValuesArray) {
-        Node<K,V>[] oldArray = valuesArray;
-        valuesArray = newValuesArray;
+    private void arrayResize_Copy() {
+        Node<K, V>[] oldArray = values;
+        values = new Node[values.length * 2];
+        size = 0;
         for (int i = 0; i < oldArray.length; i++) {
             if (oldArray[i] != null) {
-                Node<K,V> currentNode = oldArray[i];
-                while (true) {
-                    arrayIndex = (currentNode.nodeKey == null ? 0
-                            : Math.abs(currentNode.nodeKey.hashCode() % valuesArray.length));
-                    if (valuesArray[arrayIndex] == null) {
-                        valuesArray[arrayIndex] = new Node<>(currentNode.nodeKey,
-                                currentNode.nodeValue, null);
-                    } else {
-                        Node<K,V> newArrayNode = valuesArray[arrayIndex];
-                        while (true) {
-                            if (newArrayNode.next == null) {
-                                newArrayNode.next = new Node<>(currentNode.nodeKey,
-                                        currentNode.nodeValue, null);
-                                break;
-                            } else {
-                                newArrayNode = newArrayNode.next;
-                            }
-                        }
-                    }
-                    if (currentNode.next == null) {
-                        break;
-                    } else {
-                        currentNode = currentNode.next;
-                    }
-                }
+                Node<K, V> currentNode = oldArray[i];
+                do {
+                    put(currentNode.nodeKey, currentNode.nodeValue);
+                    currentNode = currentNode.next;
+                } while (currentNode != null);
             }
         }
     }
 
-    private static class Node<K,V> {
+    private int getIndexFromKey(K key) {
+        return (key == null ? 0 : Math.abs(key.hashCode() % values.length));
+    }
+
+    private void resizeIfNeeded() {
+        if (size != 0 && size > (values.length * LOAD_FACTOR)) {
+            arrayResize_Copy();
+        }
+    }
+
+    private static class Node<K, V> {
         private V nodeValue;
         private final K nodeKey;
-        private Node<K,V> next;
+        private Node<K, V> next;
 
-        Node(K nodeKey, V nodeValue, Node<K,V> next) {
+        Node(K nodeKey, V nodeValue, Node<K, V> next) {
             this.nodeKey = nodeKey;
             this.nodeValue = nodeValue;
             this.next = next;
