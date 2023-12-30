2b3fb3f9627a0f26e0db43710f124d5cc2265ed4
Vlad Bondarev <bondarev.21@icloud.com>
2023-12-30 21:47:04 +0200

    final solution

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,67 +1,42 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int START_ARRAY_CAPACITY = 16;
     private static final double LOAD_FACTOR = 0.75;
     private int size;
-    private double threshold;
-    private Node<K, V> currentNode;
     private Node<K, V>[] table;
 
     public MyHashMap() {
         table = new Node[START_ARRAY_CAPACITY];
-        threshold = table.length * LOAD_FACTOR;
     }
 
     @Override
     public void put(K key, V value) {
-        Node<K, V> nodeToAdd = new Node<>(key, value, null);
-        int index = getIndex(nodeToAdd.key);
-        if (table[index] == null) {
-            table[index] = nodeToAdd;
-        } else {
-            currentNode = table[index];
-            int numberOfElementsInTheBucket = 1;
-            while (currentNode.next != null) {
-                currentNode = currentNode.next;
-                numberOfElementsInTheBucket++;
-            }
-            Node<K, V>[] nodesInTheBucket = new Node[numberOfElementsInTheBucket];
-            currentNode = table[index];
-            for (int i = 0; i < nodesInTheBucket.length; i++) {
-                nodesInTheBucket[i] = currentNode;
-                currentNode = currentNode.next;
-            }
-            for (int i = 0; i < nodesInTheBucket.length; i++) {
-                if (checkIfKeyExists(nodesInTheBucket[i], key)) {
-                    nodesInTheBucket[i].value = value;
-                    if (nodesInTheBucket.length > 1) {
-                        nodesInTheBucket[0].next = nodesInTheBucket[1];
-                    }
-                    table[index] = nodesInTheBucket[0];
-                    return;
-                }
-            }
-            if (nodesInTheBucket.length > 1) {
-                nodesInTheBucket[numberOfElementsInTheBucket - 1].next = nodeToAdd;
-                nodesInTheBucket[0].next = nodesInTheBucket[1];
-            } else {
-                nodesInTheBucket[0].next = nodeToAdd;
-            }
-            table[index] = nodesInTheBucket[0];
-        }
-        size++;
         resize();
+        int index = getIndex(key);
+        Node<K, V> node = table[index];
+        while (node != null) {
+            if (Objects.equals(node.key, key)) {
+                node.value = value;
+                return;
+            }
+            node = node.next;
+        }
+        Node<K, V> nodeToAdd = new Node<>(key, value, table[index]);
+        table[index] = nodeToAdd;
+        size++;
     }
 
     @Override
     public V getValue(K key) {
-        currentNode = table[getIndex(key)];
-        while (currentNode != null) {
-            if (checkIfKeyExists(currentNode, key)) {
-                return currentNode.value;
+        Node<K, V> nodeToGet = table[getIndex(key)];
+        while (nodeToGet != null) {
+            if (checkIfKeyExists(nodeToGet, key)) {
+                return nodeToGet.value;
             }
-            currentNode = currentNode.next;
+            nodeToGet = nodeToGet.next;
         }
         return null;
     }
@@ -85,6 +60,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void resize() {
+        double threshold = table.length * LOAD_FACTOR;
         if (size >= threshold) {
             size = 0;
             Node<K, V>[] newTable = table;
