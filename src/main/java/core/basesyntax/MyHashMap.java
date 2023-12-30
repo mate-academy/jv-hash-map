0a56b7cce510b823f7f24388dde292ef8d9e8bf1
Serhii <haccent12@gmail.com>
2023-12-30 20:14:15 +0100

    fixed mistakes

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -9,7 +9,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private Node<K, V>[] table;
 
     public MyHashMap() {
-        this.table = (Node<K,V>[])new MyHashMap.Node[DEFAULT_INITIAL_CAPACITY];
+        this.table = (Node<K,V>[])new Node[DEFAULT_INITIAL_CAPACITY];
         this.threshold = (int) (table.length * DEFAULT_LOAD_FACTOR);
     }
 
@@ -19,7 +19,28 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             resize();
         }
         if (key == null) {
-            whenKeyNull(value);
+            Node<K,V> node = table[0];
+            if (node != null) {
+                if (node.key == null && node.next == null) {
+                    node.value = value;
+                } else if (node.key != null && node.next == null) {
+                    node.next = addNode(null,value);
+                    size++;
+                } else {
+                    while (node.next != null) {
+                        if (node.key == null) {
+                            node.value = value;
+                            return;
+                        }
+                        node = node.next;
+                    }
+                    node.next = addNode(null,value);
+                    size++;
+                }
+            } else {
+                table[0] = addNode(null,value);
+                size++;
+            }
         } else if (nodeBusyAndKeysEquals(key)) {
             table[bucketIndex(key)].value = value;
         } else if (nodeBusyAndKeysDifferent(key)) {
@@ -27,7 +48,16 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                 table[bucketIndex(key)].next = addNode(key,value);
                 size++;
             } else {
-                putWithCollision(key,value);
+                Node<K,V> current = table[bucketIndex(key)];
+                while (current.next != null) {
+                    if (current.next.key != null && current.next.key.equals(key)) {
+                        current.next.value = value;
+                        return;
+                    }
+                    current = current.next;
+                }
+                current.next = addNode(key,value);
+                size++;
             }
         } else {
             table[bucketIndex(key)] = addNode(key,value);
@@ -51,7 +81,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private void resize() {
         size = 0;
         Node<K,V>[] oldTable = table;
-        table = (Node<K,V>[])new MyHashMap.Node[table.length * 2];
+        table = (Node<K,V>[])new Node[table.length * 2];
         threshold = (int) (table.length * DEFAULT_LOAD_FACTOR);
         for (Node<K,V> node : oldTable) {
             while (node != null) {
@@ -75,51 +105,8 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                 && !(table[bucketIndex(key)].key.equals(key));
     }
 
-    private void whenKeyNull(V value) {
-        Node<K,V> node = table[0];
-        if (node != null) {
-            if (node.key == null && node.next == null) {
-                node.value = value;
-            } else if (node.key != null && node.next == null) {
-                node.next = addNode(null,value);
-                size++;
-            } else {
-                while (node.next != null) {
-                    if (node.key == null) {
-                        node.value = value;
-                        return;
-                    } else {
-                        node = node.next;
-                    }
-                }
-                node.next = addNode(null,value);
-                size++;
-            }
-        } else {
-            table[0] = addNode(null,value);
-            size++;
-        }
-    }
-
     private boolean noCollision(K key) {
         return table[bucketIndex(key)].next == null;
-    }
-
-    private void putWithCollision(K key, V value) {
-        Node<K,V> current = table[bucketIndex(key)].next;
-        while (current != null) {
-            if (current.key == null && key == null
-                    || current.key != null && current.key.equals(key)) {
-                current.value = value;
-                return;
-            } else if (current.next == null) {
-                current.next = addNode(key,value);
-                size++;
-                return;
-            } else {
-                current = current.next;
-            }
-        }
     }
 
     @Override
@@ -129,7 +116,8 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private int bucketIndex(K key) {
         int hash = key == null ? 0 : key.hashCode() % table.length;
-        return hash > 0 ? hash : -hash;
+
+        return Math.abs(hash);
     }
 
     private Node<K,V> addNode(K key, V value) {
@@ -144,55 +132,21 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         while (node != null) {
             if (node.key == null && key == null || node.key != null && node.key.equals(key)) {
                 return node.value;
-            } else {
-                node = node.next;
             }
+            node = node.next;
         }
         return null;
     }
 
     private static class Node<K,V> {
-        private int hashCode;
         private K key;
         private V value;
         private Node<K,V> next;
 
         public Node(int hash, K key, V value, Node<K, V> next) {
-            this.hashCode = this.hashCode();
             this.key = key;
             this.value = value;
             this.next = next;
-        }
-
-        @Override
-        public boolean equals(Object object) {
-            if (object == this) {
-                return true;
-            }
-            if (object == null) {
-                return false;
-            }
-            if (object.getClass() == this.getClass()) {
-                Node<K,V> nodeObject = (Node<K,V>) object;
-                return (this.hashCode == nodeObject.hashCode)
-                        && ((this.key != null && nodeObject.key != null)
-                        ? (this.key.equals(nodeObject.key))
-                        : (this.key == null & nodeObject.key == null))
-                        && ((this.value != null && nodeObject.value != null)
-                        ? (this.value.equals(nodeObject.value))
-                        : (this.value == null && nodeObject.value == null))
-                        && ((this.next != null && nodeObject.next != null)
-                        ? this.next.equals(nodeObject.next)
-                        : (this.next == null && nodeObject.next == null));
-            }
-            return false;
-        }
-
-        @Override
-        public int hashCode() {
-            return 17 * (this.key == null ? 0 : key.hashCode())
-                    + (this.value == null ? 0 : value.hashCode())
-                    + (this.next == null ? 0 : this.next.hashCode()) + hashCode;
         }
     }
 }
