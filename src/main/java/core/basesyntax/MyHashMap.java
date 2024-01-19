5d2d0bfc4ad08fe4aeda8d340c387451dbb4a819
Nazar <nazarmo13@gmail.com>
2024-01-19 17:30:58 +0200

    fixed some mistakes

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,50 +1,36 @@
 package core.basesyntax;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int MULTIPLAYER_CAPACITY = 2;
     private static final int DEFAULT_CAPACITY = 16;
     private static final double LOAD_FACTOR = 0.75;
-    private List<Node<K, V>>[] table;
+    private Node<K, V>[] table;
     private int size;
 
     public MyHashMap() {
-        table = new List[DEFAULT_CAPACITY];
+        table = new Node[DEFAULT_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
-        if ((double) size / table.length > LOAD_FACTOR) {
+        double thresholdResize = (double) size / table.length;
+        if (thresholdResize > LOAD_FACTOR) {
             resize();
         }
-        int index = getHashIndex(key);
-        if (table[index] == null) {
-            table[index] = new ArrayList<>();
-        }
-        for (Node<K, V> node : table[index]) {
-            if (Objects.equals(key, node.key)) {
-                node.value = value;
-                return;
-            }
-        }
-        table[index].add(new Node<>(key, value));
-        size++;
+        putNode(table, key, value);
     }
 
     @Override
     public V getValue(K key) {
-        int index = getHashIndex(key);
-        List<Node<K, V>> bucket = table[index];
-        if (bucket != null) {
-            for (Node<K, V> entry : bucket) {
-                if (Objects.equals(entry.key, key)) {
-                    return entry.value;
-                }
+        int index = getHashIndex(table, key);
+        Node<K, V> node = table[index];
+        while (node != null) {
+            if (Objects.equals(key, node.key)) {
+                return node.value;
             }
+            node = node.next;
         }
         return null;
     }
@@ -54,37 +40,47 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private int getHashIndex(K key) {
+    private int getHashIndex(Node<K, V>[] table, K key) {
         int index;
         if (key == null) {
-            index = 0;
+            return 0;
         } else {
-            index = key.hashCode() % table.length;
-            if (index < 0) {
-                index *= -1;
-            }
+            index = Math.abs(key.hashCode() % table.length);
         }
         return index;
     }
 
     private void resize() {
+        final int currentSize = size;
         int newCapacity = table.length * MULTIPLAYER_CAPACITY;
-        List<Node<K, V>>[] newTable = new List[newCapacity];
-
-        for (List<Node<K, V>> bucket : table) {
-            if (bucket != null) {
-                for (Node<K, V> node : bucket) {
-                    int newIndex = Math.abs(node.key.hashCode() % newCapacity);
-                    List<Node<K, V>> newBucket = newTable[newIndex];
-                    if (newBucket == null) {
-                        newBucket = new LinkedList<>();
-                        newTable[newIndex] = newBucket;
-                    }
-                    newBucket.add(node);
-                }
+        Node<K, V>[] newTable = new Node[newCapacity];
+        for (Node<K, V> node : table) {
+            while (node != null) {
+                putNode(newTable, node.key, node.value);
+                node = node.next;
             }
         }
+        size = currentSize;
         table = newTable;
+    }
+
+    private void putNode(Node<K, V>[] table, K key, V value) {
+        int index = getHashIndex(table, key);
+        if (table[index] == null) {
+            table[index] = new Node<>(key, value);
+        }
+        Node<K, V> node = table[index];
+        while (node != null) {
+            if (Objects.equals(key, node.key) && node.next != null) {
+                node.value = value;
+                return;
+            }
+            node = node.next;
+        }
+        Node<K, V> newNode = new Node<>(key, value);
+        newNode.next = table[index];
+        table[index] = newNode;
+        size++;
     }
 
     private static class Node<K, V> {
