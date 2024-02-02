734aa5cb50f6d9651ae27f09ee9f7722045d7832
moskalenkosergii <moskalenko.s.work@gmail.com>
2024-02-02 16:07:06 +0100

    refactoring, deleted unused methods, variables

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -7,18 +7,20 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
     private Node<K,V>[] table;
     private int size;
-    private int threshold;
 
     public MyHashMap() {
         table = new Node[DEFAULT_INITIAL_CAPACITY];
-        threshold = (int) (table.length * DEFAULT_LOAD_FACTOR);
     }
 
     @Override
     public void put(K key, V value) {
-        int hash = hash(key);
-        int bucket = hash % table.length;
-        Node<K,V> newNode = new Node<>(hash, key, value, null);
+        int threshold = (int) (table.length * DEFAULT_LOAD_FACTOR);
+        if (size >= threshold) {
+            resize();
+        }
+
+        int bucket = getBucketNumber(key);
+        Node<K,V> newNode = new Node<>(key, value, null);
         Node<K, V> checkNode;
 
         // check collision
@@ -29,7 +31,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
             size++;
         } else {
             do {
-                if (compareSolution(checkNode, key)) {
+                if (Objects.equals(checkNode.key, key)) {
                     checkNode.value = value;
                     break;
                 }
@@ -43,27 +45,24 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                 }
             } while (true);
         }
-        if (size >= threshold) {
-            resize();
-        }
     }
 
     @Override
     public V getValue(K key) {
-        int bucket = hash(key) % table.length;
+        int bucket = getBucketNumber(key) % table.length;
         Node<K, V> checkNode;
 
         checkNode = table[bucket];
 
         if (checkNode != null) {
-            if (compareSolution(checkNode, key)) {
+            if (Objects.equals(checkNode.key, key)) {
                 return checkNode.value;
             }
 
             if (checkNode.next != null) {
                 checkNode = checkNode.next;
                 while (checkNode != null) {
-                    if (compareSolution(checkNode, key)) {
+                    if (Objects.equals(checkNode.key, key)) {
                         return checkNode.value;
                     }
                     checkNode = checkNode.next;
@@ -81,7 +80,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private void resize() {
         int oldCap = table.length;
         int newCap = oldCap << 1;
-        threshold = (int) (newCap * DEFAULT_LOAD_FACTOR);
         size = 0;
 
         Node<K, V> checkNode;
@@ -103,23 +101,18 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    private boolean compareSolution(Node<K,V> object, K key) {
-        return Objects.equals(object.key, key) || object.key != null && object.key.equals(key);
-    }
-
-    private static int hash(Object key) {
+    private int getBucketNumber(Object key) {
         int hash = 17;
-        return (key == null) ? 0 : Math.abs((hash = hash * key.hashCode()) ^ (hash >>> 16));
+        hash = (key == null) ? 0 : Math.abs((hash = hash * key.hashCode()) ^ (hash >>> 16));
+        return hash % table.length;
     }
 
     private static class Node<K,V> {
-        private int hash;
         private Node<K,V> next;
         private final K key;
         private V value;
 
-        public Node(int hash, K key, V value, Node<K, V> next) {
-            this.hash = hash;
+        public Node(K key, V value, Node<K, V> next) {
             this.next = next;
             this.key = key;
             this.value = value;
