1b717a3e77d285f63209661c29c97a125c149649
Evhen Timofeiev <timofeevzhenya2107@gmail.com>
2024-03-06 14:09:30 +0200

    add fixes after review

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,122 +1,55 @@
 package core.basesyntax;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    static final int DEFAULT_INITIAL_CAPACITY = 16;
-    private final double defaultLoadFactor = 0.75;
-    private int threshold = (int) (DEFAULT_INITIAL_CAPACITY * defaultLoadFactor);
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private static final double DEFAULT_LOADFACTOR = 0.75;
+    private static final int GROW_FACTOR = 2;
     private Entry<K, V>[] buckets;
-    private List<Entry<K, V>> nullBucket;
     private int size;
-
-    private static class Entry<K, V> {
-        private final K key;
-        private V value;
-        private Entry<K, V> next;
-
-        public Entry(K key, V value) {
-            this.key = key;
-            this.value = value;
-        }
-
-        public K getKey() {
-            return key;
-        }
-
-        public V getValue() {
-            return value;
-        }
-
-        void setValue(V value) {
-            this.value = value;
-        }
-    }
 
     public MyHashMap() {
         buckets = new Entry[DEFAULT_INITIAL_CAPACITY];
-        nullBucket = new ArrayList<>();
     }
 
     public MyHashMap(int initialCapacity) {
-        this.threshold = (int) (initialCapacity * defaultLoadFactor);
-        this.buckets = new Entry[initialCapacity];
-        nullBucket = new ArrayList<>();
-    }
-
-    private void resize() {
-        int newCapacity = buckets.length * 2;
-        Entry<K, V>[] newBuckets = new Entry[newCapacity];
-        for (Entry<K, V> entry : buckets) {
-            while (entry != null) {
-                Entry<K, V> next = entry.next;
-                int index = (entry.getKey() == null
-                        ? 0 : Math.abs(entry.getKey().hashCode() % newCapacity));
-                entry.next = newBuckets[index];
-                newBuckets[index] = entry;
-                entry = next;
-            }
-        }
-        buckets = newBuckets;
-        threshold = (int) (newCapacity * defaultLoadFactor);
+        buckets = new Entry[initialCapacity];
     }
 
     public void put(K key, V value) {
-        if (key == null) {
-            if (size + 1 > threshold) {
-                resize();
-            }
-            for (Entry<K, V> entry : nullBucket) {
-                if (entry.getKey() == null) {
-                    entry.setValue(value);
-                    return;
-                }
-            }
-            nullBucket.add(new Entry<>(null, value));
-            size++;
-        } else {
-            if (size + 1 > threshold) {
-                resize();
-            }
-            int index = Math.abs(key.hashCode() % buckets.length);
-            Entry<K, V> e = buckets[index];
-            if (e == null) {
-                buckets[index] = new Entry<>(key, value);
-                size++;
-            } else {
-                while (e != null) {
-                    if (e.getKey() == null || e.getKey().equals(key)) {
-                        e.setValue(value);
-                        return;
-                    }
-                    if (e.next == null) {
-                        break;
-                    }
-                    e = e.next;
-                }
-                e.next = new Entry<>(key, value);
-                size++;
-            }
+        int threshold = (int) (buckets.length * DEFAULT_LOADFACTOR);
+        if (size >= threshold) {
+            resize();
         }
+        int index = Math.abs(Objects.hashCode(key) % buckets.length);
+        Entry<K, V> e = buckets[index];
+        if (e == null) {
+            buckets[index] = new Entry<>(key, value);
+            size++;
+            return;
+        }
+        while (e != null) {
+            if (Objects.equals(e.key,key)) {
+                e.value = value;
+                return;
+            }
+            if (e.next == null) {
+                break;
+            }
+            e = e.next;
+        }
+        e.next = new Entry<>(key, value);
+        size++;
     }
 
     @Override
     public V getValue(K key) {
-        if (key == null) {
-            for (Entry<K, V> entry : nullBucket) {
-                if (entry.getKey() == null) {
-                    return entry.getValue();
-                }
-            }
-            return null;
-        }
-        int index = Math.abs(key.hashCode() % buckets.length);
+        int index = Math.abs(Objects.hashCode(key) % buckets.length);
         Entry<K, V> e = buckets[index];
         while (e != null) {
-            K currentKey = e.getKey();
-            if (currentKey != null && currentKey.equals(key)) {
-                return e.getValue();
+            if (Objects.equals(e.key, key)) {
+                return e.value;
             }
             e = e.next;
         }
@@ -126,5 +59,32 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public int getSize() {
         return size;
+    }
+
+    private static class Entry<K, V> {
+        private final K key;
+        private V value;
+        private Entry<K, V> next;
+
+        private Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private void resize() {
+        int newCapacity = buckets.length * GROW_FACTOR;
+        Entry<K, V>[] newBuckets = new Entry[newCapacity];
+        for (Entry<K, V> entry : buckets) {
+            while (entry != null) {
+                Entry<K, V> next = entry.next;
+                int index = (entry.key == null
+                        ? 0 : Math.abs(Objects.hashCode(entry.key) % newCapacity));
+                entry.next = newBuckets[index];
+                newBuckets[index] = entry;
+                entry = next;
+            }
+        }
+        buckets = newBuckets;
     }
 }
