089e1873eeb3afefcbd95e37ab6a05a506119c96
John <pandastars535@gmail.com>
2024-03-06 14:02:13 +0200

    removed addition methods fixed MyHashMap конструктор

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -4,163 +4,86 @@ import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_INITIAL_CAPACITY = 16;
-    private static final int NUMBER_TO_MULTIPLY = 2;
-    private static final int ZERO = 0;
-    private Node<K, V>[] initialArray;
+    private static final float LOAD_FACTOR = 0.75f;
+    private static final int GROW_FACTOR = 2;
+
     private int size;
-    private int threshold = 12;
+    private int threshold;
+    private Node<K, V>[] table;
 
     public MyHashMap() {
-        initialArray = new Node[DEFAULT_INITIAL_CAPACITY];
+        table = new Node[DEFAULT_INITIAL_CAPACITY];
+        threshold = (int) (table.length * LOAD_FACTOR);
     }
 
-    @Override
     public void put(K key, V value) {
-        if (key == null) {
-            putNullKey(value);
-            return;
-        }
-        Node<K, V> node = newNode(key, value);
-        int index = Math.abs(getHash(key)) % initialArray.length;
-        int loader = threshold;
-        if (initialArray[index] == null) {
-            initialArray[index] = node;
-            size++;
-        } else {
-            Node<K, V> current = initialArray[index];
-            while (current.next != null) {
-                if (Objects.equals(current.key, key)) {
-                    current.value = value;
-                    return;
-                }
-                current = current.next;
-            }
-            if (Objects.equals(current.key, key)) {
-                current.value = value;
-            } else {
-                current.next = node;
-                size++;
-            }
-        }
-        if (size > loader) {
+        int index = getIndexForKey(key);
+        Node<K, V> newNode = new Node<>(key, value, null);
+        if (size >= threshold) {
             resize();
         }
+        if (table[index] == null) {
+            table[index] = newNode;
+        } else {
+            Node<K, V> node = table[index];
+            while (node != null) {
+                if (Objects.equals(key, node.key)) {
+                    node.value = value;
+                    return;
+                }
+                if (node.next == null) {
+                    break;
+                }
+                node = node.next;
+            }
+            node.next = newNode;
+        }
+        size++;
     }
 
-    @Override
     public V getValue(K key) {
-        if (key == null) {
-            return getNullKey();
-        }
-        int index = getHash(key);
-        V value = null;
-        Node<K, V> currentNode = initialArray[index];
-        while (currentNode != null) {
-            if (currentNode.key != null && currentNode.key.equals(key)) {
-                value = currentNode.value;
-                break;
+        int index = getIndexForKey(key);
+        Node<K, V> node = table[index];
+        while (node != null) {
+            if (Objects.equals(key, node.key)) {
+                return node.value;
             }
-            currentNode = currentNode.next;
-        }
-        return value;
-    }
-
-    @Override
-    public int getSize() {
-        return size;
-    }
-
-    private void resize() {
-        int newCapacity = initialArray.length * NUMBER_TO_MULTIPLY;
-        Node<K, V>[] newArray = new Node[newCapacity];
-        for (int i = 0; i < initialArray.length; i++) {
-            Node<K, V> current = initialArray[i];
-            while (current != null) {
-                Node<K, V> next = current.next;
-                int newIndex = current.hash % newCapacity;
-                current.next = newArray[newIndex];
-                newArray[newIndex] = current;
-                current = next;
-            }
-        }
-        initialArray = newArray;
-        threshold *= NUMBER_TO_MULTIPLY;
-    }
-
-    private V getNullKey() {
-        Node<K, V> currentNode = initialArray[ZERO];
-        while (currentNode != null) {
-            if (currentNode.key == null) {
-                return currentNode.value;
-            }
-            currentNode = currentNode.next;
+            node = node.next;
         }
         return null;
     }
 
-    private void putNullKey(V value) {
-        if (initialArray[ZERO] == null) {
-            initialArray[ZERO] = newNode(null, value);
-            size++;
-        } else {
-            Node<K, V> current = initialArray[0];
-            while (current.next != null) {
-                if (current.key == null) {
-                    current.value = value;
-                    return;
-                }
-                current = current.next;
+    public int getSize() {
+        return size;
+    }
+
+    private int getIndexForKey(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode() % table.length);
+    }
+
+    private void resize() {
+        size = 0;
+        Node<K, V>[] tempTable = table;
+        table = new Node[table.length * GROW_FACTOR];
+        threshold = (int) (table.length * LOAD_FACTOR);
+        for (int i = 0; i < tempTable.length; i++) {
+            Node<K, V> node = tempTable[i];
+            while (node != null) {
+                put(node.key, node.value);
+                node = node.next;
             }
-            if (current.key == null) {
-                current.value = value;
-            } else {
-                current.next = newNode(null, value);
-                size++;
-            }
-        }
-        if (size > threshold) {
-            resize();
         }
     }
 
-    private Node<K, V> newNode(K key, V value) {
-        return new Node<>(getHash(key), key, value, null);
-    }
-
-    private int getHash(K key) {
-        return key == null ? ZERO
-                : Math.abs(key.hashCode()) % DEFAULT_INITIAL_CAPACITY;
-    }
-
-    static class Node<K, V> {
-        private final int hash;
-        private final K key;
+    private static class Node<K, V> {
+        private K key;
         private V value;
         private Node<K, V> next;
 
-        public Node(int hash, K key, V value, Node<K, V> next) {
-            this.hash = hash;
+        public Node(K key, V value, Node<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(hash, key, value, next);
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            Node<?, ?> node = (Node<?, ?>) o;
-            return hash == node.hash && Objects.equals(key, node.key)
-                    && Objects.equals(value, node.value) && Objects.equals(next, node.next);
         }
     }
 }
