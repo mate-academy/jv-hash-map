40334cbd57612724bd8a0d7e727a54f6f2755336
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-03-08 08:15:22 +0200

    Node.java

---
+++ src/main/java/core/basesyntax/Node.java
@@ -0,0 +1,39 @@
+package core.basesyntax;
+
+private static class Node<K, V> {
+    private final int hash;
+    private final K key;
+    private V value;
+    private Node<K, V> next;
+
+    Node(int hash, K key, V value, Node<K, V> next) {
+        this.hash = hash;
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+
+    public int getHash() {
+        return hash;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public Node<K, V> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<K, V> next) {
+        this.next = next;
+    }
+}
