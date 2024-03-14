6cb93c68efe8484597c602a7b1c758ecc787a3de
Artem Klishch <artemklishch@gmail.com>
2024-03-14 21:01:50 +0200

    fix: should make some cganges
    BREAKING CHANGES:
    
    *Fixed 'put' method
    *Fixed reassigning values
    *Fixed getting value
    
    Task: https://mate.academy/learn/java-core/java-hash-map?section=practice&videoId=617&testTaskSlug=java_hash_map&taskSection=task-description

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,62 +1,65 @@
 package core.basesyntax;
 
-import java.util.Arrays;
 import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
     static final float DEFAULT_LOAD_FACTOR = 0.75f;
     static final int DEFAULT_START_CAPACITY = 16;
+    static final int INCREASE_CAPACITY_VALUE = 2;
     private int capacity;
     private int size;
     private int threshold;
-    private Node[] nodes;
+    private Node<K,V>[] nodes;
 
     public MyHashMap() {
         this.capacity = DEFAULT_START_CAPACITY;
         this.size = 0;
-        this.threshold = (int) DEFAULT_LOAD_FACTOR * capacity;
+        this.threshold = (int) (DEFAULT_LOAD_FACTOR * capacity);
         this.nodes = new Node[capacity];
     }
 
     @Override
     public void put(K key, V value) {
+        if (size >= threshold) {
+            resizeList(key, value);
+            return;
+        }
         int hashcode = hashCode(key);
-        Node newNode = new Node(key, value, hashcode);
+        Node<K,V> newNode = new Node<>(key, value);
+        if (key == null && nodes[0] == null) {
+            nodes[0] = newNode;
+            size++;
+            return;
+        } else if (key == null) {
+            nodes[0] = newNode;
+            return;
+        }
         int index = defineIndex(hashcode);
-        Node lastNode = getLastCollisionNode(index);
+        Node<K,V> lastNode = getLastCollisionNode(index);
         if (lastNode == null) {
             nodes[index] = newNode;
+            size++;
         } else {
-            lastNode.setNext(newNode);
+            reassignSameKeyValues(newNode, nodes[index]);
         }
-        size++;
     }
-
-    // the we'll remove it
-    @Override
-    public String toString() {
-        return "MyHashMap{" +
-                "capacity=" + capacity +
-                ", size=" + size +
-                ", threshold=" + threshold +
-                ", nodes=" + Arrays.toString(nodes) +
-                '}';
-    }
-    // the we'll remove it
 
     @Override
     public V getValue(K key) {
+        if (key == null) {
+            return nodes[0] != null ? nodes[0].getValue() : null;
+        }
         int hashcode = hashCode(key);
         int index = defineIndex(hashcode);
-        Node firsNodeOnIndex = nodes[index];
+        Node<K,V> firsNodeOnIndex = nodes[index];
         if (firsNodeOnIndex == null) {
             return null;
         }
         if (firsNodeOnIndex.next == null) {
-            return (V) firsNodeOnIndex.getValue();
+            return firsNodeOnIndex.getValue();
         }
-        Node foundNode = getCollisionNode(firsNodeOnIndex, key);
-        return foundNode == null ? null : (V) foundNode.getValue();
+        Node<K,V> foundNode = getCollisionNode(firsNodeOnIndex, key);
+        return foundNode.getValue();
     }
 
     @Override
@@ -66,24 +69,31 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public int hashCode(K key) {
-        return Objects.hash(key);
+        int hashcode = Objects.hash(key);
+        return hashcode < 0 ? (hashcode * -1) : hashcode;
     }
 
-    private class Node<K, V> {
-        private K key;
+    private static class Node<K, V> {
+        private final K key;
         private V value;
-        private int hashcode;
         private Node<K, V> next;
 
-        public Node(K key, V value, int hashcode) {
+        public Node(K key, V value) {
             this.key = key;
             this.value = value;
-            this.hashcode = hashcode;
             this.next = null;
+        }
+
+        public K getKey() {
+            return key;
         }
 
         public V getValue() {
             return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
         }
 
         public void setNext(Node<K, V> next) {
@@ -100,21 +110,59 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return hashcode % capacity;
     }
 
-    private Node getLastCollisionNode(int index) {
+    private Node<K,V> getLastCollisionNode(int index) {
         if (nodes[index] == null) {
             return null;
         }
-        Node lastNode = nodes[index];
+        Node<K,V> lastNode = nodes[index];
         while (lastNode.next != null) {
             lastNode = lastNode.next;
         }
         return lastNode;
     }
 
-    private Node getCollisionNode(Node nextNode, K key) {
-        if (nextNode != null && !nextNode.equals(key)) {
+    private Node<K,V> getCollisionNode(Node<K,V> nextNode, K key) {
+        if (nextNode.next != null && !nextNode.equals(key)) {
             nextNode = getCollisionNode(nextNode.next, key);
         }
         return nextNode;
+    }
+
+    private void reassignSameKeyValues(Node<K,V> newNode, Node<K,V> currentNode) {
+        if (currentNode == null) {
+            return;
+        }
+        K currentNodeKey = (K) currentNode.getKey();
+        if (newNode.equals(currentNodeKey)) {
+            currentNode.setValue(newNode.getValue());
+            return;
+        }
+        if (currentNode.next == null) {
+            currentNode.next = newNode;
+            size++;
+            return;
+        }
+        reassignSameKeyValues(newNode, currentNode.next);
+    }
+
+    private void resizeList(K key, V value) {
+        this.capacity = capacity * INCREASE_CAPACITY_VALUE;
+        this.threshold = (int) (DEFAULT_LOAD_FACTOR * capacity);
+        this.size = 0;
+        Node<K,V>[] prevNodes = nodes;
+        this.nodes = new Node[capacity];
+        for (Node<K,V> node : prevNodes) {
+            if (node != null) {
+                reassignValues(node);
+            }
+        }
+        put(key, value);
+    }
+
+    private void reassignValues(Node<K,V> node) {
+        put(node.getKey(), node.getValue());
+        if (node.next != null) {
+            reassignValues(node.next);
+        }
     }
 }
