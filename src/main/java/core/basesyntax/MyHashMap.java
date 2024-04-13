f63641b951f59f16e6450e4265753ea16669c498
Kate Drevilo <rabmargo@gmail.com>
2024-04-13 09:17:14 +0300

    New fixed methods: put(K key, V value) and resize()

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -3,36 +3,33 @@ package core.basesyntax;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final double LOAD_FACTOR = 0.75f;
-    private static final int RESIZE_FACTOR = 2;
     private Entry<K, V>[] buckets;
     private int size;
-    private int capacity;
-    private int resizeCount;
 
     public MyHashMap() {
-        this(DEFAULT_CAPACITY);
-    }
-
-    public MyHashMap(int initialCapacity) {
-        capacity = initialCapacity;
-        buckets = new Entry[initialCapacity];
+        buckets = new Entry[DEFAULT_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
         int index = getIndex(key);
-        Entry<K, V> entry = buckets[index];
-        while (entry != null) {
-            if (key == entry.key || key != null && key.equals(entry.key)) {
-                entry.value = value;
-                return;
+        if (buckets[index] == null) {
+            buckets[index] = new Entry<>(key, value);
+            size++;
+        } else {
+            Entry<K, V> entry = buckets[index];
+            while (entry != null) {
+                if (key == entry.key || key != null && key.equals(entry.key)) {
+                    entry.value = value;
+                    return;
+                }
+                entry = entry.next;
             }
-            entry = entry.next;
+            Entry<K, V> newEntry = new Entry<>(key, value);
+            newEntry.next = buckets[index];
+            buckets[index] = newEntry;
+            size++;
         }
-        Entry<K,V> newEntry = new Entry<>(key, value);
-        newEntry.next = buckets[index];
-        buckets[index] = newEntry;
-        size++;
         if (size > getThreshold()) {
             resize();
         }
@@ -58,39 +55,18 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private double getThreshold() {
-        return LOAD_FACTOR * (double) DEFAULT_CAPACITY * Math.pow(RESIZE_FACTOR, resizeCount);
+        return LOAD_FACTOR * buckets.length;
     }
 
     private void resize() {
-        int newCapacity = buckets.length * RESIZE_FACTOR;
-        Entry<K, V>[] newBuckets = new Entry[newCapacity];
-
-        for (int i = 0; i < buckets.length; i++) {
-            Entry<K, V> entry = buckets[i];
+        Entry<K, V>[] oldBuckets = buckets;
+        buckets = new Entry[2 * oldBuckets.length];
+        size = 0;
+        for (Entry<K, V> entry : oldBuckets) {
             while (entry != null) {
-                Entry<K, V> next = entry.next;
-                entry.next = null;
-                int index = getIndex(entry.key, newCapacity);
-                putInNewBucket(newBuckets, entry.key, entry.value);
-                entry = next;
+                put(entry.key, entry.value);
+                entry = entry.next;
             }
-        }
-        buckets = newBuckets;
-        capacity = newCapacity;
-        resizeCount++;
-    }
-
-    private void putInNewBucket(Entry<K, V>[] newBuckets, K key, V value) {
-        int index = getIndex(key, newBuckets.length);
-
-        if (newBuckets[index] == null) {
-            newBuckets[index] = new Entry<>(key, value);
-        } else {
-            Entry<K, V> current = newBuckets[index];
-            while (current.next != null) {
-                current = current.next;
-            }
-            current.next = new Entry<>(key, value);
         }
     }
 
@@ -101,21 +77,15 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return Math.abs(key.hashCode()) % buckets.length;
     }
 
-    private int getIndex(K key, int capacity) {
-        if (key == null) {
-            return 0;
-        }
-        return Math.abs(key.hashCode()) % capacity;
-    }
-
     private static class Entry<K, V> {
-        private K key;
+        private final K key;
         private V value;
         private Entry<K, V> next;
 
         public Entry(K key, V value) {
             this.key = key;
             this.value = value;
+            this.next = null;
         }
     }
 }
