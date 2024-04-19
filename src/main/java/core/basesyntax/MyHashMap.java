13ba0cadc7b8a1dd44f5b26d758e828391908b8e
Kateryna <keitkat98@gmail.com>
2024-04-19 14:29:50 +0300

    removed next null

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -85,7 +85,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         public Node(K key, V value) {
             this.key = key;
             this.value = value;
-            this.next = null;
         }
     }
 }
