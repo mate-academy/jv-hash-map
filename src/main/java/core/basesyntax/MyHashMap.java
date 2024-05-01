0d2c526fac6d0fda00827ed2412e1450499a24c2
Oleksii Bashkirov <bashkirov.o.u@ukr.net>
2024-05-02 01:13:52 +0300

    Implement methods: put(K Key, V Value) getValue() getSize()
    created for this purpose:
    static class Node<K, V,
    and methods: putNullKey(), hash(), indexFor(),addNode(), resize(), transfer()

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,114 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int INITIAL_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+
+    private Node<K, V>[] table;
+    private int size;
+
+    public MyHashMap() {
+        table = new Node[INITIAL_CAPACITY];
+        size = 0;
+    }
+
+    private static class Node<K, V> {
+        private final int hash;
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
 
     @Override
     public void put(K key, V value) {
+        if (key == null) {
+            putNullKey(value);
+            return;
+        }
+        int hash = hash(key);
+        int index = indexFor(hash, table.length);
+        for (Node<K, V> e = table[index]; e != null; e = e.next) {
+            if (e.hash == hash && Objects.equals(e.key, key)) {
+                e.value = value;
+                return;
+            }
+        }
+        addNode(hash, key, value, index);
+    }
 
+    private void putNullKey(V value) {
+        for (Node<K, V> e = table[0]; e != null; e = e.next) {
+            if (e.key == null) {
+                e.value = value;
+                return;
+            }
+        }
+        addNode(0, null, value, 0);
+    }
+
+    private int hash(K key) {
+        int h;
+        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+    }
+
+    private int indexFor(int hash, int length) {
+        return hash & (length - 1);
+    }
+
+    private void addNode(int hash, K key, V value, int bucketIndex) {
+        Node<K, V> newNode = new Node<>(hash, key, value, table[bucketIndex]);
+        table[bucketIndex] = newNode;
+        if (++size > (int) (table.length * LOAD_FACTOR)) {
+            resize(2 * table.length);
+        }
+    }
+
+    private void resize(int newCapacity) {
+        Node<K, V>[] newTable = new Node[newCapacity];
+        transfer(newTable);
+        table = newTable;
+    }
+
+    private void transfer(Node<K, V>[] newTable) {
+        Node<K, V>[] src = table;
+        for (int j = 0; j < src.length; j++) {
+            Node<K, V> e = src[j];
+            if (e != null) {
+                src[j] = null;
+                do {
+                    Node<K, V> next = e.next;
+                    int i = indexFor(e.hash, newTable.length);
+                    e.next = newTable[i];
+                    newTable[i] = e;
+                    e = next;
+                } while (e != null);
+            }
+        }
     }
 
     @Override
     public V getValue(K key) {
+        int hash = hash(key);
+        int index = indexFor(hash, table.length);
+        for (Node<K, V> e = table[index]; e != null; e = e.next) {
+            if (e.hash == hash && (key == e.key || (key != null && key.equals(e.key)))) {
+                return e.value;
+            }
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
     }
 }
