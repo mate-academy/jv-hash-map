2bff5c0e47498eca6ce6a404d764910f953f7bff
Anton <antchaycka@gmail.com>
2024-05-07 13:43:50 +0200

    implemented methods put, getValue, getSize. Created class Node, methods: hash, nullKeyCheck, resize, transfer

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,111 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_SIZE = 16;
+    private static final int MULTIPLIER = 2;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private int size;
+    private Node<K, V>[] table;
+
+    MyHashMap() {
+        table = new Node[DEFAULT_SIZE];
+    }
 
     @Override
     public void put(K key, V value) {
-
+        int hash = hash(key);
+        int index = hash & table.length - 1;
+        Node<K, V> node = table[index];
+        while (node != null) {
+            if (node.key == null && key == null) {
+                node.value = value;
+                return;
+            }
+            if (node.key != null && node.key.equals(key)) {
+                node.value = value;
+                return;
+            }
+            node = node.next;
+        }
+        Node<K, V> newNode = new Node<>(key, value, hash, null);
+        newNode.next = table[index];
+        table[index] = newNode;
+        size++;
+        resize();
     }
 
     @Override
     public V getValue(K key) {
+        if (nullKeyCheck(key) != null) {
+            return nullKeyCheck(key);
+        }
+
+        for (Node<K, V> kvNode : table) {
+            Node<K, V> node = kvNode;
+            if (node == null) {
+                continue;
+            }
+            do {
+                if (node.hash == hash(key) && key.equals(node.key)) {
+                    return node.value;
+                }
+                node = node.next;
+
+            } while (node != null);
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private int hash(K key) {
+        int h;
+        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+    }
+
+    private V nullKeyCheck(K key) {
+        for (Node<K, V> node = table[0]; node != null; node = node.next) {
+            if (node.key == null && key == null) {
+                return node.value;
+            }
+        }
+        return null;
+    }
+
+    private void resize() {
+        if ((float) size / table.length >= DEFAULT_LOAD_FACTOR) {
+            Node<K, V>[] newTable = new Node[(table.length * MULTIPLIER)];
+            table = transfer(newTable);
+        }
+    }
+
+    private Node<K, V>[] transfer(Node<K, V>[] newTable) {
+        for (int i = 0; i < table.length; i++) {
+            Node<K, V> node = table[i];
+            while (node != null) {
+                Node<K, V> next = node.next;
+                node.next = newTable[i];
+                newTable[i] = node;
+                node = next;
+            }
+        }
+        return newTable;
+    }
+
+    private static class Node<K, V> {
+        private final K key;
+        private V value;
+        private final int hash;
+        private Node<K, V> next;
+
+        Node(K key, V value, int hash, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.hash = hash;
+            this.next = next;
+        }
     }
 }
