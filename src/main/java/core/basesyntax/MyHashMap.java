2a7512337edf37f772941d4a5dab75a0bf1f8025
flamehost <flamehost@syneforge.com>
2024-05-08 19:32:36 +0300

    Implement Hash map methods

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,109 @@
 package core.basesyntax;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final double LOAD_FACTOR = 0.75;
+
+    private Node<K, V>[] table;
+    private Map<K, V> nullMap;
+    private int size;
+
+    public MyHashMap() {
+        this.table = new Node[DEFAULT_CAPACITY];
+        this.size = 0;
+        this.nullMap = new HashMap<>();
+    }
 
     @Override
     public void put(K key, V value) {
+        if (key == null) {
+            nullMap.put(null, value);
+            return;
+        }
 
+        int index = getIndex(key);
+
+        if (table[index] == null) {
+            table[index] = new Node<>(key, value);
+            size++;
+        } else {
+            Node<K, V> currentNode = table[index];
+            Node<K, V> prevNode = null;
+            while (currentNode != null) {
+                if (currentNode.key.equals(key)) {
+                    currentNode.value = value;
+                    return;
+                }
+                prevNode = currentNode;
+                currentNode = currentNode.next;
+            }
+
+            prevNode.next = new Node<>(key, value);
+            size++;
+        }
+
+        if (size >= table.length * LOAD_FACTOR) {
+            resize();
+        }
     }
 
     @Override
     public V getValue(K key) {
+        if (key == null) {
+            return nullMap.get(null);
+        }
+
+        int index = getIndex(key);
+
+        if (index < 0 || index >= table.length) {
+            return null;
+        }
+
+        Node<K, V> currentNode = table[index];
+        while (currentNode != null) {
+            if (currentNode.key.equals(key)) {
+                return currentNode.value;
+            }
+            currentNode = currentNode.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size + nullMap.size();
+    }
+
+    private int getIndex(K key) {
+        int hashCode = Objects.hashCode(key);
+        return (hashCode & (table.length - 1));
+    }
+
+    private void resize() {
+        Node<K, V>[] oldTable = table;
+        table = new Node[table.length * 2];
+        size = 0;
+
+        for (Node<K, V> currentNode : oldTable) {
+            while (currentNode != null) {
+                put(currentNode.key, currentNode.value);
+                currentNode = currentNode.next;
+            }
+        }
+    }
+
+    private static class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
