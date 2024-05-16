fb0bdec21cde35de4b5c947b6cf3b35de2f18dea
Igor Glebov <igor.glebov0312@gmail.com>
2024-05-16 17:21:57 +0300

    added changes

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,32 +5,30 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final float DEFAULT_LOAD_FACTOR = 0.75F;
-    private static final int RESIZE_FACTOR = 2;
     private Node<K, V>[] table;
     private int size;
-    private int capacity = DEFAULT_CAPACITY;
 
     public MyHashMap() {
-        this.table = new Node[DEFAULT_CAPACITY];
+        table = new Node[DEFAULT_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
         resizeIfRequired();
         int index = getIndex(key);
-        if (this.table[index] == null) {
-            this.table[index] = new Node<>(key, value, null);
+        if (table[index] == null) {
+            table[index] = new Node<>(key, value);
             size++;
             return;
         }
-        for (Node<K, V> node = this.table[index]; node != null;
+        for (Node<K, V> node = table[index]; node != null;
                 node = node.next) {
             if (Objects.equals(key, node.key)) {
                 node.value = value;
                 return;
             }
             if (node.next == null) {
-                node.next = new Node(key, value, null);
+                node.next = new Node(key, value);
                 size++;
             }
         }
@@ -39,7 +37,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public V getValue(K key) {
         int index = getIndex(key);
-        for (Node<K, V> currentNode = this.table[index]; currentNode != null;
+        for (Node<K, V> currentNode = table[index]; currentNode != null;
                 currentNode = currentNode.next) {
             if (Objects.equals(currentNode.key, key)) {
                 return currentNode.value;
@@ -50,19 +48,19 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public int getSize() {
-        return this.size;
+        return size;
     }
 
     private int getIndex(K key) {
-        return (key == null ? 0 : ((int)(Math.pow(key.hashCode(), 2)))) % capacity;
+        return (key == null ? 0 : ((int)(Math.pow(key.hashCode(), 2)))) % table.length;
     }
 
     private void resizeIfRequired() {
-        if (size >= (capacity * DEFAULT_LOAD_FACTOR)) {
-            this.capacity = capacity * RESIZE_FACTOR;
+        if (size >= (table.length * DEFAULT_LOAD_FACTOR)) {
+            int capacity = table.length << 1;
             size = 0;
-            Node<K, V>[] oldTab = this.table;
-            this.table = new Node[capacity];
+            Node<K, V>[] oldTab = table;
+            table = new Node[capacity];
             for (Node<K, V> bucket : oldTab) {
                 while (bucket != null) {
                     put(bucket.key, bucket.value);
@@ -77,10 +75,9 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         private V value;
         private Node<K, V> next;
 
-        Node(K key, V value, Node<K, V> next) {
+        Node(K key, V value) {
             this.key = key;
             this.value = value;
-            this.next = next;
         }
     }
 }
