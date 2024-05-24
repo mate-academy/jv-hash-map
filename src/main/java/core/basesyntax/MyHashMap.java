74d3b0a790abaaf949bf99d34f9e2d8e9d368ee3
dreo <mishkin878@gmail.com>
2024-05-24 09:48:23 +0300

    change put,getValue,getSize,addNode,transfer,getSizegetBucketIndex,getBucketIndexForNewTable methods and class Node

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,50 +1,52 @@
 package core.basesyntax;
 
-import java.util.Map;
-
 public class MyHashMap<K, V> implements MyMap<K, V> {
     static final float DEFAULT_LOAD_FACTOR = 0.75f;
     static final int DEFAULT_INITIAL_CAPACITY = 16;
     private Node<K, V>[] table;
     private int size;
-    private int threshold;
-    private float loadFactor;
 
     public MyHashMap() {
-        this.loadFactor = DEFAULT_LOAD_FACTOR;
-        this.threshold = (int) (DEFAULT_INITIAL_CAPACITY * DEFAULT_LOAD_FACTOR);
         this.table = new Node[DEFAULT_INITIAL_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
-        int hash = hash(key);
-        int index = indexFor(hash, table.length);
-        for (Node<K, V> node = table[index]; node != null; node = node.next) {
-            if (node.hash == hash && (node.key == key || key != null && key.equals(node.key))) {
-                node.value = value;
+        int index = getBucketIndex(key);
+        for (Node<K, V> node = table[index]; node != null; node = node.getNext()) {
+            if (node.getKey() == key || (key != null && key.equals(node.getKey()))) {
+                node.setValue(value);
                 return;
             }
         }
-        addNode(hash, key, value, index);
+        addNode(key, value, index);
     }
 
     @Override
     public V getValue(K key) {
-        int hash = hash(key);
-        int index = indexFor(hash, table.length);
-        for (Node<K, V> node = table[index]; node != null; node = node.next) {
-            if (node.hash == hash && (node.key == key || key != null && key.equals(node.key))) {
-                return node.value;
+        int index = getBucketIndex(key);
+        for (Node<K, V> node = table[index]; node != null; node = node.getNext()) {
+            if (node.getKey() == key || (key != null && key.equals(node.getKey()))) {
+                return node.getValue();
             }
         }
         return null;
     }
 
-    private void addNode(int hash, K key, V value, int bucketIndex) {
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    private int getBucketIndex(K key) {
+        int hash = (key == null) ? 0 : key.hashCode() ^ (key.hashCode() >>> 16);
+        return hash & (table.length - 1);
+    }
+
+    private void addNode(K key, V value, int bucketIndex) {
         Node<K, V> node = table[bucketIndex];
-        table[bucketIndex] = new Node<>(hash, key, value, node);
-        if (++size > threshold) {
+        table[bucketIndex] = new Node<>(key, value, node);
+        if (++size > table.length * DEFAULT_LOAD_FACTOR) {
             resize();
         }
     }
@@ -54,63 +56,54 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         Node<K, V>[] newTable = new Node[newCapacity];
         transfer(newTable);
         table = newTable;
-        threshold = (int) (newCapacity * loadFactor);
     }
 
     private void transfer(Node<K, V>[] newTable) {
-        Node<K, V>[] oldTable = table;
-        for (Node<K, V> node : oldTable) {
+        for (Node<K, V> node : table) {
             while (node != null) {
-                Node<K, V> next = node.next;
-                int index = indexFor(node.hash, newTable.length);
-                node.next = newTable[index];
+                Node<K, V> next = node.getNext();
+                int index = getBucketIndexForNewTable(node.getKey(), newTable.length);
+                node.setNext(newTable[index]);
                 newTable[index] = node;
                 node = next;
             }
         }
     }
 
-    @Override
-    public int getSize() {
-        return size;
+    private int getBucketIndexForNewTable(K key, int newTableLength) {
+        int hash = (key == null) ? 0 : key.hashCode() ^ (key.hashCode() >>> 16);
+        return hash & (newTableLength - 1);
     }
 
-    private int indexFor(int hash, int length) {
-        return hash & (length - 1);
-    }
-
-    private int hash(Object key) {
-        return (key == null) ? 0 : key.hashCode() ^ (key.hashCode() >>> 16);
-    }
-
-    static class Node<K, V> implements Map.Entry<K, V> {
-        private final int hash;
+    static class Node<K, V> {
         private final K key;
         private V value;
         private Node<K, V> next;
 
-        Node(int hash, K key, V value, Node<K, V> next) {
-            this.hash = hash;
+        Node(K key, V value, Node<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
         }
 
-        @Override
         public K getKey() {
             return key;
         }
 
-        @Override
         public V getValue() {
             return value;
         }
 
-        @Override
-        public V setValue(V newValue) {
-            V oldValue = value;
+        public void setValue(V newValue) {
             value = newValue;
-            return oldValue;
+        }
+
+        public Node<K, V> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<K, V> next) {
+            this.next = next;
         }
     }
 }
