35a0312a65ded6192782d8843e997786bc3089b0
Tomasz Baran <MarcusOfficial@tuta.io>
2024-05-29 13:58:33 +0200

    touch-up

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -9,16 +9,17 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private Node<K,V>[] nodes;
     private int size;
     private int currentCapacity;
+    private int threshold;
 
     public MyHashMap() {
         nodes = new Node[DEFAULT_CAPACITY];
         currentCapacity = DEFAULT_CAPACITY;
+        threshold = (int) (DEFAULT_CAPACITY * LOAD_FACTOR);
     }
 
     @Override
     public void put(K key, V value) {
         Node<K, V> newNode = new Node<>(key, value);
-        int threshold = (int) (currentCapacity * LOAD_FACTOR);
         if (size >= threshold) {
             resize();
         }
@@ -64,8 +65,9 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private void resize() {
         size = 0;
-        Node<K, V>[] oldNodes = nodes;
         currentCapacity *= GROWTH_FACTOR;
+        threshold = (int) (currentCapacity * LOAD_FACTOR);
+        Node<K, V>[] oldNodes = nodes;
         nodes = (Node<K, V>[]) new Node[currentCapacity];
         for (Node<K, V> node : oldNodes) {
             while (node != null) {
