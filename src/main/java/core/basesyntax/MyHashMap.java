6f53b816d37aa9fda98f87e415107df74a885699
Svitlana Galicka <svitlanakozak12332@gmail.com>
2024-05-31 22:07:47 +0300

    created hashMap and implemented methods

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,86 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int Default_Capacity = 16;
+    private int size;
+    private Node<K, V>[] table;
+
+    public MyHashMap() {
+        table = new Node[Default_Capacity];
+    }
 
     @Override
     public void put(K key, V value) {
-
+        if (size > table.length * 0.75) {
+            resize();
+        }
+        int index = getIndex(key);
+        Node<K,V> currentNode = table[index];
+        Node<K, V> myNode = new Node<>(key, value, null);
+        while (currentNode != null) {
+            if (currentNode.key == key
+                    || (key != null && key.equals(currentNode.key))) {
+                currentNode.value = value;
+                return;
+            }
+            if (currentNode.next == null) {
+                currentNode.next = myNode;
+                size++;
+                return;
+            }
+            currentNode = currentNode.next;
+        }
+        table[index] = myNode;
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        if (size == 0) {
+            return null;
+        }
+        int index = getIndex(key);
+        Node<K,V> node = table[index];
+        while (node != null) {
+            if (node.key == key || (node.key != null && node.key.equals(key))) {
+                return node.value;
+            }
+            node = node.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private void resize() {
+        size = 0;
+        int newLength = table.length * 2;
+        Node<K, V>[] oldTable = table;
+        table = new Node[newLength];
+        for (Node<K, V> current : oldTable) {
+            while (current != null) {
+                put(current.key, current.value);
+                current = current.next;
+            }
+        }
+    }
+
+    private int getIndex(K key) {
+        return (key == null) ? 0 : Math.abs(key.hashCode()) % table.length;
+    }
+
+    private static class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
