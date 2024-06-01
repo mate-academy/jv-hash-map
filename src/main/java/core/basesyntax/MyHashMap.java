e153d8a68aa2ff3b5719783d8abc9266aa152f45
Greshch <greshchook@gmail.com>
2024-06-01 15:18:56 +0300

    fix style errors 3

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -103,7 +103,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o)  {
+            if (this == o) {
                 return true;
             }
             if (o == null || getClass() != o.getClass()) {
