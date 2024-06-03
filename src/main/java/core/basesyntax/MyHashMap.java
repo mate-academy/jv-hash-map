aa9fd1a61c45634e25d8fcb3d1b3bf6c54a389b1
mgsorigo <marek.gucma@sorigo.pl>
2024-06-03 22:28:57 +0200

    fix

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -148,36 +148,4 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
         buckets = newBuckets;
     }
-
-    static class Node<K, V> {
-        private final K key;
-        private V value;
-        private Node<K, V> next;
-
-        public Node(K key, V value, Node<K, V> next) {
-            this.key = key;
-            this.value = value;
-            this.next = next;
-        }
-
-        public K getKey() {
-            return key;
-        }
-
-        public V getValue() {
-            return value;
-        }
-
-        public void setValue(V value) {
-            this.value = value;
-        }
-
-        public Node<K, V> getNext() {
-            return next;
-        }
-
-        public void setNext(Node<K, V> next) {
-            this.next = next;
-        }
-    }
 }
