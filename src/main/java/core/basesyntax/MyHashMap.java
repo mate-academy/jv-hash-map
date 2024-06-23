94c37e55cdffaded693810b3fa8bb28b6575869e
Roman Chygryn <chygryn.ra@gmail.com>
2024-06-23 15:33:50 +0200

    changes after review

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,42 +1,33 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    public static final int DEFAULT_CAPACITY = 16;
-    public static final float LOAD_FACTOR = 0.75f;
-
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final double LOAD_FACTOR = 0.75;
     private Node<K, V>[] table;
     private int size;
-    private int threshold;
 
     public MyHashMap() {
-        this.table = (Node<K, V>[]) new Node[DEFAULT_CAPACITY];
-        this.threshold = (int) (DEFAULT_CAPACITY * LOAD_FACTOR);
+        table = (Node<K, V>[]) new Node[DEFAULT_CAPACITY];
     }
 
     @Override
     public void put(K key, V value) {
-        int[] hashAndIndex = getHashAndIndex(key);
-
-        for (Node<K, V> node = table[hashAndIndex[1]]; node != null; node = node.next) {
-            if (isMatchingNode(node, hashAndIndex[0], key)) {
+        int index = getKeyIndex(key);
+        Node<K, V> node = table[index];
+        while (node != null) {
+            if (equalsKey(node.key, key)) {
                 node.value = value;
                 return;
             }
+            node = node.next;
         }
-
-        addNode(hashAndIndex[0], key, value, hashAndIndex[1]);
+        addNode(key, value, index);
     }
 
     @Override
     public V getValue(K key) {
-        int[] hashAndIndex = getHashAndIndex(key);
-
-        for (Node<K, V> node = table[hashAndIndex[1]]; node != null; node = node.next) {
-            if (isMatchingNode(node, hashAndIndex[0], key)) {
-                return node.value;
-            }
-        }
-        return null;
+        Node<K, V> node = getNode(key);
+        return (node == null) ? null : node.value;
     }
 
     @Override
@@ -44,47 +35,50 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private void addNode(int hash, K key, V value, int index) {
-        Node<K, V> newNode = new Node<>(hash, key, value, table[index]);
+    private void addNode(K key, V value, int index) {
+        Node<K, V> newNode = new Node<>(hash(key), key, value, table[index]);
         table[index] = newNode;
-        if (++size > threshold) {
-            resize(2 * table.length);
+        size++;
+        if (size > table.length * LOAD_FACTOR) {
+            resize();
         }
     }
 
-    private void resize(int newCapacity) {
-        Node<K, V>[] oldTable = table;
-        Node<K, V>[] newTable = new Node[newCapacity];
-        threshold = (int) (newCapacity * LOAD_FACTOR);
+    private Node<K, V> getNode(K key) {
+        int index = getKeyIndex(key);
+        Node<K, V> node = table[index];
+        while (node != null) {
+            if (equalsKey(node.key, key)) {
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
 
+    private boolean equalsKey(K k1, K k2) {
+        return (k1 == null && k2 == null) || (k1 != null && k1.equals(k2));
+    }
+
+    private int getKeyIndex(K key) {
+        return (key == null) ? 0 : Math.abs(hash(key) % table.length);
+    }
+
+    private int hash(K key) {
+        return (key == null) ? 0 : key.hashCode();
+    }
+
+    private void resize() {
+        int newCapacity = table.length * 2;
+        Node<K, V>[] oldTable = table;
+        table = (Node<K, V>[]) new Node[newCapacity];
+        size = 0;
         for (Node<K, V> node : oldTable) {
             while (node != null) {
-                Node<K, V> next = node.next;
-                int index = indexFor(node.hash, newCapacity);
-                node.next = newTable[index];
-                newTable[index] = node;
-                node = next;
+                addNode(node.key, node.value, getKeyIndex(node.key));
+                node = node.next;
             }
         }
-        table = newTable;
-    }
-
-    private int hash(Object key) {
-        return key == null ? 0 : key.hashCode() ^ (key.hashCode() >>> 16);
-    }
-
-    private int indexFor(int hash, int length) {
-        return hash & (length - 1);
-    }
-
-    private boolean isMatchingNode(Node<K, V> node, int hash, K key) {
-        return node.hash == hash && (node.key == key || (key != null && key.equals(node.key)));
-    }
-
-    private int[] getHashAndIndex(K key) {
-        int hash = hash(key);
-        int index = indexFor(hash, table.length);
-        return new int[] { hash, index };
     }
 
     private static class Node<K, V> {
