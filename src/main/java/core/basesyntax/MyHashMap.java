76acc011729fe3e40d6a12226089e11785fe5add
Yura Yurchuk <yurayurchuk88@gmail.com>
2024-07-03 19:41:44 +0300

    add class Node

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -16,4 +16,18 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     public int getSize() {
         return 0;
     }
+
+    private class Node<K, V> {
+        private int hashCode;
+        private K kay;
+        private V value;
+        private Node<K, V> next;
+        public Node(int hashCode, K kay, V value, Node<K, V> next) {
+            this.hashCode = hashCode;
+            this.kay = kay;
+            this.value = value;
+            this.next = next;
+        }
+
+    }
 }
