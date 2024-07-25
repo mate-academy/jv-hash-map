6681729f4f01a30e942363d1e303a5d03e40f2ad
KLICKICE <ledmaskiight@gmail.com>
2024-07-25 10:16:40 +0300

    Fixed

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -5,24 +5,12 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int INITIAL_CAPACITY = 16;
     private static final float LOAD_FACTOR = 0.75f;
+    private static final int GROW_FACTOR = 2;
     private Entry<K, V>[] table;
     private int size;
 
     public MyHashMap() {
         table = new Entry[INITIAL_CAPACITY];
-        size = 0;
-    }
-
-    private static class Entry<K, V> {
-        private K key;
-        private V value;
-        private Entry<K, V> next;
-
-        Entry(K key, V value, Entry<K, V> next) {
-            this.key = key;
-            this.value = value;
-            this.next = next;
-        }
     }
 
     @Override
@@ -76,7 +64,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     private void resize() {
         Entry<K, V>[] oldTable = table;
-        table = new Entry[oldTable.length * 2];
+        table = new Entry[oldTable.length * GROW_FACTOR];
         size = 0;
 
         for (Entry<K, V> entry : oldTable) {
@@ -84,6 +72,18 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                 put(entry.key, entry.value);
                 entry = entry.next;
             }
+        }
+    }
+
+    private static class Entry<K, V> {
+        private K key;
+        private V value;
+        private Entry<K, V> next;
+
+        Entry(K key, V value, Entry<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
         }
     }
 }
