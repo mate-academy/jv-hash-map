bc6a02d0192f51c6b1bf9207e02c0e9117720017
Bartlomiej Smreczak <b.smreczak@gmail.com>
2024-08-04 18:16:10 +0200

    Fixes after code review: removed unnecessary conditions, added variable treshold, simplify the hash() method.

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -7,47 +7,43 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     static final int MAXIMAL_CAPACITY = 1 << 30;
     static final float DEFAULT_LOAD_FACTOR = 0.75f;
     private int size = 0;
-
     private ArrayList<Entry<K,V>>[]table = new ArrayList[DEFAULT_INITIAL_CAPACITY];
+    private float threshold = (table.length * DEFAULT_LOAD_FACTOR);
 
     @Override
     public void put(K key, V value) {
         int index = hash(key);
 
-        if (index >= 0) {
-            if (size >= DEFAULT_LOAD_FACTOR * table.length) {
-                resize();
-            }
-
-            if (table[index] == null) {
-                table[index] = new ArrayList<>();
-            }
-
-            for (Entry<K,V> current : table[index]) {
-                if ((key == null && current.getKey() == null)
-                        || (key != null && key.equals(current.getKey()))) {
-                    current.setValue(value);
-                    return;
-                }
-            }
-            table[index].add(new Entry<>(key, value));
-            size++;
+        if (size >= threshold) {
+            resize();
         }
+
+        if (table[index] == null) {
+            table[index] = new ArrayList<>();
+        }
+
+        for (Entry<K,V> current : table[index]) {
+            if ((key == null && current.getKey() == null)
+                    || (key != null && key.equals(current.getKey()))) {
+                current.setValue(value);
+                return;
+            }
+        }
+        table[index].add(new Entry<>(key, value));
+        size++;
     }
 
     @Override
     public V getValue(K key) {
         int index = hash(key);
 
-        if (index >= 0) {
-            if (table[index] == null) {
-                return null;
-            }
-            for (Entry<K, V> entry : table[index]) {
-                if ((key == null && entry.getKey() == null)
-                        || (key != null && key.equals(entry.getKey()))) {
-                    return entry.getValue();
-                }
+        if (table[index] == null) {
+            return null;
+        }
+        for (Entry<K, V> entry : table[index]) {
+            if ((key == null && entry.getKey() == null)
+                    || (key != null && key.equals(entry.getKey()))) {
+                return entry.getValue();
             }
         }
         return null;
@@ -59,25 +55,18 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private int hash(K key) {
-        if (key == null) {
-            return 0;
-        }
-        int output = key.hashCode() % table.length;
-        if (output < 0) {
-            output = output * (-1);
-        }
-        return output;
+        return (key == null ? 0 : Math.abs(key.hashCode()) % table.length);
     }
 
     private void resize() {
         final ArrayList<Entry<K,V>>[] oldTable = table;
+        if (table.length == MAXIMAL_CAPACITY) {
+            return;
+        }
         int newCapacity = table.length * 2;
 
-        if (newCapacity >= MAXIMAL_CAPACITY) {
-            newCapacity = MAXIMAL_CAPACITY;
-        }
-
         table = new ArrayList[newCapacity];
+        threshold = DEFAULT_LOAD_FACTOR * table.length;
         size = 0;
 
         for (ArrayList<Entry<K,V>> bucket : oldTable) {
