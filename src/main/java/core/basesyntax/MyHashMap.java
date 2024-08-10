cf83565718a7d082c3bd4179cd663f9c5f3491e4
Denys Mazurenko <0gur4ik3992@gmail.com>
2024-08-10 23:37:19 +0300

    Implemented myHashMap class. Added put/get/getSize/clear/isEmpty/values/keySet/containsKey/containsValue/ToString/resize/calculateHash. Created class Node

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,172 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private static final int DOUBLE_SIZE = 2;
+    private Node<K,V> [] table;
+    private int size;
+    private int threshold;
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_INITIAL_CAPACITY];
+        threshold = (int) (DEFAULT_LOAD_FACTOR * table.length);
+    }
 
     @Override
     public void put(K key, V value) {
-
+        resize();
+        int tableIndex = calculateHash(key);
+        Node<K,V> node = table[tableIndex];
+        if (node == null) {
+            table[tableIndex] = new Node<>(key, value, null);
+            size++;
+            return;
+        }
+        while (node != null) {
+            if (Objects.equals(node.key,key)) {
+                node.value = value;
+                return;
+            }
+            if (node.next == null) {
+                node.next = new Node<>(key, value, null);
+                size++;
+                return;
+            }
+            node = node.next;
+        }
     }
 
     @Override
     public V getValue(K key) {
+        int index = calculateHash(key);
+        Node<K, V> current = table[index];
+        while (current != null) {
+            if (Objects.equals(current.key, key)) {
+                return current.value;
+            }
+            current = current.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    @Override
+    public void clear() {
+        Arrays.fill(table, null);
+        size = 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public Collection<V> values() {
+        List<V> values = new ArrayList<>();
+        for (Node<K, V> node : table) {
+            while (node != null) {
+                values.add(node.value);
+                node = node.next;
+            }
+        }
+        return values;
+    }
+
+    @Override
+    public Collection<K> keySet() {
+        List<K> keys = new ArrayList<>();
+        for (Node<K, V> nodes : table) {
+            Node<K, V> node = nodes;
+            while (node != null) {
+                keys.add(node.key);
+                node = node.next;
+            }
+        }
+        return keys;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        for (Node<K, V> node : table) {
+            while (node != null) {
+                if (Objects.equals(node.key, key)) {
+                    return true;
+                }
+                node = node.next;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(V value) {
+        for (Node<K, V> node : table) {
+            while (node != null) {
+                if (Objects.equals(node.value, value)) {
+                    return true;
+                }
+                node = node.next;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[ ");
+        for (Node<K, V> nodes : table) {
+            Node<K, V> node = nodes;
+            while (node != null) {
+                sb.append(node.key).append("=").append(node.value).append("  ");
+                node = node.next;
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    private void resize() {
+        if (size >= threshold) {
+            Node<K, V>[] oldTable = table;
+            table = new Node[oldTable.length * DOUBLE_SIZE];
+            threshold = (int) (table.length * DEFAULT_LOAD_FACTOR);
+            size = 0;
+            for (Node<K, V> nodes : oldTable) {
+                Node<K, V> node = nodes;
+                while (node != null) {
+                    put(node.key, node.value);
+                    node = node.next;
+                }
+            }
+        }
+    }
+
+    private int calculateHash(K hash) {
+        return hash == null ? 0 : Math.abs(hash.hashCode()) % table.length;
+    }
+
+    private static class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
--- src/main/java/core/basesyntax/MyMap.java
+++ src/main/java/core/basesyntax/MyMap.java
@@ -1,9 +1,24 @@
 package core.basesyntax;
 
+import java.util.Collection;
+
 public interface MyMap<K, V> {
+
     void put(K key, V value);
+
+    void clear();
+
+    boolean isEmpty();
+
+    boolean containsKey(K key);
+
+    boolean containsValue(V value);
 
     V getValue(K key);
 
     int getSize();
+
+    Collection<V> values();
+
+    Collection<K> keySet();
 }
