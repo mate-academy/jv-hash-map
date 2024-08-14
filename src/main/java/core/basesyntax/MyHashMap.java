fac97bc88970307dbe39bcb9b258bdd0829434f5
Nikita Lafinskiy <niklafinskiy@gmail.com>
2024-08-14 11:40:54 +0200

    made changes

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -6,16 +6,12 @@ import java.util.Objects;
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final double LOAD_FACTOR = 0.75;
+    private static final int GROWTH_FACTOR = 2;
     private Node<K, V>[] table;
-    private int threshold;
-    private int capacity;
     private int size;
 
     public MyHashMap() {
         this.table = new Node[DEFAULT_CAPACITY];
-        this.threshold = (int) (DEFAULT_CAPACITY * LOAD_FACTOR);
-        this.capacity = DEFAULT_CAPACITY;
-        this.size = 0;
     }
 
     @Override
@@ -53,7 +49,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private int findElementIndex(K key) {
-        return key == null ? 0 : Math.abs(getHash(key) % capacity);
+        return key == null ? 0 : Math.abs(getHash(key) % table.length);
     }
 
     private int getHash(K key) {
@@ -61,12 +57,10 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void resize() {
-        if (size >= threshold) {
+        if (size >= (int) (DEFAULT_CAPACITY * LOAD_FACTOR)) {
             size = 0;
-            capacity *= 2;
-            threshold = (int) (LOAD_FACTOR * capacity);
             Node<K, V>[] oldTable = table;
-            table = new Node[capacity];
+            table = new Node[table.length * GROWTH_FACTOR];
             for (Node<K, V> node : oldTable) {
                 while (node != null) {
                     put(node.key, node.value);
