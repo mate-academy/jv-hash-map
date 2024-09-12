b211407b258456c938ca77111c67342426bd8507
Hetalova <nhetalova@luxoft.com>
2024-09-12 17:35:23 +0300

    Revered changes

--- src/test/java/core/basesyntax/Car.java
+++ src/test/java/core/basesyntax/Car.java
@@ -44,12 +44,4 @@ public class Car {
     public int hashCode() {
         return Objects.hash(model, color);
     }
-
-    @Override
-    public String toString() {
-        return "Car{" +
-                "model='" + model + '\'' +
-                ", color='" + color + '\'' +
-                '}';
-    }
 }
