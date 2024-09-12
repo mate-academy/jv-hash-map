da9e247070bd9a9e1bd07c7c86efd6731fcc7fa5
Xoteki <xotekijke@gmail.com>
2024-09-12 21:36:21 +0300

    provided recommended fixed

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -2,35 +2,25 @@ package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
+    private static final int newCapacityMultiplier = 2;
     private static final float LOAD_FACTOR = 0.75f;
 
-    private Node<K, V>[] table;
-    private int size = 0;
+    private Node[] table;
+    private int size;
     private int threshold;
-
-    private static class Node<K, V> {
-        private final K key;
-        private V value;
-        private Node<K, V> next;
-
-        Node(K key, V value, Node<K, V> next) {
-            this.key = key;
-            this.value = value;
-            this.next = next;
-        }
-    }
 
     public MyHashMap() {
         table = new Node[DEFAULT_CAPACITY];
         threshold = (int) (DEFAULT_CAPACITY * LOAD_FACTOR);
     }
 
-    private int hash(K key) {
-        return (key == null) ? 0 : Math.abs(key.hashCode()) % table.length;
-    }
-
     @Override
     public void put(K key, V value) {
+
+        if (size >= threshold) {
+            resize();
+        }
+
         int index = hash(key);
 
         for (Node<K, V> node = table[index]; node != null; node = node.next) {
@@ -43,10 +33,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         Node<K, V> newNode = new Node<>(key, value, table[index]);
         table[index] = newNode;
         size++;
-
-        if (size >= threshold) {
-            resize();
-        }
     }
 
     @Override
@@ -65,13 +51,29 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private void resize() {
-        int newCapacity = table.length * 2;
-        Node<K, V>[] newTable = new Node[newCapacity];
+    private int hash(K key) {
+        return (key == null) ? 0 : Math.abs(key.hashCode()) % table.length;
+    }
 
-        for (Node<K, V> node : table) {
+    private static class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private void resize() {
+        int newCapacity = table.length * newCapacityMultiplier;
+        Node[] newTable = new Node[newCapacity];
+
+        for (Node node : table) {
             while (node != null) {
-                Node<K, V> next = node.next;
+                Node next = node.next;
                 int newIndex = (node.key == null) ? 0 : Math.abs(node.key.hashCode()) % newCapacity;
 
                 node.next = newTable[newIndex];
