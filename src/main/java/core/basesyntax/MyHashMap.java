ce94ca5efb9de5ddcd7d77e64d5ab0160391fde5
StoneBlood <brkalix@gmail.com>
2024-09-19 21:23:38 +0300

    changed the access modifier to private in the class Node

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -94,7 +94,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return key == null ? 0 : Math.abs(key.hashCode()) % DEFAULT_CAPACITY;
     }
 
-    public static class Node<K, V> {
+    private static class Node<K, V> {
         private final K key;
         private V value;
         private Node<K, V> next;
