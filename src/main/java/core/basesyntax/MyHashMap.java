0c43a619831670b3d2fef90402b4609f10eb087b
Nikita Kravtsov <deez999deez@gmail.com>
2024-09-26 13:57:34 +0300

    Implement MyHashMap interface in MyhashMap class. Create a Node<K, V> array with default capacity - 16 and load factor - 75%. Override such methods: put - to put Key, Value pair in array. Check load capacity of array and call resize method if needed. Put value in array or update exist value if key already exist and create linked list of values if there is collision. getValue - to get value from list with exist key or return null if key doesn't exist. getSize - to get amount of values in array.
    add additional methods:
    getHashCode - to get key hashCode with key.hashCode()%array.length formula
    resize - to update array length if size >= 75% of array length and reHash all values to new array.
    
    add inner Node<K,V> class with key, value and next parameters. Create constructor and override equals and hashCode methods.

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,115 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final double DEFAULT_LOAD_FACTOR = 0.75;
+    private Node<K, V>[] table;
+    private int size;
+
+    public MyHashMap() {
+        table = new Node[DEFAULT_CAPACITY];
+        size = 0;
+    }
 
     @Override
     public void put(K key, V value) {
+        if (size >= (table.length) * DEFAULT_LOAD_FACTOR) {
+            resize();
+        }
+        int index = getHashCode(key);
+        Node<K, V> newNode = new Node<>(key, value);
 
+        if (table[index] == null) {
+            table[index] = newNode;
+        } else {
+            Node<K, V> currentNode = table[index];
+            while (currentNode != null) {
+                if ((key == null && currentNode.key == null)
+                        || (key != null && key.equals(currentNode.key))) {
+                    currentNode.value = value;
+                    return;
+                }
+                if (currentNode.next == null) {
+                    break;
+                }
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+        }
+        size++;
     }
 
     @Override
     public V getValue(K key) {
+        int index = getHashCode(key);
+        Node<K, V> currentNode = table[index];
+
+        while (currentNode != null) {
+            if ((key == null && currentNode.key == null)
+                    || (key != null && key.equals(currentNode.key))) {
+                return currentNode.value;
+            }
+            currentNode = currentNode.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private int getHashCode(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode() % table.length);
+    }
+
+    private void resize() {
+        int newCapacity = DEFAULT_CAPACITY << 1;
+        Node<K, V>[] newTable = new Node[newCapacity];
+
+        for (int i = 0; i < table.length; i++) {
+            Node<K, V> currentNode = table[i];
+            while (currentNode != null) {
+                int newIndex = Math.abs(currentNode.key.hashCode() % newCapacity);
+                Node<K, V> nextNode = currentNode.next;
+                currentNode.next = newTable[newIndex];
+                newTable[newIndex] = currentNode;
+                currentNode = nextNode;
+            }
+        }
+        table = newTable;
+    }
+
+    private static class Node<K, V> {
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == this) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (obj.getClass() != getClass()) {
+                return false;
+            }
+            Node<?, ?> node = (Node<?, ?>) obj;
+            return (key == null && node.key == null) || (key != null && key.equals(node.key));
+        }
+
+        @Override
+        public int hashCode() {
+            int result = 17;
+            return key == null ? 0 : (key.hashCode() * result + 31);
+        }
     }
 }
