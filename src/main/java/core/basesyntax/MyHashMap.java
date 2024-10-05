bec5929611632deb96f851ded3c3307181855b8f
Oleh Bihun <o.bihun@ukr.net>
2024-10-05 18:19:45 +0300

    started my solution

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,10 +1,24 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_ARRAY_CAPACITY = 16;
+    private static final int RESIZE_NUMBER = 2;
+    private static final double LOAD_FACTOR = 0.75;
+    private Node<K, V>[] table;
+    private int size;
+
+    public MyHashMap() {
+        this.table = new Node[DEFAULT_ARRAY_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
+        if (size > (table.length * LOAD_FACTOR)) {
+            int capacity = DEFAULT_ARRAY_CAPACITY * RESIZE_NUMBER;
+            Node<K, V>[] newTable = (Node<K, V>[]) new Node[capacity];
 
+        }
+        size++;
     }
 
     @Override
@@ -14,6 +28,24 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    private void resize() {
+        Node<K, V>[] currentTable = table;
+        table = (Node<K, V>[]) new Node[currentTable.length * RESIZE_NUMBER];
+
+    }
+
+    private static class Node<K, V> {
+        final K key;
+        final V value;
+        final Node<K, V> next;
+
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
