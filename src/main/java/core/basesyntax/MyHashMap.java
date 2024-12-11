37a7df480adba436248983518615026b0a753df6
Valeriy <valeriym250@gmail.com>
2024-12-11 15:12:01 +0000

    fixed my solution according to your recommendation

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -6,9 +6,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
     private static final double LOAD_FACTOR = 0.75;
     private int size;
-    private int index;
     private Node<K, V>[] storage;
-    private Node<K, V> temp;
 
     public MyHashMap() {
         storage = new Node[DEFAULT_CAPACITY];
@@ -20,6 +18,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if (key == null) {
             putByIndex(null, value, 0);
         } else {
+            int index;
             index = definePosition(key);
             putByIndex(key, value, index);
         }
@@ -28,8 +27,8 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public V getValue(K key) {
         if (size > 0) {
-            index = definePosition(key);
-            temp = storage[index];
+            int index = (key == null) ? 0 : definePosition(key);
+            Node<K, V> temp = storage[index];
             while (temp != null) {
                 if (Objects.equals(key, temp.key)) {
                     return temp.value;
@@ -45,28 +44,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    public V remove(K key) {
-        index = definePosition(key);
-        temp = storage[index];
-        V result = null;
-        if (Objects.equals(storage[index].key, key)) {
-            result = storage[index].value;
-            storage[index] = storage[index].next;
-        } else {
-            while (storage[index].next != null) {
-                if (Objects.equals(temp.next.key, key)) {
-                    result = temp.next.value;
-                    temp.next = temp.next.next;
-                    break;
-                }
-
-                temp = temp.next;
-            }
-        }
-        size -= (result == null) ? 0 : 1;
-        return result;
-    }
-
     private Node<K, V>[] resizeArray() {
         Node<K, V>[] boxes = new Node[storage.length];
         System.arraycopy(storage, 0, boxes, 0, storage.length);
@@ -75,6 +52,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private void writeDataToNewArray(Node<K, V>[] boxes) {
+        Node<K, V> temp;
         for (int i = 0; i < boxes.length; i++) {
             if (boxes[i] == null) {
                 continue;
@@ -93,7 +71,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         if (storage[index] == null) {
             storage[index] = new Node<>(key, value, null);
         } else {
-            temp = storage[index];
+            Node<K, V> temp = storage[index];
             while (temp != null) {
                 if (Objects.equals(temp.key, key)) {
                     temp.value = value;
@@ -110,9 +88,6 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private int definePosition(K key) {
-        if (key == null) {
-            return 0;
-        }
         int posDigit;
         posDigit = Math.abs(key.hashCode() % storage.length);
         return posDigit;
@@ -124,7 +99,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    static class Node<K, V> {
+    private static class Node<K, V> {
         private int hash;
         private K key;
         private V value;
