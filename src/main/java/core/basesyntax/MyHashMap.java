f71a89643117a94c75444f2615a22bed5f2659ad
mr-green <sergey.slobodianiuk@gmail.com>
2025-02-03 14:26:23 +0200

    jv-hash-map task is done

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,139 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+
+    private static final int DEFAULT_CAPACITY = 1 << 4;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private int threshold;
+    private int size;
+    private final float loadFactor;
+    private int capacity;
+    private Node<K, V>[] table;
+
+    public MyHashMap() {
+        this.table = new Node[DEFAULT_CAPACITY];
+        this.loadFactor = DEFAULT_LOAD_FACTOR;
+        this.threshold = (int) (DEFAULT_LOAD_FACTOR * DEFAULT_CAPACITY);
+        this.capacity = DEFAULT_CAPACITY;
+    }
 
     @Override
     public void put(K key, V value) {
-
+        int h = hash(key);
+        putVal(h, key, value);
     }
 
     @Override
     public V getValue(K key) {
+        int index = hash(key);
+        Node<K, V> current = table[index];
+        while (current != null) {
+            if (key == null && current.key == null || (key != null && key.equals(current.key))) {
+                return current.value;
+            }
+            current = current.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    final void putVal(int h, K key, V value) {
+        if (size >= threshold) {
+            resize();
+        }
+        int index = (key == null) ? 0 : h % capacity;
+        if (table[index] == null) {
+            table[index] = new Node<>(h, key, value, null);
+        } else {
+            Node<K, V> current = table[index];
+
+            while (true) {
+                if (Objects.equals(key, current.key)) {
+                    current.value = value;
+                    return;
+                }
+                if (current.next == null) {
+                    current.next = new Node<>(h, key, value, null);
+                    break;
+                }
+                current = current.next;
+            }
+        }
+        size++;
+    }
+
+    private void resize() {
+        int newCapacity = capacity * 2;
+        Node<K, V>[] newTable = new Node[newCapacity];
+        for (Node<K, V> node : table) {
+            while (node != null) {
+                Node<K, V> next = node.next;
+                int newIndex = (node.key == null) ? 0 : Math.abs(node.key.hashCode() % newCapacity);
+                node.next = newTable[newIndex];
+                newTable[newIndex] = node;
+                node = next;
+            }
+        }
+        table = newTable;
+        capacity = newCapacity;
+        threshold = (int) (capacity * loadFactor);
+    }
+
+    private int hash(Object key) {
+        return key == null ? 0 : Math.abs(key.hashCode()) % table.length;
+    }
+
+    static class Node<K, V> {
+        final int hash;
+        final K key;
+        V value;
+        Node<K, V> next;
+
+        Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        public final K getKey() {
+            return key;
+        }
+
+        public final V getValue() {
+            return value;
+        }
+
+        public final V setValue(V newValue) {
+            V oldValue = value;
+            value = newValue;
+            return oldValue;
+        }
+
+        public final String toString() {
+            return key + " = " + value;
+        }
+
+        public final int hashCode() {
+            return Objects.hashCode(key) ^ Objects.hashCode(value);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Node<?, ?> node = (Node<?, ?>) o;
+            return hash == node.hash && Objects.equals(key, node.key) && Objects.equals(value, node.value) && Objects.equals(next, node.next);
+        }
     }
 }
