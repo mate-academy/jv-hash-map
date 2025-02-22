d69aa828426d6ae96ad1560b3c9630652cdaef11
Hanna Voloshko <aa.voloshko09@gmail.com>
2025-02-23 00:09:01 +0200

    implemented Node class

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,6 +1,40 @@
 package core.basesyntax;
 
+import java.util.Map;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    final private int DEFAULT_CAPACITY = 16;
+    final private double DEFAULT_LOAD_FACTOR = 0.75;
+
+
+    static class Node<K, V> implements Map.Entry<K, V> {
+        final int hash;
+        final K key;
+        V value;
+        Node<K, V> next;
+        Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        @Override
+        public K getKey() {
+            return this.key;
+        }
+
+        @Override
+        public V getValue() {
+            return this.value;
+        }
+
+        @Override
+        public V setValue(V value) {
+            this.value = value;
+            return value;
+        }
+    }
 
     @Override
     public void put(K key, V value) {
