4516253a4718499edd9163f6259c30eb72ec2bfe
Roman Malyarenko <malyarenko.roman@gmail.com>
2025-03-01 17:02:45 +0200

    removed empty constructor Node(), nested class Bucket, variables CurrentNode and IndexBucket, method createMyHashMap(), replace method calculateHash(K key) to outer class, changed constructor MyHashMap()

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -4,32 +4,34 @@ import java.util.Objects;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
+    private static final int DEFAULT_CAPACITY_INCREASE = 1;
     private static final int CONSTANT_FOR_HASH = 0x7FFFFFFF;
     private static final double LOAD_FACTOR = 0.75;
-    private Bucket<K, V>[] myHashMap;
+    private Node<K, V>[] table;
     private int capacity;
     private int threshold;
     private int size;
 
-    private Node<K, V> currentNode = new Node<>();
-    private int indexBucket;
-
     public MyHashMap() {
-        createMyHashMap();
+        table = new Node[DEFAULT_CAPACITY];
+        capacity = DEFAULT_CAPACITY;
+        threshold = (int) (capacity * LOAD_FACTOR);
     }
 
     @Override
     public void put(K key, V value) {
-        checkThreshold();
-        insertNodeInMap(myHashMap, new Node<>(key, value));
+        if (size >= threshold) {
+            resize();
+        }
+        insertNodeInMap(table, new Node<>(key, value));
     }
 
     @Override
     public V getValue(K key) {
-        indexBucket = indexByKey(key);
-        if (myHashMap[indexBucket] != null) {
-            currentNode = myHashMap[indexBucket].node;
-            for (int i = 0; i < myHashMap[indexBucket].bucketSize; i++) {
+        int indexTable = indexByKey(key);
+        if (table[indexTable] != null) {
+            Node<K, V> currentNode = table[indexTable];
+            do {
                 if (Objects.equals(currentNode.key, key)) {
                     return currentNode.value;
                 }
@@ -37,7 +39,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                     break;
                 }
                 currentNode = currentNode.next;
-            }
+            } while (currentNode != null);
         }
         return null;
     }
@@ -47,46 +49,33 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         return size;
     }
 
-    private void checkThreshold() {
-        if (size >= threshold) {
-            capacity = capacity << 1;
-            resize();
-        }
-    }
-
-    private Bucket<K, V>[] createMyHashMap() {
-        if (capacity == 0) {
-            capacity = DEFAULT_CAPACITY;
-        }
-        threshold = (int) (capacity * LOAD_FACTOR);
-        size = 0;
-        return myHashMap = new Bucket[capacity];
-    }
-
     private void resize() {
-        Bucket<K, V>[] copyMap = myHashMap;
-        myHashMap = createMyHashMap();
-        for (Bucket<K, V> bucket : copyMap) {
-            if (bucket == null) {
+        capacity = capacity << DEFAULT_CAPACITY_INCREASE;
+        threshold = (int) (capacity * LOAD_FACTOR);
+        Node<K, V>[] copyMap = table;
+        table = new Node[capacity];
+        size = 0;
+        for (Node<K, V> node : copyMap) {
+            if (node == null) {
                 continue;
             }
-            currentNode = bucket.node;
-            for (int i = 0; i < bucket.bucketSize; i++) {
-                Node<K, V> nextNodeInBucket = currentNode.next;
-                insertNodeInMap(myHashMap, currentNode);
-                if (nextNodeInBucket == null) {
+            Node<K, V> currentNode = node;
+            do {
+                Node<K, V> nextNode = currentNode.next;
+                insertNodeInMap(table, currentNode);
+                if (nextNode == null) {
                     break;
                 }
-                currentNode = nextNodeInBucket;
-            }
+                currentNode = nextNode;
+            } while (currentNode != null);
         }
     }
 
-    private void insertNodeInMap(Bucket<K, V>[] map, Node<K, V> node) {
-        indexBucket = indexByKey(node.key);
-        if (map[indexBucket] != null) {
-            currentNode = map[indexBucket].node;
-            for (int i = 0; i < map[indexBucket].bucketSize; i++) {
+    private void insertNodeInMap(Node<K, V>[] map, Node<K, V> node) {
+        int indexTable = indexByKey(node.key);
+        if (map[indexTable] != null) {
+            Node<K, V> currentNode = map[indexTable];
+            do {
                 if (Objects.equals(currentNode.key, node.key)) {
                     currentNode.value = node.value;
                     return;
@@ -95,33 +84,18 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
                     break;
                 }
                 currentNode = currentNode.next;
-            }
+            } while (currentNode != null);
             currentNode.next = node;
-            node.next = null;
         }
-        if (map[indexBucket] == null) {
-            map[indexBucket] = new Bucket<>(node);
-            node.next = null;
+        if (map[indexTable] == null) {
+            map[indexTable] = node;
         }
-        map[indexBucket].bucketSize++;
+        node.next = null;
         size++;
     }
 
     private int indexByKey(K key) {
-        return currentNode.calculateHash(key) % capacity;
-    }
-
-    private static class Bucket<K, V> {
-        private Node<K, V> node;
-        private int bucketSize;
-
-        private Bucket(K key, V value) {
-            this.node = new Node<>(key, value);
-        }
-
-        private Bucket(Node<K, V> node) {
-            this.node = node;
-        }
+        return key == null ? 0 : key.hashCode() & CONSTANT_FOR_HASH % capacity;
     }
 
     private static class Node<K, V> {
@@ -133,14 +107,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         private Node(K key, V value) {
             this.key = key;
             this.value = value;
-            hash = calculateHash(key);
-        }
-
-        private Node() {
-        }
-
-        private int calculateHash(K key) {
-            return key == null ? 0 : key.hashCode() & CONSTANT_FOR_HASH;
+            hash = key == null ? 0 : key.hashCode() & CONSTANT_FOR_HASH;
         }
     }
 }
