b81f1db953034d2f4eccc43911bc4258464a7d1f
Viktoriia Dragneva <viktoriiadragneva@gmail.com>
2025-03-18 22:26:14 +0200

    fix

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,25 +1,21 @@
 package core.basesyntax;
 
+import java.util.LinkedList;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
-    private final int DEFAULT_CAPACITY = 16;
-    private final double DEFAULT_LOAD_FACTOR = 0.75;
-
-    private Node<K,V> [] table;
+    private int defaultCapacity = 16;
+    private double defaultLoadFactor = 0.75;
     private int size;
-    private int capacity;
-    private int treshhold;
+    private LinkedList<Node<K, V>>[] buckets;
 
     public MyHashMap() {
-        this.capacity = DEFAULT_CAPACITY;
-        this.treshhold = (int) (DEFAULT_CAPACITY * DEFAULT_LOAD_FACTOR);
-        this.table = new Node[capacity];
+        this.buckets = new LinkedList[defaultCapacity];
+        this.size = 0;
     }
 
     private static class Node<K,V> {
         private final K key;
         private V value;
-        private Node<K,V> next;
 
         public Node(K key, V value) {
             this.key = key;
@@ -27,65 +23,51 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    private void updateTrashhold () {
-        treshhold = (int) (capacity * DEFAULT_LOAD_FACTOR);
-    }
-
-    public void resize() { // 75%
-        if (size < treshhold) {
-            return;
-        }
-        capacity *= 2;
-        updateTrashhold();
-
-        Node<K,V> [] oldTable = table;
-        table = new Node[capacity];
-
-        for (Node<K,V> node : oldTable) {
-            while (node != null) {
-                Node <K, V> nextNode = node.next;
-                int index = (node.key == null) ? 0 : Math.abs(node.key.hashCode()) % capacity;
-                node.next = table[index];
-                node = nextNode;
+    public void resize() {
+        LinkedList<Node<K, V>>[] oldBucket = buckets;
+        buckets = new LinkedList[oldBucket.length * 2];
+        size = 0;
+        for (LinkedList<Node<K, V>> bucket : oldBucket) {
+            if (bucket != null) {
+                for (Node<K, V> node : bucket) {
+                    put(node.key, node.value);
+                }
             }
         }
+    }
 
+    private int getIndex(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode()) % buckets.length;
     }
 
     @Override
     public void put(K key, V value) {
-        updateTrashhold();
-        resize();
-
-        int index = (key == null) ? 0 : Math.abs(key.hashCode()) % capacity;
-
-        Node<K, V> current = table [index];
-        while (current != null) {
-            if ((current.key == null && key == null) || (current.key != null && current.key.equals(key))) {
-                current.value = value;
+        int index = getIndex(key);
+        if (buckets[index] == null) {
+            buckets[index] = new LinkedList<>();
+        }
+        for (Node<K, V> node : buckets[index]) {
+            if ((key == null && node.key == null) || (key != null && key.equals(node.key))) {
+                node.value = value;
                 return;
             }
-            current = current.next;
         }
-        Node<K, V> newNode = new Node <> (key, value);
-        newNode.next = table[index];
-        table[index] = newNode;
+        buckets[index].add(new Node<>(key,value));
         size++;
+        if (size > buckets.length * defaultLoadFactor) {
+            resize();
+        }
     }
 
     @Override
     public V getValue(K key) {
-        int index = (key == null) ? 0 : Math.abs(key.hashCode()) % capacity;
-
-        Node<K, V> node = table[index];
-        while (node != null) {
-            if (node.key == null && key == null) {
-                return node.value;
+        int index = getIndex(key);
+        if (buckets[index] != null) {
+            for (Node<K, V> node : buckets[index]) {
+                if ((key == null && node.key == null) || (key != null && key.equals(node.key))) {
+                    return node.value;
+                }
             }
-            if (node.key != null && node.key.equals(key)) {
-                return node.value;
-            }
-            node = node.next;
         }
         return null;
     }
