a7716f459c38aedb00fe74e46f6d914c3e6101d8
Alexandros <olek4004@gmail.com>
2025-03-24 16:23:36 +0100

    solve

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -87,8 +87,24 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     }
 
     private static class Entry<K, V> {
-        K key;
-        V value;
+        private K key;
+        private V value;
+
+        public K getKey() {
+            return key;
+        }
+
+        public void setKey(K key) {
+            this.key = key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
 
         Entry(K key, V value) {
             this.key = key;
